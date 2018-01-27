dd17725ca5cbf42f2570402a41850adc099ffc1e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-01-27 15:54:35 -0500

    tested 27 Jan

---
+++ py/1sock2server.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+'''
+To test, telnet to the server port and you will see the response from either parent or child process-id
+'''
+import socket
+import os
+
+def main():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(("127.0.0.1", 8888))
+    serversocket.listen(0)
+
+    # Child Process
+    if os.fork() == 0:
+        accept_conn("child", serversocket)
+
+    accept_conn("parent", serversocket)
+
+def accept_conn(message, s):
+    message += "#pid=" + str(os.getpid())
+    while True:
+        c, addr = s.accept()
+        print 'Got connection in   %s' % message
+        c.send('Thank you for your connecting to   %s\n' % message)
+        c.close()
+
+if __name__ == "__main__":
+    main()
