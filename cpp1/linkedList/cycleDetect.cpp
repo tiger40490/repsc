e22b5053d2eaac15234b764da6de2f8b616cfe38
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-01 23:39:41 -0500

    tested today

---
+++ cpp1/linkedList/cycleDetect.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  char data;
+  Node * next;
+  Node (char payload, Node* n=NULL): data(payload), next(n){}
+};
+Node _1('4'); //tail
+Node _2('3', &_1);
+Node _3('2', &_2);
+Node _4('1', &_3);
+Node _5('0', &_4);
+Node * head = &_5;
+void dump(string const& headline){ //doesn't work with cycle
+   cout<<headline<<endl<<"New head: ";
+   for (Node* t=head; t; t=t->next)
+      cout<< static_cast<char>(t->data) <<"  # "<<t<<" -> "<<t->next<<endl;
+}
+// Above is a useful, simple set-up of linked list for coding interview
+
+
+/*See https://en.wikipedia.org/wiki/Cycle_detection#Brent.27s_algorithm
+*/
+bool hasLoopBrent(){
+  Node* currentNode = head;
+  Node* tortoise = NULL;
+  int since = 0;
+  int sinceScale = 2;
+  cout<<"tortoise ptr starts as NULL\n";
+  do {
+	cout<<currentNode->data<<" checking against tortoise ...\n"; 
+    if (tortoise == currentNode) return true;
+    if (since >= sinceScale){
+		cout<<currentNode->data<<" ... set as (payload of) new tortoise ptr, with sinceScale set to double\n";
+        tortoise = currentNode;
+        since = 0;
+        sinceScale = 2*sinceScale;
+    }
+    since++;
+  } while (currentNode = currentNode->next);
+  return false;
+}
+int main(){
+	_1.next = &_5; //creating a loop
+	cout<<hasLoopBrent();
+}
