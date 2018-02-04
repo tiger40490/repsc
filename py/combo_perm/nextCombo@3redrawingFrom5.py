07901f37057424cd36fa5c4bcc2d3fd11f91da92
tiger40490 <tiger40490@gmail.com>
2018-02-04 00:26:44 -0500

    simplified, tested

--- py/combo_perm/nextCombo@3redrawingFrom5.py
+++ py/combo_perm/nextCombo@3redrawingFrom5.py
@@ -1,36 +1,22 @@
-'''Requirement: given 5 distinct chars, generate all combinations of 3 chars by redrawing.
-I think there shoudl be 5^3 answers
-
-Once we have a combo, how do we generate the next combo?
-Start from right most and try to "increment" it. This would be an iterative algo
+'''Requirement: given 5 distinct chars, generate all combinations
+of 3 chars by redrawing, ideally in ascending order
 '''
-
 comboLen=3
-pool=sorted(['b', 'c', 'e', 'd', 'a'])
-combos=[]
-def dump():
-    print len(combos), combos
+pool=tuple(sorted(['b', 'c', 'e', 'd', 'a']))
+combos=['']   
 def add1charToEachCombo():
     global combos
-    if not combos:
-        for char in pool:
-            combos.append(char)
-        return
     newCombos=[]
     for combo in combos:
         for char in pool:
-            new = combo
-            new+= char
-            newCombos.append(new)
+            newCombos.append(combo + char)
         
     combos = newCombos
+    print '--', len(combos), combos
         
 def main():
-    for layer in xrange(comboLen):
+    for _ in xrange(comboLen):
         add1charToEachCombo()
-        print '-- after add1charToEachCombo --'
-        dump()
-
 main()
     
     
