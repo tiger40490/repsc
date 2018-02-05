a26a2c2a0a0d77450636dde4a49d83e8603c21a8
tiger40490 <tiger40490@gmail.com>
2018-02-04 20:06:31 -0500

    tested

--- py/combo_perm/nextPerm@nSocks.py
+++ py/combo_perm/nextPerm@nSocks.py
@@ -1,44 +1,41 @@
 '''Requirement -- given x partially missing pairs socks of various colors, 
 generate all the permutations, in ascending order. Each color has an value.'''
 import bisect
-perm = list('kcdcba')
+
+perm = list('kcedcb')
+perm = sorted(perm)
 N = len(perm)
 
 def swap(p1, p2):
   tmp = perm[p1]
   perm[p1] = perm[p2]
   perm[p2] = tmp
-'''find the next lowest permutation
-return false when there's no more
-'''
+  
+'''find the next lowest permutation'''
 def next_perm():
-  # locate the p2u i.e. position to upgrade.All left positions will stay unchanged.
-  print 'current perm =', perm
+  print 'b4:', perm
+  
+  # start at 2nd last pos in current permutation
   for p2u in reversed(range(N-1)):
-    #print 'pos = ', p2u
     if perm[p2u] < perm[p2u+1]: break
     if p2u == 0:
       print perm, 'is the highest permutation'
-      return ''
-    #print perm[p2u], '>=', perm[p2u+1]
-    
-  #print perm[p2u], 'is the letter to upgrade'
+      return False # can return different types:)  
+  print perm[p2u], 'is the letter to upgrade, at p2u =', p2u
     
   # pick the next higher char among the rest and use it in p2u
   li = sorted(perm[p2u+1:N])
-  pos = bisect.bisect_right(li, perm[p2u])
-  swapPos = p2u + len(li) - pos
-  #print 'pos =', pos, li[pos]
-  #print 'swapPos =', swapPos, perm[swapPos]
-  swap(p2u, swapPos)
-  
-  # the right positions simply sort
+  bis = bisect.bisect_right(li, perm[p2u])
+  swp = p2u + len(li) - bis
+  print 'swp =', swp, perm[swp]
+  swap(p2u, swp)
   return perm[0:p2u+1] + sorted(perm[p2u+1:N])
-  
   
 def main():
   global perm
+  changes=0;
   while(perm):
+    changes += 1
     perm = next_perm()
-  
+  print 'changes =', changes
 main()
