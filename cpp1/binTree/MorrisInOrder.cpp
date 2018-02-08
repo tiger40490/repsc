0416d990468e4f4db61cef74dcce4477faafcea1
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-08 17:56:15 -0500

    tested

--- cpp1/binTree/MorrisInOrder.cpp
+++ cpp1/binTree/MorrisInOrder.cpp
@@ -2,10 +2,9 @@
 #include <assert.h>
 struct Node {
     int val;
-    Node *left, *right;
-    Node(int x, Node * le = NULL, Node * ri = NULL) : val(x), left(le), right(ri) {}
+    Node *le, *ri;
+    Node(int x, Node * _le = NULL, Node * _ri = NULL) : val(x), le(_le), ri(_ri) {}
 };
-
 /*    5
     4   6
   2       8
@@ -21,45 +20,41 @@ https://en.wikipedia.org/wiki/Threaded_binary_tree#Non_recursive_Inorder_travers
     Node _8(8, &_7, &_9);
     Node _6(6, NULL, &_8);
     Node root(5, &_4, &_6);
-
 void MorrisTraversal(Node * const root) { //no recursion, no stack, extra storate for 2 extra pointers
  Node *cur = root;
  while (cur != NULL) {
-    if (cur->left == NULL) { // cur is the lowest remaining value!
+    if (cur->le == NULL) { // cur is the lowest remaining value!
         printf("%d \n", cur->val);
-        cur = cur->right;
+        cur = cur->ri;
         continue;
     }
     // Now cur is not the lowest. Now locate predecessor and set prev to the predecessor.
-    // To construct a threaded BST, we will need to set prev->right to cur
+    // To construct a threaded BST, we will need to set prev->ri to cur
     // ( In a Backward traversal, we would find successor instead. )
 
-    Node * prev=cur->left; //purpose of prev: create/remove thread link prev->right=cur
-    for (; (prev->right != NULL && prev->right != cur); prev = prev->right) {}
+    Node * prev=cur->le; //purpose of prev: create/remove thread link prev->ri=cur
+    for (; (prev->ri != NULL && prev->ri != cur); prev = prev->ri) {}
 
     printf("   %d is the predecessor of %d\n", prev->val, cur->val);
 
-    assert(cur->left != NULL);
-    if (prev->right == NULL) {
-        prev->right = cur; //create thread link
+    assert(cur->le != NULL);
+    if (prev->ri == NULL) {
+        prev->ri = cur; //create thread link
         printf(" %d 's right child set to   %d\n", prev->val, cur->val);
 
         // not ready to print cur, because cur has left child.
-        cur = cur->left;
+        cur = cur->le;
         prev = NULL; // optionally nullify prev as it is invalidated
     }else{
-        //prev->right = NULL; //optionally remove threadlink
+        //prev->ri = NULL; //optionally remove threadlink
         //printf(" %d 's right thread link removed\n", prev->val);
         printf("%d \n", cur->val);
 
         //after printing cur, no need to move left. Must move right!
-        cur = cur->right;
+        cur = cur->ri;
         prev = NULL; // optionally nullify prev as it is invalidated
     }
  }//while
  printf("\n");
 }
-
-int main() {
-    MorrisTraversal(&root);
-}
+int main() { MorrisTraversal(&root); }
