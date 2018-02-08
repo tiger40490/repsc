bb144a42d910e35804f201a4065c9f512c56e2eb
tiger40490 <tiger40490@gmail.com>
2018-02-08 09:40:51 -0500

    tested

--- py/combo_perm/nextComboOf3from6boys.py
+++ py/combo_perm/nextComboOf3from6boys.py
@@ -7,28 +7,27 @@ we only append a single Higher character. Many of the 15 combos are discarded
 pool = list('abcdef')
 
 def combo(total, pp):
-  '''return a list of two items -- 1) a string of pp chars, and a list of higher chars'''
+  '''return a list of two items --
+  1) a string of pp chars
+  2) a list of higher chars'''
   if pp==0: return [('', pool)]
 
   ret = []
   li = combo(total, pp-1)
   for partial, higherChars in li:
-    print 'in outer loop ', partial, higherChars
+    #print 'in outer loop ', partial, higherChars
     for i in xrange(len(higherChars)):
       ch=higherChars.pop(0)
       newpair = (partial+ch, list(higherChars))
-      print 'appending ', newpair
+      #print 'appending ', newpair
       ret.append(newpair)
   print 'after outerloop, return a list of len =', len(ret)
   return ret
 
 def main():
-  combo(len(pool),3)
-  
-  '''lastPrint = ''
-  
-  for completeStr, _ in perm(len(pool),3):
+  lastPrint = ''
+  for completeStr, _ in combo(len(pool),3):
     assert lastPrint < completeStr
-    # print completeStr,
-    lastPrint = completeStr'''
+    print completeStr,
+    lastPrint = completeStr
 main()
