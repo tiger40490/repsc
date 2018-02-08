4d0afbafeb59cb951edc50fba826d7e3831a6db0
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-08 09:21:25 -0500

    Update nextComboOf3from6boys.py

--- py/combo_perm/nextComboOf3from6boys.py
+++ py/combo_perm/nextComboOf3from6boys.py
@@ -1,7 +1,6 @@
-'''Req: generate in ascending order all 2-combos of 6 distinct chars.
+'''Req: generate in ascending order all 3-combos of 6 distinct chars. We should get 6C3 = 20 combos
 
-There are 6C3 = 20 combos
-
-Plan: First we generate 6C2=15 combos. For each combo (arranged as sorted string), 
-we only append a single higher character.
+Plan: First generate 6C2=15 combos. For each combo (arranged as sorted string), 
+we only append a single Higher character. Many of the 15 combos are discarded 
+-- inefficient, but simple
 '''
