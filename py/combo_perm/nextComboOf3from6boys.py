7d63f13010d658779d4a9949d8fe521d486ce7f6
tiger40490 <tiger40490@gmail.com>
2018-02-08 09:50:26 -0500

    Discard worthless intermediate results

--- py/combo_perm/nextComboOf3from6boys.py
+++ py/combo_perm/nextComboOf3from6boys.py
@@ -1,10 +1,12 @@
 '''Req: generate in ascending order all 3-combos of 6 distinct chars. We should get 6C3 = 20 combos
 
 Plan: First generate 6C2=15 combos. For each combo (arranged as sorted string), 
-we only append a single Higher character. Many of the 15 combos are discarded 
--- inefficient, but simple
+we only append a single Higher character.
+
+Many of the 15 combos are worthless -- See the "continue".
 '''
 pool = list('abcdef')
+C = 3
 
 def combo(total, pp):
   '''return a list of two items --
@@ -15,18 +17,20 @@ def combo(total, pp):
   ret = []
   li = combo(total, pp-1)
   for partial, higherChars in li:
-    #print 'in outer loop ', partial, higherChars
+    print 'in outer loop ', partial, higherChars
     for i in xrange(len(higherChars)):
       ch=higherChars.pop(0)
+      if len(higherChars) == 0 and pp < C: continue 
       newpair = (partial+ch, list(higherChars))
       #print 'appending ', newpair
       ret.append(newpair)
-  print 'after outerloop, return a list of len =', len(ret)
+  print 'After outerloop, returning a list of len =', len(ret), \
+        ', which is often lower than the combination formula thanks to CONTINUE'
   return ret
 
 def main():
   lastPrint = ''
-  for completeStr, _ in combo(len(pool),3):
+  for completeStr, _ in combo(len(pool),C):
     assert lastPrint < completeStr
     print completeStr,
     lastPrint = completeStr
