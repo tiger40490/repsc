d1f46e7cb0f324f9f5db75e541f3ce9eb186bcb3
tiger40490 <tiger40490@gmail.com>
2018-02-08 09:37:52 -0500

    briefly tested

--- py/combo_perm/nextComboOf3from6boys.py
+++ py/combo_perm/nextComboOf3from6boys.py
@@ -4,3 +4,31 @@ Plan: First generate 6C2=15 combos. For each combo (arranged as sorted string),
 we only append a single Higher character. Many of the 15 combos are discarded 
 -- inefficient, but simple
 '''
+pool = list('abcdef')
+
+def combo(total, pp):
+  '''return a list of two items -- 1) a string of pp chars, and a list of higher chars'''
+  if pp==0: return [('', pool)]
+
+  ret = []
+  li = combo(total, pp-1)
+  for partial, higherChars in li:
+    print 'in outer loop ', partial, higherChars
+    for i in xrange(len(higherChars)):
+      ch=higherChars.pop(0)
+      newpair = (partial+ch, list(higherChars))
+      print 'appending ', newpair
+      ret.append(newpair)
+  print 'after outerloop, return a list of len =', len(ret)
+  return ret
+
+def main():
+  combo(len(pool),3)
+  
+  '''lastPrint = ''
+  
+  for completeStr, _ in perm(len(pool),3):
+    assert lastPrint < completeStr
+    # print completeStr,
+    lastPrint = completeStr'''
+main()
