ee332655b1b2ad587a3b93aebb7c781b9a0f01fc
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-09 08:31:56 -0500

    cleanup

--- cpp1/spiralFB.cpp
+++ cpp1/spiralFB.cpp
@@ -1,25 +1,26 @@
+//todo: //we can get rid of this ALL thing and avoid the cloning
 #include <iostream>
 #include <vector>
 #include <deque>
 #include <assert.h>
 #define ss if (1>0) cout
 using namespace std;
-int const N = 4; // print a 4x4 matrix
-typedef deque<deque<int>> Mat;
-Mat seed1 = {{1}};
-Mat seed2 = {{4,3},{1,2}};
-vector<Mat> all={Mat(), seed1};//, seed2}; //we can get rid of this ALL thing and avoid the cloning
+int const N = 4; // goal: print a 4x4 matrix
+typedef deque<deque<int>> Square;
+Square seed1 = {{1}};
+Square seed2 = {{4,3},{1,2}};
+vector<Square> All(99); 
 
 void dump(int const n){
   assert (n > 0 && "bad input");
-  Mat arr = all[n];
+  Square arr = All[n];
   for(int row=0; row<n; ++row){
    for(int col=0; col<n; ++col) cout<<arr[row][col]<<"\t";
    cout<<endl;
   }
 }
 void shiftCopy(int const n){
-  Mat clone = all[n];
+  Square clone = All[n];
   size_t const len = clone.size();
   for (int row = 0; row <len; ++row){
 	  clone[row].push_front(0);
@@ -35,15 +36,15 @@ void shiftCopy(int const n){
   for(int row=newlen-2; row>=0; --row) clone[row][newlen-1] = i++;
   for(int col=newlen-2; col>=0; --col) clone[0][col] = i++;  
   
-  //assert(all.size() == len+1);
-  all.push_back(Mat());
-  all.push_back(clone);
+  All[clone.size()]=(clone);
   dump(len+2);
 }	
 
 int main(){
-	dump(1);
-	shiftCopy(1);
+	All[1] = seed1;
+	All[2] = seed2;
+	dump(2);
+	shiftCopy(2);
 }
 /*Requirement: https://wp.me/p74oew-4ae
 */
