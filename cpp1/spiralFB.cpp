53581de26b82fbdb0f8f9883df61fb991358ea19
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-09 01:24:35 -0500

    instrumentation support added

--- cpp1/spiralFB.cpp
+++ cpp1/spiralFB.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <assert.h>
 using namespace std;
 int const N = 4; // print a 4x4 matrix
-int arr[N][N];
-void dump(){
-  for(int row=0; row<N; ++row){
-   for(int col=0; col<N; ++col) cout<<arr[row][col]<<"\t";
+typedef vector<vector<int>> Mat;
+Mat seed1 = {{1}};
+Mat seed2 = {{4,3},{2,1}};
+vector<Mat> all={Mat(), seed1, seed2};
+
+void dump(int const n){
+  assert (n > 0 && "bad input");
+  Mat arr = all[n];
+  for(int row=0; row<n; ++row){
+   for(int col=0; col<n; ++col) cout<<arr[row][col]<<"\t";
    cout<<endl;
   }
 }
 
+
 int main(){
-	dump();
+	dump(2);
 }
 /*Requirement: https://wp.me/p74oew-4ae
 */
