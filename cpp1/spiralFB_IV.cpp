3a79acefae87c795fca7489079343bf3e58eb6f6
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-11 20:26:28 -0500

    macro demo

--- cpp1/spiralFB_IV.cpp
+++ cpp1/spiralFB_IV.cpp
@@ -1,12 +1,14 @@
 /* showcasing multi-dimensional array syntax using deque of deque
  * showcasing deque constructed on the fly
+ * showcasing very short macro name "h" for a longer, more meaningful var name
  */
 #include <iostream>
 #include <deque>
 #include <assert.h>
 #define ss if (1>0) cout
+#define h highestIndex
 using namespace std;
-int const N = 4; // requirement: print a NxN matrix
+int const N = 5; // requirement: print a NxN matrix
 typedef deque<deque<int>> Square;
 Square sq = N%2 ? Square({{1}}) : Square({{4,3},{1,2}}); //seeding
 
@@ -28,12 +30,12 @@ void grow(){ //expand by one layer
   sq.push_front(deque<int>(lenAf,0));
   sq.push_back (deque<int>(lenAf,0));
   //Now we have a bigger square matrix (not a staggered 2D array. Populate the new cells
-  size_t const highestIndex = lenAf-1;
-  size_t const nextHighest  = lenAf-2;
+  int const h = lenAf-1; //no size_t please: subtraction could gets negative
+  int const nextHighest  = lenAf-2;
   int i = lenB4*lenB4 + 1; //next value to use
   for(int row=1;        row<lenAf; ++row) sq[row][0] = i++;
-  for(int col=1;        col<lenAf; ++col) sq[highestIndex][col] = i++;
-  for(int row=nextHighest; row>=0; --row) sq[row][highestIndex] = i++;
+  for(int col=1;        col<lenAf; ++col) sq[h][col] = i++;
+  for(int row=nextHighest; row>=0; --row) sq[row][h] = i++;
   for(int col=nextHighest; col>=0; --col) sq[0][col] = i++;
   dump();
 }
