67211f396c077ff43f88b33a9e325f9201c340e3
tiger40490 <tiger40490@gmail.com>
2018-02-24 17:35:21 -0500

    tested and cleaned up

--- py/2d/sudoku4.py
+++ py/2d/sudoku4.py
@@ -1,72 +1,80 @@
-sz=4
-Mat = [[0 for _ in range(sz)] for _ in range(sz)]
+COLORS = 4 # this many colors, each represented by 1,2,3... till this value
+UNFILLED_COLOR = COLORS+1
+GAME_OVER = -1
+Mat = [[UNFILLED_COLOR for _ in range(COLORS)] for _ in range(COLORS)]
 
-def f(r,c): # fill 1 cell. Return True if game won
-  print 'entering f() with', r,c
+def fill1cell(r,c): # fill 1 cell. Return True if game won
+  #print 'entering fill1cell() with', r,c
   global Mat
   Mat[r][c]=87 
   rNext,cNext=findNextCell2fill() 
-  for guess in range(1,sz+1):
+  for guess in range(1,COLORS+1):
     Mat[r][c]=guess
     #print 'trying:'; dump()
     if failedSomething(): continue # next guess
     #print ' ... passed:)'
-    if rNext < 0: return True
-    if f(rNext,cNext): return True
-  Mat[r][c] = 0 # restore
-  return False # all values tried
+    if rNext == GAME_OVER: return True
+    if fill1cell(rNext,cNext): return True
+  Mat[r][c] = UNFILLED_COLOR # restore
+  #return False by default
 
-def findNextCell2fill(): # return 0 to indicate no more
-  for r in range(sz):
-    for c in range(sz):
-      if Mat[r][c] == 0: 
+def findNextCell2fill(): 
+# can't use global var. Every stack frame remembers the "cell-after"
+  for r in range(COLORS):
+    for c in range(COLORS):
+      if Mat[r][c] == UNFILLED_COLOR: 
         return r,c
-  return -1,-1
+  return GAME_OVER, GAME_OVER
 def failedSomething():
-  if groupFailed(Mat[0][0], Mat[0][1], Mat[1][1], Mat[1][0]): return True
-  if groupFailed(Mat[0][2], Mat[0][3], Mat[1][3], Mat[1][2]): return True
-  if groupFailed(Mat[3][3], Mat[3][2], Mat[2][2], Mat[2][3]): return True
-  if groupFailed(Mat[3][0], Mat[3][1], Mat[2][1], Mat[2][0]): return True
-  for i in range(sz):
-    if groupFailed(Mat[i][0], Mat[i][1], Mat[i][2], Mat[i][3]): return True #i th row
-    if groupFailed(Mat[0][i], Mat[1][i], Mat[2][i], Mat[3][i]): return True #i th column
-   # return False by default  
-def groupFailed(a,b,c,d): # 
-   assert 0<=a and a<=4
-   assert 0<=b and b<=4
-   assert 0<=c and c<=4
-   assert 0<=d and d<=4
-   #print "in groupFailed():",a,b,c,d,
-   cnt = {0:0, 1:0, 2:0, 3:0, 4:0}
+  if failed1group(Mat[0][0], Mat[0][1], Mat[1][1], Mat[1][0]): return True
+  if failed1group(Mat[0][2], Mat[0][3], Mat[1][3], Mat[1][2]): return True
+  if failed1group(Mat[3][3], Mat[3][2], Mat[2][2], Mat[2][3]): return True
+  if failed1group(Mat[3][0], Mat[3][1], Mat[2][1], Mat[2][0]): return True
+  for i in range(COLORS):
+    if failed1group(Mat[i][0], Mat[i][1], Mat[i][2], Mat[i][3]): return True 
+    if failed1group(Mat[0][i], Mat[1][i], Mat[2][i], Mat[3][i]): return True
+  # return False by default  
+def failed1group(a,b,c,d): # four cell values must have no dupes except zeros
+   assert 0<=a and a<=UNFILLED_COLOR
+   assert 0<=b and b<=UNFILLED_COLOR
+   assert 0<=c and c<=UNFILLED_COLOR
+   assert 0<=d and d<=UNFILLED_COLOR
+   #print "in failed1group():",a,b,c,d,
+   cnt = {1:0, 2:0, 3:0, 4:0, UNFILLED_COLOR:0}
    cnt[a]+=1; cnt[b]+=1; cnt[c]+=1; cnt[d]+=1
    #print cnt
-   for i in range(1,sz+1):
+   for i in range(1,COLORS+1):
      if cnt[i] > 1: return True
    # return False by default
 def dump():
-  for r in range(sz):
+  for r in range(COLORS):
     print Mat[r]  
   print '--------'    
-def init():
+def init1board():
   global Mat
   Mat[0][1]=2; Mat[0][3]=1; 
   Mat[1][2]=3; # Mat[1][]=; 
   Mat[2][1]=4; #Mat[2][]=; 
   Mat[3][0]=2; Mat[3][2]=1; 
-def init2():
+def init2board():
   global Mat
   Mat[0][0]=2; Mat[0][3]=1; 
   Mat  [1][2]=4; # Mat[1][]=; 
   Mat [2][1]=2; #Mat[2][]=; 
   Mat[3][0]=3; #Mat[3][2]=4; 
+def init():
+  global Mat
+  Mat  [0][2]=2; #Mat[0][3]=1; 
+  Mat[1][0]=2; Mat[1][3]=1; 
+  Mat[2][0]=3; #Mat[2][]=; 
+  Mat [3][1]=4; #Mat[3][2]=4; 
 def main():
-  init2()
+  init()
   dump()
   rNext,cNext=findNextCell2fill()
-  print f(rNext,cNext)
-  dump()
-  #for i in range(2,3):
-  #  print groupFailed(Mat[i][0], Mat[i][1], Mat[i][2], Mat[i][3])   
+  if fill1cell(rNext,cNext): 
+    dump()
+  else: print 'Impossible'
 main()
 '''Every empty cell should start a new stack frame
 '''
