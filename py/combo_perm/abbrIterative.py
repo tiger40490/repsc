cc53e4f21e9b353e97fc7b22639780b5370f9b3b
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-26 15:58:34 -0500

    comments

--- py/combo_perm/abbrIterative.py
+++ py/combo_perm/abbrIterative.py
@@ -1,6 +1,6 @@
-'''I wrote a recursive c++ solution to output in ascending order. 
+'''Abbrv generator. I wrote a recursive c++ solution to output in ascending order. 
 Today I will forgo ascending order and try an iterative soution
-Supose we have canned 3 chars in the word, and we have a collection of N abbreviations. 
+Supose we have scanned 3 chars in the word, and we have a collection of N abbreviations. 
 For the next char 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
 Join the lists into a list of 2N.
 '''
