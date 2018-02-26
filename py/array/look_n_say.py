b831ec3186042602dd8e132d61bedee7a0b3a1c6
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-25 22:52:55 -0500

    refactor

--- py/array/look_n_say.py
+++ py/array/look_n_say.py
@@ -1,39 +1,36 @@
 '''
 showcase simple VO class to simplify design
+showcase list clearing without rebinding the variable
+
+Since Vo.save() needs to update "line", it should be a global var (i.e. module variable), so next() should not take in arg
 '''
 line=[1]
 class Vo:
   def __init__(self, val):
     self.v=val
     self.c=1
-  def pr(self):
-    global line
+  def save(self):
     line.extend([self.c, self.v])
-    #print self.c, self.v,
 
-def next(s): # print next line
-  global line
-  line=list()
-  sz = len(s)
-  vo = Vo(s[0])
+def next(): 
+  origLine=list(line)
+  line[:]=[] # no "global" needed
+  sz = len(origLine)
+  vo = Vo(origLine[0])
   for i in range(1,sz):
-    if vo.v==s[i]:
+    if vo.v==origLine[i]:
       vo.c+=1
       continue
-    vo.pr()
-    vo=Vo(s[i])
-  vo.pr() 
-  return list(line)
+    vo.save()
+    vo=Vo(origLine[i])
+  vo.save() 
+  print line
   
-
 def main():
-  global line
-  for _ in range(8):
-    line = next(line)
-    print line
-  #next([1,1,1,2,2,1]);
+  for _ in range(9):
+    next()
 main()
 
 '''https://bintanvictor.wordpress.com/2018/02/07/look-n-say-sequence-fb/
-I took 6 min to write the basic function on window and another 5 min to test it with one list
+I took 6 min to write the basic function on window and another 5 min to test it with one input
 '''
