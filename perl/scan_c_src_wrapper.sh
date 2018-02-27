7688b7fb35610caaac49e99a03d3be157775a4ec
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-02-27 16:30:33 -0500

    comments

--- perl/scan_c_src_wrapper.sh
+++ perl/scan_c_src_wrapper.sh
@@ -1,6 +1,7 @@
-g (){
-    g1 "$@" | less -ER
+g1(){ # single-hit
+  find .|scan_c_src.pl "$@"
 }
-g1 (){
-    find . | scan_c_src.pl "$@"
+g(){ # multi-hit
+  # the trailing q(less) is for the normal "multi-hit" case. In the single-hit case, it would mess up the exec(less ....)
+  g1 "$@" |less -ER
 }
