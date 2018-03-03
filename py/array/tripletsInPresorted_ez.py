406e709364ceca139360f31156e2cdd36436dc88
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-02 20:48:53 -0500

    tested

---
+++ py/array/tripletsInPresorted_ez.py
@@ -0,0 +1,17 @@
+li=sorted([12,3,6,2,4,20,15,8,10,9,1,5])
+sz=len(li)
+pairs=[]
+
+def main():
+  for s in range(sz-1):
+    for b in range(s+1, sz):
+      sm,bi = li[s],li[b]
+      if bi%sm: continue
+      newPair = (sm,bi)
+      for _sm,_bi in pairs:
+        if sm == _bi: print 'match -', _sm,_bi,bi
+      pairs.append(newPair)
+      print newPair
+main()
+'''Requirement: Within a sequence of natural numbers with no duplicates, count the occurrence of so-called triplets of (a,b,c) where a divides b and b divides c. 
+'''
