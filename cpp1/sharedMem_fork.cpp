486f15f720e5da04538f70141777f283ae08c92b
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-04 15:05:32 -0500

    refactor

--- cpp1/sharedMem_fork.cpp
+++ cpp1/sharedMem_fork.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
+void* shmem;
 void* create_shared_memory(size_t size) {
   int protection = PROT_READ | PROT_WRITE; //rw access
 
@@ -15,27 +16,29 @@ void* create_shared_memory(size_t size) {
   // but the manpage for `mmap` explains their purpose.
   return mmap(NULL, size, protection, visibility, 0, 0);
 }
+void processP(){
+    printf("Parent picked up: %s\n", shmem);
+    sleep(1);
+    printf("After 1s, parent picked up: %s\n", shmem);
+}
+void processC(){
+    printf("Child picked up: %s\n", shmem);
+    char const* child_message = "goodbye"; // child process will then write this one
+    memcpy(shmem, child_message, sizeof(child_message));
+    printf("Child wrote: %s\n", shmem);
+}
 
 int main() {
   char const* parent_message = "hello";  // parent process will write this message
-  char const* child_message = "goodbye"; // child process will then write this one
 
-  void* shmem = create_shared_memory(128);
+  shmem = create_shared_memory(128);
 
   memcpy(shmem, parent_message, sizeof(parent_message));
 
   int pid = fork();
 
-  if (pid == 0) {
-    printf("Child picked up: %s\n", shmem);
-    memcpy(shmem, child_message, sizeof(child_message));
-    printf("Child wrote: %s\n", shmem);
-
-  } else {
-    printf("Parent picked up: %s\n", shmem);
-    sleep(1);
-    printf("After 1s, parent picked up: %s\n", shmem);
-  }
+  if (pid == 0) processC(); //child
+  else processP();
 }
 /* based on https://stackoverflow.com/questions/5656530/how-to-use-shared-memory-with-linux-in-c
- */
+*/
