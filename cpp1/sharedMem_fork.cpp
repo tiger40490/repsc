70a9b01ec0d336375e4b78964f13138185872080
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-04 16:47:51 -0500

    Update sharedMem_fork.cpp

--- cpp1/sharedMem_fork.cpp
+++ cpp1/sharedMem_fork.cpp
@@ -3,12 +3,31 @@
 #include <string.h>
 #include <string>
 #include <unistd.h>
+#include <pthread.h>
+//#include <iostream>
 using namespace std;
 
-void* shmem;
 int const MAX=9;
 int sleepMultiplier=1;
-void* create_shared_memory(size_t size) {
+
+struct mapped {
+  unsigned char    array[1024];
+  long long        version;
+  pthread_mutex_t  mutex; //embeded, not a pointer
+};
+//mapped * shmem = NULL;
+void die(char const *msg) {
+  perror(msg);
+  exit(1);
+}
+mapped * create_shared_memory() {
+  size_t sz = sizeof(mapped);
+  pthread_mutexattr_t mutexattr;
+  int rc = pthread_mutexattr_init(& mutexattr);
+  if (rc != 0) die("pthread init");
+  rc = pthread_mutexattr_setpshared(& mutexattr, PTHREAD_PROCESS_SHARED);
+  if (rc != 0) die("pthread_mutexattr_setpshared");
+
   int protection = PROT_READ | PROT_WRITE; //rw access
 
   // The buffer will be shared (meaning other processes can access it), but
@@ -17,37 +36,49 @@ void* create_shared_memory(size_t size) {
   int visibility = MAP_ANONYMOUS | MAP_SHARED;
 
   // The remaining parameters to `mmap()` are not important for this use case,
-  // but the manpage for `mmap` explains their purpose.
-  return mmap(NULL, size, protection, visibility, 0, 0);
+  auto shmem = static_cast<mapped*>(mmap(NULL, sz, protection, visibility, 0, 0));
+  rc = pthread_mutex_init(& (shmem->mutex), &mutexattr);
+  if (rc != 0) die("pthread_mutex_init");
+  return shmem;
 }
-void processB(){
-  for(long long i=0; i<MAX; i+= 2){
-    printf("B picked up: %s\n", shmem);
+void processB(mapped * shmem){
+  long long lastWrote=-1;
+  for(long long i=0; i<=MAX; i+= 2){
+    while (shmem->version <= lastWrote) usleep(1);
+    printf("B   picked up: %s\n", shmem);
+    lastWrote = i;
     char const* msg = to_string(i).c_str();
+    pthread_mutex_lock(&(shmem->mutex));
     memcpy(shmem, msg, sizeof(msg));
+    shmem->version=i;
+    pthread_mutex_unlock(&(shmem->mutex));
     printf("B wrote: %s\n", shmem);
-    usleep(3*sleepMultiplier);
+    usleep(1*sleepMultiplier);
   }
 }
-void processA(){
-  for(long long i=1; i<MAX; i+= 2){
-    printf(" A picked up: %s\n", shmem);
+void processA(mapped * shmem){
+  long long lastWrote=-1;
+  for(long long i=1; i<=MAX; i+= 2){
+    while (shmem->version <= lastWrote) usleep(1);
+    printf(" A   picked up: %s\n", shmem);
+    lastWrote = i;
     char const* msg = to_string(i).c_str();
+    pthread_mutex_lock(&(shmem->mutex));
     memcpy(shmem, msg, sizeof(msg));
+    shmem->version=i;
+    pthread_mutex_unlock(&(shmem->mutex));
     printf(" A wrote: %s\n", shmem);
-    usleep(2*sleepMultiplier);
+    usleep(1*sleepMultiplier);
   }
 }
 
 int main() {
-  shmem = create_shared_memory(128);
-  char const* message = "initial_content";
-  memcpy(shmem, message, sizeof(message));
+  mapped* shmem = create_shared_memory();
+  char const* message = "init_content";
+  memcpy(shmem, message, strlen(message));
 
-  int pid = fork();
-
-  if (pid == 0) processA(); //child
-  else processB();
+  if (fork() == 0) processA(shmem); //child
+  else processB(shmem);
 }
 /* based on https://stackoverflow.com/questions/5656530/how-to-use-shared-memory-with-linux-in-c
 */
