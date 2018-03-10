56605142353bf5d7d4cc8591078865f441165e95
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-10 15:34:06 -0500

    tested

--- java/com/xx/Intern.java
+++ java/com/xx/Intern.java
@@ -1,15 +1,16 @@
 package com.xx;
-import java.lang.System.out;
+import java.lang.System;
 
 public class Intern {
     public static void main(String[] args) {
-        String s1 = "Test";
-        String s2 = "Test";
-        String s3 = new String("Test");
-        final String s4 = s3.intern();
-        System.out.println(s1 == s2); //true 2 literals
-        System.out.println(s2 == s3); //false by default, no intern
-        System.out.println(s3 == s4); //false ? 
-        System.out.println(s1 == s4); //true thanks to intern()
+        String literal1 = "Test";
+        String literal2 = "Test";
+        String s3 = new String("Test"); // different object
+        final String internalized = s3.intern(); // returns a reference of the literal
+		// results in Java 8:
+        System.out.println(literal2 == literal1); //true 2 literals
+        System.out.println(literal2 == internalized); //true s4 object IS the s2 object
+        System.out.println(s3 == literal2 ); //false by default, no intern
+        System.out.println(s3 == internalized); //false 
     }
 }
