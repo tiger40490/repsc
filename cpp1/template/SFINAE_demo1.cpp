cc117873daa2ccfd9f107f7ada9921fa1f016dbf
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-16 13:35:37 -0400

    rename a dummy type

--- cpp1/template/SFINAE_demo1.cpp
+++ cpp1/template/SFINAE_demo1.cpp
@@ -8,21 +8,24 @@ struct AddRefreshOrderStruct {
 };
 struct EmptyStruct { };
 
+// T is pinned to the type you spell out like AddRefreshOrderStruct. Compler has no leeway.
+//
+// There are 3 candidate overloaded functions named func():
+// U will be the same type as T if one of Candidate XX or YY gets picked by the compiler.
+// Otherwise, Candidate ZZ is picked and U is unused.
+// function-template-overload resolution follows SNIFAE.
+
 template<typename T> class FieldCheck{ // a class template with a nested class template + static method templates
-//T is pinned to the type you spell out like AddRefreshOrderStruct. Compler has no leeway.
-//U will be the same type as T if one of the non-default candidates gets picked by the compiler.
-//Otherwise, U is unbound/unused in the default candidate.
-//function-template-overload resolution follows SNIFAE.
 
-    template<typename U, U non_dummy_type_templ_param> class Check; // This nested class template is unreadably deep and convoluted.
+    template<typename A, A non_dummy_type_templ_param> class Check; // This nested class template is unreadably deep and convoluted.
 
-  
+
     //first template arg in Check<> below can use T or U, but somehow 2nd item
     //must not use T... I guess the SFINAE rules are non-trivial.
-    template<typename U> static int    func(Check<long  U::*,          &U::sendTimeNS>*);//2nd item is a field
-    template<typename U> static double func(Check<long (U::*)() const, &U::sendTimeNS>*);//2nd item is a method, somehow
+    template<typename U> static int    func(Check<long  U::*,          &U::sendTimeNS>*);//Candidate XX: 2nd item is a field
+    template<typename U> static double func(Check<long (U::*)() const, &U::sendTimeNS>*);//Candidate YY: 2nd item is a method, somehow
 
-    template<typename U> static char   func(...); //default overload candidate, must be a func template
+    template<typename U> static char   func(...); //Candidate ZZ: default overload candidate, must be a func template
   public:
     static const //if no const, we can't initialize this filed here
       size_t result = sizeof(func<T>(NULL)); // 8 means has a method; 4 means has field
