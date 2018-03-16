e83ad3a534c181282e31b0393b546fec7534c848
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-16 13:52:31 -0400

    more comments. tested

--- cpp1/template/SFINAE_demo1.cpp
+++ cpp1/template/SFINAE_demo1.cpp
@@ -8,27 +8,33 @@ struct AddRefreshOrderStruct {
 };
 struct EmptyStruct { };
 
-// T is pinned to the type you spell out like AddRefreshOrderStruct. Compler has no leeway.
+// Three layers of templates
+// 1) FieldCheck is a shell, slightly simpler than the other two.
+// 2) Checker is a nested class template ... deep and convoluted.
+// 3) func() is a group of 3 overloaded-static-member-function-templates.
+// * They use the Checker class.
+// * Overload resolution follows SNIFAE.
 //
-// There are 3 candidate overloaded functions named func():
-// U will be the same type as T if one of Candidate XX or YY gets picked by the compiler.
+// T is pinned to the type you spell out, like AddRefreshOrderStruct. Compler has no leeway.
+//
+// Compiler will pick one of the 3 candidate functions named func():
+// U will be the same type as T if Candidate XX or YY gets picked by the compiler.
 // Otherwise, Candidate ZZ is picked and U is unused.
-// function-template-overload resolution follows SNIFAE.
 
 template<typename T> class FieldCheck{ // a class template with a nested class template + static method templates
 
-    template<typename A, A non_dummy_type_templ_param> class Check; // This nested class template is unreadably deep and convoluted.
-
+    template<typename A, A non_dummy_type_templ_param> class Checker;
 
     //first template arg in Check<> below can use T or U, but somehow 2nd item
     //must not use T... I guess the SFINAE rules are non-trivial.
-    template<typename U> static int    func(Check<long  U::*,          &U::sendTimeNS>*);//Candidate XX: 2nd item is a field
-    template<typename U> static double func(Check<long (U::*)() const, &U::sendTimeNS>*);//Candidate YY: 2nd item is a method, somehow
+    template<typename U> static int    func(Checker<long  U::*,          &U::sendTimeNS>*);//Candidate XX: 2nd item is a field
+    template<typename U> static double func(Checker<long (U::*)() const, &U::sendTimeNS>*);//Candidate YY: 2nd item is a method, somehow
+
 
     template<typename U> static char   func(...); //Candidate ZZ: default overload candidate, must be a func template
   public:
-    static const //if no const, we can't initialize this filed here
-      size_t result = sizeof(func<T>(NULL)); // 8 means has a method; 4 means has field
+    static const //if no const, we can't initialize this field here
+      size_t result = sizeof(func<T>(NULL)); // 8 means T has method; 4 means T has field
 };
 
 int main() {
