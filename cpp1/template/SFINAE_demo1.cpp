91abae8228f6afc0ec304ec6caf48115b54af6c2
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-16 13:20:33 -0400

    comments only

--- cpp1/template/SFINAE_demo1.cpp
+++ cpp1/template/SFINAE_demo1.cpp
@@ -8,21 +8,19 @@ struct AddRefreshOrderStruct {
 };
 struct EmptyStruct { };
 
-
 template<typename T> class FieldCheck{ // a class template with a nested class template + static method templates
-
 //T is pinned to the type you spell out like AddRefreshOrderStruct. Compler has no leeway.
-//U will be the same type if one of the non-default candidates gets picked by the compiler.
+//U will be the same type as T if one of the non-default candidates gets picked by the compiler.
 //Otherwise, U is unbound/unused in the default candidate.
 //function-template-overload resolution follows SNIFAE.
 
-    template<typename U, U non_dummy_type_templ_param> class Check;
+    template<typename U, U non_dummy_type_templ_param> class Check; // This nested class template is unreadably deep and convoluted.
 
+  
     //first template arg in Check<> below can use T or U, but somehow 2nd item
-    //must not use T ! I guess the SFINAE rules are non-trivial.
-
-    template<typename U> static int    func(Check<long U::*, &U::sendTimeNS>*);//2nd item is a field
-    template<typename U> static double func(Check<long(U::*)() const, &U::sendTimeNS>*);
+    //must not use T... I guess the SFINAE rules are non-trivial.
+    template<typename U> static int    func(Check<long  U::*,          &U::sendTimeNS>*);//2nd item is a field
+    template<typename U> static double func(Check<long (U::*)() const, &U::sendTimeNS>*);//2nd item is a method, somehow
 
     template<typename U> static char   func(...); //default overload candidate, must be a func template
   public:
