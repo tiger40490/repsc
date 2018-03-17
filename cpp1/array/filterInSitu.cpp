8c482ad582b4ebc49434ab2f0aa2b71c2e3f3c8f
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-03-17 00:38:29 -0400

    tested

--- cpp1/array/filterInSitu.cpp
+++ cpp1/array/filterInSitu.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 using ele=int;
 bool isBad(ele const & item){return item < 0;}
+bool isGood(ele const & item){return !isBad(item);}
 bool comp(ele const & a, ele const & b){ return isBad(a)<isBad(b); }
 void dump(vector<ele> const & arr){
   for(auto & e: arr) cout<<e<<" ";
@@ -16,10 +17,23 @@ void swp(vector<ele> & arr, size_t le, size_t ri){
   arr[le] = arr[ri];
   arr[ri] = tmp;
 }
+void solution4(vector<ele> & arr){
+  auto nextBad=arr.begin(); //next good
+  auto nextGood=arr.begin(); //next bad
+  for(;;){
+    nextBad = find_if(nextBad, arr.end(), isBad);
+    if (nextBad == arr.end()) break;
+	nextGood =find_if(max(nextBad+1, nextGood), arr.end(), isGood);
+    if (nextGood == arr.end()) break;
+	swap(*nextBad, *nextGood);
+  }
+  cout<<*nextBad<<endl;
+  dump(arr);
+}
 void solution3(vector<ele> & arr){
   int le=0, ri=arr.size()-1;
   for(;le<=ri;){
-    if (!isBad(arr[le])){
+    if (isGood(arr[le])){
 	  ++le;
 	  continue;
 	}
@@ -29,7 +43,6 @@ void solution3(vector<ele> & arr){
 	}
 	swp(arr, le,ri);
   }
-  //cout<<le;
   arr.resize(le);
   dump(arr);
 }
@@ -41,10 +54,9 @@ void solution2(vector<ele> & arr){//by stable_sort
 
 int main(){
   vector<ele> arr={1,3,-51,-33,8,16,17,-31,20,-28,-27,24,-10,25};
-  solution3(arr);
+  solution4(arr);
 }
-/* general requirements: don't use global variables
-
+/*CVA coding question. general requirements: don't use global variables
 requirement 1: Given a vector (not a linked list) and a predate function, remove all bad elements
 requirement 2: constant space
 requirement 3: linear time
