fda1d2765750f4a10aba11f6f9289a68ba153b98
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-04 12:25:01 -0400

    add comments

--- cpp1/downCastMI.cpp
+++ cpp1/downCastMI.cpp
@@ -9,7 +9,11 @@ struct B2{
 };
 struct D: public B1, B2{} der;
 int main(){
-  cout<<&der<<" = addr of derived object\n";
+  cout<<&der<<" = addr of derived object \n";
   cout<<dynamic_cast<B1*>(&der)<<" = addr after casting to B1* \n"; //same as above
   cout<<dynamic_cast<B2*>(&der)<<" = addr after casting to B2* \n"; //different
 }
+/* Q: dynamic_cast always gives the same address?
+   A: not always. Consider MI.
+   A: if one of the two base classes is a pure-interface (i.e. no instance field) then YES
+*/
