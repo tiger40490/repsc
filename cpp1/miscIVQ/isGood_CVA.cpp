b7ba4e49ef81536fcdb2c37729b9ca6abc0d2d18
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-05 21:23:44 -0400

    Update isGood_CVA.cpp

--- cpp1/miscIVQ/isGood_CVA.cpp
+++ cpp1/miscIVQ/isGood_CVA.cpp
@@ -1,5 +1,19 @@
 //untested
-bool isGood(int k){
-  
+bool isGood(unsigned int k){
+  while(1){
+    if (k%3 == 0){
+      k = k/3;
+      continue;
+    }// now no more 3's in k
+    if (k%5 == 0){
+      k = k/5;
+      continue;
+    }
+    if (k%7 == 0){
+      k = k/7;
+      continue;
+    }
+    return k == 1;
+  }
 }
 /*return true if k has no other factors beside 3,5 or 7*/
