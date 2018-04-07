0078269465a0a3ead660b32ba7f4a5f947de5ba3
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-07 17:40:47 -0400

    passed some tests

--- cpp1/array/checkArr0-N.cpp
+++ cpp1/array/checkArr0-N.cpp
@@ -1,43 +1,52 @@
 #include<iostream>
 #include<vector>
+using namespace std;
 
-vector<int> v={1,2,0,2,0};
-size_t const sz=v.size();
+vector<int> a={1,2,0,2,0};
+size_t const sz=a.size();
 void dump(int pos, string headline=""){
-  cout<<"--------"<<headline<<"------- current position = "<<pos<<endl;
+  cout<<"-------- "<<headline<<" .. current position == "<<pos<<endl;
   for(auto i=0; i<sz; ++i) cout<<i<<"\t";
   cout<<endl;
   for(auto i=0; i<sz; ++i) {
-    if (v[i]< 0 || sz <= v[i]) cout<<(char)v[i]<<"\t";
-    else cout<<v[i]<<"\t";
+    if (a[i]< 0 || sz <= a[i]) cout<<(char)a[i]<<"\t";
+    else cout<<a[i]<<"\t";
   }
   cout<<endl;
 }
 int main(){
-	for (int i=0, th=a[i]/*target home*/; i<sz; ++i, th=a[i])while(1){
+  dump(0, "original");
+  
+	for (size_t i=0; i<sz; ++i){
+    size_t mf = i; //moved from
+    int th=a[i]; /*target home*/
+    while(1){
       if (th < 0 || sz <= th){
         dump(i, "cur is not a num");
         break; 
       }
       if (th == a[th] || a[th] == '-'){
-        a[i] = '-';
+        a[mf] = '-'; //or a[i]??
         a[th]= '+';
         dump(i, "after moving");
         break;
       }else if('+' == a[th] || a[th] == 'x'){
+        a[mf] = '-';
         a[th] = 'x';
-        cout<<th<<" marked as duplicated\n";
         dump(i, "after x");
+        cout<<th<<" marked as duplicated\n";
         break;
       }
       a[i] = '-';
       
       auto evict = a[th];
+      mf = th;
       a[th] = '+';
       th = evict;
-      cout<<evict<<" = evicted object\n";
       dump(i, "after evicting");
-  }//end of while loop, go back to for loop
+      cout<<evict<<" = evicted object\n";
+    }//end of while loop, go back to for loop
+  }
   dump(0, "game over");
 }
 /* Requirement: https://bintanvictor.wordpress.com/2018/04/07/check-array-of-0-to-n-nasdaq/
