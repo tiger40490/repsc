9414ef0f2470358714d64ac57604f4fc95677f22
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-07 23:18:18 -0400

    one test case

--- cpp1/array/oracleDayTrader_Nsdq.cpp
+++ cpp1/array/oracleDayTrader_Nsdq.cpp
@@ -9,13 +9,21 @@ using namespace std;
 typedef double Px;
 typedef size_t Ts; //timestamp
 typedef bool BS;
-
-void dump(map<Ts, BS>  const & a, string headline=""){  
-  cout<<"-------- "<<headline<<" .. size == "<<a.size()<<endl;
-  for (auto it=a.begin(); it!=a.end(); ++it){
-    cout<<it->first<<":"<<(it->second? "B":"S")<<"\t";
+Ts sz=0;
+double dump(map<Ts, BS>  const & trades, vector<Px> const & a, string headline=""){  
+  double accu = 0;
+  auto fi=trades.begin();
+  for (auto ri=trades.rbegin();
+       fi->first < ri->first; ++fi, ++ri){
+    assert(fi->second != ri->second);
+    accu += a[fi->first] * (fi->second ?-1:1);
+    accu += a[ri->first] * (ri->second ?-1:1);
   }
-  cout<<endl;
+  cout<<"-------- "<<headline<<" .. total profit = $"<<accu<<endl;
+  for (auto it=trades.begin(); it!=trades.end(); ++it){
+    cout<<it->first<<":"<<(it->second? "B":"S")<<" $"<<a[it->first]<<"\t";
+  }cout<<endl;
+  return accu;
 }
 void dump(vector<Px> const & a, string headline=""){  
   cout<<"-------- "<<headline<<" .. size == "<<a.size()<<endl;
@@ -36,19 +44,37 @@ void shrink(vector<Px> & a){ //remove useless points
     assert ((*prev(it) - *it) * (*it - *next(it)) < 0);
   }  
 }
+void wrapUp(vector<Px> & a, map<Ts, BS> & trades){
+  if (trades.size()%2 == 0){// If the trades collection has matched buys/sells, then we need to add a pair of buy/sell at the two ends. 
+      if (a[0]==a[sz-1]) return;
+      trades.insert(make_pair(0,   a[0]<a[sz-1]));
+      trades.insert(make_pair(sz-1,a[0]>a[sz-1]));
+      return;
+  }
+  //if first trade is buy, then we need a sell at higher of the two ends
+  bool closingHigher = a[0]<=a[sz-1];
+  BS buyNeeded = !trades.begin()->second;
+ 
+  if (closingHigher && !buyNeeded ||
+     !closingHigher &&  buyNeeded ){ 
+    trades.insert(make_pair(sz-1, !trades.begin()->second)); //add an reverse of the first trade
+  }else{
+    trades.insert(make_pair(0, !trades.begin()->second)); 
+  }
+}
 int solve(vector<Px> a){ //return total profit
   if (a.size() < 2) return 0;
   shrink(a); dump(a, "after shrink");
   
+  sz=a.size();
   map<Ts, BS> trades; //yes = buy; no = Sell
-  for(Ts i=1; i<a.size(); ++i){ 
+  for(Ts i=1; i<sz-1; ++i){ 
     trades.insert(make_pair(i,   a[i-1]>a[i])); //was falling=>buy
     //cout<<ii<<" is the current price"<<endl;
   }
-  dump(trades);
-    //Px const pp = *prev(it);
-    //Px const nn = *next(it);
-    //Px const ii = *it;
+  dump(trades, a, "b4 wrapUp");
+  wrapUp(a, trades);
+  dump(trades, a, "final");
 }
 int main(){
   //solve({4,5,3,6,1,2});
@@ -58,7 +84,5 @@ int main(){
 Strategy: every time we see a downturn, we sell (every upturn, we buy). 
 
 If the trades collection has an extra sell (buy), then both extreme ends point down(up) so we pick the lower(higher) between the two. 
-
-If the trades collection has matched buys/sells, then we need to add a pair of buy/sell at the two ends. Be careful.
 
 */
