11e70bfd5eed7ac7470afd18c9aa2dfac40204c6
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-08 00:38:46 -0400

    4 test cases

--- cpp1/array/oracleDayTrader_Nsdq.cpp
+++ cpp1/array/oracleDayTrader_Nsdq.cpp
@@ -9,7 +9,7 @@ typedef double Px;
 typedef size_t Ts; //timestamp
 typedef bool BS;
 void dump(vector<Px> const & a, string headline=""){  
-  cout<<"-------- "<<headline<<" .. size == "<<a.size()<<endl;
+  cout<<"-------- "<<headline<<" .. array size = "<<a.size()<<endl;
   auto const sz = a.size();
   for(Ts i=0; i<sz; ++i) cout<<i<<"\t";
   cout<<endl;
@@ -27,7 +27,7 @@ double dump(map<Ts, BS>  const & trades, vector<Px> const & a, string headline="
       accu += a[ri->first] * (ri->second ?-1:1);
     }
   }
-  cout<<"-------- "<<headline<<" .. total profit = $"<<accu<<endl;
+  cout<<"-------- "<<headline<<".. total profit = $"<<accu<<endl;
   for (auto it=trades.begin(); it!=trades.end(); ++it){
     cout<<it->first<<":"<<(it->second? "B":"S")<<" $"<<a[it->first]<<"\t";
   }cout<<endl;
@@ -75,11 +75,13 @@ int solve(vector<Px> a){ //return total profit
   }
   dump(trades, a, "b4 tieUpEnds");
   tieUpEnds(a, trades);
-  dump(trades, a, "final");
+  return dump(trades, a, "final .......");
 }
 int main(){
-  //solve({4,5,3,6,1,2});
-  solve({4,4,5, 4.11, 3, 4.22, 6,6,2,1,2});
+  assert (7 == solve({5,3,6,1,2}));
+  assert (7 == solve({4,5,3,6,1}));
+  assert (9 == solve({4,5,3,6,1,2}));
+  assert (9 == solve({4,4,5, 4.11, 3, 4.22, 6,6,2,1,2}));
 }
 /* Requirement: https://bintanvictor.wordpress.com/2018/04/07/best-day-trading-play-in-hindsight-nasdaq/
 Strategy: every time we see a downturn, we sell (every upturn, we buy). 
