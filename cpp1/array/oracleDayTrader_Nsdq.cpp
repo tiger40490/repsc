e4ea86d448e2a6bfc4c7ef2ffbbfaba2b80706b9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-07 23:42:54 -0400

    refactor

--- cpp1/array/oracleDayTrader_Nsdq.cpp
+++ cpp1/array/oracleDayTrader_Nsdq.cpp
@@ -1,7 +1,6 @@
 //showcasing vector erase while iterating
 #include <iostream>
 #include <vector>
-#include <deque>
 #include <map>
 #include <iterator>
 #include <assert.h>
@@ -9,22 +8,6 @@ using namespace std;
 typedef double Px;
 typedef size_t Ts; //timestamp
 typedef bool BS;
-Ts sz=0;
-double dump(map<Ts, BS>  const & trades, vector<Px> const & a, string headline=""){  
-  double accu = 0;
-  auto fi=trades.begin();
-  for (auto ri=trades.rbegin();
-       fi->first < ri->first; ++fi, ++ri){
-    assert(fi->second != ri->second);
-    accu += a[fi->first] * (fi->second ?-1:1);
-    accu += a[ri->first] * (ri->second ?-1:1);
-  }
-  cout<<"-------- "<<headline<<" .. total profit = $"<<accu<<endl;
-  for (auto it=trades.begin(); it!=trades.end(); ++it){
-    cout<<it->first<<":"<<(it->second? "B":"S")<<" $"<<a[it->first]<<"\t";
-  }cout<<endl;
-  return accu;
-}
 void dump(vector<Px> const & a, string headline=""){  
   cout<<"-------- "<<headline<<" .. size == "<<a.size()<<endl;
   auto const sz = a.size();
@@ -33,18 +16,36 @@ void dump(vector<Px> const & a, string headline=""){
   for(Ts i=0; i<sz; ++i) cout<<a[i]<<"\t";
   cout<<endl;
 }
-void shrink(vector<Px> & a){ //remove useless points
+double dump(map<Ts, BS>  const & trades, vector<Px> const & a, string headline=""){  
+  double accu = 0;
+  if (trades.size()%2 == 0){
+    auto fi=trades.begin();
+    for (auto ri=trades.rbegin();
+         fi->first < ri->first; ++fi, ++ri){
+      assert(fi->second != ri->second);
+      accu += a[fi->first] * (fi->second ?-1:1);
+      accu += a[ri->first] * (ri->second ?-1:1);
+    }
+  }
+  cout<<"-------- "<<headline<<" .. total profit = $"<<accu<<endl;
+  for (auto it=trades.begin(); it!=trades.end(); ++it){
+    cout<<it->first<<":"<<(it->second? "B":"S")<<" $"<<a[it->first]<<"\t";
+  }cout<<endl;
+  return accu;
+}
+void shrink(vector<Px> & a){ //remove useless price points
   for(auto it=next(a.begin()); it!=prev(a.end());){
     if ((*prev(it) - *it) * (*it - *next(it)) >= 0){ it=a.erase(it);
     }else{ ++it; }
-  }
+  }  //now verify
   int i=1; //can't put into for-loop header !
   for(auto it=next(a.begin()); it!=prev(a.end()); ++it, ++i){
     //cout<<"Every prince point should be a turning point... Checking tiomestamp # "<<i<<endl; 
     assert ((*prev(it) - *it) * (*it - *next(it)) < 0);
   }  
 }
-void wrapUp(vector<Px> & a, map<Ts, BS> & trades){
+void tieUpEnds(vector<Px> & a, map<Ts, BS> & trades){
+  auto sz=a.size();
   if (trades.size()%2 == 0){// If the trades collection has matched buys/sells, then we need to add a pair of buy/sell at the two ends. 
       if (a[0]==a[sz-1]) return;
       trades.insert(make_pair(0,   a[0]<a[sz-1]));
@@ -66,14 +67,14 @@ int solve(vector<Px> a){ //return total profit
   if (a.size() < 2) return 0;
   shrink(a); dump(a, "after shrink");
   
-  sz=a.size();
+  auto sz=a.size();
   map<Ts, BS> trades; //yes = buy; no = Sell
   for(Ts i=1; i<sz-1; ++i){ 
     trades.insert(make_pair(i,   a[i-1]>a[i])); //was falling=>buy
     //cout<<ii<<" is the current price"<<endl;
   }
-  dump(trades, a, "b4 wrapUp");
-  wrapUp(a, trades);
+  dump(trades, a, "b4 tieUpEnds");
+  tieUpEnds(a, trades);
   dump(trades, a, "final");
 }
 int main(){
