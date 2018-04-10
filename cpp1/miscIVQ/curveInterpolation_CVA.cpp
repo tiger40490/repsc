46856358e4823ed0f5f9dfcdab11c4a20a87cdc9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-10 19:20:48 -0400

    partial

--- cpp1/miscIVQ/curveInterpolation_CVA.cpp
+++ cpp1/miscIVQ/curveInterpolation_CVA.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+typedef pair<double, double> Pair;
+typedef vector<Pair> Pairs;
 
-struct MyLess: public binary_function<pair<double, double>>{
-  bool operator()(pair<double, double> a, pair<double, double> b) const{
+Pairs input={{4,4}, {1,1}, {2,2}, {3,3}};
+
+void dump(Pairs const & v, string const & headline=""){
+  for(int i=0; i<v.size(); ++i){
+    cout<<v[i].first<<":"<<v[i].second<<"  ";
+  }cout<<endl;
+}
+
+struct MyLess: public binary_function<Pair, Pair, bool>{
+  bool operator()(Pair a, Pair b) const{
     return a.first < b.first;
   }
-};
+} myLess;
+int main(){
+  sort(input.begin(), input.end(), myLess);
+  dump(input);
+}
+/*requirement: https://bintanvictor.wordpress.com/2018/05/05/cva-c-iv-2/
+*/
