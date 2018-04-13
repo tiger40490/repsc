72427a428b0583981d139db3d90a5c050bc59884
Victor Tan <vtan@CPEX.com>
2018-04-13 11:36:56 -0400

    untested

---
+++ py/milePerGallon.py
@@ -0,0 +1,67 @@
+import sys,bisect
+from pprint import pprint
+
+allrec = dict()
+
+class Trip:
+  def __init__(self, car, date, miles, fuel):
+    self.car = car
+    self.date = int(date)
+    self.miles = float(miles)
+    self.fuel = float(fuel)
+  def __repr__(self):
+    return self.__str__() # supports pprint
+  def __str__(self):
+    return self.car + ' ' + str(self.date) + ': miles=' + str(self.miles) + ' fuel=' + str(self.fuel);
+
+def load():
+  with open('data.txt') as f:
+    lines = f.readlines()
+  for line in lines:
+    if  not line.strip(): continue
+    tok = [x.strip() for x in line.split(',')]
+    r = Trip(tok[1], tok[4], tok[2], tok[3])
+    #print r
+    name = tok[0]
+    if allrec.has_key(name):
+      allrec[name].append(r)
+    else:
+      allrec[name] = [r]
+
+  #pprint (allrec)
+
+  for name in allrec:
+    allrec[name].sort(key=lambda x: x.date)
+  #pprint (allrec)
+
+def getMPG(name, d1, d2):
+  if not allrec.has_key(name): return 'name not found'
+  li = allrec[name]
+  keys = [rec.date for rec in li]
+  idx1 = bisect.bisect_left(keys, d1)
+  idx2 = bisect.bisect_left(keys, d2)
+  #print idx1, idx2
+  #print li
+
+  perCar = dict()
+  for idx in range(idx1, idx2+1):
+    rec = li[idx]
+    # put each trip under the given car
+    if perCar.has_key(rec.car):
+      perCar[rec.car][0]+=rec.miles
+      perCar[rec.car][1]+=rec.fuel
+    else:
+      perCar[rec.car]=[rec.miles, rec.fuel]
+
+  for car in perCar:
+    tmp = perCar[car]
+    print car, tmp[0], tmp[1], tmp[0]/tmp[1]
+
+def main():
+  load()
+  getMPG('John', 20160920, 20170920)
+
+if __name__== "__main__": # best practice.. make this script usable as a module
+    sys.exit(main())
+
+# Requirement: https://wp.me/p74oew-3zx 
