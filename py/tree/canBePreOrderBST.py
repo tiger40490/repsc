e11adc0306faa999fedb3b275c9e89c8d83edbb5
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-15 22:04:26 -0400

    unfinished

--- py/tree/canBePreOrderBST.py
+++ py/tree/canBePreOrderBST.py
@@ -4,22 +4,49 @@ class Node(object):
         self.up = up_node
         self.le = left_node
         self.ri = right_node
-        print 'Constructed:', data, (up_node.data if up_node else 'no_uplink')
+        #print 'new', data, (up_node.data if up_node else 'no_uplink')
         
 '''   5
   2       7
  1 3    6   8
              9
 '''
-#root = None
+root = None
+def visit(node):
+    if node.le: visit(node.le)
+    print node.data,
+    if node.ri: visit(node.ri)        
+def inOrderDftRecursive():
+    print '------- dump ------'
+    visit(root)
+def insertNew(val):
+#find the place to insert new node
+    parent = root
+    while 1:
+      if val <= parent.data:
+        if parent.le is None: 
+          new = Node(val, parent)
+          parent.le = new
+          return
+        parent = parent.le
+        continue
+      assert(val > parent.data)
+      if parent.ri is None:
+          new = Node(val, parent)
+          parent.ri = new
+          return
+      parent = parent.ri
 
 def canBePreOrderBST(li):
-  #global root
+  global root
   root = Node(li[0], None)
   for i in xrange(1, len(li)):
-    Node(li[i], None)
+    insertNew(li[i])
+    
+  inOrderDftRecursive()
   return True
 
 def main():
   assert (canBePreOrderBST([5,2,1,3,7,6,8,9]))
+  
 main()
