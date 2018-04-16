fabb7913d2be9008649d6a2a2642fb81b38f9d0f
tiger40490 <vtan@CPEX.com>
2018-04-16 09:26:20 -0400

    more tests

--- py/trivial/intsHaving3_BNYM.py
+++ py/trivial/intsHaving3_BNYM.py
@@ -14,7 +14,7 @@ def main():
   assert( solution(3,5,23) == '13 23')
   assert( solution(1,0,12) == '1 10 11 12') # digit=1 is more tricky
   assert( solution(1,90,111) == '91 100 101 102 103 104 105 106 107 108 109 110 111') #
-  assert( solution(0,5,23) == '10 20')
+  assert( solution(0,0,23) == '0 10 20')
 main()
 ''' Requirement: show all integers between start and end (inclusive) that shows the target digit
 if none, the return empty string
