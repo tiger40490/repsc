49fb8f44bab97be77d1ce40560a86c3f9887627d
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-15 22:15:23 -0400

    basic tests

--- py/tree/canBePreOrderBST.py
+++ py/tree/canBePreOrderBST.py
@@ -4,7 +4,7 @@ class Node(object):
         self.up = up_node
         self.le = left_node
         self.ri = right_node
-        #print 'new', data, (up_node.data if up_node else 'no_uplink')
+        #print 'new', data, '->', (up_node.data if up_node else 'isRoot')
         
 '''   5
   2       7
@@ -19,34 +19,39 @@ def visit(node):
 def inOrderDftRecursive():
     print '------- dump ------'
     visit(root)
-def insertNew(val):
+def insertNewFailed(val):
 #find the place to insert new node
+    print '---- inserting', val
     parent = root
     while 1:
       if val <= parent.data:
         if parent.le is None: 
+          if parent.ri: return val #failure
           new = Node(val, parent)
           parent.le = new
-          return
+          return 0
         parent = parent.le
+        #print 'parent set to', parent.data
         continue
       assert(val > parent.data)
       if parent.ri is None:
-          new = Node(val, parent)
-          parent.ri = new
-          return
+        new = Node(val, parent)
+        parent.ri = new
+        return 0
       parent = parent.ri
+      #print 'parent set to', parent.data
 
 def canBePreOrderBST(li):
   global root
   root = Node(li[0], None)
   for i in xrange(1, len(li)):
-    insertNew(li[i])
+    if insertNewFailed(li[i]): return False
     
   inOrderDftRecursive()
   return True
 
 def main():
+  assert (not canBePreOrderBST([5,2,3,1]))
   assert (canBePreOrderBST([5,2,1,3,7,6,8,9]))
   
 main()
