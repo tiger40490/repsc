7913af8fd4d35001475470d8a3e4c2f35ffbeb01
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-23 21:32:10 -0400

    tested

--- cpp1/miscIVQ/push_back_SIG.cpp
+++ cpp1/miscIVQ/push_back_SIG.cpp
@@ -4,37 +4,45 @@ todo: prevent memory leak in the face of placement-new exception
 showcase placement new
 showcase unique_ptr::release()
 showcase unique_ptr::get()
+showcase scoped enum
 */
 #include <iostream>
 #include <memory> //unique_ptr
-#include <vector>
+#include <assert.h>
 using namespace std;
 
-template<typename T> struct MyVec: public vector<T>{
+enum class AllocMode{DC, PN};
+template<typename T> struct MyVec{
 	size_t sz, cap;
-	T* arr; //rename to ar
+	AllocMode mode;
+	T* arr;
 	size_t size()    {return sz;}
 	size_t capacity(){return cap;}
-	void init(size_t aa){
-		arr = new T[aa]; //basic impl
-		arr[0]=0;
-		sz=aa;
-		cap=aa;
+	void uninit (AllocMode const m){ //leave raw memory uninitialized
+		this->mode=m;
+		size_t const cnt1=1;
+		arr = new T[cnt1];
+		cap=cnt1;
+		sz=0;
 	}
-	T* alloc1(size_t newcap){//tested
+	T* alloc1(size_t const newcap){
+		assert(this->mode == AllocMode::DC);
 		unique_ptr<T[]> //need to ensure delete[] is called
 		newArr(new T[newcap]); //default construct newcap instances
 		std::copy(arr, arr+sz, newArr.get());
+		cout<<"Returning from alloc1()\n";
 		return newArr.release();
 	}
 /*Above (inefficient) uses default ctor on raw memory, followed by op=()
 Below (efficient) uses placement new to copy-construct.
 */
-	T* alloc2(size_t newcap){//tested
+	T* alloc2(size_t const newcap){//tested
+		assert(this->mode == AllocMode::PN);	
 		char * raw = (char*) malloc( sizeof(T)*newcap );
 		for(int i=0; i<sz; ++i){
 			new (raw+i*sizeof(T)) T( *(arr+i) ); //what if throws?
 		}
+		cout<<"Returning from alloc2()\n";
 		return (T*)raw;
 	}
 	void dump(string const & headline){
@@ -45,11 +53,15 @@ Below (efficient) uses placement new to copy-construct.
 	}
 	void push_back(T const & t){ //redefine
 		size_t const sz=size();
-		if (sz == capacity()){
-			size_t newcap = sz*2;
+		if (sz == capacity() || sz==0){
+			size_t newcap = sz==0? 1:sz*2;
 			cerr<<newcap<<" <--- will allocate to this capacity\n";
 			T* const tmp = this->arr;
-			this->arr = alloc2(newcap);
+			if      (this->mode==AllocMode::DC)
+  			  this->arr = alloc1(newcap);
+			else if (this->mode==AllocMode::PN)
+  			  this->arr = alloc2(newcap);
+		  
 			delete[] tmp;
 			this->cap=newcap;
 		}
@@ -60,14 +72,10 @@ Below (efficient) uses placement new to copy-construct.
 };
 int main(){
 	MyVec<int> v;
-	v.init(1);
-	for (int i =1; i<9; ++i){
-		//cout<<i<<" being inserted..\n";
-		v.push_back(i*10);
+	for (int m=0; m<2; ++m){
+		v.uninit(static_cast<AllocMode>(m));
+		for (int i =0; i<5/*9 is good*/; ++i) v.push_back(i+10);
 	}
 }
-
 /*Requirement: implement vector push_back().
-
-I feel it's not easy to test it. So I will just compile.
 */
