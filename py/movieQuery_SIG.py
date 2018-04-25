24add8369004fa805cf18cdac80a9c99321f2b22
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-25 08:55:46 -0400

    comment

--- py/movieQuery_SIG.py
+++ py/movieQuery_SIG.py
@@ -78,7 +78,8 @@ def main():
   #inst.get_movies('drama', 1992, 1992)
   #inst.get_movies('drama', 1992, 1993)
 main()
-'''
+'''Requirement: Given sample data file below (unsorted), implement an efficient query get_movies(genre, start_year, end_year)
 #movieId,title,date,genres
 1,Toy Story,1995,Adventure|Animation|Children|Comedy|Fantasy
+
 '''
