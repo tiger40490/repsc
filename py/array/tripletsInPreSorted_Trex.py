37c769291e37a6cbcdbebd9063c22b92b1211691
tiger40490 <vtan@CPEX.com>
2018-04-26 18:04:17 -0400

    improve performance using hashtable

--- py/array/tripletsInPreSorted_Trex.py
+++ py/array/tripletsInPreSorted_Trex.py
@@ -1,44 +1,21 @@
-'''
-todo: save the pairs in a dict!
-
-Same cost in both solutions, but I feel sol3 is more clever
-'''
-li=sorted([12,3,6,2,4,20,15,8,10,9,1,5])
-li=sorted([1,2,4,8,16,32,64,128,256,512,1024,2048])
-sz=len(li)
-pairs=[]
-
-def sol3loop(): 
+def sol3loop(input): 
+  li=sorted(input); sz=len(li)
+  factorsOf=dict() # key is big number, value is a list of factorsOf
   cost=0; cnt=0
   for s in range(sz-1):
     for b in range(s+1, sz):
       sm,bi = li[s],li[b]
       if bi%sm: continue
-      newPair = (sm,bi)
-      for _sm,_bi in pairs:
-        cost += 1
-        if sm == _bi: cnt+=1; print 'match -', _sm,_bi,bi
-      pairs.append(newPair)
-      #print newPair
-  print 'cost =', cost, ', cnt =', cnt
-
-def sol2(): # based on Shanyou's input
-  cost=0; cnt=0
-  for s in range(sz-1):
-    for b in range(s+1, sz):
-      sm,bi = li[s],li[b]
-      if bi%sm == 0: 
-        pairs.append((sm,bi))
-  for i in xrange(len(pairs)):
-    sm, bi = pairs[i]
-    for j in xrange(i+1, len(pairs)):
-      sm2, bi2 = pairs[j]
-      cost += 1
-      if bi == sm2: cnt+=1; print 'match -', sm,bi,bi2
-  print 'cost =', cost, ', cnt =', cnt
+      for factor in factorsOf.get(sm, []):
+        cnt+=1; print 'match -', factor,sm,bi
+      if bi in factorsOf:
+        factorsOf[bi].append(sm);
+      else:
+        factorsOf[bi]=[sm]
+  return cnt
+  
 def main():
-  sol3loop()
-  #sol2()
+  assert(220 == sol3loop([1,2,4,8,16,32,64,128,256,512,1024,2048]))
 main()
 '''Requirement: https://bintanvictor.wordpress.com/2018/03/03/find-triplet-x-divide-y-y-divides-z/
 '''
