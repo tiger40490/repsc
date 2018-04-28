8b03b6bb06bab161c960d792ac8c011488ba7b84
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-28 15:49:29 -0400

    better design

--- cpp1/66miscIVQ/quasiConst_subsequence_Mak.cpp
+++ cpp1/66miscIVQ/quasiConst_subsequence_Mak.cpp
@@ -1,12 +1,27 @@
 #include <vector>
 #include <set>
+#include <map>
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <assert.h>
 #define ss if(1>0)cout //to mass-disable cout before uploading to hacker rank
 using namespace std;
+template<typename K, typename V> ostream & operator<<(ostream & os, pair<K,V> const & p){
+   os<<p.first<<":"<<p.second;
+   return os;
+}
+template<typename K, typename V, int min_width=4> ostream & operator<<(ostream & os,  map<K,V> const & c){
+   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
+   os<<endl;
+   return os;
+}
 template<typename T,             int min_width=4> ostream & operator<<(ostream & os, vector<T> const & c){
+   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
+   os<<endl;
+   return os;
+}
+template<typename T,             int min_width=4> ostream & operator<<(ostream & os, multiset<T> const & c){
    for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
    os<<endl;
    return os;
@@ -14,9 +29,24 @@ template<typename T,             int min_width=4> ostream & operator<<(ostream &
 typedef int Level; // can be negative
 int solution(vector<int> &A) {
     size_t N = A.size(); ss<<N<<" = N\n";
-	set<Level> const levels(A.begin(), A.end());
-    if (levels.size()==1) return N; //original sequence's length
-    size_t ret=1;
+    map<Level, size_t> frq;
+    for(Level const & i: A) frq[i]++;
+    ss<<frq;
+    if (frq.size()==1) return N; //original sequence's length
+    size_t ret=1;	
+    for(auto it=frq.begin(); it!=frq.end(); ++it){
+      /*a "band" consists of 2 adjacent levels like levels 6/7, or 3/4 but not 3/5. 
+      We scan from lowest band to highest band, and count how many elements are in each band*/
+	  Level const & b=it->first;
+      ss<<b<<" is the lower value of current band\n";
+	  auto const cnt = it->second + (frq.count(b+1)? frq[b+1]:0);
+      ss<<cnt<<" = cnt\n";
+      if (ret < cnt) ret = cnt;
+	}
+	return ret;
+	
+	multiset<Level> const levels(A.begin(), A.end());
+	ss<<levels;
     for(auto it=levels.begin(); it!=levels.end(); ++it){
 	  Level const & b=*it;
       ss<<b<<" is the lower value of current band\n";
