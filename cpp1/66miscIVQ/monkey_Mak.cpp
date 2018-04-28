f03d4d40a9a38eef36e52dc172116f5b85fa865e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-28 02:27:12 -0400

    improve performance

--- cpp1/66miscIVQ/monkey_Mak.cpp
+++ cpp1/66miscIVQ/monkey_Mak.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
@@ -11,7 +12,7 @@ template<typename T,             int min_width=4> ostream & operator<<(ostream &
    return os;
 }
 typedef int ts; //timestamp;
-typedef unsigned int Position; //0 to N, N being the destination
+typedef int Position; //0 to N, N being the destination
 typedef int Gap; //between 0 and 2, gap is 2
 size_t N=0;
 //in the vector param, true at 0th position means stone visible there
@@ -32,13 +33,14 @@ bool isGood(vector<bool> const & visible, int const D){
 int solution(vector<int> &A, int D) {
     N = A.size(); ss<<N<<" = dest position\n";
     if (D >= (int)(1+N)) return 0;//N+1 gaps apart,single jump of size(N+1)will do
-    ts const lastTimeStamp = *max_element(A.begin(), A.end());
-    ss<<lastTimeStamp<<endl;
 
     //Play the movie of stones at time 0, time 1, time 2...
     //check isGood() at each snapshot. If good, return the timestamp.
     //if still no good at end of the movie, then return -1
-    for(ts t=0; t<=lastTimeStamp; ++t){
+    set<ts> const times(A.begin(), A.end());
+    for(auto it =times.begin(); it!=times.end(); ++it){
+      ts const t=*it;
+      if (t<0) continue;
       ss<<t<<" = timestamp\n";
       vector<bool> vis(N);
       for(Position p=0; p<N; ++p) vis[p] = A[p] <= t && A[p]!=-1;
@@ -48,14 +50,15 @@ int solution(vector<int> &A, int D) {
     return -1;
 }
 int sol(vector<ts> A, int D){
-    //ss<<A;
-    return solution(A, D);
+    auto ret = solution(A, D);
+    ss<<ret<<" <----- returned\n";
+    return ret;
 }
 int main(){
     //cout<<isGood({false, false, true,false,false, false}, 2);
-    ss<<sol({1, 7,-1,6, 0, 2, 8,3, 5}, 3)<<" returned\n";
-    ss<<sol({1, -1, 0, 2, 3, 5}, 3)<<" returned\n";
-    ss<<sol({3, 2, 1}, 1)<<" returned\n";
-    ss<<sol({1, 2, 3, 4, -1, -1, -1}, 3)<<" returned\n";
+    assert(6==sol({1, 7,-1,6, 0, 2, 8,3, 5}, 3));
+    assert(2==sol({1, -1, 0, 2, 3, 5}, 3));
+    assert(3==sol({3, 2, 1}, 1));
+    assert(-1==sol({5, 2, -1, 4, -1, -1, -1}, 3));
 }
 //Requirement: https://bintanvictor.wordpress.com/2018/04/28/monkey-crossing-river-codility/
