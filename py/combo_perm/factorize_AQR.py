f547171d6f7b54c0f93be9bd318726633a2471b8
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-04-29 14:25:29 -0400

    clean up

--- py/combo_perm/factorize_AQR.py
+++ py/combo_perm/factorize_AQR.py
@@ -1,15 +1,19 @@
 from math import sqrt
-#smallFactors is sorted from low to high
 cnt=0
 def factorize(bigNum, smallFactors=tuple()):
   '''find each formula to factorize bigNum, but when print it, also print smallFactors
   '''
+  assert sorted(smallFactors) == list(smallFactors), 'should be sorted low to high'
   print smallFactors, bigNum
   global cnt
-  if smallFactors: cnt+=1
-  else: cnt=0
-  tmp = smallFactors[-1] if smallFactors else 2 #highest existing factor
-  for f in xrange(tmp, int(sqrt(bigNum))+1):
+  if smallFactors: 
+      cnt+=1
+      assert smallFactors[-1] <= bigNum
+      _start = smallFactors[-1] # highest existing factor
+  else: 
+    cnt=0
+    _start = 2
+  for f in xrange(_start, int(sqrt(bigNum))+1):
     if bigNum%f: continue
     factorize(bigNum/f, smallFactors + (f,))
   return cnt
@@ -22,8 +26,6 @@ def main():
   assert(3  == factorize(12));  
 main()
 '''Requirement: https://bintanvictor.wordpress.com/2017/04/16/factorize-a-natural-number-aqr/
-Based on https://github.com/tiger40490/repo1/blob/cpp1/cpp1/combo_permu/factorize_AQR.cpp
-
 --for 36 we want
 fac 36 () iterating from 2 to 6
   fac 18 (+2) iterating from 2 to 4
@@ -34,9 +36,4 @@ fac 36 () iterating from 2 to 6
     fac 4 (3, +3)
   fac 9 (+4)    
   fac 6 (+6)
---for 12 we want
-factorize 12 ()
- factorize 6 (+2)
-  factorize 3 (2,+2)
- factorize 4 (+3)
 '''
