bbc88439bf85ab656c24fb99fc6777f71a7aa3b3
tiger40490 <vtan@CPEX.com>
2018-05-01 21:31:36 -0400

    clean up

--- cppProj/tetris/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd $(dirname "${BASH_SOURCE[0]}") 
-/bin/rm -fv bin/* 
-set -ex
-g++ -I. --std=c++0x -c src/ValueObj.cpp -o bin/ValueObj.o
-g++ -I. --std=c++0x bin/ValueObj.o  src/main.cpp -o bin/main.out
-bin/main.out | tee data/_console_output.log
