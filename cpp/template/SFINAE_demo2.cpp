9a1f2dc03fc674fd7bb206e03606aa3978e611c1
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-10 09:04:50 +0800

    clean up

--- cpp/template/SFINAE_demo2.cpp
+++ cpp/template/SFINAE_demo2.cpp
@@ -1,41 +1,41 @@
 #include <assert.h>
 #include <iostream>
-//using namespace std; //problematic
+using namespace std; 
 
 struct type9{ char dummy[9]; };
-template <class T> struct is_pointer{
+template <class T> struct isCustomPtr{
   template <class U>
-  static char is_ptr(U *); //U can be int
+  static char f281(U *); //U can be int
 
   template <class U>
-  static short is_ptr(U (*)());
+  static short f281(U (*)());
 
   template <class X, class Y>
-  static float is_ptr(Y  X::*);
+  static float f281(Y  X::*);
 
   template <class X, class Y>
-  static double is_ptr(Y (X::*)(std::ostream));
+  static double f281(Y (X::*)(ostream));
 
-  static type9 is_ptr(...); //default overload
+  static type9 f281(...); //default overload
   static T t;
-  static size_t const value = sizeof(is_ptr(t));
+  static size_t const value = sizeof(f281(t));
 };
 
 struct Foo {
   long bar; 
-  int f1(std::ostream);
+  int f1(ostream);
   //float f2();
 };
 
 int main(void){
   typedef int * IntPtr;
   typedef long Foo::* FooMemberPtr;
-  typedef int (Foo::*FooMemFunPtr)(std::ostream) ;
+  typedef int (Foo::*FooMemFunPtr)(ostream) ;
   typedef int (*FuncPtr)();
 
-  assert(1==is_pointer<IntPtr>::value); 
-  assert(2==is_pointer<FuncPtr>::value);
-  assert(4==is_pointer<FooMemberPtr>::value);
-  assert(8==is_pointer<FooMemFunPtr>::value);
-  std::cout<<is_pointer<float>::value;
+  assert(1==isCustomPtr<IntPtr>::value); 
+  assert(2==isCustomPtr<FuncPtr>::value);
+  assert(4==isCustomPtr<FooMemberPtr>::value);
+  assert(8==isCustomPtr<FooMemFunPtr>::value);
+  cout<<isCustomPtr<float>::value;
 }//based on https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/SFINAE
