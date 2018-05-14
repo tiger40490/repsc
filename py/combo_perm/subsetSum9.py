df036469831fe6ee8cb3904847b63ee65316f822
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-14 12:36:21 +0800

    add test

--- py/combo_perm/subsetSum9.py
+++ py/combo_perm/subsetSum9.py
@@ -1,12 +1,16 @@
 # showcase: efficiency : avoid sum() over a long list
 # todo assert on result
+hits=0
 def recurs(hand, tgtSum, cards, used=()):
+  global hits
   if tgtSum<0: return
+  if not used: hits=0
   #print 'hand=',hand,'; tgtSum=',tgtSum,'; rem=',cards,'; used=',used
   if hand==0:
     if 0 == tgtSum:
       print 'hit', used
-    return  
+      hits+=1
+    return hits
   firstCard=used[-1]+1 if used else 1
   for card in range(firstCard, cards[-1]+1):
     remain=list(cards)
@@ -14,14 +18,13 @@ def recurs(hand, tgtSum, cards, used=()):
     u2=list(used)
     u2.append(card)
     recurs(hand-1, tgtSum-card, remain, u2)
+  return hits
 
 def solve(hand, tgtSum, cards):
-  recurs(hand, tgtSum, range(1,cards+1))
+  return recurs(hand, tgtSum, range(1,cards+1))
 def test():
-  solve(3, 11, 9)
+  assert 5==solve(3,11,9)
 test()
 '''Req: given pocker cards numbered 1 to 9, find all hands of 3 producing target tgtSum of 11
 See blog 
-
-Can use a simplification of the combo code in the same dir
 '''
