f0ae27ee3ad34a4c533efbdfa63002009b8e5bac
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-17 08:23:08 +0800

    add comments to explain the logic

--- cpp/linkedList/partition_LNet.cpp
+++ cpp/linkedList/partition_LNet.cpp
@@ -10,6 +10,9 @@ template<typename T,             int min_width=4> ostream & operator<<(ostream &
    os<<endl;
    return os;
 }
+/*invariant: bb is at or before ee. We check this before every 
+inc/dec
+*/
 template<typename I, typename P>
 I myPartition(I bb, I const end, P isgood){
   auto ee = prev(end);
@@ -17,39 +20,35 @@ I myPartition(I bb, I const end, P isgood){
     if (ee == bb) return bb;
     --ee;
   }
-  assert ( isgood(*ee) && "ee is now initialized to a good node");
+  assert ( isgood(*ee) && "ee is now initialized to the last good node in original sequence");
+  int swapCnt=0;
   for (;;){
-    if (bb == ee){
-      cout<<111111<<endl;
-    }
-    //adjust bb and check exit conditions
     while(isgood(*bb)){
-      ++bb;
-      if (bb==ee){
+      if (next(bb)==ee){ 
         cout<<22222<<endl;
-        if (ee == prev(end)) return end;
+        if (ee == prev(end)) return end; //no bad node
+        assert(swapCnt);
+        assert(!isgood(*ee) && "if there's any swap, then ee must be Bad now");
         return ee;
       }
+      ++bb;
     }
     assert ( !isgood(*bb) && "bb now points to a bad node");
-    //adjust ee and check exit conditions
     while(!isgood(*ee)){
-      --ee;
-      if (bb==ee){
-        cout<<33333333<<endl;
-        return ee;
+      if (next(bb)==ee){
+        cout<<"both are bad and adjcent"<<endl;
+        return bb;
       }
+      --ee;
     }
     assert ( isgood(*ee) && "ee now points to a good node");
     //cout<<*bb<<" -swapping- "<<*ee<<endl;
     swap(*bb, *ee);
+    ++swapCnt;
   }
   return end; 
 }
-
 bool isOdd(int i){ return i%2; }
-
-
 void test1(list<int> li){
   auto firstBad = myPartition(li.begin(), li.end(), isOdd);
   cout<<li<<" --> ";
@@ -60,11 +59,14 @@ void test1(list<int> li){
   }
 }
 int main(){
+  test1({4,6,7});
   test1({1,2,3,4,5,6,7,8});
   test1({1,2,3,4,5,6,7,8,9});
   test1({1,3,5,2,4,6,8,7,9});
   test1({1,3,5,7});
 }
 /*Req: ....
+Note our iterator doesn't support comparison.
+
 My solution is overcomplicated since I move two rather than one pointer.
 */
