d552f626dafeb95ee9dcb156e5ee5c8f3be04edd
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-28 10:47:23 -0400

    comments

--- py/2d/classic_pathCountQ9.py
+++ py/2d/classic_pathCountQ9.py
@@ -34,7 +34,7 @@ def main():
   work(test1)
   work(test2)
 main()
-'''Req: See pdf in the same dir. Here are some key points:
+'''Req: See Q9.pdf in the email to Ashish. Here are some key points:
 
 Consider a maze mapped to a matrix with an upper left corner at coordinates (row, column) = (0, 0). Any movement must be in increasing row or column direction. You must determine the
 number of distinct paths through the maze. You will always start at position (0, 0), the top left, and end up at (max(row), max(column)), the bottom right. 
@@ -42,8 +42,11 @@ number of distinct paths through the maze. You will always start at position (0,
 1 1 0 1
 1 1 1 1 
 
-As an example, consider the above diagram where 1 indicates an open cell and 0 indicates blocked. You can only travel through open cells, so no path can go through the cell
-at (0, 2). There are two distinct paths to the goal.
+As an example, consider the above matrix where 1 indicates an open cell and 0 indicates blocked. You can only travel through open cells, so no path can go through the cell at (0, 2). There are two distinct paths to the goal. As a 2nd  example, matrix below has 10 paths:
+
+1 1 1 1 
+1 1 1 1 
+1 1 1 1
 
 The function must return the number of paths through the matrix, modulo (10^9 + 7).
 
