dc28fab2588fcac0a83eb6f9e572fee60e37cab0
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2018-05-28 12:14:27 -0400

    Comments only

--- py/2d/classic_pathCountQ9.py
+++ py/2d/classic_pathCountQ9.py
@@ -38,31 +38,4 @@ def main():
   work(test1)
   work(test2)
 main()
-'''Req: See Q9.pdf in the email to Ashish. Here are some key points:
-
-Consider a maze mapped to a matrix with an upper left corner at coordinates (row, column) = (0, 0). Any movement must be in increasing row or column direction. You must determine the
-number of distinct paths through the maze. You will always start at position (0, 0), the top left, and end up at (max(row), max(column)), the bottom right. 
-
-1 1 0 1
-1 1 1 1 
-
-As an example, consider the above matrix where 1 indicates an open cell and 0 indicates blocked. You can only travel through open cells, so no path can go through the cell at (0, 2). There are two distinct paths to the goal. As a 2nd  example, matrix below has 10 paths:
-
-1 1 1 1 
-1 1 1 1 
-1 1 1 1
-
-The function must return the number of paths through the matrix, modulo (10^9 + 7).
-
-1 <= n, m <= 1000
-
---Analysis:
-Better use a 64-bit int
-
-I see a binary tree, where each node is a cell. Cell (0,0) has down/left node (1,0) + right node (0,1). I feel this is similar to the classic problem "count paths between 2 nodes in a binary tree"
-
-Brute force: I implemented something like a DFT but it was too slow with some test cases
-
-From any cell, we can compute how many distinct paths. Save it in a lookup{cell -> cnt}
-
-'''
+# Req: https://bintanvictor.wordpress.com/2018/05/28/count-paths-between-2-tree-nodes/
