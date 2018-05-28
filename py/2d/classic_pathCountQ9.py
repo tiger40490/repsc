c7bf902a61ee6c8e43d558b376716e6a5bfdc6ef
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-28 12:59:16 -0400

    more test + more instrumentation

--- py/2d/classic_pathCountQ9.py
+++ py/2d/classic_pathCountQ9.py
@@ -1,5 +1,10 @@
 m = list() 
 revisits = dict()
+def test3():
+  global m
+  tmp=100
+  m = [[1 for x in xrange(tmp)] for y in xrange(tmp)]
+  assert len(m) == tmp and len(m[0]) == tmp
 def test2():
   global m
   m.append([1,1,1,1])
@@ -9,22 +14,22 @@ def test1():
   global m
   m.append([1,1,0,1])
   m.append([1,1,1,1])
-def start(r,c): 
+def start(r,c, verbose): 
   global finalCnt, revisits
   addr=(r,c)
   revisits[addr] = revisits.get(addr, 0) + 1
-  if revisits[addr] > 4: print addr, 'visited', revisits[addr], 'times :('
+  if verbose and revisits[addr] > 4: print addr, 'visited', revisits[addr], 'times :('
   if m[r][c]==0: #dead end
     return
   if r < height-1:
-    start(r+1, c)
+    start(r+1, c, verbose)
   if c < width-1:
-    start(r, c+1)
+    start(r, c+1, verbose)
     
   if r == height-1 and c == width-1:
     finalCnt += 1
-    print 'incremented finalCnt to', finalCnt
-def work(setup1test):
+    if verbose: print 'incremented finalCnt to', finalCnt
+def work(setup1test, verbose=1):
   global m, width, height, finalCnt, revisits
   m = list()
   setup1test()
@@ -33,9 +38,11 @@ def work(setup1test):
   revisits=dict()
   print '-----------'
   
-  start(0,0)
+  start(0,0, verbose)
 def main():
-  work(test1)
+  #work(test3, verbose=0)
+  #return
   work(test2)
+  work(test1)
 main()
 # Req: https://bintanvictor.wordpress.com/2018/05/28/count-paths-between-2-tree-nodes/
