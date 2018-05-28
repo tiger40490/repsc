e0bebaa64805470a8d2223bda471132c1d9d465e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-28 09:47:08 -0400

    logging

--- py/array/maxProfit_buy1sellAny.py
+++ py/array/maxProfit_buy1sellAny.py
@@ -1,9 +1,8 @@
-def trade1day(li):
+def trade1day(li, verbose=1):
   holding=0; cashflow=0
-  for i in xrange(len(li)-1): # visit all but last point
-    #print li[i]
+  for i in xrange(len(li)-1): # visit all but Final minute
     if li[i]<=li[i+1]:
-      #print 'buy 1 @', li[i]
+      if verbose: print 'buy 1 @', li[i]
       holding += 1
       cashflow -= li[i]
       assert abs(cashflow)<9223372036854775807, 'this int should be 64-bit signed'
@@ -13,14 +12,14 @@ def trade1day(li):
         cashflow += holding * li[i]
         holding=0
 
-  print 'Last point: sell', holding, '@', li[-1]
+  print 'Final minute: sell', holding, '@', li[-1]
   cashflow += holding * li[-1]
-  #print 'returning max profit =', cashflow
+  #if verbose: print 'returning max profit =', cashflow
   return cashflow
 def main():
   tmp = [1 for _ in xrange(500000-1) ]
   tmp.append(100000)
-  assert 499999*99999 ==trade1day(tmp)
+  assert 499999*99999 ==trade1day(tmp, verbose=0)
   assert 0  ==trade1day([500000*100000, 500000*100000])
   assert 0  ==trade1day([50000])
   assert 2  ==trade1day([1,3,3,2])
