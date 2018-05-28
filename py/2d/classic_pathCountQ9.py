04597ea3cf628dfbdf1243f89739ab9361946cb2
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-05-28 11:00:41 -0400

    instrumentation for efficiency

--- py/2d/classic_pathCountQ9.py
+++ py/2d/classic_pathCountQ9.py
@@ -1,17 +1,19 @@
 m = list() 
-
-def test1():
+revisits = dict()
+def test2():
   global m
   m.append([1,1,1,1])
   m.append([1,1,1,1])
   m.append([1,1,1,1])
-def test2():
+def test1():
   global m
   m.append([1,1,0,1])
   m.append([1,1,1,1])
 def start(r,c): 
-  '''every time we find a path, inc'''
-  global finalCnt
+  global finalCnt, revisits
+  addr=(r,c)
+  revisits[addr] = revisits.get(addr, 0) + 1
+  if revisits[addr] > 4: print addr, 'visited', revisits[addr], 'times :('
   if m[r][c]==0: #dead end
     return
   if r < height-1:
@@ -23,12 +25,14 @@ def start(r,c):
     finalCnt += 1
     print 'incremented finalCnt to', finalCnt
 def work(setup1test):
-  global m, width, height, finalCnt
+  global m, width, height, finalCnt, revisits
   m = list()
   setup1test()
   height = len(m)
   width = len(m[0])
-  finalCnt=0;
+  finalCnt=0
+  revisits=dict()
+  print '-----------'
   start(0,0)
 def main():
   work(test1)
