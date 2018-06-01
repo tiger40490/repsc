e3a6b7775bb43bff8a9583a709febba6f4330779
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2018-06-01 13:33:48 -0400

    two opposite ways

--- bash/read_n_exit.sh
+++ bash/read_n_exit.sh
@@ -1,3 +1,9 @@
-echo -en "\n\n\n Run FixShell1 and grep log? [any_other_key / n ] ";
-read $REPLY;
-[ "$REPLY" = "n" ] && exit # use "return" if this is a sourced script
+printf "\n Continue? [ y / any_other_key to quit] " #printf is better than q[echo -en]
+read $REPLY
+[ "$REPLY" = "y" ] || return # use q[ exit ] if this is a standalone script
+
+
+########  Here's the "opposite" way
+echo -en "\n Continue? [any_other_key to confirm / n ] "
+read $REPLY
+[ "$REPLY" = "n" ] && exit # use q[ return ] if this is a sourced script
