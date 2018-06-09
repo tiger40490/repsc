182f9ac8a608b76edbcf1e33369c13829ba43240
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-09 14:50:39 -0400

    new file

---
+++ py/array/oracleDayTrader_Nsdq.py
@@ -0,0 +1,9 @@
+
+
+def main():
+  
+main()
+'''Req: https://1330152open.wordpress.com/2018/06/09/to-landlord-self-intro/
+Perhaps sort the data points and lose the order. Top half sell, bottom half buy.
+
+'''
