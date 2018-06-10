5fd3ff3595b0da8f4ee9450568c824a017c0d79e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-10 19:50:56 -0400

    add better solution

--- py/str/testAbbr_ez.py
+++ py/str/testAbbr_ez.py
@@ -8,13 +8,22 @@ def main():
   assert     isAbbr("internationalization", "in")
   assert     isAbbr("mamalonkey", "mamalon")
   assert     isAbbr("mamalonkey", "monkey")
-def findLongestAbbr(hay, li):
-  li.sort(key=len, reverse=True) # in-place sort !
-  for abb in li:
-    print 'trying', abb, hay
-    if isAbbr(hay, abb): return len(abb)
-  return None 
-def isAbbr(hay, abb):
+def isAbbr(hay, abb): #hay is driver
+'''Q: Why is this solution simpler?
+A: no nested loop. If abb drives outer loop, we need inner loop to scan hay:(
+A: we need to increment i only in one place
+'''
+  i=0
+  sz=len(abb)
+  for ch in hay:
+    if ch == abb[i]:
+      if i == sz-1: 
+        print abb, 'is good'
+        return True
+      i+=1
+  assert i < sz   
+  return False
+def isAbbr1(hay, abb): #abb is driver
   if len(abb) * len(hay) == 0: return False
   i=0
   sz=len(hay)
@@ -28,6 +37,12 @@ def isAbbr(hay, abb):
     i+=1
   print abb, 'is good'
   return True
+def findLongestAbbr(hay, li):
+  li.sort(key=len, reverse=True) # in-place sort !
+  for abb in li:
+    print 'trying', abb, hay
+    if isAbbr(hay, abb): return len(abb)
+  return None   
 main()
 '''Req: https://bintanvictor.wordpress.com/2017/12/16/bbg-eq-longest-abbreviation/
 '''
