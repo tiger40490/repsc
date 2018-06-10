88644457d239f7171834d78d805409f961a47fa9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-10 18:26:28 -0400

    fix major bugs

--- py/str/testAbbr_ez.py
+++ py/str/testAbbr_ez.py
@@ -1,17 +1,32 @@
+# showcase defining a function after calling it
 
-def isAbbr(abb, hay):
+def main():
+  findLongestAbbr("internationalization", 
+    ['inter','national','nation','in','int','nationaliz'])
+  assert not isAbbr("int", 'iii')
+  assert not isAbbr("i", 'i5')
+  assert     isAbbr("internationalization", "in")
+  assert     isAbbr("mamalonkey", "mamalon")
+  assert     isAbbr("mamalonkey", "monkey")
+def findLongestAbbr(hay, li):
+  li.sort(key=len, reverse=True) # in-place !
+  for abb in li:
+    print 'trying', abb, hay
+    if isAbbr(hay, abb): return len(abb)
+  return None 
+def isAbbr(hay, abb):
   i=0
   sz=len(hay)
   for ch in abb:
+    if i == sz: return False # past the end of haystack
     while ch != hay[i]:
+      if i == sz-1: 
+        return False
       i+=1
-      if i == sz: 
-        return False # past hay!
-    assert ch == hay[i]
-    if i == sz-1:
-      return True
-def main():
-  assert isAbbr("monkey", "mamalonkey")
+    assert ch == hay[i]    
+    i+=1
+  print abb, 'is good'
+  return True
 main()
 '''Req: https://bintanvictor.wordpress.com/2017/12/16/bbg-eq-longest-abbreviation/
 '''
