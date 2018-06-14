0787ac2dac03bb891883b0b0de58cbbe7306610e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-13 22:32:26 -0400

    can compile

--- cpp/thr/singleThrPool_bbg.cpp
+++ cpp/thr/singleThrPool_bbg.cpp
@@ -1,13 +1,13 @@
 /* showcasing nested class
- * showcasing my own RAII soped lock
+ * showcasing my own RAII scoped lock
  *
- * No main(). Compile with -c. Unable to test
+ * Dummy main(). Unable to test
  */
 #include <queue>
 #include <pthread.h>
 typedef void (*func_t)(); //func ptr
 
-class Data {
+class Tpool {
     std::queue<func_t> jobs;
     pthread_cond_t     cond; //initialized somehow
     pthread_mutex_t    mutex;
@@ -15,15 +15,18 @@ class Data {
     void unlock() { pthread_mutex_unlock(&mutex); }
     void wait()   { pthread_cond_wait(&cond, &mutex); }
     void signal() { pthread_cond_signal(&cond); }
-  public:
-    Data();
 // -------- end of skeleton given ---------
     struct ScopedLock{
-      ScopedLock(Data* m): _m(m){ m->lock();}
+      ScopedLock(Tpool* m): _m(m){ m->lock();}
       ~ScopedLock(){_m->unlock();}
-      Data* _m;
+      Tpool* _m;
     };
-    void run(){
+  public:
+    Tpool(){
+      mutex = PTHREAD_MUTEX_INITIALIZER;
+      cond  = PTHREAD_COND_INITIALIZER;
+    }
+    void run(){ //dequeue
       for(;;){
         func_t job = NULL;
         { ScopedLock lk(this);
@@ -42,7 +45,8 @@ class Data {
         jobs.push(job);
       }
 
-      // if this function call actually saw an empty queue, the size() may still return a non-one
+      // if this function call actually saw an empty queue, the size() call 
+      // may still return a value greater than one.
       // However, another thread Must have seen size() == 1 and sent the signal!
       // If I were to put this line inside the ScopedLock block, then it's more reliable,
       // but checking size() is too lengthy for lock-holder threads.
@@ -51,7 +55,9 @@ class Data {
       if (jobs.size() == 1) signal();
     }
 };
-/* Write the enqueue and dequeue methods for a single thread pool.
+int main(){
+}
+/* Requirement: Write the enqueue and dequeue methods for a single thread pool.
  *
  * Given the following skeleton, write the run() method that runs in a single worker thread and repeatedly removes jobs from the queue and executes them,
  * and write the enqueue() method that is called from the main thread and adds jobs to the queue.
