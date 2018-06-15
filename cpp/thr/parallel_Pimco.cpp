676e2a0b690d2ba4565563a844f0ac1c9acd3cc7
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-15 00:45:24 -0400

    clean up

--- cpp/thr/parallel_Pimco.cpp
+++ cpp/thr/parallel_Pimco.cpp
@@ -7,20 +7,18 @@
 #include <iostream>
 using namespace std;
 typedef int StartIndex;
-//typedef int Result;
 
 long long const N = 100;
 int const M = 6; 
 long long const correctAns = (1+N)*N/2;
 vector<int> arr(N);
-//vector<Result> results(M); // all zeros
 template<typename CT> void dump(CT const & cont) { 
     for(auto const & i: cont)        cout<<i<<endl;
 }
 void * run(void* arg) {
   StartIndex * ptr = static_cast<StartIndex*>(arg);
   StartIndex const startIdx = *ptr;
-  delete ptr;
+  delete ptr;  //we know the addr is on heap
   //cout<<"Th-"<<pthread_self()<<" : "<<startIdx<<" = startIdx\n";
   long long sum=0;
   for (int idx=startIdx; idx<N; /* last valid index is N-1*/
@@ -29,21 +27,16 @@ void * run(void* arg) {
        sum += arr[idx];
        usleep(1);
   }
-  //results[startIdx] = sum;
   //cout<<"Th-"<<pthread_self()<<" returning "<<sum<<endl;
   return new int(sum);
 }
 int main(){
   std::iota (arr.begin(), arr.end(), 1);  
   assert (arr[N-1] == N);
-  
-  vector<StartIndex> starts(M); 
-  std::iota (starts.begin(), starts.end(), 0);  
-  //dump(starts);
-  
+    
   vector<pthread_t> th(M);
   for (int i=0; i<M; ++i){
-    pthread_create(&th[i], NULL, &run, new int(i));
+    pthread_create(&th[i], NULL, run, new int(i));
   }
 
   long long total = 0;
@@ -51,8 +44,8 @@ int main(){
   for (int i=0; i<M; ++i){
     pthread_join(th[i], &thrResult);  
     int * subtotal = (int*) thrResult;
-    //cout<<*subtotal<<" returned"<<endl;
-    total += *subtotal; //results[i];
+    cout<<*subtotal<<" returned"<<endl;
+    total += *subtotal;
     delete subtotal; //we know the addr is on heap
   }
   assert (total == correctAns);
