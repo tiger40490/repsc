ef9bc93566c80ec6d8076d0f5aec01520656ab05
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-20 07:37:35 -0400

    comment

--- cpp/linkedList/ez_removeBadNodes_Flex.cpp
+++ cpp/linkedList/ez_removeBadNodes_Flex.cpp
@@ -1,9 +1,11 @@
-//showcase switch-case + command line arg to specif one test case. Below is a command to run multiple tests:
+//showcase switch-case + command line arg to specify a single test case. Below is a composite command to run multiple tests:
 //  g++ ez_removeBadNodes_Flex.cpp && ./a.exe 0 && ./a.exe p && ./a.exe a && ./a.exe 1
+
+//todo: So far we have two in-place solutions. Add a copy-filter solution, less efficient but hopefully simpler.
+
 #include <iostream>
 #include <cassert>
 using namespace std;
-#define ss if(1>0) cout
 struct Node{
   float data; Node * next;
   Node (float payload, Node* n=nullptr): data(payload), next(n){}
