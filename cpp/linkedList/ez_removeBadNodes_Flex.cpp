36ff470c8b4d70413a10afdb423e9fe1a9dfa3f7
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-06-20 07:10:49 -0400

    add new solution

--- cpp/linkedList/ez_removeBadNodes_Flex.cpp
+++ cpp/linkedList/ez_removeBadNodes_Flex.cpp
@@ -1,3 +1,4 @@
+//todo: add tests
 #include <iostream>
 #include <cassert>
 using namespace std;
@@ -16,20 +17,22 @@ struct Node{
   _2(120, &_3),
   _1(110, &_2);
 Node * head = &_1;
-void dump(Node *h){
+size_t dump(Node *h){
+  size_t ret = 0;
   for (; h; h=h->next){
     cout<<h->data<<"->";
+    ++ret;
   }
   cout<<"nullptr\n";
+  return ret;
 }
-Node* removeNodes(Node* listHead, int x) {
+Node* solution1(Node* listHead, int x) {
     while (listHead != nullptr && listHead->data > x){
         Node* a = listHead;
         listHead = listHead->next;
     } // from now on, listHead is the correct value to return
     Node* p = listHead;
     if (p == nullptr){
-      cout<<9999<<endl;
       return nullptr;
     }
     Node* q = p->next;   
@@ -49,8 +52,28 @@ Node* removeNodes(Node* listHead, int x) {
     }
     return listHead;
 }
-int main(){
-  head=removeNodes(head, 35);
-  dump(head);
+Node* solution2(Node* listHead, int x) {
+  Node FakeHead(0, listHead);
+  Node * p=&FakeHead;
+  for (Node * q=p->next; q!=nullptr; q=q->next){
+    if (q->data > x){
+      //cout<<"continue after "<<q->data<<endl;
+      continue;
+    }
+    assert(q->data <= x);
+    p->next=q;
+    p=q;
+  }
+  p->next = nullptr; //?
+  return FakeHead.next;
+}
+Node* removeNodes(Node* listHead, int x) {
+  return solution1(listHead, x);
+}
+int main(){ //run one of these tests, not 2 together!
+  //assert(dump(removeNodes(head, 15))==0);
+  //assert(dump(removeNodes(head, 35))==1);
+  //assert(dump(removeNodes(head, 55))==5);
+  assert(dump(removeNodes(head, 5555))==10);
 }/*Req: write a function to remove all nodes whose data > x. Return the head of the modified list.
 */
