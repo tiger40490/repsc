b3820e0a949b2f525753f011fc125feb35dfed77
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2018-06-23 18:46:12 -0400

    basic tests done

---
+++ cpp/array/binSearchRoatedArr.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#define ss if(1>0)cout //to mass-disable cout before uploading to hacker rank
+using namespace std;
+
+template<typename T,             int min_width=4> ostream & operator<<(ostream & os, vector<T> const & c){
+   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
+   os<<endl;
+   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
+   os<<endl;
+   return os;
+}
+class Solution {
+public:
+    size_t End;
+    int _2high(vector<int>& nums, int target) {
+        cout<<"2high.. Must be in right segment\n";
+        return -1; //to be implemented
+    }
+    int _2low(vector<int>& nums, int target) {
+        cout<<"2low..  Must be in left segment\n";
+        vector<int>& a=nums;
+        size_t cand = End/2, le=0, ri=End;
+        for(;nums[cand]<target; ){
+            if (le >= ri){
+                cout<<"non-existent\n";
+                return -1;
+            }
+            if (a[le] < a[cand]){
+              cout<<"rightward";
+              le = cand;
+            }else{
+              cout<<"leftward";
+              ri = cand;
+            }
+            cand = (le + ri)/2;
+            cout<<"  trying "<<cand<<endl;
+        }
+        if (a[cand] == target) return cand;
+        assert(a[le] < target && target < a[cand]); // only need to search between a[0] and a[cand]
+        cout<<"binary searching from "<<le<<" to "<<cand<<endl;
+        ri=cand;
+        for(;le<ri; cand=(le+ri)/2){
+          if (a[cand] == target) return cand;
+          if (a[cand] < target){ le = cand+1;
+          }else{ ri = cand-1;
+          }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        vector<int>& a=nums;
+        cout<<a;
+        End = a.size()-1;
+        if (a[0] == target) return 0;
+        bool isLeEndTooLow = a[0] < target;
+
+        if (a[End] == target) return End;
+        bool isRiEndTooLow = a[End] < target;
+        if ( !isLeEndTooLow && isRiEndTooLow ){ //
+            cout<<"caught between left-end and right-end:("<<endl;
+            return -1;
+        }
+        assert (a[0] >= a[End]);
+        if ( isLeEndTooLow && !isRiEndTooLow ){
+          assert (false && "array is monotonic wihtout rotation :(");
+        }
+        assert (isLeEndTooLow == isRiEndTooLow);
+        if (isLeEndTooLow) return _2low(nums, target);
+        else               return _2high(nums,target);
+    }
+} inst;
+int main(){
+  vector<int> a={33,35,37,39,40,42,44,55,1,3,4,11,22};
+  auto ret = inst.search(a,62);
+  cout<<ret<<" is the answer\n";
+}//Req: https://bintanvictor.wordpress.com/2018/06/23/binary-search-in-rotated-sorted-array/
