82e445fa7f8c47915f0c12d250e8c7a50c7b75d4
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2018-06-30 12:19:09 -0400

    comment

--- cpp/array/robNever2homesAdjacent.cpp
+++ cpp/array/robNever2homesAdjacent.cpp
@@ -1,12 +1,13 @@
 //accepted at Leetcode. Not very efficient, but relatively simple
+// There are simpler solutions but I find them less intuitive to me.
 #include <vector>
 #include <iostream>
 using namespace std;
 
 int rob(vector<int> const& nums) {
         int bn = 0, //best plan up to current house
-          bp=0, //best up to prev
-          bpp=0; // bestUpToPrevPrev
+            bp=0, //best up to prev
+            bpp=0; // bestUpToPrevPrev
         for (int i=0; i<nums.size(); ++i){
             int tmp = bpp;
             bpp = bp;
@@ -19,8 +20,8 @@ int main(){
   cout<< rob({2,1,1,2});
 }/* Req: https://leetcode.com/problems/house-robber/description/
 
-You are a robber planning to rob houses along a street. Each house has a certain 
+You are a robber planning to rob houses along a street. Each house has some 
 amount of money stashed, the only constraint stopping you from robbing each of them
-is that adjacent houses have security system connected and it will automatically 
+is that they have security system connected and it will automatically 
 contact the police if two adjacent houses were broken into.
 */
