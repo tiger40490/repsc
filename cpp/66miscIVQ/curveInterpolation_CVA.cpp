c2422307f03570b2cb24f78406e1e02cdb5870cc
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-01 14:34:47 -0400

    minor refactor

--- cpp/66miscIVQ/curveInterpolation_CVA.cpp
+++ cpp/66miscIVQ/curveInterpolation_CVA.cpp
@@ -3,14 +3,25 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <iomanip>
 #include <assert.h>
 using namespace std;
 typedef pair<double, double> Pair;
 typedef vector<Pair> Pairs;
-void dump(Pairs const & v, string const & headline=""){
-  for(int i=0; i<v.size(); ++i){
-    cout<<v[i].first<<":"<<v[i].second<<"  ";
-  }cout<<endl;
+
+template<typename K, typename V> ostream & operator<<(ostream & os, pair<K,V> const & p){
+   stringstream tmp2;
+   tmp2<<p.first<<":"<<p.second;
+   os<<tmp2.str();
+   return os;
+}
+
+template<typename T,             int min_width=4> ostream & operator<<(ostream & os, vector<T> const & c){
+   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
+   os<<endl;
+   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
+   os<<endl;
+   return os;
 }
 
 struct MyLess: public binary_function<Pair, Pair, bool>{
@@ -51,12 +62,13 @@ struct Curve{
 };
 int main(){
   Curve c({{4,4}, {1,1}, {2,2}, {3,3}});
-  dump(c.points);
-  c.lookup(0.1);
+  cout<<c.points;
+  c.lookup(0.1); //extrapolate
   c.lookup(1);
   c.lookup(1.5);
   c.lookup(4);
-  c.lookup(4.1);
+  c.lookup(4.1); //extrapolate
 }
-/*requirement: https://bintanvictor.wordpress.com/2018/05/05/cva-c-iv-2/
+/*requirement: https://bintanvictor.wordpress.com/2018/05/05/cva-c-iv-2/ piecewise linear interpolation
+This question is not tough algorithm but syntax
 */
