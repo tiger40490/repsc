394b3c5d2d9caa1a8fc9f8b2f30faab2768707ac
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-04 00:27:23 -0400

    more tests

--- cpp/template/FibTailRecurse.cpp
+++ cpp/template/FibTailRecurse.cpp
@@ -1,31 +1,33 @@
 //showcase compile time recursive template instantiation
+//showcase enum with a specific base typedef
+//showcase macro
 #include <iostream>
 #include <cassert>
+#define check(N) assert (recurs(N) == Fibonacci<N>::value);
 using namespace std;
 
 typedef long long fnum; //a Fib number
 
 /* 0,1,1,2,3,5...
 */
-recurs(size_t distance, fnum first=0, fnum second=1){
+fnum recurs(size_t distance, fnum first=0, fnum second=1){
 	if (distance==0) return first;
 	//cout<<first<<","<<second<<","<<distance<<endl;
 	return recurs(distance-1, second, first+second);
 }
 
 template <int DISTANCE, fnum A=0, fnum B=1> struct Fibonacci{
-    enum { //recursive computation is done at compile time
+    enum : fnum { //recursive computation is done at compile time
       value = Fibonacci<DISTANCE-1, B, A+B>::value 
     };
 };
 
 //specialization as a recursion exit
 template <fnum A, fnum B> struct Fibonacci<0, A, B> {
-    enum { value = A };
+    enum : fnum { value = A };
 };
 
 int main(){
-    assert (recurs(45) == (Fibonacci<45>::value));
-    assert (recurs(6) == (Fibonacci<6>::value));
-    assert (recurs(3) == (Fibonacci<3>::value));
+    check(91); //largest Fib number for long long
+    check(90);
 }
