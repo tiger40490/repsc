99082096d6ecc47f17a93a96374a761938dc3778
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-04 00:17:13 -0400

    more tests

--- cpp/template/FibTailRecurse.cpp
+++ cpp/template/FibTailRecurse.cpp
@@ -1,8 +1,9 @@
+//showcase compile time recursive template instantiation
 #include <iostream>
 #include <cassert>
 using namespace std;
 
-typedef int fnum; //a Fib number
+typedef long long fnum; //a Fib number
 
 /* 0,1,1,2,3,5...
 */
@@ -13,18 +14,18 @@ recurs(size_t distance, fnum first=0, fnum second=1){
 }
 
 template <int DISTANCE, fnum A=0, fnum B=1> struct Fibonacci{
-    enum { 
+    enum { //recursive computation is done at compile time
       value = Fibonacci<DISTANCE-1, B, A+B>::value 
     };
 };
 
-//specialization
+//specialization as a recursion exit
 template <fnum A, fnum B> struct Fibonacci<0, A, B> {
     enum { value = A };
 };
 
 int main(){
-    assert (recurs(3)  == (Fibonacci<3>::value));
-    assert (recurs(7) == (Fibonacci<7>::value));
-    assert (recurs(6)  == (Fibonacci<6>::value));
+    assert (recurs(45) == (Fibonacci<45>::value));
+    assert (recurs(6) == (Fibonacci<6>::value));
+    assert (recurs(3) == (Fibonacci<3>::value));
 }
