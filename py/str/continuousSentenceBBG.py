356fd517dc33435bfeaa38ca1964e7978936a8e6
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-04 18:28:02 -0400

    add more efficient solution

--- py/str/continuousSentenceBBG.py
+++ py/str/continuousSentenceBBG.py
@@ -1,9 +1,10 @@
 import bisect
 Hopeless = -99
 input = ('al', 'ali', 'alice', 'bob', 'dan')
+input = ["cats","dog","sand","and","cat"]
 
 hay = sorted(input)
-engDict = set(input) # the English dictionary
+wordDict = hay # the English dictionary
 matched=list() # list of words parsed
 
 def myReturn(pw): # to reduce indentation levels
@@ -13,7 +14,7 @@ def myReturn(pw): # to reduce indentation levels
 
 def check1char(char):
       print 'check1char', char
-      for word in engDict:
+      for word in wordDict:
         if char in word: 
           return True
       return False
@@ -21,11 +22,11 @@ def check1char(char):
 def parse(remain):
   sz = len(remain)
   if sz == 0: return True # entire sentence parsed :)
-  if remain in engDict: return myReturn(remain)
+  if remain in wordDict: return myReturn(remain)
   for i in range(1,sz+1):
     pw = remain[0:i] # possible word. remain[0:n] would return the first n characters from "remain".
     print i, '= i, trying possible word', pw
-    if pw in engDict:    
+    if pw in wordDict:    
       childResult = parse(remain[i:])
       if childResult == Hopeless: return Hopeless
       if childResult: 
@@ -38,17 +39,34 @@ def parse(remain):
       tmp = bisect.bisect_left(hay, pw)
       le = hay[tmp-1] if tmp>=1 else '<empty_str>'
       tmp = bisect.bisect_right(hay, pw)
-      ri = hay[tmp]   if tmp<len(engDict) else '<empty_str>'
-      #print pw, 'not in engDict. left/right are: ', le, '/', ri 
+      ri = hay[tmp]   if tmp<len(wordDict) else '<empty_str>'
+      #print pw, 'not in wordDict. left/right are: ', le, '/', ri 
       if not   (le.startswith(pw) or ri.startswith(pw)):
         #print pw, 'is not a prefix of any word.. giving up on', remain
         return False;
   # end of for loop    
   return False       
+
+def sol1(sentence): # one-pass
+  sz = len(sentence)
+  aboveWater = [False] * sz
+  # Within the sentence, a position is "above water" if the char thereat is the last char of a known word, and we can hop off it.
+  for idx in xrange(sz): 
+    # print 'setting', idx
+    for word in wordDict:
+      hopBack = idx - len(word)
+      if hopBack < -1: continue #word is too long and we are near beginning of sentence
+      if word == sentence [hopBack+1 : hopBack+1+len(word)]:
+         # -1 means this word is at the start of sentence
+        if hopBack == -1 or aboveWater[hopBack]:
+           # print word, 'provides a hop from ', hopBack
+           aboveWater[idx] = True
+           break
+  return aboveWater[-1]
+  
 def main():
-  print engDict
-  result = parse('aliceali')
-  print result, (matched if result else '')
+  print wordDict
+  print sol1("catsandog")
 main()
 '''Req: https://bintanvictor.wordpress.com/2018/02/24/splitcontinuoussentenceusingdictionary/
 '''
