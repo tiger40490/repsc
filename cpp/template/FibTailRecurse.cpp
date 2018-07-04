6c72f1415b71206989712236e701d2e493e5ab1c
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-04 10:45:35 -0400

    rename objects

--- cpp/template/FibTailRecurse.cpp
+++ cpp/template/FibTailRecurse.cpp
@@ -3,27 +3,27 @@
 //showcase macro
 #include <iostream>
 #include <cassert>
-#define check(N) assert (recurs(N) == Fibonacci<N>::value);
+#define check(N) assert (recurFunc(N) == FibCompileTime<N>::value);
 using namespace std;
 
 typedef long long fnum; //a Fib number
 
 /* 0,1,1,2,3,5...
 */
-fnum recurs(size_t distance, fnum first=0, fnum second=1){
+fnum recurFunc(size_t distance, fnum first=0, fnum second=1){
 	if (distance==0) return first;
 	//cout<<first<<","<<second<<","<<distance<<endl;
-	return recurs(distance-1, second, first+second);
+	return recurFunc(distance-1, second, first+second);
 }
 
-template <int DISTANCE, fnum A=0, fnum B=1> struct Fibonacci{
+template <int DISTANCE, fnum A=0, fnum B=1> struct FibCompileTime{
     enum : fnum { //recursive computation is done at compile time
-      value = Fibonacci<DISTANCE-1, B, A+B>::value 
+      value = FibCompileTime<DISTANCE-1, B, A+B>::value 
     };
 };
 
 //specialization as a recursion exit
-template <fnum A, fnum B> struct Fibonacci<0, A, B> {
+template <fnum A, fnum B> struct FibCompileTime<0, A, B> {
     enum : fnum { value = A };
 };
 
