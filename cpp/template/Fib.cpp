2dcbf482e6208a50a73ac58d89be6a6717205f94
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-04 00:04:43 -0400

    verify against non-template

--- cpp/template/Fib.cpp
+++ cpp/template/Fib.cpp
@@ -2,6 +2,18 @@
 #include <cassert>
 using namespace std;
 
+typedef int fn; //a Fib number
+
+/* 0,1,1,2,3,5...
+recurs(0,1,0) == 0
+recurs(0,1,1) == 1
+*/
+recurs(size_t distance, fn first=0, fn second=1){
+	if (distance==0) return first;
+	//cout<<first<<","<<second<<","<<distance<<endl;
+	return recurs(distance-1, second, first+second);
+}
+
 template <int DISTANCE, int A=0, int B=1> struct Fibonacci{
     enum { 
       value = Fibonacci<DISTANCE-1, B, A+B>::value 
@@ -14,7 +26,7 @@ template <int A, int B> struct Fibonacci<0, A, B> {
 };
 
 int main(){
-    assert (2 == (Fibonacci<3>::value));
-    assert (13 == (Fibonacci<7>::value));
-    assert (8 == (Fibonacci<6>::value));
+    assert (recurs(3)  == (Fibonacci<3>::value));
+    assert (recurs(7) == (Fibonacci<7>::value));
+    assert (recurs(6)  == (Fibonacci<6>::value));
 }
