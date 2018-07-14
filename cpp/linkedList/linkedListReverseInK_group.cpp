d66f1d702abab3ffe404b6dcfb64b9700645a84e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-14 17:13:45 -0400

    partially done

--- cpp/linkedList/linkedListReverseInK_group.cpp
+++ cpp/linkedList/linkedListReverseInK_group.cpp
@@ -6,7 +6,7 @@ struct Node{
   Node * next;
   Node (int payload, Node* n=NULL): data(payload), next(n){}
   friend ostream & operator<<(ostream & os, Node const & t){
-    os<<static_cast<char>(t.data) <<"  # "<<&t<<" -> "<<t.next;
+    os<<static_cast<char>(t.data) <<"  @ "<<&t<<" -> "<<t.next;
     return os;
   }
 } _1('1'), //tail
@@ -16,14 +16,19 @@ _4('4', &_3),
 _5('5', &_4),
 _6('6', &_5),
 _7('7', &_6),
-* head = &_7;
+* head = &_6;
 void dump(string const& headline){
    cout<<headline<<endl<<"New head: ";
-   for (Node* t=head; t; t=t->next)
+   size_t cnt=0;
+   for (Node* t=head; t; t=t->next){
       cout<< *t <<endl;
+      if (++cnt > 9) break;
+   }
    cout<<"==================\n";
 }
 // Above is a useful, simple set-up of linked list for coding interview
+
+short const K = 2; //reverse every group of K nodes.
 
 /** Each recursive call modifies exactly one node, b in this case
  * Pre-condition: A used to point to b but now A has already been fixed in the previous call
@@ -32,7 +37,7 @@ void dump(string const& headline){
 void * reverse1(Node* A, Node* b){
   assert(A->next != b);
   Node * c = b->next;
-  b->next = A; cout<<*b<<" fixed\n"; //fix node b
+  b->next = A;  //cout<<*b<<" fixed\n";
   if (c == NULL) return head = b; //exit condition
   reverse1(b,c);
 }
@@ -42,31 +47,62 @@ void tailRecursion(){
         reverse1(head, b);
 }
 
+size_t size(){
+  size_t ret=0;
+  for (Node * i=head; ; ++ret, i = i->next){
+    if (i == NULL) return ret;
+  }
+}
 void iterative(){
+  size_t sz = size();
+  assert(sz%K == 0); //for now, i don't support stub group
+  
   Node * a=head;
   Node * b=a->next;
   Node * c=b->next;
-  // marker variables on 3 consecutive nodes
+  Node * prevGroupTail = a;
+  Node * xGroupTail = NULL;
 
   a->next=NULL; // first fix the head node
+  static size_t cnt=1;
+  bool isG1 = true;
 
   for (;;){
     b->next = a; //fix the "b" node
+    cout<<++cnt<<": fixing "<<*b<<" \n";
+    if (cnt % K ==0){
+      cout<<"one group done\n";
+
+      if (isG1){
+        head = b;
+        if (c == NULL){
+          break;
+        }
+        isG1 = false;
+        
+      }else{
+        prevGroupTail->next = b; 
+        if (c == NULL){
+          xGroupTail->next = NULL;
+          break;
+        }
+        prevGroupTail = xGroupTail;
+        cout<<(char)prevGroupTail->data<<" === prevGroupTail\n";
+        //save tail of current group needs to point it at head of new group
+      }
+      xGroupTail = c;
+      cout<<(char)xGroupTail->data<<" = xGroupTail\n";
+    }
+    assert(c != NULL);
     a=b; b=c; c = c->next; //shift down the 3 markers
     // now between a and b there's no link; a is fixed; b is intact
-
-    if (c == NULL){
-      head = b;
-      head->next = a;
-      break;
-    }
   }
 }
 int main(int argc, char *argv[]) {
-   iterative();
-   dump("---Iterative Reverse Completed:");
    tailRecursion();
    dump("---Tail recursion Reverse Completed:");
+   iterative();
+   dump("---Iterative Reverse Completed:");
 }/*Req: https://bintanvictor.wordpress.com/2018/07/14/reverse-link-list-in-k-groups/
 I won't bother to pass all leetcode tests. Real interview will focus on the key points, not those corner cases
 */
