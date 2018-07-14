634047d70b36d035aa9381202281ac491662aa6f
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-14 11:29:45 -0400

    partially working, K-group not implemented

---
+++ cpp/linkedList/linkedListReverseInK_group.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+struct Node{
+  int const data;
+  Node * next;
+  Node (int payload, Node* n=NULL): data(payload), next(n){}
+} _1('1'), //tail
+_2('2', &_1),
+_3('3', &_2),
+_4('4', &_3),
+_5('5', &_4);
+Node * head = &_5;
+void dump(string const& headline){
+   cout<<headline<<endl<<"New head: ";
+   for (Node* t=head; t; t=t->next)
+      cout<< static_cast<char>(t->data) <<"  # "<<t<<" -> "<<t->next<<endl;
+}
+// Above is a useful, simple set-up of linked list for coding interview
+
+/** Each recursive call modifies exactly one node, b in this case
+ * Pre-condition: A used to point to b but now A has already been fixed in the previous call
+ * Return type is unused, but makes the exit if-block shorter
+ */
+void * reverse1(Node* A, Node* b){
+  assert(A->next != b);
+  Node * c = b->next;
+  b->next = A; //fix node b
+  if (c == NULL) return head = b; //exit condition
+  reverse1(b,c);
+}
+void tailRecursion(){
+        Node * b = head->next;
+        head->next = NULL;
+        reverse1(head, b);
+}
+
+void iterative(){
+  Node * a=head;
+  Node * b=a->next;
+  Node * c=b->next;
+  // marker variables on 3 consecutive nodes
+
+  a->next=NULL; // first fix the head node
+
+  for (;;){
+    b->next = a; //fix the "b" node
+    a=b; b=c; c = c->next; //shift down the 3 markers
+    // now between a and b there's no link; a is fixed; b is intact
+
+    if (c == NULL){
+      head = b;
+      head->next = a;
+      break;
+    }
+  }
+}
+int main(int argc, char *argv[]) {
+   iterative();
+   dump("Iterative Reverse Completed:");
+   tailRecursion();
+   dump("Tail recursion Reverse Completed:");
+}/*Req: https://bintanvictor.wordpress.com/2018/07/14/reverse-link-list-in-k-groups/
+I won't bother to pass all leetcode tests. Real interview will focus on the key points, not those corner cases
+*/
