1cb067767ccf3d6f29d0a9ea3be4752c574b62a2
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-14 23:23:49 -0400

    partially working

--- py/str/dragonSearch.py
+++ py/str/dragonSearch.py
@@ -1,23 +1,39 @@
 from pprint import pprint
-words =sorted(['fooo', 'barr', 'wing', 'ding', 'wing'])
+words =sorted(['fooo', 'barr', 'wing', 'ding']) #, 'wing'])
+wordCnt=len(words)
 S = 'lingmindraboofooowingdingbarrwingmonkeypoundcake'
 W = len(words[0])
 hm = dict()
-arr=range(len(S))
-def build():
+sz = len(S)-W+1
+arr=range(sz)
+def solutionA():
   for i in xrange(len(words)):
     hm[ words[i] ] = i + 10
     assert len( words[i] ) == W
   pprint(hm)
-  for pos in xrange(len(S)-W+1):
+  for pos in xrange(sz):
     slice = S[pos:pos+W]
     #print pos, slice
     wid = hm.get(slice, None) # 2-digits
     arr[pos] = wid;
     if (wid): assert slice == words[wid-10]
-    
+  #pprint(arr) 
+  ret=list()
+  for dragonHead in xrange(sz):
+    used=set()
+    #print '---dragon search at', dragonHead # only need arr and used
+    for i in xrange(dragonHead,sz,W):
+      if not arr[i]: break
+      if arr[i] in used: break;
+      used.add(arr[i])
+      #print arr[i], words[arr[i]-10], 'found at', i
+    if len(used) == wordCnt: 
+      ret.append(dragonHead)
+      print '   ! dragon found starting at', dragonHead
+  return ret
+      
 def main():
-  build()
+  print solutionA()
 main()
 '''
 '''
