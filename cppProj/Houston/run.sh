72d09008749a3a641ef5dacd1bebb31373033d14
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-22 15:25:10 -0400

    use c++17

--- cppProj/Houston/run.sh
+++ cppProj/Houston/run.sh
@@ -1,9 +1,9 @@
 # This script builds the code and runs tests
 set -ex
-g++ -I. -g -std=c++0x -c AbstractEngine.cpp
-g++ -I. -g -std=c++0x -c Engine1.cpp
-g++ -I. -g -std=c++0x -c Engine3.cpp
-g++ -I. -g -std=c++0x AbstractEngine.o Engine1.o Engine3.o main.cpp
+g++ -I. -g -std=c++17 -c AbstractEngine.cpp
+g++ -I. -g -std=c++17 -c Engine1.cpp
+g++ -I. -g -std=c++17 -c Engine3.cpp
+g++ -I. -g -std=c++17 AbstractEngine.o Engine1.o Engine3.o main.cpp
 set +e
 printf "\n----- build done, now running -----\n" 
 rm -v output.csv
