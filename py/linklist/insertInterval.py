445b745f18a95a782f5ec906af3e5ca1a97b0414
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-25 00:09:55 -0400

    improve instrumentation

--- py/linklist/insertInterval.py
+++ py/linklist/insertInterval.py
@@ -2,33 +2,35 @@ A='A'
 B='B'
 class Node(object): # each node is a segment
     cnt=0
-    def __init__(self, leftMark, prev_node=None, type=A):
+    def __init__(self, leftMark, prev_node=None, color=A):
         assert leftMark > 0
         Node.cnt+=1
-        self.leftMark = leftMark
-        assert type in 'AB'
-        self.type=type
+        self.leftMark = leftMark # rightMarkDerived would be next.leftMark-1
+        assert color in 'AB'
+        self.color=color
         self.prev = prev_node
         self.next = None
         if prev_node:
           prev_node.next = self
     def __str__(self):
-      return str(self.leftMark)
+      return str(self.leftMark)+' '+self.color
     
-_1 = Node(1) 
-_2 = Node(2, _1, B)
-_3 = Node(3, _2)
-_4 = Node(4, _3, B)
-_5 = Node(5, _4)
-_6 = Node(6, _5, B)
-_7 = Node(7, _6)
-_8 = Node(8, _7, B)
-_9 = Node(9, _8)
+_1 = Node(11) 
+_2 = Node(22, _1, B)
+_3 = Node(33, _2)
+_4 = Node(44, _3, B)
+_5 = Node(55, _4)
+_6 = Node(66, _5, B)
+_7 = Node(77, _6)
+_8 = Node(88, _7, B)
+_9 = Node(99, _8)
 head=_1
 
 def print3(node):
   if node:
-    print node.prev, '<= [', node.leftMark, '] =>', node.next
+    a = node.next
+    rightMarkDerived = a.leftMark-1 if a else 99999
+    print node.prev, '<= [', str(node), rightMarkDerived, '] =>', node.next
   else:
     print '_none_'
 def dump():
@@ -41,8 +43,8 @@ def dump():
       line += 'end'
       break
     print3(node)
-    line += str(node.leftMark) + ' > '
-    assert node.leftMark >= last; last=node.leftMark
+    line += str(node) + ' > '
+    assert node.leftMark > last; last=node.leftMark
     next = node.next
     if next: assert next.prev == node
     node = next
