5c4bc37064f1078d7da7410b9a5597371e7c5b34
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-26 19:27:48 -0400

    rename class

--- py/linklist/insertInterval.py
+++ py/linklist/insertInterval.py
@@ -2,14 +2,15 @@
 showcase double linked list
 '''
 import sys, bisect
-A='A'; B='B'
+A='A' # Above-water 
+B='B' # Below-Water
 class Node(object): # each node is a segment
     cnt=0
     def __init__(self, leftMark, prev_node=None, color=A):
         assert leftMark > 0
         Node.cnt+=1
         self.leftMark = leftMark # rightMark would be self.next.leftMark-1. I choose not to save rightMark into each node instance
-        assert color in 'AB'
+        assert color in [A,B]
         self.color=color
         self.prev = prev_node
         self.next = None
@@ -17,6 +18,7 @@ class Node(object): # each node is a segment
           prev_node.next = self
     def __str__(self):
       return str(self.leftMark)+' '+self.color
+'''some test data      
 _1 = Node(11) 
 _2 = Node(22, _1, B)
 _3 = Node(33, _2)
@@ -25,14 +27,16 @@ _5 = Node(55, _4)
 _6 = Node(66, _5, B)
 _7 = Node(77, _6)
 _8 = Node(88, _7, B)
-_9 = Node(99, _8)
-class DL_list(object):
+_9 = Node(99, _8)'''
+class DoublyLinkedList(object):
   def __init__(self, head):
     assert head is not None
     self.head=head
     
+  #print 3+1 attributes of a node  
+  # to support None, this can't be a Node method
   @staticmethod  
-  def print3(node): #print 3+1 attributes of a node
+  def print3(node): 
     if node:
       nx = node.next
       rightMark = nx.leftMark-1 if nx else sys.maxint
@@ -48,7 +52,7 @@ class DL_list(object):
       if node is None:
         line += 'end'
         break
-      DL_list.print3(node)
+      DoublyLinkedList.print3(node)
       line += str(node) + ' > '
       assert node.leftMark > last; last=node.leftMark
       nx = node.next
@@ -63,7 +67,7 @@ class DL_list(object):
 ### Above is a fairly reusable doubly-linked list
 
 def solD(intervals, incoming): # dlist-based solution
-  Node.cnt=-1
+  Node.cnt=-1 # for isStrict
   gap = head = Node(sys.maxint)
   nodePointers=list()
   for a,b in intervals: # b is left mark of ensuing gap
@@ -73,7 +77,7 @@ def solD(intervals, incoming): # dlist-based solution
     nodePointers.extend([itv,gap])
   head = head.next
   head.prev=None
-  dlist = DL_list(head)
+  dlist = DoublyLinkedList(head)
   dlist.dumpList(True)  
   # dlist constructed :)
   li = [node.leftMark for node in nodePointers]
@@ -81,16 +85,17 @@ def solD(intervals, incoming): # dlist-based solution
   incoming[1] -= 1
   idx = [bisect.bisect_right(li, i)-1 for i in incoming]
   segment1=nodePointers[idx[0]]
-  DL_list.print3(segment1)
+  DoublyLinkedList.print3(segment1)
   segment2=nodePointers[idx[1]]
-  DL_list.print3(segment2)
+  DoublyLinkedList.print3(segment2)
+  
   # now the different cases
   #if segment1.color == A:
     
   
 def main():
   solD([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])
-  #DL_list(_1).dumpList()
+  #DoublyLinkedList(_1).dumpList() # unit test
 main()
 '''Req: https://bintanvictor.wordpress.com/2018/07/29/merge-intervals/
 '''
