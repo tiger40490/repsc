d77f8e0403167601a2301c4018676edc6942c0d3
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-28 18:54:30 -0400

    major new feature

--- cpp/template/friendFactory.cpp
+++ cpp/template/friendFactory.cpp
@@ -1,7 +1,10 @@
 //showcase: singleton enforced by private ctor and factory (instead of static method)
-//showcase: factory maintains a single instance of each type. The lookup map uses typeid as key, and void pointer as value.
 //showcase: template factory function to support any number of target classes
-//showcase: 
+
+/*Showcasing two ways to keep cached instances:
+1) static local variable in each distinct instantiation of create(). 
+2) factory maintains a single instance of each type. The lookup map uses typeid as key, and void pointer as value.
+*/
 #include <typeindex>
 #include <typeinfo>
 #include <map>
@@ -12,15 +15,18 @@ using namespace std;
 static map<type_index, void*> lookup;
 
 template<typename T> T* create(){
+  static T* inst;
   auto const & type //must use a reference, as typeid has a private ctor
     = typeid(T);
   T* ret = static_cast<T*>(lookup[type]);
   if (ret) {
     cout<<"returning cached instance... won't create another instance of this type.\n";
+    assert(inst == ret);
     return ret; 
   }
   ret= new T();
   lookup[type] = ret;
+  inst = ret;
   return ret;
 }
 class C{
@@ -39,6 +45,9 @@ int main(){
     = create<C>();
   auto * ptrAgain = create<C>();
   assert(ptrAgain == ptr);
-  auto * ptrD = create<D>();
+  
+  auto * d = create<D>();
+  auto * dAgain = create<D>();
+  assert(dAgain == d);
   delete ptr;
 }
