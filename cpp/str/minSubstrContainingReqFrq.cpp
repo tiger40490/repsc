d9cdba1c3e26b2a9791cff8b760b8e1a7d2a6e54
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-28 10:57:02 -0400

    skeleton

---
+++ cpp/str/minSubstrContainingReqFrq.cpp
@@ -0,0 +1,13 @@
+#include <cassert>
+#include <iostream>
+#include <vector> //reqFrq
+using namespace std;
+
+string minWindow(string s, string t) {
+  return "";
+}
+int main(){
+}
+/*Req: https://bintanvictor.wordpress.com/2018/07/28/find-min-substring-containing-all-my-chars/
+Given a string Haystack and a string T, find the minimum window in Haystack which contains all the characters in T in complexity O(n).
+*/
