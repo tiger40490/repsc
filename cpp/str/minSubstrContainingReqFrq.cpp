8743a168c0d0f7a951b512fd0ab9d959a4896d03
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-28 11:05:56 -0400

    incomplete

--- cpp/str/minSubstrContainingReqFrq.cpp
+++ cpp/str/minSubstrContainingReqFrq.cpp
@@ -4,9 +4,17 @@
 using namespace std;
 
 string minWindow(string s, string t) {
+  string const & haystack = s;
+  vector<int> frq(52, 0);
+  //first build the frq table
+  for (auto const c: t){
+    ++frq[c-'a'];
+  }
+  
   return "";
 }
 int main(){
+  minWindow("bbaabccabcc", "bbc");
 }
 /*Req: https://bintanvictor.wordpress.com/2018/07/28/find-min-substring-containing-all-my-chars/
 Given a string Haystack and a string T, find the minimum window in Haystack which contains all the characters in T in complexity O(n).
