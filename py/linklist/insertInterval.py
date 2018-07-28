7b61ddaccbc25b5b612434a22e246ef1019e0073
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-27 21:43:14 -0400

    rename class

--- py/linklist/insertInterval.py
+++ py/linklist/insertInterval.py
@@ -1,14 +1,14 @@
 '''
-showcase double linked list
+showcase homemade double linked list
 '''
 import sys, bisect
 A='A' # Above-water 
 B='B' # Below-Water
-class Node(object): # each node is a segment
+class Segment(object): # each node is a segment
     cnt=0
     def __init__(self, leftMark, prev_node=None, color=A):
         assert leftMark > 0
-        Node.cnt+=1
+        Segment.cnt+=1
         self.leftMark = leftMark # rightMark would be self.next.leftMark-1. I choose not to save rightMark into each node instance
         assert color in [A,B]
         self.color=color
@@ -19,15 +19,15 @@ class Node(object): # each node is a segment
     def __str__(self):
       return str(self.leftMark)+' '+self.color
 '''some test data      
-_1 = Node(11) 
-_2 = Node(22, _1, B)
-_3 = Node(33, _2)
-_4 = Node(44, _3, B)
-_5 = Node(55, _4)
-_6 = Node(66, _5, B)
-_7 = Node(77, _6)
-_8 = Node(88, _7, B)
-_9 = Node(99, _8)'''
+_1 = Segment(11) 
+_2 = Segment(22, _1, B)
+_3 = Segment(33, _2)
+_4 = Segment(44, _3, B)
+_5 = Segment(55, _4)
+_6 = Segment(66, _5, B)
+_7 = Segment(77, _6)
+_8 = Segment(88, _7, B)
+_9 = Segment(99, _8)'''
 class DoublyLinkedList(object):
   def __init__(self, head):
     assert head is not None
@@ -40,7 +40,7 @@ class DoublyLinkedList(object):
     node1.next = node2
     node2.prev = node1
   #print 3+1 attributes of a node  
-  # to support None, this can't be a Node method
+  # to support None, this can't be a Segment method
   @staticmethod  
   def print3(node): 
     if node:
@@ -68,19 +68,19 @@ class DoublyLinkedList(object):
       assert dumpCnt < 999
     print line, '\n'
     if isStrict:
-      assert Node.cnt == dumpCnt
+      assert Segment.cnt == dumpCnt
 
 ### Above is a fairly reusable doubly-linked list
 
 def solD(intervals, incoming): # dlist-based solution
-  Node.cnt=-1 # for isStrict
-  gap = head = Node(sys.maxint)
+  Segment.cnt=-1 # for isStrict
+  gap = head = Segment(sys.maxint)
   segmentPointers=list()
   leftMarks=list()
   for a,b in intervals: # b is left mark of ensuing gap
     #print a, b
-    itv = Node(a, gap)
-    gap = Node(b, itv, B)
+    itv = Segment(a, gap)
+    gap = Segment(b, itv, B)
     segmentPointers.extend([itv,gap])
     leftMarks.extend([a,b]) # b is left mark of a gap segment
   head = head.next
@@ -102,10 +102,10 @@ def solD(intervals, incoming): # dlist-based solution
     return dlist
   elif segP.color == B == segQ.color:
     nextInterval = segQ.next
-    newInterval = Node(incoming[0], segP)
+    newInterval = Segment(incoming[0], segP)
     if segP is segQ:
       print 'tiny-interval-in-gap case like [23,24]'
-      newGap = Node(incomingEnd, newInterval, B)
+      newGap = Segment(incomingEnd, newInterval, B)
       DoublyLinkedList.link2(newGap, nextInterval)
       return dlist
     print 'wipeout case like [23,87]'
