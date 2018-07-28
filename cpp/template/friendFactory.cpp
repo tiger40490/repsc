5ca9ffe7862fdefd733b6dc5bf5e5f6212b410c1
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-28 17:55:56 -0400

    improving

--- cpp/template/friendFactory.cpp
+++ cpp/template/friendFactory.cpp
@@ -1,8 +1,9 @@
+//todo: singleton
 #include <iostream>
 using namespace std;
 
 template<typename T> T* create(){
-  return (new T());
+  return new T();
 }
 class C{
   C() {cout<<"C ctor, private\n";}
@@ -10,8 +11,13 @@ public:
   ~C(){cout<<"C dtor, must be public to support delete\n";}
   friend C* create<C>();
 };
-
+class C2{
+  C2() {cout<<"C2 ctor, private\n";}
+  friend C2* create<C2>();
+};
 int main(){
-  C* ptr = create<C>();
+  auto ptr //asterisk is optional but better IMO 
+    = create<C>();
   delete ptr;
+  auto * ptr2 = create<C2>();
 }
