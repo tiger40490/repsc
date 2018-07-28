578285229c0d3a88897ca56a2442f5e54706b3a0
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-28 18:37:15 -0400

    add new features

--- cpp/template/friendFactory.cpp
+++ cpp/template/friendFactory.cpp
@@ -1,23 +1,41 @@
 //todo: singleton
+#include <typeindex>
+#include <typeinfo>
+#include <map>
+#include <unordered_map>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
+static map<type_index, void*> lookup;
+
 template<typename T> T* create(){
-  return new T();
+  auto const & type //must use a reference as typeid ctor is private
+    = typeid(T);
+  if (lookup.count(type)) {
+    cout<<"returning cached instance... won't create another instance of this type.\n";
+    return (T*)lookup[type]; 
+  }
+  T* ret= new T();
+  lookup[type] = ret;
+  return ret;
 }
 class C{
+protected:
   C() {cout<<"C ctor, private\n";}
 public:  
   ~C(){cout<<"C dtor, must be public to support delete\n";}
   friend C* create<C>();
 };
-class C2{
-  C2() {cout<<"C2 ctor, private\n";}
-  friend C2* create<C2>();
+class D: public C{
+  D() {cout<<"D ctor, private, after calling base ctor\n";}
+  friend D* create<D>();
 };
 int main(){
   auto ptr //asterisk is optional but better IMO 
     = create<C>();
+  auto * ptrAgain = create<C>();
+  assert(ptrAgain == ptr);
+  auto * ptrD = create<D>();
   delete ptr;
-  auto * ptr2 = create<C2>();
 }
