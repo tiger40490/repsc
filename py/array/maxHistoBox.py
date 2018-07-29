63e121d2f090513f32a3f390e3000870be9127cf
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-29 17:47:37 -0400

    one test passed

--- py/array/maxHistoBox.py
+++ py/array/maxHistoBox.py
@@ -1,18 +1,15 @@
 '''
-showcase
-todo: sort objects by a field
+showcase __repr__ using __str__ to support pprint
 '''
-from pprint import pprint
-class Rec(object): #RunRecord
-  NO_CURRENT_RUN = '-'
-  def __init__(self, hei, currentRunStart=NO_CURRENT_RUN):
+class RunRecord(object): #RunRecord
+  def __init__(self, hei):
     self.hei=hei
     self.maxRun=0
     
-    # crs is 1-based, not 0-based
-    self.crs=currentRunStart #'-' to indicate no current run
+    # currentRunStart is 1-based, not 0-based
+    self.currentRunStart=0 # 0 to indicate no current run
   def __str__(self):
-    return str(self.hei)+"->"+str(self.maxRun)+'|'+str(self.crs)
+    return str(self.hei)+":"+str(self.maxRun)+'!'+str(self.currentRunStart)
   def __repr__(self):
     return self.__str__()
     
@@ -21,41 +18,58 @@ def largestRectangleArea(heights):
     di=dict()
     for h in heights:
       assert h > 0
-      di[h]= Rec(h)
-    tree=[ di[key] for key in sorted(di.iterkeys())] 
+      di[h]= RunRecord(h)
+    tree=[ di[key] for key in sorted(di.iterkeys())]
     #print tree # sorted array of non-repeating RunRecords
     
   arr=[0]
   arr.extend(heights)
   arr.append(0)
-  szh=len(heights)
-  sza=len(arr)
+  sz=len(arr)
   # i prefer to iterate over arr, to take care of fist bar
-  for i in xrange(sza-1):
+  for i in xrange(sz-1):
     j=i+1
-    assert j <= sza-1, 'j should grow to last index'
+    assert j <= sz-1, 'j should grow to last index'
     prev= arr[i]
     cur = arr[j]
     print cur, 
-    if   arr[i]==arr[j]:
+    if   prev==cur:
       print 'flatline'
-    elif arr[i]< arr[j]: 
+    elif prev< cur: 
       print 'rising'
       for rec in tree: # iterate from prev +1 to cur
         if rec.hei <= prev: 
-          assert rec.crs > 0
+          assert rec.currentRunStart > 0
           continue 
         if rec.hei > cur: break
-        rec.crs = j
+        rec.currentRunStart = j
         print 'started a run in', rec
-    elif arr[i]> arr[j]:
-      print 'falling'
     else:
-      assert 1==2, 'should never reach here'
+      assert prev> cur
+      print 'falling'
+      for rec in tree: # iterate from cur+1 to prev
+        if rec.hei <= cur:
+          assert rec.currentRunStart > 0
+          continue
+        if rec.hei > prev: break
+        #print 'updating', rec
+        # end the current run, update maxRun
+        lastRun = j - rec.currentRunStart
+        if rec.maxRun < lastRun: 
+           rec.maxRun = lastRun
+           print 'updated maxRun in', rec
+        rec.currentRunStart = 0
   
-  return 10
+  # now select final winner
+  ret = 0
+  for rec in tree:
+    area = rec.hei * rec.maxRun
+    if ret < area: 
+       ret = area
+  return ret
 def main():
-  assert(10==largestRectangleArea([2,1,5,6,2,3]))
+  assert(10==largestRectangleArea([2,2,5,2,2]))
+  assert(10==largestRectangleArea([2,1,5,6,2,3]))  
 main()
 '''Req:  https://bintanvictor.wordpress.com/2018/07/29/max-rectangle-histogram/
 
