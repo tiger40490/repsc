26916d1dc235c17e8200d63ae882c4cf40aa7e07
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-31 21:33:17 -0400

    minor impr

--- cpp/combo_permu/abbrTest.cpp
+++ cpp/combo_permu/abbrTest.cpp
@@ -3,12 +3,8 @@
 using namespace std;
 
 int main() {
-  string tmp = "abcd";
-  deque<char> v(tmp.begin(), tmp.end());
-  deque<deque<char> > ret = recurs(v);
-#ifndef DEBUG
+  deque<deque<char> > ret = generateAsc("abcd");
   show(ret);
-#endif
   set<deque<char> > unique(ret.begin(), ret.end());
   cout<<calls<<"  calls to the recursive funcion to generate "<<ret.size()<<" abbreviations. Including the empty, unique count = "<<unique.size()<<endl;
 }
