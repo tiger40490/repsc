ec4c59950ec881bb24e5151363fa6178c5bdbe7c
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-07-31 23:20:50 -0400

    skeleton

--- cpp/combo_permu/abbr_iterative.h
+++ cpp/combo_permu/abbr_iterative.h
@@ -1,1 +1,12 @@
-//Not implemented yet
+/*Will impleent in python first
+
+*/
+
+std::list<std::deque<char> > const & recurs(std::deque<char> const & pool){
+}
+auto const & generateAsc(std::string const & s){
+  auto const & ret = recurs(v, true);
+  return ret;
+}
+/* abbreviation or subsequence generator, from longest to shortest
+*/
