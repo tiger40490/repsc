7317461463075dc75386bb38cda505f2be2de9c6
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-01 00:58:05 -0400

    trivial

--- py/str/abbrLongest1st.py
+++ py/str/abbrLongest1st.py
@@ -9,12 +9,13 @@ def genlevel(original):
   while len(next(iter(oldBatch))) > 1:
     for abbr in oldBatch:
       chars=list(abbr)
-      for i in range(len(chars)):
+      for i in range(len(chars)): #allocating a new abbr
         newBatch.add( ''.join(chars[:i] + chars[i+1:]) )
-    print len(newBatch), newBatch
+    print len(newBatch), newBatch # here u can process the abbreviations
     cnt += len(newBatch)
     oldBatch=newBatch
     newBatch=set()
+    
   if len(original) == len(set(original)):
     assert cnt+2 == 2**len(original)
   
