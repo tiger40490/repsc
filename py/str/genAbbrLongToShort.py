df177bdb53d4f99e89955720a91995468ee2865f
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-01 00:35:40 -0400

    refactor

--- py/str/genAbbrLongToShort.py
+++ py/str/genAbbrLongToShort.py
@@ -1,26 +1,25 @@
 
-
+''' Efficiency -- relies on hash table. I try to minimize memory allocation
+'''
 def genlevel(original):
-  ''' Efficiency -- relies on hash table. I try to minimize memory allocation
-  '''
-  smallhm=set([original])
-  bighm=set()
+  old = set([original])
+  new = set()
   cnt=0
-  while len(next(iter(smallhm))) > 1:
-    for abbr in smallhm:
+  while len(next(iter(old))) > 1:
+    for abbr in old:
       chars=list(abbr)
       for i in range(len(chars)):
-        shorter = tuple(chars[:i] + chars[i+1:])
-        if shorter not in bighm:
-          bighm.add(shorter)
-    print len(bighm), [''.join(tu) for tu in bighm]
-    cnt += len(bighm)
-    smallhm=bighm
-    bighm=set()
+        newAbbr = tuple(chars[:i] + chars[i+1:])
+        if newAbbr not in new:
+          new.add(newAbbr)
+    print len(new), [''.join(tu) for tu in new]
+    cnt += len(new)
+    old=new
+    new=set()
   assert cnt+2 == 2**len(original)
   
 def main():
- genlevel('abcdef')
+ genlevel('abcde')
  
 main()
 '''Req: https://wp.me/p74oew-5V3
