427e066bd3f6af27f749ca2dc0574636baf3a96a
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-01 09:15:44 -0400

    major impro

--- py/str/abbrIterative.py
+++ py/str/abbrIterative.py
@@ -1,23 +1,20 @@
 '''Abbrv generator. I wrote a recursive c++ solution to output in ascending order. 
 Today I will forgo ascending order and try an iterative soution
-Supose we have scanned 3 chars in the word, and we have a collection of N abbreviations. 
+Supose we have scanned 3 chars in the original, and we have a collection of N abbreviations. 
 For the next char 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
 Join the lists into a list of 2N.
-'''
-word="abbcd"
-growing=set(['']) # start with a single empty string
-
-def dump():
-  print len(growing), growing
-def add1char(ch):
-  tmp = []
-  for abbr in growing:
-    tmp.append(abbr+ch)
-  growing.update(tmp)
-  dump()
-  
+'''  
+def genShortestFirst(original):
+  growing=set(['']) # start with a single empty string
+  for ch in original: # I would now avoid this in favor of the more common for i in range(len(original))
+      tmp = []
+      for abbr in growing:
+        tmp.append(abbr+ch)
+      growing.update(tmp)
+      print len(growing), growing
+  if len(original) == len(set(original)):
+    assert len(growing) == 2**len(original)
+      
 def main():
-  for ch in word: # I would now avoid this in favor of the more common for i in range(len(word))
-    add1char(ch)
-  
-main()
+  genShortestFirst('abcde')
+if __name__ == '__main__': main()
