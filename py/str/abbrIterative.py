202dcc53ee20e8de45706692c1375d9f58d73157
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-01 09:18:23 -0400

    merge two scripts

--- py/str/abbrIterative.py
+++ py/str/abbrIterative.py
@@ -1,10 +1,29 @@
-'''Abbrv generator. I wrote a recursive c++ solution to output in ascending order. 
-Today I will forgo ascending order and try an iterative soution
-Supose we have scanned 3 chars in the original, and we have a collection of N abbreviations. 
-For the next char 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
-Join the lists into a list of 2N.
-'''  
+def genLongestFirst(original):
+  ''' Efficiency -- relies on hash table. 
+  I try to minimize memory allocation in the innermost loop
+  '''
+  oldBatch = set([original]) # longer abbreviations
+  newBatch = set() # slightly shorter abbreviations
+  cnt=0
+  while len(next(iter(oldBatch))) > 1:
+    for abbr in oldBatch:
+      chars=list(abbr)
+      for i in range(len(chars)): 
+        #allocating a new abbr
+        newBatch.add( ''.join(chars[:i] + chars[i+1:]) )
+    print len(newBatch), newBatch # here u can process the abbreviations
+    cnt += len(newBatch)
+    oldBatch=newBatch
+    newBatch=set()
+  #end of while  
+  if len(original) == len(set(original)):
+    assert cnt+2 == 2**len(original)
+
 def genShortestFirst(original):
+  '''Supose we have scanned 3 chars in the original, and we have a collection of N abbreviations. 
+  For the next char 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
+  Join the lists into a list of 2N.
+  '''  
   growing=set(['']) # start with a single empty string
   for ch in original: # I would now avoid this in favor of the more common for i in range(len(original))
       tmp = []
@@ -17,4 +36,5 @@ def genShortestFirst(original):
       
 def main():
   genShortestFirst('abcde')
+  genLongestFirst('abcde')
 if __name__ == '__main__': main()
