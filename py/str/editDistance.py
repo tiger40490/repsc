6629c8a106112654540c6bd0eeda9592cfb474a9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-06 18:54:52 -0400

    partial

--- py/str/editDistance.py
+++ py/str/editDistance.py
@@ -3,6 +3,10 @@ showcase
 '''
 from pprint import pprint
 
+def a(mat, r, c):
+  if r<0 or c<0: return 0
+  return mat[r][c]
+
 def solve(aa, bb): # find distance between aa and bb
   if len(aa) > len(bb):
     tmp=aa
@@ -16,10 +20,13 @@ def solve(aa, bb): # find distance between aa and bb
   for r in xrange(sz1):
     for c in xrange(sz2):
       print 'comparing', aa[r], bb[c]
+      diag=a(mat, r-1, c-1)
       if aa[r] == bb[c]:
-        mat[r][c] = mat[r-1][c-1]
+        mat[r][c] = diag
         continue
+      mat[r][c] = 1+min(diag, a(mat,r-1,c), a(mat,r,c-1))
   # end of loop
+  pprint(mat)
   return mat[-1][-1]
   
 def main():
