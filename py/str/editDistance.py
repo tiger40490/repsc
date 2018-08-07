8501371d2ea11c66405c723c888eadcae1e60098
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-06 20:26:15 -0400

    one test passed

--- py/str/editDistance.py
+++ py/str/editDistance.py
@@ -1,10 +1,11 @@
 '''
+todo: first row numbers are too low. Perhaps extract the min() as read function?
 showcase 
 '''
 from pprint import pprint
 
-def a(mat, r, c):
-  if r<0 or c<0: return 0
+def read(mat, r, c):
+  if r<0 or c<0: return 999
   return mat[r][c]
 
 def solve(aa, bb): # find distance between aa and bb
@@ -15,16 +16,19 @@ def solve(aa, bb): # find distance between aa and bb
   sz1=len(aa)
   sz2=len(bb)
   assert sz1 <= sz2  
-  mat=[ [0 for j in range(sz2)] for _ in range(sz1) ]
+  mat=[ [0 for _ in range(sz2)] for _ in range(sz1) ]
   # pprint(mat)
   for r in xrange(sz1):
     for c in xrange(sz2):
-      print 'comparing', aa[r], bb[c]
-      diag=a(mat, r-1, c-1)
+      if r==0 and c==0: 
+        mat[r][c]= 0 if aa[r] == bb[c] else 1
+        continue
+      diag=read(mat, r-1, c-1)
       if aa[r] == bb[c]:
         mat[r][c] = diag
-        continue
-      mat[r][c] = 1+min(diag, a(mat,r-1,c), a(mat,r,c-1))
+      else:
+        mat[r][c] = 1+min(diag, read(mat,r-1,c), read(mat,r,c-1))
+      print 'comparing', aa[r], bb[c], '.. set to', mat[r][c]
   # end of loop
   pprint(mat)
   return mat[-1][-1]
