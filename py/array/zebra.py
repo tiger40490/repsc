871fe617b0c67fe29002768fd1dfcfc7bf210f7f
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-09 09:37:13 -0400

    prepare for more tests

--- py/array/zebra.py
+++ py/array/zebra.py
@@ -1,4 +1,6 @@
 '''
+todo: automated tests using random list -> set
+todo: simplify further, like more del
 showcase defining 2 vars to one value, on the same line
 showcase defining 2 unrealted vars on the same line;; separated by semicolon
 '''
@@ -8,10 +10,13 @@ class Stripe(object):
     self.max=q
   def __str__(self):
     return 'Stripe['+str(self.min) + ' ' + str(self.max) + ']\t@ ' + str(id(self)%1000)
-    
-def solve2(unsorted):
+
+def solve2Wrapper(randomList):
+  return solve2(set(randomList))
+def solve2(uniq):
+  print 'uniq =', uniq
   max=0; seen=dict() # item -> stripe. Every item must be the min or max of a stripe
-  for q in unsorted:
+  for q in uniq:
     print 'procssing --> ', q
     assert q not in seen, 'original values should be unique'
     stripeLe=stripeRi=None
@@ -20,12 +25,16 @@ def solve2(unsorted):
       assert stripeLe.max+1==q
       stripeLe.max=q
       seen[q]=stripeLe
+      tmp = stripeLe.max+1-stripeLe.min
+      if max < tmp: max = tmp
       print stripeLe, 'expanded to right'
     if q+1 in seen:
       stripeRi=seen[q+1]
       assert stripeRi.min-1==q
       stripeRi.min=q
       seen[q]=stripeRi
+      tmp = stripeRi.max+1-stripeRi.min
+      if max < tmp: max = tmp      
       print stripeRi, 'expanded to left'
     if stripeLe and stripeRi: #they both contain "q"
       del seen[q] # to reduce dict size...sometimes broken
@@ -39,13 +48,14 @@ def solve2(unsorted):
     if q not in seen:
       seen[q]=Stripe(q)
       #print seen[q], 'created'
+  print 'returning', max
   return max
 def dump(seen):
   for key, hit in seen.iteritems():    
     print key, ':', hit
 def main():
   assert 15==solve2([4, 10, 13,5, 8,12, 1,9,11,6,15,3, 7,14,2])
-  assert 4 ==solve2([100, 4, 200, 1, 3, 2])
+  assert 4 ==solve2Wrapper([100, 4, 200, 1, 3, 2])
 main()
 '''Req: 
 '''
