994985345fa7cc848c3b8b7d29f8a317966116f9
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2018-08-10 15:30:23 -0400

    comments

--- py/array/tripleJump.py
+++ py/array/tripleJump.py
@@ -1,4 +1,5 @@
 '''
+todo: more tests
 showcase 
 '''
 def solve1greedy(li): # O(NN)
@@ -8,7 +9,6 @@ def solve1greedy(li): # O(NN)
   # bln[0] is unused and remains -1 or sz. If it were 0, that would mean that from Node #0 the leftmost node we can jump to is #0 .. nonsense
   #print bln
   
-  # now update bln:
   isOver=False
   for i in xrange(sz):
     maxJ = li[i]
@@ -32,17 +32,6 @@ def solve1greedy(li): # O(NN)
 def main():
   assert 2==solve1greedy([2,3,1,1,4])
 main()
-'''Req: Given an array of non-negative integers, you are initially positioned at the first index of the array. Each element in the array represents the maximum permitted jump length from that position.
-
-Typical greedy algorithm. I will jump leftward.
-
-Suppose there are N=99 nodes. I will pre-scan the N nodes to build a shadow array of integer records, each a BestLefNode. (The first record is unused ... can put in -1)
-
-If BestLefNode[44] == 33, it means that based on known data the left-most (furthest) node we can jump to from Node #44 is Node #33. 
-
-When we visit Node #7 during the scan, we will update BestLefNode record #8 onwards. 
-
-As soon as we update BestLefNode[N-1] i.e. right-most record, we can exit the initial scan since the optimal solution is now available.
-
-Once the BestLefNode records are built, I simply follow them! For example, if rightmost BestLefNode has value #88, that means the furthest node we can reach from the right end is Node #88, so we will jump to #88 and then check the best destination from #88
+'''Req: https://bintanvictor.wordpress.com/2018/08/10/least-jumps-to-the-end-triple-jump/
+Given an array of non-negative integers, you are initially positioned at the first index of the array. Each element in the array represents the maximum permitted jump length from that position.
 '''
