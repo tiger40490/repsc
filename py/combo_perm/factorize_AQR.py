fd7cdfa39261dc96611b582471ffc020a5199f48
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-16 12:27:01 -0400

    extract a module

--- py/combo_perm/factorize_AQR.py
+++ py/combo_perm/factorize_AQR.py
@@ -1,47 +1,15 @@
-'''
-todo: replace "remain" with a sorted "group" list derived from the list of non-unique prime factors. Each "group" is the product of any group of the non-unique prime factors
-
+'''todo: clean up
 todo extract to module get_divisors
 '''
 from math import sqrt
 #from operator import mul
 from collections import defaultdict
+import generic_factorize
 cnt=0; recursionLevel=0
-
-def get_primes(tgt): # simple Sieve of Eratosthenes 
-   n = int(sqrt(tgt))+1 
-   odds = xrange(3, n+1, 2)
-   sieve = set(sum([range(q*q, n+1, q+q) for q in odds],[]))
-   return [2] + [p for p in odds if p not in sieve]
-def get_nupf(tgt): # generate the non-unique prime factors
-  allPrimes=get_primes(tgt) # empty list means tgt is prime
-  frq = defaultdict(lambda: 0);   i=0
-  while tgt > 1 and i < len(allPrimes):
-    primeFac = allPrimes[i]
-    if tgt%primeFac:
-      i+=1
-      continue
-    tgt = tgt/primeFac
-    frq[primeFac] += 1
-  print 'nupf =', dict(frq)
-  return frq
-def get_divisors(tgt): # generate all divisors of tgt
-  frq = get_nupf(tgt)
-  divisors = [1]; checkSum=1
-  for prime, cnt in frq.iteritems():
-    checkSum *= (cnt+1)
-    divisors.extend([g * prime**i for g in divisors for i in xrange(1,cnt+1)])
-  print len(divisors), 'divisors', divisors
-  assert checkSum == len(divisors)
-  divisors = sorted(divisors)
-  assert divisors[0] == 1
-  assert divisors[-1] == tgt
-  return divisors, frq
-##### reusable functions to be extracted into module
   
 def getSmallDivisors(bigNum): # to optimize away
   ret = list()
-  divisors, frq = get_divisors(bigNum)
+  divisors, frq = generic_factorize.get_divisors(bigNum)
   for i in divisors:
     if i == 1: continue
     ret.append(i)
