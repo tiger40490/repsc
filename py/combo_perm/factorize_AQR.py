4f050f3c1a007db9f1aafdcfbe4d00ca7b370b7c
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-16 08:59:23 -0400

    tested briefly

--- py/combo_perm/factorize_AQR.py
+++ py/combo_perm/factorize_AQR.py
@@ -1,79 +1,78 @@
 '''
 todo: replace "remain" with a sorted "group" list derived from the list of non-unique prime factors. Each "group" is the product of any group of the non-unique prime factors
 
-If the nupf list has xxxyyz, then there are (3+1)*(2+1)*(1+1) factors. 
-
+todo extract to module get_divisors
 '''
 from math import sqrt
-from operator import mul
+#from operator import mul
 from collections import defaultdict
 cnt=0; recursionLevel=0
 
-def primes(tgt): # simple Sieve of Eratosthenes 
+def get_primes(tgt): # simple Sieve of Eratosthenes 
    n = int(sqrt(tgt))+1 
-   odds = range(3, n+1, 2)
+   odds = xrange(3, n+1, 2)
    sieve = set(sum([range(q*q, n+1, q+q) for q in odds],[]))
    return [2] + [p for p in odds if p not in sieve]
-   
-def get_divisors(bigNum): # generate the non-uniq prime factors and all the partial products they could produce i.e. all the factors of bigNum
-  tgt = bigNum
-  p=primes(tgt) # empty list means tgt is prime
+def get_nupf(tgt): # generate the non-unique prime factors
+  allPrimes=get_primes(tgt) # empty list means tgt is prime
   frq = defaultdict(lambda: 0);   i=0
-  while tgt > 1 and i < len(p):
-    primeFac = p[i]
+  while tgt > 1 and i < len(allPrimes):
+    primeFac = allPrimes[i]
     if tgt%primeFac:
       i+=1
       continue
     tgt = tgt/primeFac
     frq[primeFac] += 1
   print 'nupf =', dict(frq)
-  factors = [1]; checkSum=1
+  return frq
+def get_divisors(tgt): # generate all divisors of tgt
+  frq = get_nupf(tgt)
+  divisors = [1]; checkSum=1
   for prime, cnt in frq.iteritems():
     checkSum *= (cnt+1)
-    new = list()
-    for i in xrange(1,cnt+1):
-      for g in factors:
-        new.append(g * prime**i)
-    factors.extend(new)
-  print len(factors), 'factors', factors
-  factors = sorted(factors)
-  assert factors[0] == 1
-  assert factors[-1] == bigNum
-  assert checkSum == len(factors)
+    divisors.extend([g * prime**i for g in divisors for i in xrange(1,cnt+1)])
+  print len(divisors), 'divisors', divisors
+  assert checkSum == len(divisors)
+  divisors = sorted(divisors)
+  assert divisors[0] == 1
+  assert divisors[-1] == tgt
+  return divisors, frq
+##### reusable functions to be extracted into module
   
-  return factors[1:]
-def getSmallDivisors(bigNum):
+def getSmallDivisors(bigNum): # to optimize away
   ret = list()
-  for i in get_divisors(bigNum):
+  divisors, frq = get_divisors(bigNum)
+  for i in divisors:
     if i == 1: continue
     ret.append(i)
     if i > sqrt(bigNum): break
   return ret
       
-def recurs(bigNum, remain, lowerFactors=tuple()):
-  '''job: find each formula to factorize remain, but when print it, also print lowerFactors
-  '''
+def recurs(bigNum, divisors, lowerFactors=tuple()):
   global cnt, recursionLevel
-  if bigNum == 1: return # to optimize
-  #print lowerFactors
+  if bigNum == 1: return # to optimize away
   assert sorted(lowerFactors) == list(lowerFactors), 'should be sorted low to high'
   assert len( lowerFactors ) == recursionLevel
   recursionLevel += 1
   if lowerFactors: # i.e. non-empty
-    #assert lowerFactors[-1] <= remain, 'last lowerFactors item should not exceed remain'
+    #assert lowerFactors[-1] <= divisors, 'last lowerFactors item should not exceed divisors'
     cnt+=1
     _start = lowerFactors[-1] # highest existing factor -- #1 trick in this algorithm    
     print '. '*(recursionLevel-2) + str(lowerFactors), bigNum
   else: 
-    print '------- factorizing', remain
+    print '------- factorizing', bigNum
     cnt=0
     _start = 2 # smallest factor in the universe    
     
-  for f in remain:
+  '''  for f in xrange(_start, int(sqrt(remain))+1):
+    if remain%f: continue
+    recursCSY(remain/f, lowerFactors+(f,))
+  '''    
+  for f in divisors: # scan all divisors from _start to sqrt of current bigNum
     if f < _start: continue
     if bigNum % f: continue
-    if lowerFactors and bigNum/f < f: break
-    recurs(bigNum/f, remain, lowerFactors+(f,))
+    if bigNum/f < f: break
+    recurs(bigNum/f, divisors, lowerFactors+(f,))
   recursionLevel -= 1
 def recursCSY(remain, lowerFactors=tuple()):
   '''job: find each formula to factorize remain, but when print it, also print lowerFactors
@@ -102,9 +101,8 @@ def factorize(bigNum):
   recurs(bigNum, getSmallDivisors(bigNum))
 def main():
   #print get_divisors(600)
-  factorize(36); assert(8  == cnt)
-  return
   factorize(60); assert(10 == cnt)
+  factorize(36); assert(8  == cnt)
   factorize(24); assert(6  == cnt)
   factorize(12); assert(3  == cnt)
   return
