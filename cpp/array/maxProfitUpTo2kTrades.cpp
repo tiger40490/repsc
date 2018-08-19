aa295b3c441dca341e5b14a4eaad1ac95b5bbe65
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-19 18:15:15 -0400

    refactor

--- cpp/array/maxProfitUpTo2kTrades.cpp
+++ cpp/array/maxProfitUpTo2kTrades.cpp
@@ -1,6 +1,7 @@
 /* showcase 
 */
 #include <vector>
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <assert.h>
@@ -11,11 +12,12 @@ template<typename T,             int min_width=4> ostream & operator<<(ostream &
    os<<" ";
    for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
    os<<endl;
-   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
-   os<<endl;
+   //for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
+   //os<<endl;
    return os;
 }
-typedef int Price;
+using Price=int;
+using Profit=int;
 struct Rec{
   Price val;
   bool hi;
@@ -37,27 +39,36 @@ int sol1(vector<Price> const orig){
   for (int i=1; i<orig.size(); ++i){
     if (orig[i-1] != orig[i]) v.push_back(orig[i]);
   } //all flats removed
-  vector<Rec> arr;
-  if (v[0]<v[1]) arr.push_back(Rec(v[0], false));
+  vector<Rec> zigzag;
+  if (v[0]<v[1]) zigzag.push_back(Rec(v[0], false));
   for (int i=1; i<v.size()-1; ++i){
     if ( (v[i-1]-v[i]) * (v[i]-v[i+1]) >=0) continue;
     assert (v[i-1] != v[i]);
-    arr.push_back(Rec(v[i], v[i-1]<v[i]));
+    zigzag.push_back(Rec(v[i], v[i-1]<v[i]));
     //ss<<v[i]<<" saved\n";
   }
+  if (zigzag.empty()) return 0;
   // now deal with the last price point
   Price last = v[v.size()-1];
-  auto & b4last = arr[arr.size()-1];
+  auto & b4last = zigzag[zigzag.size()-1];
   if     (b4last.lo && b4last.val < last)
-    arr.push_back(Rec(last, true));
+    zigzag.push_back(Rec(last, true));
   else if(b4last.hi && b4last.val > last)
-    arr.push_back(Rec(last, false));
-  ss<<arr;
+    zigzag.push_back(Rec(last, false));
+  ss<<zigzag;
+  vector<Profit> profits;
+  for (int i=1; i<zigzag.size(); i +=2){
+    profits.push_back(zigzag[i].val - zigzag[i-1].val);
+  }
+  sort(profits.begin(), profits.end());
+  reverse(profits.begin(), profits.end());
+  ss<<"profits: "<<profits;
 }
 int main(){
   assert(sol1({3,3,5,0,0,3,1,2,4}));
-  //assert(sol1({4,3,5,0,0,3,1,2,4}));
+  assert(sol1({4,3,5,0,0,3,1,2,4}));
+  sol1({7,5,4,2,1});
+  assert(sol1({2,3,5,0,10,15,11,4}));
 }
 /*Requirmenet: 
-
 */
