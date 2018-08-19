153564575cab94a5976ca67957a48a5642d7a3cd
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-19 15:36:15 -0400

    more tests

--- cpp/combo_permu/staircase_CSY.cpp
+++ cpp/combo_permu/staircase_CSY.cpp
@@ -1,27 +1,26 @@
-//#include <map>
-//#include <unordered_map>
-#include <list>
+/*
+todo: clean-up
+todo: figure out why pow(2, staircase-1)
+*/
 #include <vector>
 #include <queue>
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 #include <cassert>
 using namespace std;
 
 template<typename T,             int min_width=8> ostream & operator<<(ostream & os, vector<T> const & c){
    for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
    os<<endl;
-   //for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
-   //os<<endl;
    return os;
 }
-//using Step=int;
 using Length=int;
 using Path=vector<Length>;
-int countFormulas(Length staircase){
-  queue<Path> q; q.push(vector<Length>());
+int solBFT(Length staircase){
+  bool isVerbose = staircase < 7;
+  queue<Path> q; q.push(Path());
   int cnt=0;
-  //enqueue the empty path
   for (;!q.empty();){
     Path pa=q.front(); q.pop();
     Length gap = staircase;
@@ -35,23 +34,17 @@ int countFormulas(Length staircase){
       q.push(newpa);
     }
     pa.push_back(gap);
-    cout<<":)"<<pa;
+    if (isVerbose) cout<<":)"<<pa;
+    ++cnt;
   }
+  assert( pow(2,staircase-1) == cnt);
+  cout<<staircase<<"-level staircase: returning "<<cnt<<endl;
+  return cnt;
 }
 
 int main(){
-  //vector<Step> v={1,2,3,5};
-  //cout<<v;
-  countFormulas(5);
-}/*
---jargon file:
-a STEP from LEVEL 0 to 2 has LENGTH=2, and skips level 1; a PATH consists of 1 or more STEPS; a FORMULA is a complete PATH, and always starts at level 0 and may hit intermediate levels #2, #5, #6 ...
---BFT solution. suppose N = 5. We model each path as a directory path from root. Each queue item is a path represented by a linked list or vector of capacity N.
-
-I hope this solution avoids duplicates ..
-
-enqueue all "first levels" /1; /2; /3; /4; /5
-then dequeue and visit first path i.e. "1". In this case, there are 4 levels remaining in the staircase, so enqueue /1/1;  /1/2;  /1/3;  /1/4
-then dequeue and visit 2nd path in the queue i.e. "/2". enqueue /2/1; /2/2;  /2/3
-Every time (after dequeue) we see a path has total length==N, we print it out as a formula.
+  solBFT(5);
+  solBFT(9);
+  solBFT(13);
+}/*Req: https://wp.me/p74oew-61P
 */
