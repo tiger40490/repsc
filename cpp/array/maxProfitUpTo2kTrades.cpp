adedc7aa277c10ae055e74adc0e72c0264bd5070
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-08-19 17:56:12 -0400

    refactor cleanup

--- cpp/array/maxProfitUpTo2kTrades.cpp
+++ cpp/array/maxProfitUpTo2kTrades.cpp
@@ -11,42 +11,46 @@ template<typename T,             int min_width=4> ostream & operator<<(ostream &
    os<<" ";
    for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
    os<<endl;
-   for(int i=0; i<c.size(); ++i){ os<<setw(min_width+2)<<i<<" "; }
+   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
    os<<endl;
    return os;
 }
-typedef int Num;
+typedef int Price;
 struct Rec{
-  Num val;
+  Price val;
   bool hi;
   bool lo;
-  Rec(Num a, bool h): val(a), hi(h), lo(!h){}
+  Rec(Price a, bool h): val(a), hi(h), lo(!h){}
   friend ostream & operator<<(ostream & os, Rec const & r){
-    os<<r.val;
-    if (r.hi) os<<"^ ";
-    else      os<<"v ";
+    stringstream sstr;
+    sstr<<r.val;
+    if (r.hi) sstr<<"^ ";
+    else      sstr<<"v ";
+    os<<sstr.str();
+    return os;
   }
 };
 
-int sol1(vector<Num> const orig){
-  vector<Num> v;
+int sol1(vector<Price> const orig){
+  vector<Price> v;
   v.push_back(orig[0]);
   for (int i=1; i<orig.size(); ++i){
-    if (orig[i-1] == orig[i]) continue;
-    v.push_back(orig[i]);
+    if (orig[i-1] != orig[i]) v.push_back(orig[i]);
   } //all flats removed
   vector<Rec> arr;
   if (v[0]<v[1]) arr.push_back(Rec(v[0], false));
   for (int i=1; i<v.size()-1; ++i){
     if ( (v[i-1]-v[i]) * (v[i]-v[i+1]) >=0) continue;
+    assert (v[i-1] != v[i]);
     arr.push_back(Rec(v[i], v[i-1]<v[i]));
     //ss<<v[i]<<" saved\n";
   }
-  Num last = v[v.size()-1];
-  auto & tmp = arr[arr.size()-1];
-  if     (tmp.lo && tmp.val < last)
+  // now deal with the last price point
+  Price last = v[v.size()-1];
+  auto & b4last = arr[arr.size()-1];
+  if     (b4last.lo && b4last.val < last)
     arr.push_back(Rec(last, true));
-  else if(tmp.hi && tmp.val > last)
+  else if(b4last.hi && b4last.val > last)
     arr.push_back(Rec(last, false));
   ss<<arr;
 }
