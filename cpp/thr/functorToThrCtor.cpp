487338fcb6868fcfb5d7bdf97cb08a874d545c19
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-09-29 16:45:29 -0400

    working

--- cpp/thr/functorToThrCtor.cpp
+++ cpp/thr/functorToThrCtor.cpp
@@ -1,28 +1,27 @@
-//todo: 
+//showcase: std::ref to produce a copyable wrapper object containing a given reference
+//showcase: stateful functor object to start a thread. Rather useful.
 #include <thread>
 #include <iostream>
+#include <unistd.h> //usleep() and sleep()
 using namespace std;
 
-class Worker{
-public:
+struct Worker{
     Worker() : _value(0) {}
-    void operator()(unsigned int value);
+    void operator()(unsigned int input) {
+      this->_value = input*input;
+      long tmp=3;
+      cerr<<pthread_self()<<"-Thr: doing work for "<<tmp<<" seconds ..."<<endl;
+      sleep(tmp);
+    }
     unsigned int get_value() {return this->_value;}
-private:
     unsigned int _value;
 };
 
-void Worker::operator()(unsigned int value)
-{
-    this->_value = value;
-}
-
-int main()
-{
+int main(){
     Worker worker;
-    thread thread(ref(worker), 13);
+    thread thread(std::ref(worker), 13);
     thread.join();
     unsigned int value = worker.get_value();
-    cout << "value: " << value << endl;
+    cout << pthread_self()<< "-Thr: value = " << value << endl;
 }/*based on https://stackoverflow.com/questions/42894629/correct-use-of-functor-for-c-stl-thread
 */
