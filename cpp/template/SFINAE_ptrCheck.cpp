14f5a216ce1e8dfdb3ef514b692ce5b425e5a6dc
bintIview11 <35847749+tiger40490@users.noreply.github.com>
2018-09-29 10:54:30 -0400

    try out is_pointer<T>::value

--- cpp/template/SFINAE_ptrCheck.cpp
+++ cpp/template/SFINAE_ptrCheck.cpp
@@ -1,4 +1,4 @@
-// https://github.com/tiger40490/repo1/blob/cpp1/cpp/88miscLang/arrayNewDtor.cpp (??) shows simpler ways to test if a type is a ptr type, but techniques here are valuable at least in job interviews.
+//showcase std::is_pointer<T> etc
 #include <assert.h>
 #include <iostream>
 using namespace std; 
@@ -21,12 +21,12 @@ template <class T> struct isCustomPtr{
   static type9 f281(...); //default overload
   static T t;
   static size_t const value = sizeof(f281(t));
+  static bool const bool_value = std::is_pointer<T>::value;
 };
 
 struct Foo {
   long bar; 
   int func1(ostream){return 0; }
-  //float f2();
 };
 
 int main(void){
@@ -37,9 +37,19 @@ int main(void){
   typedef int (*FuncPtr)();
 
   assert(1==isCustomPtr<IntPtr>::value); 
+  assert(isCustomPtr<IntPtr>::bool_value);
+
   assert(2==isCustomPtr<FuncPtr>::value);
+  assert(isCustomPtr<FuncPtr>::bool_value);
+
   assert(4==isCustomPtr<FooMemberPtr>::value);
+  assert(is_member_pointer<FooMemberPtr>::value);
+
   assert(8==isCustomPtr<FooMemFunPtr>::value);
+  assert(is_member_function_pointer<FooMemFunPtr>::value);
+
   assert(9==isCustomPtr<float>::value); //not a ptr at all
+  assert( ! isCustomPtr<float>::bool_value); //not a ptr at all
   cout<<"All tests passed";
 }//based on https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/SFINAE
+// https://github.com/tiger40490/repo1/blob/cpp1/cpp/88miscLang/arrayNewDtor.cpp (??) shows simpler ways to test if a type is a ptr type, but techniques here are valuable at least in job interviews.
