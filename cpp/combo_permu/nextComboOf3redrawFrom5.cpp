5865235d187bc46a7520793d23a6184d9a2ebee5
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2018-12-08 16:24:39 -0500

    add new solution

--- cpp/combo_permu/nextComboOf3redrawFrom5.cpp
+++ cpp/combo_permu/nextComboOf3redrawFrom5.cpp
@@ -1,5 +1,5 @@
 /* 
-todo: implement the other solutions
+todo: assert is buggy
 
 showcase decltype on a container object to extract element type
 showcase vector::assign(itr, itr) instead of clear() then insert(itr, itr)
@@ -13,7 +13,7 @@ Without loss of generality, each combination is internally represented as a sort
 #include <algorithm>  //sort
 #include <assert.h>
 using namespace std;
-using id=size_t; //index into letters<T>
+using ID=size_t; //index into letters<T>
 size_t L, changes=0, calls=0;
 size_t C; //how many in each combination
 //set<int> const AllChoices({11,22,33,44,55});
@@ -21,52 +21,80 @@ set<char> const AllChoices({'a','b','c','d','e'});
 vector<decltype(AllChoices)::value_type> letters;
 
 template<typename ITR> bool isAscending (ITR const b, ITR const end){
-  for (ITR last = b, i = b; i!=end; ++i){
+  //cout<<*b<<" < - > "<<*end<<endl;
+  for (ITR last = b, i = b; i!=end;){
+    //cout<<*i<<endl;
     if (*last > *i) {
       cout<<*last<<" should be lower (or equal) but is higher than "<<*i<<endl;
       return false;
     }
+    last = i;
+    ++i;
   }
   return true;
 }
-void dump(vector<id> & v,  bool isAssert = true){
+void dump(vector<ID> & v,  bool isAssert = true){
   for(int i=0; i<v.size(); ++i) {
-    cout<<setw(5)<<letters[v[i]];
+//    cerr<<setw(5)<<letters[v[i]];
+    cerr<<setw(5)<<v[i];
   }
   cout<<endl;
   if(isAssert){
-    assert(isAscending(v.begin(), v.begin()+C) && "1st C elements should be ascending after next_combo (not next_perm)");
+    assert(isAscending(v.begin(), v.end()) && "1st C elements should be ascending after next_combo (not next_perm)" );
   }
 }
-bool next_combo(vector<id> & v){
+//Solution N, yielding the next higher combination
+bool next_combo(vector<ID> & v){
   ++calls;
   dump(v );
   for(int p2u=C-1; /*last position*/ p2u >=0 ;--p2u){
     if (v[p2u] == L-1) continue;
     //cout<<"identified "<<p2u<<" as position to upgrade... and return"<<endl;
-    id const nextChoice = v[p2u]+1;
+    ID const nextChoice = v[p2u]+1;
     for (; p2u<C; ++p2u) v[p2u] = nextChoice;
     return true;
   }//for
-  cout<<"no more higher combo. This is the end"<<endl;
+  cout<<"no more higher combo. This is the end of next_combo()"<<endl;
   return false;
+}
+//solution A, based on appending
+set<vector<ID>> combos;
+int solA(){
+  combos.clear();
+  for (ID i=0; i<L; ++i) combos.insert(vector<ID>({i})); //initialized
+  for(size_t sz=1; sz < C; ++sz){
+    set<vector<ID>> newCombos;
+    for (auto const & v: combos){ //solA to v
+      for (ID tip = v[v.size()-1]; tip < L; ++tip){
+        vector<ID> newV(v);
+        newV.push_back( tip );
+        newCombos.insert(newV);
+        dump(newV);
+      }
+    }
+    combos = newCombos;
+  }
+  cout<<"-----end of solA() ----\n";
+  return combos.size();
 }
 template<typename T> int test1(int le, int repeat, set<T> coll){
   vector<T> vec1(coll.begin(), coll.end());
   C = repeat;    L = le;
   letters.assign(vec1.begin(), vec1.begin()+le);
-  vector<id> v(C, 0); //first combo
+  int const ret = solA();
+  vector<ID> v(C, 0); //first combo
   for(calls=0; calls<9999; ){
       if (!next_combo(v)){
          cout<<changes<<" changes performed till the highest combo; next_combo() call count = "<<calls<<endl;
+         assert (ret == calls);
          return calls;
       }
   }
   assert(1==0);
 }
 int main() {
-  assert (10==test1(4,2, AllChoices));
   assert (4 ==test1(2,3, AllChoices));
+  assert (10==test1(4,2, AllChoices));
   assert (10==test1(3,3, AllChoices));
   assert (15==test1(3,4, AllChoices));
   assert (21==test1(3,5, AllChoices));
