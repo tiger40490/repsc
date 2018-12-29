252063b3377859cf217e84885169d0285c04d1d6
unknown <35847749+tiger40490@users.noreply.github.com>
2018-12-29 18:23:37 -0500

    white space

--- cpp/linkedList/3waySorterLinkList_FB.cpp
+++ cpp/linkedList/3waySorterLinkList_FB.cpp
@@ -13,52 +13,52 @@ auto mm = li.begin(), hh = li.begin(), un = li.begin();
 // un is first item in unclassified section
 // if hh equals un, it means there's no real H section.
 void dump(){
-        for(auto it=li.begin(); it!=li.end(); ++it) cerr<<*it<<" ";
-        cerr<<"- ";
+  for(auto it=li.begin(); it!=li.end(); ++it) cerr<<*it<<" ";
+  cerr<<"- ";
 
-        if(un == li.end()) cerr<<"end()";
-        else cerr<<*un;
-        cerr<<" <-un, ";
+  if(un == li.end()) cerr<<"end()";
+  else cerr<<*un;
+  cerr<<" <-un, ";
 
-        if(mm == li.end()) cerr<<"end()";
-        else if(mm == un) cerr<<"un";
-        else cerr<<*mm;
-        cerr<<" <-mm, ";
+  if(mm == li.end()) cerr<<"end()";
+  else if(mm == un) cerr<<"un";
+  else cerr<<*mm;
+  cerr<<" <-mm, ";
 
-        if(hh == li.end()) cerr<<"end()";
-        else if(hh == un) cerr<<"un";
-        else cerr<<*hh;
-        cerr<<" <-hh\n";
+  if(hh == li.end()) cerr<<"end()";
+  else if(hh == un) cerr<<"un";
+  else cerr<<*hh;
+  cerr<<" <-hh\n";
 }
 void reshuffle(){
-        for (; un != li.end();){
-                cout<<">> ";
-                dump();
-                item val = *un;
-                if (val < 10){ //L item
-                        { if (un == mm) ++mm;
-                          if (un == hh) ++hh;
-                          un = li.erase(un);
-                        }
-                        li.insert(mm, val);
-                        cerr<<val<<" moved to before mm\n";
-                        continue;
-                }
-                if(val < 100){ // M item
-                        { if (un == hh) ++hh;
-                          un = li.erase(un);
-                        }
-                        auto inserted = li.insert(hh, val);
-                        if(hh == mm) mm=inserted;
-                        cerr<<val<<" moved to before hh. Before this move, mm ptr was not necessarily at first Medium item\n";
-                        continue;
-                }
-                ++un;
-                cerr<<"un incremented. Until this increment, hh ptr was not necessarily at first High item\n";
-        }
+  for (; un != li.end();){
+    cout<<">> ";
+    dump();
+    item val = *un;
+    if (val < 10){ //L item
+      { if (un == mm) ++mm;
+        if (un == hh) ++hh;
+        un = li.erase(un);
+      }
+      li.insert(mm, val);
+      cerr<<val<<" moved to before mm\n";
+      continue;
+    }
+    if(val < 100){ // M item
+      { if (un == hh) ++hh;
+        un = li.erase(un);
+      }
+      auto inserted = li.insert(hh, val);
+      if(hh == mm) mm=inserted;
+      cerr<<val<<" moved to before hh. Before this move, mm ptr was not necessarily at first Medium item\n";
+      continue;
+    }
+    ++un;
+    cerr<<"un incremented. Until this increment, hh ptr was not necessarily at first High item\n";
+  }
 }
 int main(){
-        reshuffle();
-        dump();
+  reshuffle();
+  dump();
 }
 /*https://wp.me/p74oew-4Cj is requirement. If input is array, I have another (harder) implementation in the "array" folder. */
