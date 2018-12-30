ebd36a77073f2498095fb39198154d77ef061ae4
unknown <35847749+tiger40490@users.noreply.github.com>
2018-12-30 11:16:46 -0500

    protect list::back() with empty check; clean up iterator variables

--- cpp/linkedList/merge2slist_ez.cpp
+++ cpp/linkedList/merge2slist_ez.cpp
@@ -1,4 +1,6 @@
 /*
+showcase: use ptr to indirectly advance two iterators in a single loop
+showcase: declare multiple iterator variables in for-loop header
 showcase: typedef for readability
 */
 #include <iostream>
@@ -14,31 +16,28 @@ template<typename T,             int min_width=2> ostream & operator<<(ostream &
    os<<endl;
    return os;
 }
-list<item> process(){
+list<item> merge(){
+  li1.sort();  li2.sort();// should be sorted already.
   list<item> ret;
-  auto i1=li1.cbegin(), i2=li2.cbegin();
-  for (;;){
-    auto i = &i2;
-    if (*i1 < *i2){
-      i = &i1;
-    }
-    assert(ret.back()<=**i);
+  for (auto i1=li1.cbegin(), i2=li2.cbegin();;){
+    auto i = &(*i1 < *i2? i1: i2);
+    assert(ret.empty() || ret.back() <= **i);
     ret.push_back(**i);
-    //cout<<(**i)<<" ";
-    ++*i;
-    //if ( i == i1){
-    if (*i == li1.end()){
-      for (i=&i2; *i != li2.end(); ++*i)     ret.push_back(**i);
-      return ret;
+    ++*i; //indirectly advance one of the iterators
+    if (i1 == li1.end()){
+      for (; i2 != li2.end(); ++i2)     ret.push_back(*i2);
+      break;
     }
-    if (*i == li2.end()){
-      for (i=&i1; *i != li1.end(); ++*i)     ret.push_back(**i);
-      return ret;
+    if (i2 == li2.end()){
+      for (; i1 != li1.end(); ++i1)     ret.push_back(*i1);
+      break;
     }
   }
+  assert(ret.size() == li1.size() + li2.size());
+  return ret;
 }
 int main(){
-  cout<<process();
+  cout<<merge();
 }
-/*
+/*Req: given 2 pre-sorted slists, merge-sort them to a new slist
 */
