9019109248fe99024910c17e6a7cf223c0c05ad7
unknown <35847749+tiger40490@users.noreply.github.com>
2019-02-01 18:30:52 +0800

    new file

---
+++ cpp/diskHogMaker.cpp
@@ -0,0 +1,21 @@
+/*
+showcase check file existence
+showcase check file size 
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+  size_t const mb = 20;
+  string fullpath= "./" + to_string(mb) + "MB_diskHogMade]cpp.dat";
+  if (ifstream(fullpath)){
+    cout<<fullpath<<" will be overwritten ..\n";
+  }
+  ofstream outfile(fullpath);
+  for(auto i=0; i<mb*1024*1024; ++i) outfile<<'.';
+  outfile.close();
+  
+  ifstream tmp2(fullpath);
+  tmp2.seekg(0,ios_base::end); //will return size 0 without this
+  cout<<fullpath<<" created with "<<tmp2.tellg()<<" bytes.\n";
+}
