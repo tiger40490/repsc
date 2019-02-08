0feb4daa9cf9838aae4d8a188b75ee1b9065e994
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-02-08 18:31:19 -0500

    comment only

--- cpp/template/CRTP_demo1.cpp
+++ cpp/template/CRTP_demo1.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-template <typename Sub> struct NumericalFunctions {
+template <typename Sub> struct NumericalFunctions { //templateMethod demo
     void square(){ //a reusable code to be "inherited" by any subclass
         Sub& underlying = static_cast<Sub&>(*this);
         // cast to Sub* i.e. pointer is probably more common and I tested too.
 
         //Now we can Access subclass instance without using virtual function!
         underlying.setValue(underlying.getValue() * underlying.getValue());
+
         cout<<"from inside superclass square(), you can even access subclass field: "<<underlying._value<<endl;
     }
 };
@@ -21,4 +22,5 @@ int main(){
   inst->setValue(17);
   inst->square();
   cout<<inst->getValue();
-}
+}/* https://wp.me/p74oew-3zE
+*/
