c15292e9afd0b2a0f73ab6cf10589ed18cae4839
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-02-10 13:24:21 -0500

    new file

---
+++ cpp/66miscIVQ/throwingDtor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct Trade{
+    ~Trade()  {  throw 1; }
+};
+
+int main(){
+    try {
+	    Trade b;
+		throw 0;
+	}
+	catch ( int a ) {
+	    std::cout << "Caught : " << a << std::endl;
+	}
+}/* https://bintanvictor.wordpress.com/2018/04/02/c11-dtorexception-rules-iv/ explains that c++11 assumes every dtor is a noexcept, so this code hits std::terminate()
+
+Deepak gave me this code after a MS interview.
+*/
