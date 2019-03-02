d8c3edf668579071ca4bfe3cb4042a4dac808b7b
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-02 16:46:27 -0500

    major changes

--- cpp/88miscLang/ptrArithmeticMI.cpp
+++ cpp/88miscLang/ptrArithmeticMI.cpp
@@ -1,20 +1,14 @@
 /*
-demo 
+showcase pointer arithmetic uses sizeof(declared type)
+showcase create a class with a target sizeof
+showcase pointer subtraction result can be negative
+showcase function declaration without return type defauls to int
 */
 #include <stdio.h>
 #include <stddef.h>
+#include <assert.h>
 #include <iostream>
 using namespace std;
-
-struct A { 
-  char age[3];
-  funcWithoutRetType(long){return 0;}; 
-};
-struct B { 
-  char age[7]; //same name!
-  bf(int); 
-};
-struct C: public A, public B{};
 
 void basicPtrArithmetic() {
     int a[20] = {0};
@@ -26,15 +20,32 @@ void basicPtrArithmetic() {
     ptrdiff_t diff2 = y - x; // This is 10 times sizeof(int)
     printf("diff1=%d, diff2=%d\n", (int)diff1, (int)diff2);
 }
+struct B1 { char nickname[3];
+  funcWithoutRetType(long){return 0;}; 
+};
+struct B2 { char nickname[7]; /*same name*/ };
+struct C: public B1, public B2{};
 int main(void) {
   basicPtrArithmetic();
-  cout<<A().funcWithoutRetType(8)<<" <- default ret type is int\n";;
-  cout<<sizeof(A)<<" = sizeof(A)"<<endl;
-  cout<<sizeof(B)<<" = sizeof(B)"<<endl;
-  cout<<sizeof(C)<<" = sizeof(C)"<<endl;
+  cout<<B1().funcWithoutRetType(8)<<" <- implicit return type is int\n";
   C* pc = new C;
-  cout<<(A*) pc<<" = A-object address\n";
-  cout<<(B*) pc<<" = B-object address\n";
-  cout<<     pc<<" = C-object address\n";
+
+  cout<<sizeof(B1)<<" = sizeof(B1)"<<endl;
+  cout<<  (B1*) pc<<" = B1 sub-object address\n";
+  cout<<1+(B1*) pc<<" = incremented\n";
+
+  cout<<sizeof(B2)<<" = sizeof(B2)"<<endl;
+  cout<<  (B2*) pc<<" = B2 sub-object address\n";
+  cout<<1+(B2*) pc<<" = incremented\n";
+  cout<<2+(B2*) pc<<" = incremented again\n";
+
+  cout<<sizeof(C)<<" = sizeof(C)"<<endl;
+  cout<<  pc<<" = C object address (same as B1)\n";
+  cout<<1+pc<<" = incremented\n";
+  
+  auto ptrSubtraction = (char*)pc - (char*)(B2*)pc; //won't compare without cast
+  cout<< ptrSubtraction<<" = addr diff #negative because B2 sub-object has higher address than B1 i.e. C \n";
+  assert(ptrSubtraction == -1 * (int)sizeof(B1));
+
 }/*demo of sizeof ^ ptr arithmetic
 */
