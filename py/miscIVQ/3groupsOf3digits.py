861c68d30547205536140d6cfc8672c7fde1ca50
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-15 22:24:54 -0400

    minor changes

--- py/miscIVQ/3groupsOf3digits.py
+++ py/miscIVQ/3groupsOf3digits.py
@@ -1,10 +1,12 @@
-# showcase omit main(), to maximize screen real estate
+# showcase: omit main(), to maximize screen real estate
+# showcase: default return value is None, which evaluates to False 
+import sys
 def failed_to_add(myset, digit): # return False when failed
     return digit == 0 or len(myset) == ( myset.add(digit) or len(myset))
 def other_number_failed(myset, num): # returns False by default if everything good
     for digit in(num/100, num/10%10, num%10):
         if failed_to_add(myset, digit):     return True
-def check(number1, myset):
+def check(number1, myset): # a well-defined function
     number2 = 2*number1
     if other_number_failed(myset, number2): return 
     number3 = 3*number1
@@ -13,18 +15,19 @@ def check(number1, myset):
     winners.append([number1, number2, number3])
 
 winners=list()
-nine_digits = tuple(range(1,10))
+nine_digits = tuple(range(1,10)) #prevent accidental change
 for digit1 in (1,2,3):
-    last8 = list(nine_digits)
+    last8 = list(nine_digits) #make a copy
     last8.remove(digit1)
     for digit2 in last8:
         last7 = list(last8)
         last7.remove(digit2)
         for digit3 in last7:
             number1 = digit1*100 + digit2*10 + digit3
-            if number1 > 329: break
+            if number1 > 329: 
+               print "-- lucky winners --", winners
+               sys.exit()
             check(number1, {digit1, digit2, digit3})
-print "-- lucky winners --", winners
 
 '''Q: Find three 3-digit numbers with a ratio of 1:2:3. These numbers pick their 3 digits from a range of 1 to 9. You must use up all distinct 9 digits. For example, a set of 123:246:369 doesn't qualify.
 
---
+++ py/tmpl4timedTest.py
@@ -0,0 +1,4 @@
+import sys, os; from pprint import pprint
+pprint(list((1,3,'a'))); sys.exit()
+'''This is a template for timed online tests. It saves screen real estate
+'''
