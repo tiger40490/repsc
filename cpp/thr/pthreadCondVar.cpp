3a94d3b55456f5b337ab210f981275cd8b65f6e8
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-15 20:48:36 -0400

    fixed for windows

--- cpp/thr/pthreadCondVar.cpp
+++ cpp/thr/pthreadCondVar.cpp
@@ -1,6 +1,3 @@
-/* g++ -pthread pthreadCondVar.cpp
-This simple demo is based on https://gist.github.com/rtv/4989304
- */
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h> //sleep()
@@ -8,7 +5,7 @@ This simple demo is based on https://gist.github.com/rtv/4989304
 #include <iostream>
 using namespace std;
 
-const size_t THREAD_CNT = 4;
+const size_t THREAD_CNT = 10;
 int Done = 0;
 
 //global vars not on heap:
@@ -17,23 +14,18 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 /* Note: error checking on pthread_X calls omitted for clarity - you
    should always check the return values in real code. */
-
-/* Note: passing the thread id via a void pointer is cheap and easy,
- * but the code assumes pointers and long ints are the same size
- * (probably 64bits), which is a little hacky. */
-
-void* ThreadEntry( void* id ){
-  assert(sizeof(void*) == sizeof(long long));
-  const int myid = (long long)id; // force the pointer to be a long integer
+void* ThreadEntry( void*){
+  cerr<<"[thread "<<pthread_self()<<"] grabbing lock ..\n";
   
-  const int workloops = 3;
-  for( int i=1; i<=workloops; i++ ){
-      printf( "[thread %d] working (%d/%d), possibly interleaved\n", myid, i, workloops );
-      sleep(1); // simulate doing some costly work
-  }
   pthread_mutex_lock( &mutex ); //need to write to shared mutable var
+  cerr<<"[thread "<<pthread_self()<<"] got lock :)\n";
+  const int workloops = 2;
+  for( int i=1; i<=workloops; i++ ){
+      printf( "[thread %d] working (%d/%d), possibly interleaved\n", pthread_self(), i, workloops );
+      sleep(2); // simulate doing some costly work
+  }
   Done++; // increase the count of threads that have finished their work.
-  printf( "[thread %d] Done is now %d. Signaling cond.\n", myid, Done );
+  printf( "[thread %d] Done is now %d. Signaling cond.\n", pthread_self(), Done );
 
   // wake up the main thread (if it is sleeping) to test the value of Done  
   pthread_cond_signal( &cond ); 
@@ -41,10 +33,10 @@ void* ThreadEntry( void* id ){
   return NULL;
 }
 
-int main( int argc, char** argv ){
+int main(){
   pthread_t threads[THREAD_CNT];
   for( int t=0; t<THREAD_CNT; t++ )
-    pthread_create( &threads[t], NULL, ThreadEntry, (void*)(long long)t );
+    pthread_create( &threads[t], NULL, ThreadEntry, NULL );
 
   pthread_mutex_lock( &mutex ); //needed to read Done and use the cond var
   while( Done < THREAD_CNT ){
@@ -52,11 +44,15 @@ int main( int argc, char** argv ){
 	      Done, (int)THREAD_CNT );
       
       /* block this thread until another thread signals cond. While
-	 blocked, the mutex is released, then re-aquired before this
-	 thread is woken up and the call returns. */ 
+	       blocked, the mutex is released, then re-aquired before this
+	       thread is woken up and the call returns. */ 
       pthread_cond_wait( & cond, & mutex ); 
       cout<<"[thread main] wake - cond was signaled.\n"; 
   }
   printf( "[thread main] Done == %d so everyone is Done\n", (int)THREAD_CNT );
   pthread_mutex_unlock( & mutex );
-}
+}/* This simple demo is based on https://gist.github.com/rtv/4989304
+g++ -g -pthread pthreadCondVar.cpp && gdb ./a.exe
+
+q[info threads] shows 12 threads for 10 worker threads. The main thread and a last thread are out of my control.
+*/
