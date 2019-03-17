a912f6ff047f3e34de92f70edd974c4cd06317c3
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-17 01:50:54 -0400

    rename fields

--- py/tree/simpleBST.py
+++ py/tree/simpleBST.py
@@ -2,29 +2,29 @@
 '''
 class Node:
   def __init__(self, val):
-    self.l_child = None
-    self.r_child = None
+    self.le = None
+    self.ri = None
     self.data = val
 def add(root, node):
   if root is None: root = node
   else:
     if root.data > node.data:
-      if root.l_child is None: root.l_child = node
-      else: add(root.l_child, node)
+      if root.le is None: root.le = node
+      else: add(root.le, node)
     else:
-      if root.r_child is None: root.r_child = node
-      else: add(root.r_child, node)
+      if root.ri is None: root.ri = node
+      else: add(root.ri, node)
 def in_order_print(node, depth=0): 
   if node is None: return
-  in_order_print(node.l_child, depth+1)
+  in_order_print(node.le, depth+1)
   print ' ' *3*depth + str(node.data)
-  in_order_print(node.r_child, depth+1)
+  in_order_print(node.ri, depth+1)
 
 def pre_order_print(node):
   if not node: return    
   print node.data
-  pre_order_print(node.l_child)
-  pre_order_print(node.r_child)  
+  pre_order_print(node.le)
+  pre_order_print(node.ri)  
 def main():
   rt = Node(3)
   add(rt, Node(7))
