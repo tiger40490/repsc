31affa359a1d1dfab6443fbdc11aa03ceb9e8365
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-17 21:49:21 -0400

    new file

---
+++ py/randomGenUtil.py
@@ -0,0 +1,11 @@
+import sys, os, random; from pprint import pprint
+
+''' simple utility for timed coding tests
+'''
+def randomGen(cnt, minOutputVal, min=0):
+  assert cnt <= minOutputVal-min+1 #optional input validation
+  ret=range(min, minOutputVal+1, 1);
+  random.shuffle(ret); return ret[:cnt]
+  
+pprint(randomGen(5,9))
+pprint(randomGen(5,4))
