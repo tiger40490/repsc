80c51d8f01996e0c9ab8996a5486d48eceddcc53
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-17 23:51:24 -0400

    major feature

--- py/tmpl4timedTest.py
+++ py/tmpl4timedTest.py
@@ -1,12 +1,13 @@
 import sys, os, random; from pprint import pprint
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # worked
 li=range(9); random.shuffle(li); pprint(li)
-## list methods:
-print li.pop(), li 
+## list methods + operators:
+print li.pop(), '  ', li
 li.insert(0,-55) #insert at 0th position
 li.remove(-55) #remove an item equal to 55, assuming it exists
-del li[0] #; print li # remove by index
-print sum(li)
+del li[0] ; print 'del[0]:',li # remove by index
+print 'sum =',sum(li)
+del li[:]; print li #wipe clean in-place
 ## dict init:
 pprint({1:'j', 3+2:'a'}); sys.exit()
 
