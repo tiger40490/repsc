fe5e3be0f1492926d8d31e6fe614be7c27b747e9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-18 19:43:42 -0400

    new file

---
+++ py/0cleanUtil4iv/slist4iv.py
@@ -0,0 +1,26 @@
+#from pprint import pprint
+class Node(object):
+  def __init__(self, data, next=None):
+    self.data = data
+    self.next = next
+  def print2(self):
+    nx = self.next
+    print '[' + str( self.data ) + '] => ' + str( id(self.next) if self.next else 0 )
+_7 = Node(7)
+_6 = Node(6, _7)
+_5 = Node(5, _6)
+_4 = Node(4, _5)
+_3 = Node(3, _4)
+_2 = Node(2, _3)
+_1 = Node(1, _2)
+root = _1
+
+def dump(node):
+  cnt=0
+  while 1:
+    if node is None or cnt > 9: break
+    cnt+=1
+    node.print2()
+    node = node.next
+dump(root)
+  
