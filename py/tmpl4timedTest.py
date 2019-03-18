9c681f59cb75f45728c49271e33dcb28d53dbf66
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-17 20:35:18 -0400

    one line change

--- py/tmpl4timedTest.py
+++ py/tmpl4timedTest.py
@@ -1,4 +1,5 @@
 import sys, os; from pprint import pprint
+sys.stdout = os.fdopen(sys.stdout.fileno(), ‘w’, 0) # worked
 pprint({1:'j', 3+2:'a'}); sys.exit()
 
 '''This is a template for timed online tests. It saves screen real estate
