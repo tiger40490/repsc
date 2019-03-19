54510329649f96c0e2ea89ceb47776a79df255de
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-03-18 20:57:18 -0400

    minor

--- py/0cleanUtil4iv/tmpl4timedTest.py
+++ py/0cleanUtil4iv/tmpl4timedTest.py
@@ -3,13 +3,15 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # worked
 li=range(9); random.shuffle(li); pprint(li)
 ## list methods + operators:
 print li.pop(), '  ', li
-li.insert(0,-55) #insert at 0th position
-li.remove(-55) #remove an item equal to 55, assuming it exists
+li.insert(0,-55); print li #insert at 0th position
+li.remove(-55) #remove an item equal to -55, assuming it exists
 del li[0] ; print 'del[0]:',li # remove by index
 print 'sum =',sum(li)
 del li[:]; print li #wipe clean in-place
 ## dict init:
-pprint({1:'j', 3+2:'a'}); sys.exit()
+di={1:'j', 3+2:'a'}; 
+for k,v in di.items(): print v,k
+print di.keys(); sys.exit()
 
 '''This is a template for timed online tests. It saves screen real estate
 '''
