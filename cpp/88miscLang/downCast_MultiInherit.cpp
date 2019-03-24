651b5548e673b248c5f881b860deacb35a80a60e
unknown <35847749+tiger40490@users.noreply.github.com>
2019-03-24 10:42:28 -0400

    add asserts

--- cpp/88miscLang/downCast_MultiInherit.cpp
+++ cpp/88miscLang/downCast_MultiInherit.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 struct B1{
-  int f1; //without this field, all 3 addresses are identical
+  int f1; //without this field, all 3 object addresses are identical
 };
 struct B2{
-  int f2; //without this field, all 3 addresses are identical
+  int f2; //without this field, all 3 object addresses are identical
 };
 struct D: public B1, B2{} der;
 int main(){
-  cout<<&der<<" = addr of derived object \n";
-  cout<<dynamic_cast<B1*>(&der)<<" = addr after casting to B1* \n"; //same as above
-  cout<<dynamic_cast<B2*>(&der)<<" = addr after casting to B2* \n"; //different
+  auto pd = &der;
+  cout<<pd<<" = addr of derived object \n";
+  cout<<dynamic_cast<B1*>(pd)<<" = addr after casting to B1* \n"; //same as der
+  assert((void*)dynamic_cast<B1*>(pd)==(void*)pd);
+  cout<<dynamic_cast<B2*>(pd)<<" = addr after casting to B2* \n"; //different from der
+  assert((void*)dynamic_cast<B2*>(pd)!=(void*)pd);
 }
-/* Q: dynamic_cast always gives the same address?
+/* Q: dynamic_cast always(?) gives the same address?
    A: not always. Consider MI.
-   A: if one of the two base classes is a pure-interface (i.e. no instance field) then YES
+   A: if any one of the two base classes is a pure-interface (i.e. no instance field) then YES
 */
