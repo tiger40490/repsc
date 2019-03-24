fd3850d258302239a62d289f7fa77c73ae5f5f44
unknown <35847749+tiger40490@users.noreply.github.com>
2019-03-24 13:32:44 -0400

    major

--- cpp/88miscLang/downCast_MultiInherit.cpp
+++ cpp/88miscLang/downCast_MultiInherit.cpp
@@ -1,49 +1,40 @@
+//todo: sfinae
 #include <iostream>
 #include <assert.h>
 using namespace std;
 
-struct B1f{
+struct B1empty{};
+struct B2empty{};
+struct B1nonEmpty{
   int f1; //without this field, all 3 object addresses are identical
 };
-struct B2f{
+struct B2nonEmpty{
   int f2; //without this field, all 3 object addresses are identical
 };
-#define B1 B1f
-#define B2 B2f
-struct D: public B1, B2{} der;
-int testWithFields(){
-  auto pd = &der;
-  auto pb1 = dynamic_cast<B1*>(pd);
-  auto pb2 = dynamic_cast<B2*>(pd);
-  cout<<pd<<" = addr of derived object \n";
-  cout<<pb1<<" = addr after casting to B1* \n"; //same as der
-  cout<<pb2<<" = addr after casting to B2* \n"; //different from der
-
-  assert((void*)pb1==(void*)pd);
-  assert((void*)pb2!=(void*)pd);
-}
-////////
-struct B1empty{
-  //int f1; //without this field, all 3 object addresses are identical
+template<typename B1, typename B2, char mode>
+struct D: public B1, B2{
+  test(){
+    auto pb1 = dynamic_cast<B1*>(this);
+    auto pb2 = dynamic_cast<B2*>(this);
+    cout<<(int)mode<<" <===== new mode in a new test\n";
+    cout<<this<<" = addr of derived object \n";
+    cout<<pb1<<" = addr after casting to B1* \n"; //same as der
+    cout<<pb2<<" = addr after casting to B2* \n"; //different from der
+    assert((void*)pb1==(void*)this);
+    
+    if     (mode == 2)
+      assert((void*)pb2!=(void*)this);
+    else if (mode <= 1)
+      assert((void*)pb2==(void*)this);
+    else 
+      assert(1==0);
+  }
 };
-struct B2empty{
-  ///int f2; //without this field, all 3 object addresses are identical
-};
-#define B1 B1empty
-#define B2 B2empty
-int testWithoutFields(){
-  auto pd = &der;
-  auto pb1 = dynamic_cast<B1*>(pd);
-  auto pb2 = dynamic_cast<B2*>(pd);
-  cout<<pd<<" = addr of derived object \n";
-  cout<<pb1<<" = addr after casting to B1* \n"; //same as der
-  cout<<pb2<<" = addr after casting to B2* \n"; //different from der
-
-  assert((void*)pb1==(void*)pd);
-  assert((void*)pb2!=(void*)pd);
-}
 int main(){
-  testWithFields();
+  D<B1nonEmpty, B2nonEmpty, 2>().test();
+  D<B1empty, B2nonEmpty, 1>().test();
+  D<B1nonEmpty, B2empty, 1>().test();
+  D<B1empty, B2empty, 0>().test();
 }/* Q: dynamic_cast always(?) gives the same address?
    A: not always. Consider MI.
    A: if any one of the two base classes is a pure-interface (i.e. no instance field) then YES
