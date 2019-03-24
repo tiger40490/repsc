a9b89e55989f2b7daf18cc7cc13ced8d4ff7c841
unknown <35847749+tiger40490@users.noreply.github.com>
2019-03-24 02:43:35 -0400

    clarify

--- cpp/88miscLang/arrayNewDtor.cpp
+++ cpp/88miscLang/arrayNewDtor.cpp
@@ -7,7 +7,7 @@ struct C{
   static int cnt;
   int const _i;
   C(): _i(cnt+=2){
-    if (_i>8) throw string("bad value in data member");
+    if (_i>8) throw string("bad value in data member this->_i = ") + to_string(this->_i);
     cout<<_i<<" constructed\n";
   }
   ~C(){cout<<_i<<" destructed in reverse sequence\n"; }
