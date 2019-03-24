331e0773e1f623726c8d7aa91d14fc89d306547d
unknown <35847749+tiger40490@users.noreply.github.com>
2019-03-24 10:53:52 -0400

    refactor

--- cpp/88miscLang/downCast_MultiInherit.cpp
+++ cpp/88miscLang/downCast_MultiInherit.cpp
@@ -11,11 +11,14 @@ struct B2{
 struct D: public B1, B2{} der;
 int main(){
   auto pd = &der;
+  auto pb1 = dynamic_cast<B1*>(pd);
+  auto pb2 = dynamic_cast<B2*>(pd);
   cout<<pd<<" = addr of derived object \n";
-  cout<<dynamic_cast<B1*>(pd)<<" = addr after casting to B1* \n"; //same as der
-  assert((void*)dynamic_cast<B1*>(pd)==(void*)pd);
-  cout<<dynamic_cast<B2*>(pd)<<" = addr after casting to B2* \n"; //different from der
-  assert((void*)dynamic_cast<B2*>(pd)!=(void*)pd);
+  cout<<pb1<<" = addr after casting to B1* \n"; //same as der
+  cout<<pb2<<" = addr after casting to B2* \n"; //different from der
+
+  assert((void*)pb1==(void*)pd);
+  assert((void*)pb2!=(void*)pd);
 }
 /* Q: dynamic_cast always(?) gives the same address?
    A: not always. Consider MI.
