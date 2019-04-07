9a96d425fe33feccc30458dbe437e29eadd6ad1d
unknown <35847749+tiger40490@users.noreply.github.com>
2019-04-07 01:14:15 -0400

    use template rather than macro

--- cpp/88miscLang/sizeofNew.cpp
+++ cpp/88miscLang/sizeofNew.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
-#define MeasureNew(T, text) { \
+#define M2(T) MeasureNew<T>(#T)
+using namespace std;
+template<typename T> void MeasureNew(string text){
+  cout<<text<<" "<<sizeof(T)<<"  ";
+  char* lastp = nullptr;
+  for (int i=0; i<11; ++i){
+    T* p=new T;
+    auto thisp = reinterpret_cast<char*>(p);
+    if (lastp != 0) cout<<" " <<thisp-lastp;
+    lastp = thisp;
+  } cout<< endl;
+}
+struct i1{int i; };
+struct ic{int i; char c; };
+struct c1{char c; };
+struct ci{char c; int i; };
+int main(){
+  //M2(c1);
+  //M2(i1);
+  M2(ci);
+  M2(ic);
+}/*Goal: measure total memory usage by each new() allocation
+based on P186 [[Programming Pearls]]. 
+
+Note: first call to MeasureNew() in main() gives best i.e. most consistent result. If you call MeasureNew(ic) after other MeasureNew(...) calls, then result is hard to interpret. I guess this is the behavior of the new() allocator.
+
+Note: Macro below worked better than template at the time of the book.
+
+#define MeasureNewOriginal(T, text) { \
   cout<<text<<" "<<sizeof(T)<<"  "; \
   char* lastp = 0; \
   for (int i=0; i<11; ++i){ \
@@ -9,20 +37,4 @@
     lastp = thisp; \
   } cout<< endl; \
 }
-using namespace std;
-struct c1 {char c; };
-struct i1 {int i; };
-struct ic{int i; char c; };
-struct ci{char c; int i; };
-int main(){
-  MeasureNew(c1, "c1");
-  MeasureNew(i1, "i1");
-  MeasureNew(ci, "ci");
-  MeasureNew(ic, "ic");
-}/*Goal: measure total memory usage by each new() allocation
-based on P186 [[Programming Pearls]]. 
-
-Note: first call to MeasureNew() in main() gives best i.e. most consistent result. If you call MeasureNew(ic) after other MeasureNew(...) calls, then result is hard to interpret. I guess this is the behavior of the new() allocator.
-
-Note: Macro worked better than template at the time of writing.
 */
