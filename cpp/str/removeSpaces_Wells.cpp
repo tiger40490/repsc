f02c4cdc0371fdec8eaf2c4f766f0bd8a06cd519
unknown <35847749+tiger40490@users.noreply.github.com>
2019-04-13 09:54:05 -0400

    tested

--- cpp/str/removeSpaces_Wells.cpp
+++ cpp/str/removeSpaces_Wells.cpp
@@ -1,6 +1,6 @@
-//O(1) space, one scan
+//one-scan, 2-ptr algorithm in O(1) space
 //Avoid strlen() which has O(N) cost
-//Avoid unnecessary char-copying in the initial part since most strings start with non-spaces.
+//Avoid unnecessary char-copying in the initial part since many realistic strings start with non-space.
 
 #include <iostream>
 #include <cassert>
@@ -8,23 +8,23 @@ using namespace std;
 
 size_t removeSpaces(char * s){ //returns # of copy operations, to be minimized
   cout<<s<<"__ is original\n";
-  int cost=0, back=0;
-  for(int front=back; s[front]!=0; ++front){
+  int cost=0, back=0, front=0; //front can be declared in for-loop header but in speed coding this way is simpler
+  for (; s[front]; ++front){
     if (isspace(s[front])) continue;
-    if (back<front) {
-      s[back] = s[front];
-      cost++;
+    if (back < front){
+        s[back] = s[front];
+        ++cost;
     }
-    back++;
+    ++back;
   }
-  s[back]=0;
+  s[back]=0; //in coding tests, 0 is faster to type
   cout<<s<<"__ is afterwards\n";
   return cost;
 }
 int main(){
   char s[]="  at  b c dd ";
   assert(6==removeSpaces(s));
-  char s2[]="  at  b c dd";
-  assert(6==removeSpaces(s2));
+  char s2[]="at  b c dd";
+  assert(4==removeSpaces(s2));
 }
 //requirement: https://bintanvictor.wordpress.com/2015/12/10/my-most-efficient-removespaceschar/
