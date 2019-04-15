e6a4ca1b1d7fa24b627e2ec8ae4019d2b94f5132
unknown <35847749+tiger40490@users.noreply.github.com>
2019-04-15 19:09:04 -0400

    add more tests

--- py/0cleanUtil4iv/slist4iv.py
+++ py/0cleanUtil4iv/slist4iv.py
@@ -1,13 +1,14 @@
-#from pprint import pprint
+import sys
+from pprint import pprint
 class Node(object):
   def __init__(self, data, next=None):
     self.data = data
     self.next = next
-  def print2(self):
+  def pr(self): # shorthand 
     print self
   def __repr__(self):
-    _nx = self.next
-    return '[' + str( self.data ) + '] => ' + str(id(_nx) if _nx else 0)
+    nx = self.next
+    return '[' + str( self.data ) + '] => ' + str(id(nx) if nx else 0)
 _7 = Node(7)
 _6 = Node(6, _7)
 _5 = Node(5, _6)
@@ -22,7 +23,7 @@ def dump(node):
   while 1:
     if node is None or cnt > 9: break
     cnt+=1
-    node.print2()
+    node.pr()
     node = node.next
+print str(_5) ; pprint(_5); sys.exit();
 dump(root)
-  
