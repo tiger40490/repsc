00ece7301da9455052cb58cb3fba3b79ffb28db7
unknown <35847749+tiger40490@users.noreply.github.com>
2019-04-23 19:22:34 -0400

    major

--- cpp/66miscIVQ/pushback_SCB.cpp
+++ cpp/66miscIVQ/pushback_SCB.cpp
@@ -7,49 +7,45 @@ Q: why is push_back() always calling the copy ctor?
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <cassert>
 using namespace std;
+long choice=0;
 struct Payload{
   Payload(){cout<<"Payload ctor\n"; }
 };
 struct Wrapper1{
-  shared_ptr<Payload> sp=make_shared<Payload>();
-  Wrapper1(){cout<<"Wrapper1 ctor\n"; }  
+  shared_ptr<Payload> sp;
+  Wrapper1(): sp(make_shared<Payload>()) {cout<<"Wrapper1 ctor\n"; }  
   Wrapper1(Wrapper1 && rhs){
-	cout<<"Wrapper1 move ctor\n"; 
-	this->sp = rhs.sp;
+	swap(this->sp, rhs.sp); //not sure
+	cout<<"Wrapper1 move ctor done "<<this->sp.get()<<"=this->sp , rhs.sp="<<rhs.sp<<endl; 
+    choice = 1000+'m';
   }
-  Wrapper1(Wrapper1 const & rhs) 
+  Wrapper1(Wrapper1 const & rhs)
   : sp(rhs.sp){ cout<<"Wrapper1 copy ctor\n"; }
 };
+struct Wrapper2{ //mv-ctor deleted
+  shared_ptr<Payload> sp;
+  Wrapper2(): sp(make_shared<Payload>()) {cout<<"Wrapper2 ctor\n"; }  
+  Wrappper2(Wrapper2 &&) = delete;
+  Wrapper2(Wrapper2 const & rhs)
+  : sp(rhs.sp){ cout<<"Wrapper2 copy ctor\n"; choice = 2000+'c';}
+};
 template <typename W> test(){
-  //W tmp;
-  //W tmp2 = move(tmp);
   vector<W> vec;
-//  vec.push_back(move(W()) );
-}
-void testMove(Wrapper1 && rhs){
-  cout<<"testMove\n";
-  Wrapper1 local=move(rhs);
+  vec.push_back( W() );//favors move ctor due to naturally-occurring rval object
 }
 int main(){
-  Wrapper1 w;
-  testMove(move(w));
-#ifdef aaaa
   test<Wrapper1>();
+  assert(choice==1000+'m');
   test<Wrapper2>();
+  assert(choice==2000+'c');
+#ifdef aaaa
   testFactory<Wrapper3>();
   testFactory<Wrapper4>();
 #endif
 }
 
-struct Wrapper2{
-  shared_ptr<Payload> sp=make_shared<Payload>();
-  Wrapper2(){cout<<"Wrapper2 ctor\n"; }
-  Wrappper2(Wrapper2 &&) = delete;
-  Wrapper2(Wrapper2 const & rhs): sp(rhs.sp){
-	cout<<"Wrapper2 copy ctor\n"; 
-  }
-};
 class Wrapper3{
   shared_ptr<Payload> sp=make_shared<Payload>();
   Wrapper3(){cout<<"Wrapper--3 ctor\n"; }
