95002d17256978ae2c16e3bd6d1ad2829b465ecb
unknown <35847749+tiger40490@users.noreply.github.com>
2019-05-02 19:38:46 -0400

    new stuff

--- cpp/algo_miscIVQ/concretize.cpp
+++ cpp/algo_miscIVQ/concretize.cpp
@@ -1,9 +1,21 @@
 #include <stack>
 #include <iostream>
+#include <sstream> //getline
 using namespace std;
 
 double evalRpn(string expr){
-  
+  stringstream ss(expr); 
+  int i;
+  string op;
+  while(! ss.bad()){
+    if (ss>> i){
+		cout<<i<<" found an int\n";
+	}else{
+		ss>>op;
+		cout<<op<<" found an operator\n";
+	}
+  }
+  return 0;
 }
 
 int main(){
