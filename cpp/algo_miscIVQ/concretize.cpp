37b90fe60e13170bed3f705ec3c7f0c09eadd2d3
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-04 16:25:42 -0400

    major refactor

--- cpp/algo_miscIVQ/concretize.cpp
+++ cpp/algo_miscIVQ/concretize.cpp
@@ -1,17 +1,13 @@
 /*
+todo: parse input text same as in java
 showcase local alias via q[using]
 showcase map::emplace()
 showcase fwd declare a class template...necessary evil
 showcase template default type-arg and where explicit is needed
-minor todo: add more assertions to aid refactor
-minor todo: make Cell constructable on heap only
-bug: when i get formula for X9, i should not construct new cell but should take exiting cell and update it
-todo: populate the graph
-todo: populate the top-level cells
-
 */
 #include <vector>
 #include <list>
+#include <set>
 #include <map>
 #include <unordered_map>
 #include <string>
@@ -46,50 +42,58 @@ template<typename T,             int min_width=2> ostream & operator<<(ostream &
    for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
    return os;
 }
+template<typename T,             int min_width=2> ostream & operator<<(ostream & os, set<T> const & c){
+   for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
+   return os;
+}
 
 template<typename I_TYPE=int, typename O_TYPE=double, size_t maxTokenCnt=10> class Cell; //fwd declaration required by rclookup map
 
+map<rcid, set<rcid>> p2d; //precedent -> all depdendents
+
 //Global singleton holding all Cells (each saved here upon construction). 
 map<rcid, Cell<int>* > rclookup; 
+inline char idexist(rcid const & id){return rclookup.count(id);} //can rewrite using find()
 
 template<typename I_TYPE, typename O_TYPE, size_t maxTokenCnt> class Cell{
   vector<string> tokenArray;
   rcid const & id;
   O_TYPE concreteValue = NAN; //initialize to not-a-number i.e. pending
-  list<rcid> uu; //unconcretized upstream references
-  list<rcid> downstream;
+  set<rcid> uu; //unconcretized upstream references
+  //list<rcid> downstream;
   friend ostream & operator<<(ostream & os, Cell const & c){
-    os<<c.id<<" {unresolved refs="<<c.uu<<"; downstream cells="<<c.downstream<<"; val="<<c.concreteValue<<" }";
+    os<<c.id<<" {unresolved refs="<<c.uu<<"; val="<<c.concreteValue<<" }";
     return os;
   }
   friend void ctorTest();
   /*saves tokenArray into a list of strings
     saves upstream references 
     no validation of formula
-    This ctor is called only if the name is not already in the lookup map?
   */
   Cell(string const & name, string const & expr): id(name){ 
-    //assert(rclookup.count(id) == 0);
+    assert(!idexist(id));
     stringstream ss(expr);
     this->tokenArray.reserve(maxTokenCnt); //preempt reallocation
     for(string token; getline(ss,token,' ');){
       if ('A' <= token[0] && token[0] <= 'Z'){
-        Cell* upstream;
-        if (rclookup.count(token)){
-          upstream = rclookup[token];
+        //Cell* upstream;
+        p2d[token].insert(id);
+        if (idexist(token)){
+          Cell * upstream = rclookup[token];
+          if (isnan(upstream->concreteValue)){
+            tokenArray.push_back(token);
+            uu.insert(token);
+            ss1<<token<<" is a unresolved precedent\n";
+          }else{
+            ss1<<token<<" is a concretized precedent:)\n";
+            tokenArray.push_back(to_string(upstream->concreteValue));
+          }
         }else{
-          upstream = Cell::makeCell(token, "");
+          uu.insert(token);
+          ss1<<token<<" is a unconstructed precedent\n";
         }
-        if (isnan(upstream->concreteValue)){
-          upstream->downstream.push_back(id);
-          tokenArray.push_back(token);
-          uu.push_back(token);
-        }else{
-          ss1<<token<<" is a concretized upstream\n";
-          tokenArray.push_back(to_string(upstream->concreteValue));
-        }
-      }else{
-         tokenArray.push_back(token);		  
+      }else{ //not a reference
+         tokenArray.push_back(token);
       }
     }
     this->evalRpn();
@@ -100,17 +104,13 @@ public:
   static Cell* makeCell(rcid const & id, string const & expr){
     ss1<<"makeCell at "<<id<<" ...\n";
     Cell* newCell = new Cell(id, expr);
-    if (rclookup.count(id)){
-      newCell->downstream = move(rclookup[id]->downstream);
-      delete rclookup[id];
-      ss1<<*newCell<<" updated\n";
-    }
+    assert (!idexist(id) );
     rclookup[id] = newCell;    
     return newCell;
   }
   char evalRpn(){
     if (uu.size()) return 0; //0 indicates "not ready"
-    if (tokenArray.empty()) return 0; 
+    assert (tokenArray.size()); // return 0; 
     if (! isnan(this->concreteValue)) return 'd'; 
     
     using stack=vector<O_TYPE>;  
@@ -145,17 +145,16 @@ public:
 };
 void ctorTest(){
   Cell<>* ptr = Cell<>::makeCell("C2", "A1 1 5 + * 4 - 2 /"); //(3*(1+5)-4)/2
-  ptr->evalRpn();
 
   ptr = Cell<>::makeCell("A1", "3 1 5 + * 6 / 4 - 2 /"); //(3*(1+5)/6-4)/2
-  ptr->evalRpn(); assert(ptr->concreteValue == -0.5);
+  assert(ptr->concreteValue == -0.5);
   
   ptr = Cell<>::makeCell("B4", "3 1 5 + * 4 - 2 /"); //(3*(1+5)-4)/2
-  ptr->evalRpn(); assert(ptr->concreteValue == 7);
+  assert(ptr->concreteValue == 7);
   
-  Cell cell2("X9", "D3 1 A1 + * E6 / B4 - 2 /"); //(3*(1+5)/6-4)/2
-  cout<<cell2<<endl;
+  Cell<> cell2("X9", "D3 1 A1 + * E6 / B4 - 2 /"); //(3*(1+5)/6-4)/2
   cout<<*rclookup["A1"]<<endl;
+  cout<<p2d;
 }	
 int main(){
   ctorTest();
