cbf824ae354b2476edecc5a0ba3fbb19f72b0da7
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-05 04:42:37 -0400

    relocated

--- cpp/algo_miscIVQ/concretize.sh
+++
@@ -1,5 +0,0 @@
-g++ -std=c++17 concretize.cpp || exit
-./a.exe myTest3 < concretize3.dat || exit
-./a.exe myTest2 < concretize2.dat || exit
-./a.exe myTest1 < concretize.dat || exit
-./a.exe myTestC < concretizeCyc.dat || exit
