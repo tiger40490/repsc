0bc25b12ba1cedbdf9bb8f5ba2d5508cb5944b2d
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-06 09:58:19 -0400

    trivial

--- py/algo_str/hiding.py
+++ py/algo_str/hiding.py
@@ -1,9 +1,7 @@
 '''
 todo: more tests
-todo: bottom up solution
+todo: add a bottom up, non-recursive solution
 
-showcase one-liner function
-showcase one-liner to remove spaces
 showcase one-liner to clear a dict
 showcase one-liner to assign two vars
 
@@ -15,23 +13,23 @@ def td(NN, HH): #top-down DP with memoization
   if tu in memo: return memo[tu]
   
   print NN + '>' + HH
-  if   sz1 == sz2: ret = (1 if NN==HH else 0) #most likely scenario first 
-  elif sz1 == 0:   ret = 1 # minor optimization
+  if   sz1 == sz2: ret = (1 if NN==HH else 0)
+  elif sz1 == 0:   ret = 1 # optional (minor) optimization
  #elif sz1 >  sz2: ret = 0 # this check was moved into solve().. should not stay in this "hot" function
   else:
     assert sz1 < sz2
-    ret = td(NN, HH[:-1]) 
-    if NN[-1:] == HH[-1:] :  ret += td(NN[:-1], HH[:-1])
+    ret = td(NN, HH[:-1]) # these hiding places don't include haystack's last char
+    if NN[-1:]==HH[-1:] : ret += td(NN[:-1] , HH[:-1]) 
   memo[(sz1,sz2)] = ret
   return ret
-def packed(haystack):  return ''.join(haystack.split()) # can inline if used only once 
 def solve(needle, haystack):
-  haystack = packed(haystack); assert len(needle) <= len(haystack), "invalid input"
+  haystack = haystack.replace(' ',''); assert len(needle) <= len(haystack), "invalid input"
   memo.clear()
   ret = td(needle, haystack); print SEP,ret,SEP; return ret
 assert 6+5+3==solve('as', 'as ass asss')
 assert 6==solve('11', '1111')
 assert 4==solve('Gks', 'Geeks For Geeks')
+# def packed(haystack):  return ''.join(haystack.split()) # less efficient than replace()
 '''Req:
 Given longer and short strings Haystack vs Needle, can string-N match a sub-sequence (not necessarily substring) of string-H? You can say we are checking if string-N is hiding in string-H. If yes how many hiding places (or sub-sequences) can you find? 
 
