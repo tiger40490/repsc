9f5cf76d16952acfe59cea17abe89914d325dd79
unknown <35847749+tiger40490@users.noreply.github.com>
2019-05-06 07:25:59 -0400

    comments.. add tests

--- py/algo_arr/hiding.py
+++ py/algo_arr/hiding.py
@@ -1,4 +1,6 @@
 '''todo: if n is 0 length?
+todo: more tests
+todo: bottom up solution
 '''
 di = dict()
 def td(needle, haystack): #top-down DP with memoization
@@ -18,13 +20,13 @@ def td(needle, haystack): #top-down DP with memoization
   return ret
 
 def solve(needle, haystack):
-  print td(needle, haystack), 'hiding places'
+  return td(needle, haystack)
 
-solve('Gks', 'GeeksForGeeks')
+assert 4==solve('Gks', 'GeeksForGeeks')
 '''Req:
-Given longer and short strings Haystack vs Needle, can string-N match a sub-sequence (not necessarily substring) of string-H? You can say we are checking if string-N is hiding in string-H. If yes how many hiding places or sub-sequences can you find? 
+Given longer and short strings Haystack vs Needle, can string-N match a sub-sequence (not necessarily substring) of string-H? You can say we are checking if string-N is hiding in string-H. If yes how many hiding places (or sub-sequences) can you find? 
 
-For example, if length of string-N is 3, then each hiding place is a tuple of 3 subscripts into string-H...
+For example, if length of string-N is 3, then each hiding place is a tuple of 3 subscripts into string-H, where the 3 characters would form a string equal to string-N
 
 Eg: H = GeeksForGeeks; N = Gks. Answer is 4 subsequences i.e. 4 hiding places
 '''
