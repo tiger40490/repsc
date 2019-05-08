6b1c53d642e54bcf994c48efb447062142ae4ba9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-07 23:15:54 -0400

    minor

--- py/algo_str/editDistance.py
+++ py/algo_str/editDistance.py
@@ -2,8 +2,6 @@
 Q: is thee min() in "==" scenario needed?
 todo: fix the bottomUp algo
 todo: add tests and simplify
-
-I always designate the shorter string as aa and longer as bb. Is it necessary? Not sure, but it simplifies my thinking
 '''
 import sys
 from pprint import pprint
@@ -46,23 +44,22 @@ def topDown(aa,bb):
   print aa+' > '+bb, ret
   return ret
 def compare(aa,bb): #non-recursive
-  if len(aa) > len(bb): tmp=aa;aa=bb;bb=tmp
-  memo.clear(); td=topDown(aa,bb)
+  if len(aa) > len(bb): tmp=aa;aa=bb;bb=tmp #for printing convenience only
+  td=topDown(aa,bb); print len(memo), 'mem'; memo.clear()
   #assert     td == bottomUp(aa,bb)
   return td
   #return bottomUp(aa,bb)
-assert 3==compare("example", "samples") 
 assert 6==compare("sturgeon", "urgently")
-assert 1==compare('i', 'ai')
 assert 1==compare('islander', 'slander')
 assert 2==compare('yixin', 'yiting')
 assert 1==compare('geek', 'gesek')
 assert 3==compare('Sunday', 'Saturday')
 assert 1==compare('cat', 'cut')
-assert 3==compare('tape', 'hat')
 assert 6==compare("abcdefg", "xabxcdxxefxgx")
 assert 6==compare("levenshtein", "frankenstein")
 assert 5==compare("distance", "difference")
+assert 3==compare("example", "samples") 
+assert 3==compare('tape', 'hat')
 '''
 Req: given strings (shorter) aa and bb (longer), how many transformations from aa to bb? Return the minimum steps or "edits"?
 '''
