36f74283dadf5cb68a3c43a788a3f44fb32b9e3c
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-08 21:18:47 -0400

    major refac

--- py/algo_str/hiding_Rahul.py
+++ py/algo_str/hiding_Rahul.py
@@ -1,21 +1,17 @@
 '''
-todo: get rid of the if r==0
-todo: init matrix to 1?
 todo: more tests
-todo: add a bottom up, non-recursive solution
 
+showcase using an invalid value (0 is valid) to mark the unused region of matrix. See blogpost
 showcase one-liner to clear a dict
 showcase one-liner to assign two vars
 
 Note for empty string e, e[:-1] is itself
 '''
-from pprint import pprint
 memo = dict(); SEP = ' ^^^ '
 def td(NN, HH): #top-down DP with memoization
   sz1,sz2 = len(NN),len(HH);  tu=(sz1,sz2)
   if tu in memo: return memo[tu]
   
-  print NN + '>' + HH
   if   sz1 == sz2: ret = (1 if NN==HH else 0)
   elif sz1 == 0:   ret = 1 # optional (minor) optimization
  #elif sz1 >  sz2: ret = 0 # this check was moved into solve().. should not stay in this "hot" function
@@ -24,29 +20,42 @@ def td(NN, HH): #top-down DP with memoization
     ret = td(NN, HH[:-1]) # these hiding places don't include haystack's last char
     if NN[-1:]==HH[-1:] : ret += td(NN[:-1] , HH[:-1]) 
   memo[(sz1,sz2)] = ret
+  print NN + ' > ' + HH + ' ..', ret
   return ret
+### end of topDown; rest is bottomUp:
+mat = list()
+def pprint():
+  for r in xrange(len(mat)):
+    for c in xrange(len(mat[0])): print '%3s' % read(r,c),
+    print
+def read(r,c):
+  if r > c: return 0
+  if r==0: return 1
+  return mat[r][c]
 def bottomUp(NN,HH):
   sz1,sz2 = len(NN),len(HH);
-  mat = [[ 1 for _ in xrange(1+sz2) ] for _ in xrange(1+sz1) ]
-  for r in xrange(1+sz1):
-    mat[r][r] = (1 if NN[:r]==HH[:r] else 0)
-    for c in xrange(r+1, 1+sz2):
-      if r==0: mat[r][c] = 1; continue
-      print r,c
-      cnt = mat[r][c-1]
+  global mat; mat=[[999999 for _ in xrange(1+sz2)]for _ in xrange(1+sz1) ]
+  for r in xrange(1, 1+sz1): #inddex:=len(left substr), so loop till sz1
+    for c in xrange(r, 1+sz2):
+      mat[r][c] = read(r,c-1)
       if NN[r-1]==HH[c-1]:
-        cnt += mat[r-1][c-1]
-      mat[r][c]=cnt
-  pprint(mat)
-  return mat[-1][-1]
+        mat[r][c] += read(r-1,c-1)
+  print '     '+'  '.join(list(HH))      
+  pprint()
+  return read(-1,-1)
 def solve(needle, haystack):
   haystack = haystack.replace(' ',''); assert len(needle) <= len(haystack), "invalid input"
+  retT = td      (needle, haystack); memo.clear(); 
   retB = bottomUp(needle, haystack)
-  retT = td(needle, haystack); memo.clear(); 
+  
   assert retB==retT; print SEP,retT,SEP; return retT
-assert 6+5+3==solve('as', 'as ass asss')
-assert 6==solve('11', '1111')
+assert 6+5+3==solve('as', '0 as ass asss')
+assert 6==solve('aa', 'babbabaa')
 assert 4==solve('Gks', 'Geeks For Geeks')
+assert 0==solve('aaaa', 'bababa')
+assert 0==solve('a', 'b')
+assert 1==solve('a', 'ab')
+assert 1==solve('a', 'ba')
 # def packed(haystack):  return ''.join(haystack.split()) # less efficient than replace()
 '''Req:
 Given longer and short strings Haystack vs Needle, can string-N match a sub-sequence (not necessarily substring) of string-H? You can say we are checking if string-N is hiding in string-H. If yes how many hiding places (or sub-sequences) can you find? 
