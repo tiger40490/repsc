7b8c9a379732e64782fa7c9278bce287d8b5addf
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-12 14:19:53 -0400

    new feature

--- py/algo_combo_perm/cookbook.py
+++ py/algo_combo_perm/cookbook.py
@@ -1,8 +1,10 @@
 '''
+todo: verify all tests
 todo: understand why
-todo: add abbreviation
 todo: what if the pool has dupes?
 '''
+import sys,itertools
+
 def _com(func, items, n, logLevel=0): #internal recursive generator function
   # n == how many additional items wanted
   if n==0: yield [] # recursion end
@@ -16,10 +18,20 @@ def _com(func, items, n, logLevel=0): #internal recursive generator function
       for newList in _com(func, func(items,i), n-1, logLevel): #recurse down
         if logLevel: print indent, listLedByVal + newList
         yield listLedByVal + newList #concat 2 lists
+def abbr(word, n): #generates all abbreviations of length==n
+  def after_i_th_item(pool,i):
+    return pool[i+1:]
+  return _com(after_i_th_item, word,n,True)
+def allAbbr(word):
+  def genOrig(): yield word
+  gen = itertools.chain(genOrig())
+  for n in reversed(xrange(0,len(word))):
+    gen = itertools.chain(gen, abbr(word,n))
+  return gen
 def redraw(pool, n): # redraw n times from same pool. If pool size=55, then 55^n outcomes
   def keepAll(pool,i): return pool
-  return _com(keepAll, pool, n, True)
-def combo(pool, n):
+  return _com(keepAll, pool, n)
+def combo(pool, n): #preserves original order
   def after_i_th_item(pool,i):
     return pool[i+1:]
   return _com(after_i_th_item, pool,n)#,True)
@@ -27,7 +39,6 @@ def subsetPerm(pool, n): #accepts distinct items
   def skip_i_th_Item(pool, i):
     return pool[:i] + pool[i+1:]
   return _com(skip_i_th_Item, pool, n)# returns generator object, to be used in iteration context
-  
 def testRedraw():
   print '  v v  --  redraw  --  v v'
   fmt1, fmt2='', ''; cnt=0; unique=dict()
@@ -40,6 +51,18 @@ def testRedraw():
     unique[word]=1
   print fmt2, fmt1
   assert len(unique)==9==cnt
+def testAbbr():
+  print '  v v  --  abbr  --  v v'
+  fmt1, fmt2='', ''; cnt=0; unique=dict()
+  nonRepeat='abcd'
+  for out in allAbbr(nonRepeat): #, 3):
+    word = ''.join(out)
+    fmt2 += word + ' '
+    fmt1 += str(out)
+    cnt += 1
+    unique[word]=1
+  print fmt2, fmt1
+  assert len(unique)==2**len(nonRepeat)==cnt
 def testCombo():
   print '  v v  --  combo  --  v v'
   fmt1, fmt2='', ''; cnt=0; unique=dict()
@@ -72,6 +95,7 @@ def main():
   testPerm()
   testCombo()
   testRedraw()
+  testAbbr()
 main()
 '''based on P725 [[python cookbook]]
 I hope this helps me understand the classic generator algorithms 
