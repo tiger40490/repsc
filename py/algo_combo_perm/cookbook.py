1d3acebd00c911c7e6b08f5aa637c3482f3e7c5a
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-12 13:53:44 -0400

    add new feature

--- py/algo_combo_perm/cookbook.py
+++ py/algo_combo_perm/cookbook.py
@@ -16,6 +16,9 @@ def _com(func, items, n, logLevel=0): #internal recursive generator function
       for newList in _com(func, func(items,i), n-1, logLevel): #recurse down
         if logLevel: print indent, listLedByVal + newList
         yield listLedByVal + newList #concat 2 lists
+def redraw(pool, n): # redraw n times from same pool. If pool size=55, then 55^n outcomes
+  def keepAll(pool,i): return pool
+  return _com(keepAll, pool, n, True)
 def combo(pool, n):
   def after_i_th_item(pool,i):
     return pool[i+1:]
@@ -25,7 +28,20 @@ def subsetPerm(pool, n): #accepts distinct items
     return pool[:i] + pool[i+1:]
   return _com(skip_i_th_Item, pool, n)# returns generator object, to be used in iteration context
   
+def testRedraw():
+  print '  v v  --  redraw  --  v v'
+  fmt1, fmt2='', ''; cnt=0; unique=dict()
+  nonRepeat='abc'
+  for out in redraw(nonRepeat, 2):
+    word = ''.join(out)
+    fmt2 += word + ' '
+    fmt1 += str(out)
+    cnt += 1
+    unique[word]=1
+  print fmt2, fmt1
+  assert len(unique)==9==cnt
 def testCombo():
+  print '  v v  --  combo  --  v v'
   fmt1, fmt2='', ''; cnt=0; unique=dict()
   nonRepeat='abcd'
   for out in combo(nonRepeat, 2):
@@ -37,6 +53,7 @@ def testCombo():
   print fmt2, fmt1
   assert len(unique)==6==cnt
 def testPerm():
+  print '  v v  --  subsetPerm  --  v v'
   fmt1, fmt2='', ''; cnt=0; unique=dict()
   nonRepeat='abcd'
   for out in subsetPerm(nonRepeat, 2):
@@ -52,8 +69,9 @@ def testPerm():
   unique=dict(enumerate(li))
   assert len(unique)==24==len(li)
 def main():
-  #testPerm()
+  testPerm()
   testCombo()
+  testRedraw()
 main()
 '''based on P725 [[python cookbook]]
 I hope this helps me understand the classic generator algorithms 
