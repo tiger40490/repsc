b5be22c583578779c30b9415190860a12fa3618e
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-12 19:26:02 -0400

    refactor, minor

--- cpp/algo_slist/merge2slist_ez.cpp
+++ cpp/algo_slist/merge2slist_ez.cpp
@@ -2,42 +2,50 @@
 showcase: use ptr to indirectly advance two iterators in a single loop
 showcase: declare multiple iterator variables in for-loop header
 showcase: typedef for readability
+showcase: auto const & as a simpler variable type
 */
 #include <iostream>
 #include <iomanip>
 #include <list>
+#include <algorithm> //is_sorted
 #include <assert.h>
 using namespace std;
 typedef int item;
-list<item> li2={6,12,16,33,46,48,48,68,73,75,76,77,78};
-list<item> li1={5,11,31,34,38,50,61,66,71,74};
 template<typename T,             int min_width=2> ostream & operator<<(ostream & os, list<T> const & c){
    for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
    os<<endl;
    return os;
 }
-list<item> merge(){
-  li1.sort();  li2.sort();// should be sorted already.
+list<item> _merge(list<item> & liA, list<item> & liB){
+  assert(is_sorted(liA.begin(), liA.end()));
+  assert(is_sorted(liB.begin(), liB.end()));
   list<item> ret;
-  for (auto i1=li1.cbegin(), i2=li2.cbegin();;){
-    auto i = &(*i1 < *i2? i1: i2);
+  for (auto iA=liA.cbegin(), iB=liB.cbegin();;){
+    auto const& i = &(*iA < *iB? iA: iB);
     assert(ret.empty() || ret.back() <= **i);
     ret.push_back(**i);
     ++*i; //indirectly advance one of the iterators
-    if (i1 == li1.end()){
-      for (; i2 != li2.end(); ++i2)     ret.push_back(*i2);
-      break;
+    
+    if (iA == liA.end()){
+      ret.insert(iA, iB, liB.cend());
+      return ret; //RVO should take place. No move() please
     }
-    if (i2 == li2.end()){
-      for (; i1 != li1.end(); ++i1)     ret.push_back(*i1);
-      break;
+    if (iB == liB.end()){
+      for (; iA != liA.end(); ++iA)ret.push_back(*iA); //for-loop is easier to remember
+      return ret; //RVO should take place. No move() please
     }
   }
-  assert(ret.size() == li1.size() + li2.size());
-  return ret;
+}
+list<item> merge(list<item> & liA, list<item> & liB){
+  auto const & ret = _merge(liA, liB);
+  assert(ret.size() == liA.size() + liB.size());
+  assert(is_sorted(ret.begin(), ret.end()));
+  return ret; //RVO should take place. No move() please
 }
 int main(){
-  cout<<merge();
+  list<item> liB={6,12,16,33,46,48,48,68,73,75,76,77,78};
+  list<item> liA={5,11,31,34,38,50,61,66,71,74};
+  cout<<merge(liA, liB);
 }
 /*Req: given 2 pre-sorted slists, merge-sort them to a new slist
 */
