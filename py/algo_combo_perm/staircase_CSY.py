c502fb78323afc2d4b9a6fd9f85da6a6391887a8
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-12 23:02:47 -0400

    major improvement

--- py/algo_combo_perm/staircase_CSY.py
+++ py/algo_combo_perm/staircase_CSY.py
@@ -8,24 +8,32 @@ Note the generator function is top-down recursive without memoization.  How to m
 from pprint import pprint
 from collections import defaultdict
 uniqueGenerator=defaultdict(int)
+antiRepeat=dict() #N -> list of tuples for N
 def gen(n):
   #print 'entering gen() with n =', n
   if n==0: yield (); return
   for i in xrange(1,n+1):
-    generator = gen(n-i)
-    #print 'using generator', generator
-    uniqueGenerator[id(generator)] += 1
-    for tu in generator: 
+    arg = n-i
+    if arg in antiRepeat:
+      listOfTu = antiRepeat[arg]
+    else:
+      generator = gen(arg)
+      print arg, ': using generator', id(generator)
+      uniqueGenerator[id(generator)] += 1
+      listOfTu = list(generator)
+      antiRepeat[arg] = listOfTu
+    for tu in listOfTu: 
       yield (i,) + tu #concat two tuples
 def test(n):
   li = list(gen(n))
   uniq = set(li)
-  pprint(li)
+  #pprint(li)
   print len(uniqueGenerator), 'unique generators: ', uniqueGenerator
-  #assert len(uniqueGenerator)==n, 'each time gen(11) is called it should return the same generator object'
+  assert len(uniqueGenerator)==n, 'each time gen(11) is called it should return the same generator object'
   uniqueGenerator.clear()
+  antiRepeat.clear()
   assert len(li)==len(uniq)==2**(n-1)
-test(8)
+test(13)
 test(4)
 '''
 Req: https://bintanvictor.wordpress.com/2018/07/29/ladder-problem-csy-bbg/, which lists a c++ solution in my github
