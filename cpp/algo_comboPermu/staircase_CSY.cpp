114885563cd5a0474a0447e23607aa6d1476a175
unknown <35847749+tiger40490@users.noreply.github.com>
2019-05-13 08:04:50 -0400

    important comments

--- cpp/algo_comboPermu/staircase_CSY.cpp
+++ cpp/algo_comboPermu/staircase_CSY.cpp
@@ -19,10 +19,13 @@ template<typename T,             int min_width=8> ostream & operator<<(ostream &
 }
 using Length=int;
 using Path=vector<Length>;
-int solBFT(Length staircase){ //by myself, not CSY
+int solBFT(Length staircase){ //by myself, not CSY. Longer than the standard solution but fairly efficient and versatile technique. 
+// :( no duplicate paths, probably due to the tree structure
+// :( No memoization required because non-recursive
+// :) the total count of 2^(n-1) is hard to see
   bool isVerbose = staircase < 8;
-  queue<Path> q; q.push(Path());
-  for (int cnt=0;;++cnt){
+  queue<Path> q; q.push(Path()); //empty path
+  for (int cnt=0;;++cnt){ //a variation of while(BFT queue not empty)
     if (q.empty()){
       assert( pow(2,staircase-1) == cnt);
       cout<<staircase<<"-level staircase: returning "<<cnt<<endl;
@@ -30,12 +33,13 @@ int solBFT(Length staircase){ //by myself, not CSY
     }
     Path pa=q.front(); q.pop();
     Length gap = staircase - accumulate(pa.begin(), pa.end(), 0);
-    assert(gap > 0);
-    for (Length step=1; step<gap; ++step){ 
-      q.push(pa); //vector clone
-      q.back().push_back(step); //append on the cloned vector
+    assert(gap > 0); // gap can be saved with path, in a wrapper struct 
+    for(Length step=1; step<gap; //Note step never grows to Equal gap. Equal means a complete path
+          ++step){
+      q.push(pa); //vector clone to create new, incomplete path
+      q.back().push_back(step); //enqueue the cloned path
     }
-    pa.push_back(gap); //a complete path
+    pa.push_back(gap); //a complete path created for every pop()
     if (isVerbose){
       assert( accumulate(pa.begin(), pa.end(), 0) == staircase && "a complete path must have total length == staircase");
       cout<<":)"<<pa; 
@@ -44,9 +48,9 @@ int solBFT(Length staircase){ //by myself, not CSY
 }
 
 int main(){
-  solBFT(4);
   solBFT(5);
   solBFT(7);
   solBFT(13);
+  solBFT(4);
 }/*Req: https://wp.me/p74oew-61P count how many ways to climb a staircase of length N
 */
