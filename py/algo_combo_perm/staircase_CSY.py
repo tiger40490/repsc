ed120bcaf48635fe41cf943aba4a17042a12fa57
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-12 20:05:48 -0400

    new code

---
+++ py/algo_combo_perm/staircase_CSY.py
@@ -0,0 +1,19 @@
+'''
+showcase: yielding items in sorted sequence
+'''
+from pprint import pprint
+def gen(n):
+  if n==0: yield (); return
+  for i in xrange(1,n+1):
+    for tu in gen(n-i): 
+      yield (i,) + tu #concat two tuples
+def test(n):
+  li = list(gen(n))
+  uniq = set(li)
+  pprint (li)
+  assert len(li)==len(uniq)==2**(n-1)
+test(4)
+test(7)
+'''
+Req: https://bintanvictor.wordpress.com/2018/07/29/ladder-problem-csy-bbg/, which lists a c++ solution in my github
+'''
