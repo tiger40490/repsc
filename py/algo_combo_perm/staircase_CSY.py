5c2f4959b63caa877c2db1c71930f0ef74f903d9
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-15 23:25:04 -0400

    new algo

--- py/algo_combo_perm/staircase_CSY.py
+++ py/algo_combo_perm/staircase_CSY.py
@@ -11,17 +11,29 @@ Note the generator function is top-down recursive without memoization.  How to m
 '''
 from pprint import pprint
 from collections import defaultdict
+import time, os, sys
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # worked
+                                         
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print '%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts)
+        return result
+    return timed
 uniqueGenerator=defaultdict(int)
 antiRepeat=dict() #N -> list of tuples for N
-def genenrateWithMemoization(n):
-  #print 'entering genenrateWithMemoization() with n =', n
+def gen_memoize(n): #consistently faster than buttom_up, much faster than the simple gen()
+  #print 'entering gen_memoize() with n =', n
   if n==0: yield (); return
   for i in xrange(1,n+1):
     arg = n-i
     if arg in antiRepeat:
       listOfTu = antiRepeat[arg]
     else:
-      generator = genenrateWithMemoization(arg)
+      generator = gen_memoize(arg)
       #print arg, ': using generator', id(generator)
       uniqueGenerator[id(generator)] += 1
       listOfTu = list(generator)
@@ -33,21 +45,30 @@ def gen(n): #simple generator, easy to memorize and reproduce
   for step1 in xrange(1, n+1):
     for steps in gen(n-step1):
       yield (step1,) + steps
-def bottomUp(n): #produce a collection of tuples, each a path
-  pass
+def buttom_up(n): #produce a collection of tuples, each a formula
+  ret = [ [] for i in range(n+1) ]
+  ret[0] = [()] # crucial
+  for height in xrange(1, n+1): 
+    formulas=ret[height]; # assert id(formulas) == id(ret[height])
+    for step1 in xrange(1, height+1) : #step1 can be size 1 to size height
+      for path in ret[height-step1] :
+        formulas.append(  (step1,) + path )
+    #print 'height =',height, formulas
+  return ret[n]
+@timeit
 def verify(n, algo):
   li = list(algo(n)); uniq = set(li)
   if n < 5: pprint(li) # all paths
   assert len(li)==len(uniq)==2**(n-1)
-  
 def test(n):
+  verify(n, buttom_up)
+  verify(n, gen_memoize)
   verify(n, gen)
-  verify(n, genenrateWithMemoization)
   # now check the generators
   #print len(uniqueGenerator), 'unique generators: ', uniqueGenerator
   assert len(uniqueGenerator)==n, 'each time gen(11) is called it should return the same generator object'
   uniqueGenerator.clear(); antiRepeat.clear()
-test(13)
+test(21) #21 ok
 test(4)
 '''
 Req: https://bintanvictor.wordpress.com/2018/07/29/ladder-problem-csy-bbg/, which lists a c++ solution in my github
