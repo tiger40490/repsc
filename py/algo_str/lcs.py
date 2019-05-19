0bbfc34adf443ef7742d2a249d3fb0ccc202b218
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-19 00:18:04 -0400

    minor refactor

--- py/algo_str/lcs.py
+++ py/algo_str/lcs.py
@@ -8,26 +8,28 @@ from pprint import pprint
 from copy import deepcopy
 from collections import defaultdict
 from itertools import izip
+def read(r,c): return a2[ (r,c) ] #easier to read
 def bu():
   global m; m=[['.' for _ in range(len(y))] for _ in range(len(x))]
+  global a2; 
   a2 = defaultdict(lambda: [[],[]])
   a2[(0, 0)] =  [[0],[0]] if x[0]==y[0]   else [[],[]]
-  m  [0][0] = len(a2[(0,0)])
+  m  [0][0] = len(read(0,0))
   for r in xrange(len(x)): 
    for c in xrange(len(y)):
      #print r,c
-     if len( a2[(r-1,c)][0] )  > len( a2[(r,c-1)][0] ):
-       det = deepcopy(a2[(r-1,c)])
+     if len( read(r-1,c)[0] )  > len( read(r,c-1)[0] ):
+       det = deepcopy(read(r-1,c))
      else:
-       det = deepcopy(a2[(r,c-1)])
+       det = deepcopy(read(r,c-1))
      if x[r] == y[c]:
-         det=deepcopy(a2[(r-1,c-1)]) #
-         #det=a2[(r-1,c-1)]
+         det=deepcopy(read(r-1,c-1)) #
+         #det=read(r-1,c-1) #working?
          det[0].append(r)
          det[1].append(c)
      a2[(r,c)] = det
      m[r][c] = len(det[0])
-  for X,Y in izip(*(a2[(r,c)])): assert x[X] == y[Y]; print X, x[X], Y
+  for X,Y in izip(*read(r,c)): assert x[X] == y[Y]; print X, x[X], Y
   pprint(m)
   return m[r][c]
 def cleanup(dirtyX,dirtyY):
