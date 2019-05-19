90d279fb64378e152ef942f0cec058e3e9c1ed69
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-19 00:25:41 -0400

    minor

--- py/algo_str/lcs.py
+++ py/algo_str/lcs.py
@@ -1,38 +1,38 @@
 '''
-todo: shorten
-todo: check if deepcopy needed
-
+showcase defaultdict(lambda...) to easily handle negative indices
 showcase passing a singe list object into izip() which expects two args
+showcase deepcopy
 '''
+import sys
 from pprint import pprint
 from copy import deepcopy
 from collections import defaultdict
 from itertools import izip
 def read(r,c): return a2[ (r,c) ] #easier to read
 def bu():
-  global m; m=[['.' for _ in range(len(y))] for _ in range(len(x))]
-  global a2; 
-  a2 = defaultdict(lambda: [[],[]])
+  m=[['.' for _ in range(len(y))] for _ in range(len(x))]
+  # Above 2D array is easy to print
+  # Below defaultdict easily handles negative indices
+  global a2; a2 = defaultdict(lambda: [[],[]])
   a2[(0, 0)] =  [[0],[0]] if x[0]==y[0]   else [[],[]]
   m  [0][0] = len(read(0,0))
   for r in xrange(len(x)): 
    for c in xrange(len(y)):
      #print r,c
      if len( read(r-1,c)[0] )  > len( read(r,c-1)[0] ):
-       det = deepcopy(read(r-1,c))
+       match = deepcopy(read(r-1,c))
      else:
-       det = deepcopy(read(r,c-1))
+       match = deepcopy(read(r,c-1))
      if x[r] == y[c]:
-         det=deepcopy(read(r-1,c-1)) #
-         #det=read(r-1,c-1) #working?
-         det[0].append(r)
-         det[1].append(c)
-     a2[(r,c)] = det
-     m[r][c] = len(det[0])
+         match=deepcopy(read(r-1,c-1)) #
+         match[0].append(r)
+         match[1].append(c)
+     a2[(r,c)] = match
+     m[r][c] = len(match[0])
   for X,Y in izip(*read(r,c)): assert x[X] == y[Y]; print X, x[X], Y
-  pprint(m)
+  if len(m[0]) < 15: pprint(m)
   return m[r][c]
-def cleanup(dirtyX,dirtyY):
+def wrapper(dirtyX,dirtyY):
   global x,y
   x = dirtyX.replace(' ','')
   y = dirtyY.replace(' ','')
@@ -43,10 +43,11 @@ def cleanup(dirtyX,dirtyY):
     print '\n'
   return bu()
 def main():
-  assert 10==cleanup('abbbbb-bbbbbb-a', 'aabbaabaababbabaaba')
-  assert 4==cleanup('AGGTAB', 'GXTXAYB')
-  assert 3==cleanup('ABCDGH','AEDFHR')
-  assert 7==cleanup('thisisatest', 'testing123testing')
+  assert 4==wrapper('AGGTAB', 'GXTXAYB')
+  assert 3==wrapper('ABCDGH','AEDFHR')
+  assert 7==wrapper('thisisatest', 'testing123testing')
+  assert 10==wrapper('abbbbb-bbbbbb-a', 'aabbaabaababbabaaba')
+  assert 22==wrapper('abb--bbbb-bbbb-bbbb aaaaa bbbb aaaaaa', 'aabbabbaabaabaaababababaabaababbabaaba')
 main()
 '''Req: longest common subsequence (not sub-string) between two strings X and Y
 '''
