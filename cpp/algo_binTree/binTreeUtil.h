99e20404e0ce6a8c2e9c0ef621b84b90e596b1b0
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2019-05-20 23:28:15 -0400

    comment

--- cpp/algo_binTree/binTreeUtil.h
+++ cpp/algo_binTree/binTreeUtil.h
@@ -1,3 +1,4 @@
+//todo: indent can be a non-type template param
 #include <iostream>
 #include <iomanip>
 
@@ -14,4 +15,5 @@ void dumpTreeNode(Node const * n, size_t const indent=4, size_t const depth=0){
     std::cout<<  "       ============  ^ ^ ^ ^\n";
   }
 }/*Based on P458 [[data structure and other objects using c++]]
+Can this function serialize/deserialize a binary tree? I think it can.
 */
