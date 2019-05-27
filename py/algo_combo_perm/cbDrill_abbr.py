dc2c5ac01fe732e9d2c7574f9fd25dce7ff90e93
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-05-28 07:58:33 +0800

    new drill

---
+++ py/algo_combo_perm/cbDrill_abbr.py
@@ -0,0 +1,13 @@
+def gen(fn, pool, n):
+  if n == 0: yield []; return
+  for pos, val in enumerate(pool):
+    for newList in gen(fn, fn(pool,pos), n-1):
+      yield [val] + newList
+	  
+pool='abcde'
+def f(pool, pos):
+  return pool[pos+1:]
+for length in xrange(1,len(pool)):
+  for abbr in gen(f, pool, length):
+    print abbr
+# all abbr
