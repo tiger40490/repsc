57fb59601ad3e66438a6c3e15963accf49748afd
unknown <35847749+tiger40490@users.noreply.github.com>
2019-06-18 07:22:47 -0400

    clean up

--- py/88lang/asteriskArg.py
+++ py/88lang/asteriskArg.py
@@ -1,15 +1,21 @@
+'''I think the ** operator unpacks a dictionary
+'''
 def simpleFunc(age, ssn, grade, status=''):
-  print age, ssn, grade
+  print 'in simpleFunc()', age, ssn, grade, status
 def kwFunc(**kwa):
   print kwa
   
   
 di={'age': 44, 'ssn': '123-45-6789', 'grade': 'A+'} 
-status='GC'
-rank='ED' 
 simpleFunc(**di) # we CAN pass kwargs to a simple function
+d2={'age': 44, 'ssn': '123-45-6789', 'grade': 'A+', 'status': 'GC'} 
+simpleFunc(**d2)
+
 #simpleFunc(**locals()) #all locals must be declared as named parameters in simpleFunc() .. impractical
+
+print '----'
+
 kwFunc(**di)
-kwFunc(**locals())
-'''Testing asterisk args
+kwFunc(**locals()) # fine
+'''Testing a few usages of asterisk args
 '''
