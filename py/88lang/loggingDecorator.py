697dfdee5bb5802aff4c25c3e188e941dc2aedac
unknown <35847749+tiger40490@users.noreply.github.com>
2019-06-18 07:12:35 -0400

    comments

--- py/88lang/loggingDecorator.py
+++ py/88lang/loggingDecorator.py
@@ -1,3 +1,7 @@
+'''
+I don't feel comfortable with the magic here. I criticized my son for 不求甚解,照猫画虎.
+However, Now I feel No need to ask about underlying / internals. This domain is really GTD, not QQ. Unlike c++/java, Few python interviewer will treat it as QQ
+'''
 def log3(funcOrMsg=None, named_arg=None):
     '''arg is Optional. You can use any of:
     @log3(named_arg='specific msg') # some prefer named argument for clarity
@@ -8,7 +12,7 @@ def log3(funcOrMsg=None, named_arg=None):
     arg1_isStr      = isinstance(funcOrMsg, basestring)
     arg1_isNone     = funcOrMsg is None
  
-    def decorated(func):
+    def decorated(func): #returns a decorated func
  
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -18,9 +22,11 @@ def log3(funcOrMsg=None, named_arg=None):
               extra={'name_override' : func.__name__})
               ### set name_override to func.__name__ in a kwarg to info()
             return func(*args, **kwargs)
-        return wrapper
-        ## end wrapper
- 
+        ## end of wrapper() func definition
+        
+        return wrapper # decorated() unconditionally returns this callable functor
+    ## end of decorated() func definition
+    
     if arg1_isCallable:
         return decorated(funcOrMsg) # decorator received no-arg
     else:
