89dc86db423c85159b5ad2de393b71132794e241
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-06-22 14:08:21 -0400

    use templ-templ param

--- cpp/lang_33template/containerOutputOperator.cpp
+++ cpp/lang_33template/containerOutputOperator.cpp
@@ -1,4 +1,6 @@
 //todo: use enable_if or something else to disallow std::string
+//showcase: templ-templ param
+//Showcase: non-dummy-type template param min_width.
 #include <map>
 #include <unordered_map>
 #include <set>
@@ -6,7 +8,6 @@
 #include <list>
 #include <vector>
 //
-// Showcase non-dummy-type template param min_width.
 // Note body of map ^ vector dumpers are identical, except some optional features.
 // Below is minimum code block for copy-paste.
 #include <iostream>
@@ -27,29 +28,27 @@ template<typename K, typename V, int min_width=8> ostream & operator<<(ostream &
    os<<endl;
    return os;
 }
-template<typename T,             int min_width=8> ostream & operator<<(ostream & os, vector<T> const & c){
-   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
-   os<<endl;
-   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
-   os<<endl;
-   return os;
-}
-template<typename T,             int min_width=2> ostream & operator<<(ostream & os, list<T> const & c){
-   for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
-   os<<endl;
-   return os;
-}
 template<typename T,             int min_width=2> ostream & operator<<(ostream & os, Set<T> const & c){
    for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
    os<<endl;
    return os;
 }
-
+template<  template<typename,class> class Container, 
+typename V, class Alloc, int min_width=8>
+std::ostream& operator<<(std::ostream& os, Container<V, Alloc> const& c){
+   // takes any (container or otherwise) template having 2 templ params, but not maps (4) or sets (3)
+   for(auto const & it : c){ os<<setw(min_width)<<it<<" "; }
+   os<<endl;
+   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
+   os<<endl;
+   return os;
+}
 int main(){
   list<double> li={3.1, 5,2,9};
   Set<string> myset={"s1", "s2", "s2", "s1"};
   vector<string> vec={"dabao", "meimei", "princess"};
   Map<int, int> tm={{1,11}, {2,22}};
   ss<<vec<<tm;
+  ss<<" <-- vector and map; list and set -->\n";
   ss<<li<<myset;
 }
