6a501c863f92df4127c7d9577a62ba3827391fa1
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-06-23 15:50:56 -0400

    trivialRefac

--- py/algo_combo_perm/1fromEachSet.py
+++ py/algo_combo_perm/1fromEachSet.py
@@ -1,24 +1,25 @@
 '''
 showcase passing generator output into another generator function. 
-If you try to print list(theGenerator) for debugging you would inadvertently exhaust all the items. 
+If you try to print list(theGenerator) for debugging, you would inadvertently exhaust all items. 
 So It's better to construct a list from the generator output and pass in the list. 
 
-showcase *args
+showcase *args: passing, iterating, printing
 '''
 def gen(lists, nextset):
   #print 'in gen()', lists, nextset
   for b in nextset:
     for accu in lists:
       yield accu + str(b)	
-def dp(*pool): #N set of strings, all unique
-  print 'entering dp() with', pool
+def solve(*pool): #N set of strings, all unique
+  print 'entering solve() with', pool
   output=list([''])
-  for li in pool:
-    output = list(gen(output, set(li)))
+  for se in pool: # for each set in pool
+    output = list(gen(output, set(se)))
   print 'returning', output
   return output #collection of concatenated strings
+
 def test(exp, *args):
-  output = dp(*args)
+  output = solve(*args)
   assert len(output) == exp
 test(18,  ['aa','ab', 'bb'], [11,12,22], ['x','xx'] )
 
