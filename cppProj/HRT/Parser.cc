58a785e25adc305d9bb75a859648b6ce58bf7875
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-07-07 05:22:57 +0000

    major

--- cppProj/HRT/Parser.cc
+++ cppProj/HRT/Parser.cc
@@ -2,13 +2,27 @@
 
 #include <string>
 #include <cstdio>
+#include <arpa/inet.h>
+#include <iostream>
+#include <deque>
+using namespace std;
 
-Parser::Parser(int date, const std::string &outputFilename)
-{
+Parser::Parser(int date, const std::string &outputFilename) {
 }
 
-void Parser::onUDPPacket(const char *buf, size_t len)
-{
+struct PacketHeader{
+  mutable uint16_t sz;
+  mutable uint32_t seq;
+  PacketHeader const * cleanup() const{
+     sz = ntohs(sz);
+     seq = ntohs(seq);
+     return this;
+  }
+};
+void Parser::onUDPPacket(const char *buf, size_t len) {
     printf("Received packet of size %zu\n", len);
+    // optionally validate len
+    PacketHeader const * hdr = reinterpret_cast<PacketHeader const*>(buf)->cleanup();
+    cout<<"pkg hdr sz = "<<hdr->sz<<" , seq = "<<hdr->seq<<endl;
 }
 
