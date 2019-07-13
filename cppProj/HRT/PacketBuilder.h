787cf5ea382bf8ae470987022bdfe1d8580be3d2
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-07-13 05:52:56 +0000

    new feature

--- cppProj/HRT/PacketBuilder.h
+++ cppProj/HRT/PacketBuilder.h
@@ -1,18 +1,26 @@
 #pragma once
 #include "OrderMsg.h"
+#include "PacketHeader.h"
 
 //This class is only used for generating test packets, not production
 class PacketBuilder{
   char buf[65536] ; //big enough for a UDP packet. no need to initialize
-  short offset=6; //for the packet header
+  short offset= sizeof(PacketHeader);
+
 public:
   template<class T,   typename... Args>
   char* fakeMsg(Args... args){
     char* ret = T::fakeMsg(args...);
     size_t sz = sizeof(T);
-    
     memcpy(buf + offset, ret, sz);
     dumpBuffer(buf + offset, sz, "builder's buffer");
     offset += sz;
+    return ret; //not in use
+  }
+
+  char pack_n_send (Parser * parser, short seq){
+    char* hdr = PacketHeader::fakeMsg(this->offset,seq);
+    memcpy(buf, hdr, sizeof(PacketHeader));
+    parser->onUDPPacket(this->buf, this->offset);
   }
 };
--- cppProj/HRT/PacketHeader.h
+++ cppProj/HRT/PacketHeader.h
@@ -9,4 +9,15 @@ struct PacketHeader{
      seq = betoh(seq);
      return this;
   }
+
+  static char* fakeMsg(uint16_t h_pktSz, uint32_t h_seq){ //h_ means in host endianness
+    static size_t const sz=sizeof(PacketHeader);
+    static PacketHeader msg;
+    msg.sz = htobe(h_pktSz);
+    msg.seq = htobe(h_seq);
+    char* ret = reinterpret_cast<char*>(&msg);
+    //dumpBuffer(ret, sz, "serialized fake Packet Header");
+    return ret;
+  }
+
 } __attribute__((packed));
