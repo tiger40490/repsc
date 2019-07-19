dff33a42229306721e6eff4a5c9fb962db51df3a
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-07-18 20:54:53 -0400

    working now

--- py/88miscIVQ/itr_K_presortedLists_FB.py
+++ py/88miscIVQ/itr_K_presortedLists_FB.py
@@ -1,40 +1,46 @@
+'''
+todo: add asserts
+'''
 from heapq import *
+'''
 class Item:
   def __init__(self, payload, whichList):
     self.v = payload
     self.lid = whichList
     #self.idx .. not needed, due to ii
-a = [[1,2,5,7],[1,2,3,5],[2,4,6,8],[3,5,7]]
+'''
+a = [[1,3,5,7],[0],[2,3,5],[0,2,4,6,8],[17]]
 k=len(a)
 ii = [0] * k # k internal iterators
 heap = []
+cnt=0
 def next():
+ while True: #needed by yield
+  if len(heap) == 0: return
   ret = heappop(heap)
-  lid = ret.lid
+  lid = ret[1]
   picked = a[lid]
   if ii[lid] < len(picked) - 1:
-     ii[lid] += 1
-     heappush(heap, Item(picked[ii[lid]], lid) )
-     # note if ii[55] is already at the last item of a[55], then this list will not get picked up again
-  return ret.v
+    ii[lid] += 1
+    heappush(heap, (picked[ii[lid]], lid) )
+    # note if ii[55] is already at the last item of a[55], then this list will not get picked up again
+  yield ret[0]
 def init():
-  for i in xrange(k):
-    heap.append ( Item(a[i][0], i) )
+  global cnt
+  for w in xrange(k): # w=whichList
+    heap.append ( (a[w][0], w) )
+    cnt += len(a[w])
   heapify(heap)
 def hasNext():
   return len(heap)>0
-'''Requirement: given K pre-sorted immutable lists, each up to N items, return an iterator that yields all (up to K*N) items sorted sequence
+def test():
+  init()  
+  output = [ i for i in next() ]
+  print output
+  assert output == sorted(output)
+  assert len(output) == cnt
+test()
+'''Requirement: https://bintanvictor.wordpress.com/wp-admin/post.php?post=31626&action=edit
 
-I first proposed pair-wise merge. Since there are logK passes,
-Time complexity = O(NK logK)
-Very first pass i must create a single list up to NK items. then I can reuse this list in each merge. so
-space complexity = NK, but I said NK*logK
-
-Then interviewer Richard suggested iterating over the K lists so I implemented the solution here.
-Space complexity = K
-Time complexity:
-next() O(logK) due to popping
-init() O(K)
-hasNext() O(1)
-
+Q: given K pre-sorted immutable lists, each up to N items, return an iterator that on demand yields each of the (up to K*N) items in sorted sequence.
 '''
