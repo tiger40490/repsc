5993c580115a3a18e30afd7b11aa8978858d3501
unknown <35847749+tiger40490@users.noreply.github.com>
2019-07-23 21:22:09 -0400

    minor

--- py/0cleanUtil4iv/randomGenUtil.py
+++ py/0cleanUtil4iv/randomGenUtil.py
@@ -2,14 +2,17 @@ import sys, os, random; from pprint import pprint
 
 ''' simple utility for timed coding tests.
 
-random.shuffle() is easier to remember than random.sample() !
+random.shuffle() is easier to remember than random.sample() ! Some complexities are handled by range()
 random.shuffle() can handle dupe items, as demonstrated in "extend" line
+
+Function returns distinct items if cnt is not too high. 
 '''
-def randomGen(cnt, maxOutputVal, min=0):
-  assert cnt <= maxOutputVal-min+1 #optional input validation
+def randomGen(cnt, maxOutputVal, min=1):
   ret=range(min, maxOutputVal+1, 1);
-  # ret.extend(ret); print ret #add the same items
-  random.shuffle(ret); return ret[:cnt]
+  for i in xrange(cnt/(maxOutputVal-min+1)):
+    ret.extend(ret); 
+  #print len(ret), 'items in pool:', ret
+  ret = ret[:cnt]; random.shuffle(ret); return ret
   
-print randomGen(5,9)
+print randomGen(9,9)
 print randomGen(5,4)
