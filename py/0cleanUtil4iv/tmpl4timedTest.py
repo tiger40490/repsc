cb18260eb00a5780c3665010482c310312445343
unknown <35847749+tiger40490@users.noreply.github.com>
2019-07-31 19:01:37 -0400

    minor

--- py/0cleanUtil4iv/tmpl4timedTest.py
+++ py/0cleanUtil4iv/tmpl4timedTest.py
@@ -6,16 +6,17 @@ print li.pop(), '  ', li
 li.insert(0,-55); print li #insert at 0th position
 li.remove(-55) #remove an item equal to -55, assuming it exists
 del li[0] ; print 'del[0]:',li # remove by index
-print 'sum =',sum(li)
+print 'sum =', sum(li)
 del li[:]; print li #wipe clean in-place
 ## dict init:
-di={1:'j', 3+2:'a'}; 
-#di=dict(red=1, blue=2) # no quoting required
-for k,v in di.items(): print v,k
-print di.keys()
-# di.pop(
-#from collections import defaultdict
-
+di=dict(red=1, blue=2) # no quoting required
+for k,v in di.items(): print k,v
+di={1:'j', 3+2:'a'}; # supports computed key
+for k,v in di.items(): print k,v
+print di.keys(), di.values()
+from collections import defaultdict as ddict
+di=ddict(lambda: [[2,3],[5,4]])
+print di['invalidKey']
 
 ## matrix
 width,height=3,2
