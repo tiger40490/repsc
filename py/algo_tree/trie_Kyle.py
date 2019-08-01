9d024f1e4202e88a79826a4cd6bbea677b5c8b12
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-08-01 19:11:54 -0400

    new file

---
+++ py/algo_tree/trie_Kyle.py
@@ -0,0 +1,27 @@
+class Trie:
+  def __init__(self):
+    # up to 26 sub-tries
+    self.letters = dict()
+
+  def isLetterIn(self, letter):
+    return letter in self.letters
+
+  def addLetter(self, letter):
+    if not self.isLetterIn(letter):
+      # hang a new empty trie off host node
+      self.letters[letter] = Trie()
+
+  def addWord(self, word):
+    if word:
+      ch = word[0]
+      self.addLetter(ch)
+      self.letters[ch].addWord(word[1:])
+  def __repr__(self):
+    return str(self.letters)
+
+baseTrie = Trie()
+baseTrie.addWord("dogs")
+baseTrie.addWord("doc")
+print(baseTrie.letters["d"].letters["o"])
+print(baseTrie.letters["d"])
+print(baseTrie)
