196981c7a7be3ce9c4dc4b99c82ada7862ed0eac
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-08-04 18:54:54 -0400

    add new feature

--- cpp/algo_binTree/subTreeStats.cpp
+++ cpp/algo_binTree/subTreeStats.cpp
@@ -1,19 +1,26 @@
+/*
+showcase comparing 3 numbers
+*/
 #include "binTreeUtil.h" //tree builder
 #include <iostream>
+#include <algorithm> //max( {1,2,3} )
 #include <unordered_map>
 using namespace std;
 
 auto root = Node<>::preOrderBuildTree({{6, 1.1},
-{3, 2.2},{1, 4.4},{2,9.9},{5, 5.5},{4, 10}, //left subtree
+{3,-2.2},{1,  -4},{2,0.9},{5, 5.5},{4, -10}, //left subtree
 {9, 3.3},{7, 6.6},{8, 13},{11,7.7},{10,14},{12, 15}}); //right subtree
 
-struct Stat{
+struct SubtreeStat{
   size_t size=0, height=0;
-  size_t maxPathSumLe=0, maxPathSumRi=0; //max path sum to any leaf
+  double maxPathSum=0; /*max path sum from here to 
+  any subtree node. non-empty path, so sum can be neg*/
 };
 
+typedef int InOrderId;
+static unordered_map<InOrderId, SubtreeStat> testing;
 void postOrder(Node<> * n){
-  static unordered_map<Node<>*, Stat> stat={{nullptr, Stat()}}; 
+  static unordered_map<Node<>*, SubtreeStat> stat={{nullptr, SubtreeStat()}}; 
   static int lvl=0;
   ++lvl;
   if (n->le){ postOrder(n->le); }
@@ -24,10 +31,17 @@ void postOrder(Node<> * n){
                 + stat[n->ri].size;
   vo.height = 1+max(stat[n->le].height, 
                     stat[n->ri].height);
-  cout<<n->data<<"\theight= "<<vo.height<<" size= "<<vo.size<<" level= "<<lvl<<endl;
+  vo.maxPathSum = max({stat[n->le].maxPathSum,
+                       stat[n->ri].maxPathSum, 0.0}) + n->data;
+  cout<<n->data<<"\tmax_path_sum_from_me = "<<vo.maxPathSum<<"  height= "<<vo.height<<" size= "<<vo.size<<" level= "<<lvl<<endl;
+  testing[n->id]=vo;
   --lvl;
 }
 int main(){
     dumpSubTree(root);
     postOrder(root);
+    assert(testing[1].maxPathSum==-3.1);
+    assert(testing[3].maxPathSum==3.3);
+    assert(testing[3].size==5);
+    assert(testing[6].size==12); //root
 }
