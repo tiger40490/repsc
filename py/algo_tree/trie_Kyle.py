9335868335a2a865b0651f72440b722c15f894d6
unknown <35847749+tiger40490@users.noreply.github.com>
2019-08-03 21:21:59 -0400

    add major new feature

--- py/algo_tree/trie_Kyle.py
+++ py/algo_tree/trie_Kyle.py
@@ -1,37 +1,42 @@
 '''
 todo: add tests
-
+feature: use explicit sentinel to enhance clarity and reduce ambiguity
 '''
 class Trie:
   def __init__(self):
     # up to 26 sub-tries
-    self.letters = dict()
+    self.nextLetters = dict() #letter->sub-trie
   def hasThisLetter(self, letter):
-    return letter in self.letters
+    return letter in self.nextLetters
   def addLetter(self, letter):
     if not self.hasThisLetter(letter):
       # hang a new empty trie off host node
-      self.letters[letter] = Trie()
+      self.nextLetters[letter] = Trie()
   def addWord(self, word):
       if len(word)==0: return
       ch = word[0]
       self.addLetter(ch)
-      self.letters[ch].addWord(word[1:])
-  def search(self, wordd):
-    if len(word)==0: return True #????
-    if hasThisLetter(word[0]): 
-      return self.letters(word[0]).search(word[1:])
+      self.nextLetters[ch].addWord(word[1:])
+  def isSentinel(self):
+    return 0==len(self.nextLetters)
+  def search(self, word):
+    if len(word)==0: return self.isSentinel()
+    print 'seeking', word, 'among', self.nextLetters.keys()
+    if not self.hasThisLetter(word[0]): return False
+    return self.nextLetters[word[0]].search(word[1:])
   def __repr__(self):
-    return str(self.letters)
+    return 'SEN ' if self.isSentinel() else str(self.nextLetters)
 
 inst = Trie()
 inst.addWord("dogs")
 inst.addWord("doc")
-print(inst.letters["d"].letters["o"])
-print(inst.letters["d"])
+print(inst.nextLetters["d"].nextLetters["o"])
+print(inst.nextLetters["d"])
 print(inst)
-#assert inst.search("doc")
-#assert not inst.search("do")
+assert inst.search("doc")
+assert not inst.search("dog")
+assert not inst.search("do")
+assert not inst.search("o")
 '''
 simple trie to represent a collection of words
 '''
