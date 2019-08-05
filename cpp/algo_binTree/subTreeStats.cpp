9b86060051a83627927e92f62e572f45c5411b03
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-08-04 22:36:44 -0400

    add level support

--- cpp/algo_binTree/subTreeStats.cpp
+++ cpp/algo_binTree/subTreeStats.cpp
@@ -13,6 +13,7 @@ auto root = Node<>::preOrderBuildTree({{6, 1.1},
 
 struct SubtreeStat{
   size_t size=0, height=0;
+  size_t level=1; //root defines level 1
   double maxPathSum=0; /*max path sum from here to 
   any subtree node. non-empty path, so sum can be neg*/
 };
@@ -21,21 +22,23 @@ typedef int InOrderId;
 static unordered_map<InOrderId, SubtreeStat> testing;
 void postOrder(Node<> * n){
   static unordered_map<Node<>*, SubtreeStat> stat={{nullptr, SubtreeStat()}}; 
-  static int lvl=0;
-  ++lvl;
-  if (n->le){ postOrder(n->le); }
-  if (n->ri){ postOrder(n->ri); }
-  
   auto & vo = stat[n];
+  if (n->le){
+    stat[n->le].level = vo.level + 1;
+    postOrder(n->le); 
+  }
+  if (n->ri){ 
+    stat[n->ri].level = vo.level + 1;
+    postOrder(n->ri); 
+  }
   vo.size   = 1 + stat[n->le].size 
                 + stat[n->ri].size;
   vo.height = 1+max(stat[n->le].height, 
                     stat[n->ri].height);
   vo.maxPathSum = max({stat[n->le].maxPathSum,
                        stat[n->ri].maxPathSum, 0.0}) + n->data;
-  cout<<n->data<<"\tmax_path_sum_from_me = "<<vo.maxPathSum<<"  height= "<<vo.height<<" size= "<<vo.size<<" level= "<<lvl<<endl;
+  cout<<n->data<<"\tmax_path_sum_from_me = "<<vo.maxPathSum<<"  height= "<<vo.height<<" size= "<<vo.size<<" level= "<<vo.level<<endl;
   testing[n->id]=vo;
-  --lvl;
 }
 int main(){
     dumpSubTree(root);
@@ -43,5 +46,6 @@ int main(){
     assert(testing[1].maxPathSum==-3.1);
     assert(testing[3].maxPathSum==3.3);
     assert(testing[3].size==5);
+    assert(testing[11].level=3);
     assert(testing[6].size==12); //root
 }
