b3a98b3e6c205b21e3b457526499d68425285313
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-08-10 10:05:13 -0400

    clean up refac

--- py/algo_str/cntSerialize.py
+++ py/algo_str/cntSerialize.py
@@ -1,24 +1,21 @@
-def do(raw): #returns a compact form
-  raw = raw.replace(' ','')
+def compact(raw): #returns a compact form
+  raw = raw.replace(' ','')+' ' #space as terminator
   if raw == '': return raw
-  vec=[raw[0]]
+  vec=[]
   repeat=1
   for i,ch in enumerate(raw):
-    if i==0: continue
-    if raw[i-1] == ch: repeat += 1; continue
+    if i==0: continue #tolerable code smell
+    if     raw[i-1] == ch: repeat += 1; continue
     assert raw[i-1] != ch
-    vec.append(str(repeat))
-    vec.append(ch)
+    vec.extend( [raw[i-1], repeat] )
     repeat = 1
-  vec.append(str(repeat))
-  ret = ''.join(vec)
-  print ret
-  return ret
+  return ''.join(str(it) for it in vec)
 
-assert do('ab')  == 'a1b1'
-assert do('abbc')== 'a1b2c1'
-assert do('abb') == 'a1b2'
-assert do('aab') == 'a2b1'
+assert compact('ab')  == 'a1b1'
+assert compact('abbc')== 'a1b2c1'
+assert compact('abb') == 'a1b2'
+assert compact('aab') == 'a2b1'
+assert compact('a') == 'a1'
 
 '''Req: serialize a string by saving repetition count
 clean, readable code
