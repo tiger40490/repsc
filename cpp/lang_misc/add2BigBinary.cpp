9d559d8dd5260599abd3406d27c9aa3a64bdd494
unknown <35847749+tiger40490@users.noreply.github.com>
2019-08-20 22:22:47 -0400

    minorRefac

--- cpp/lang_misc/add2BigBinary.cpp
+++ cpp/lang_misc/add2BigBinary.cpp
@@ -1,13 +1,18 @@
 /*
-Note unsigned char is harder to print than uint8_t
+todo: uint4_t?
+
+showcase: type alias to seamlessly swap in uint8_t vs Byte
+
+Note unsigned char is harder to print than uint8_t. Also more ambiguous
 */
 #include <vector>
 #include <iostream>
 #include <iomanip>
 #include <cassert>
 using namespace std;
+using Byte=uint8_t;
 size_t sz=0;
-ostream & operator<<(ostream & os, unsigned char const * x){
+ostream & operator<<(ostream & os, Byte const * x){
   // can't pass array size into operator<<(), so I use a global var
    for(size_t i=0; i<sz; ++i){ 
      unsigned int tmp = x[i];   
@@ -26,8 +31,8 @@ std::ostream& operator<<(std::ostream& os, Container<V, Alloc> const& c){
    os<<endl;
    return os;
 }
-void add(unsigned char *A, size_t const lenA,
-         unsigned char *B, size_t const lenB){
+void add(Byte *A, size_t const lenA,
+         Byte *B, size_t const lenB){
   assert (lenA >= lenB && "if A is shorter, then user should pass it in as 2nd argument");
   int const offset  = lenA - lenB;
   bool carry=false;
@@ -40,25 +45,25 @@ void add(unsigned char *A, size_t const lenA,
   cout<<(carry?"1(carry)":"")<<A<<".. is the result ----\n";
 }  
 void test1(){
-  vector<unsigned char> A = {129, 1, 255};
-  vector<unsigned char> B = {128, 2, 255};
+  vector<Byte> A = {129, 1, 255};
+  vector<Byte> B = {128, 2, 255};
   cout<<A<<B<<".. are the inputs\n";
   sz = A.size();
   add(A.data(), sz, B.data(), B.size());
-  assert ( A == vector<unsigned char>({1,4,254}) );
+  assert ( A == vector<Byte>({1,4,254}) );
 }  
 void test2(){
-  vector<unsigned char> A = {255,255, 129, 0,   255};
-  vector<unsigned char> B = {         128, 255, 255};
+  vector<Byte> A = {255,255, 129, 0,   255};
+  vector<Byte> B = {         128, 255, 255};
   cout<<A<<B<<".. are the inputs\n";
   sz = A.size();
   add(A.data(), sz, B.data(), B.size());
-  assert ( A == vector<unsigned char>({0,0,2,0,254}) );
+  assert ( A == vector<Byte>({0,0,2,0,254}) );
 }  
 int main(){
   test1();
   test2();
 }/*Req: Q:  Given 2 BigBinary numbers which are extremely long (size not mentioned), how would you add the numbers? How would you design the function .. how would you represent the input to be function? Remember, the memory usage should be minimal.
 
-https://bintanvictor.wordpress.com/wp-admin/post.php?post=34037&action=edit has more context
+https://bintanvictor.wordpress.com/wp-admin/post.php?post=34037&action=edit has more details
 */
