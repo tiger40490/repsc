fe65dc99ad1cc28c0317b1582a6b495e0be28b05
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-08-21 10:30:50 -0400

    minorEnhance

--- cpp/lang_66mem/fixedSizedFreeList.cpp
+++ cpp/lang_66mem/fixedSizedFreeList.cpp
@@ -1,7 +1,8 @@
 /*
 todo: add more tests
-todo: check the MAX before placement-new
 showcase placement-new
+
+Realistic app Should check the MAX before placement-new
 
 Note: placement-new should be minimized inside the free list 
 */
@@ -13,7 +14,13 @@ template <size_t CHUNK=3, size_t MAX=100> struct FreeList{
   struct Node{
     Node * next=nullptr;
     uint8_t payload[CHUNK];
-    Node(): next(nullptr), payload{0} {}
+    Node(): next(nullptr), payload{1} //c++11 syntax
+    { //experimenting with array initialization:
+      assert(payload[0]==1); payload[0]=0;
+      if (CHUNK > 0)
+        assert(payload[1]==0 && "All elements except first is default-initialized to 0");
+      assert(payload[CHUNK-1]==0);
+    }
   } __attribute__((packed));
 
   FreeList(){
@@ -22,13 +29,13 @@ template <size_t CHUNK=3, size_t MAX=100> struct FreeList{
     this->head = nullptr;
     cout<<mallocSz<<" bytes allocated at "<<base<<endl;
   }
-  void ffree(void * const ptr){//fairly fast
-    char * const tmp = (char*) ptr - PTR_SZ;
+  void ffree(void * const ptr_arg){//fairly fast
+    char * const tmp = (char*) ptr_arg - PTR_SZ;
+    //We will assume ptr_arg was originally from a Node
     Node * node = static_cast<Node*> ((void*)tmp);
-    node->next = this->head;
-    this->head = node;
-    cout<<ptr<<" passed into ffree(). New free list head = "<<node<<endl;
-    assert ( this->head != ptr);
+    node->next = this->head; this->head = node;
+    cout<<ptr_arg<<" passed into ffree(). New free list head = "<<node<<endl;
+    assert ( this->head != ptr_arg);
     assert (!this->isNullHead());
   }
   /* 
@@ -37,19 +44,22 @@ template <size_t CHUNK=3, size_t MAX=100> struct FreeList{
   Updates this->base iFF allocating from the raw array
   */
   void * fmalloc(size_t sz){
-    assert(sz <= CHUNK);
+    if (sz > CHUNK){
+      cout<<"fmalloc() received over-sized request\n";
+      return nullptr;
+    }
     if(head == nullptr){
-      head  = new (base) Node();
-      assert (head->next == nullptr);
+      head  = new (base) Node(); //create new Node from raw array
+      assert (head->next == nullptr && "Node() should null-initialize");
       char * tmp = (char*)this->base - sizeof(Node);
       this->base = (void*) tmp;
       cout<<this->base<<" = new BASE\n";
     }else{
-      cout<<" :) Lucky to find a head node with a live follower :)\n";
+      cout<<" :) Lucky to find an existing head node, probably recycled :)\n";
     }
     assert (head != nullptr);
     void * const ret = this->head->payload;   
-    assert (ret != nullptr);    
+    assert (ret != nullptr);
     head = head->next;
     cout<<head<<" = the new free list head, after returning "<<ret<<" from fmalloc()\n";
     return ret;
@@ -65,29 +75,36 @@ int main(){
   struct Trade{
     int id;
     Trade(int i): id(i){
-      cout<<"   New Trade created@ "<<this<<" with id = "<<id<<endl;
+      cout<<"   New Trade @ "<<this<<" with id = "<<id<<endl;
     }
   };
   size_t const chunk=4;
   FreeList<chunk> flist;
   void * 
-  newAddr = flist.fmalloc(2);
+  newAddr = flist.fmalloc(9999);  assert (newAddr == nullptr);
+  newAddr = flist.fmalloc(sizeof(Trade));
   assert (flist.isNullHead());  
   Trade * trade2 = new (newAddr) Trade(-22);
 
   void * 
   newAddrX = flist.fmalloc(1);
   assert (flist.isNullHead());  
+
   newAddr = flist.fmalloc(4);
   assert (flist.isNullHead());
   Trade * trade4 = new (newAddr) Trade(-44);
+
   flist.ffree(newAddrX);
-  flist.ffree(newAddr);
+  flist.ffree(trade4);
   
   newAddr = flist.fmalloc(3);
   assert (!flist.isNullHead());
   Trade * trade3 = new (newAddr) Trade(-33);
+
   assert (trade2->id == -22);
+  flist.ffree(trade2);
+
+  assert (trade3->id == -33);  
 }/*Req: https://bintanvictor.wordpress.com/wp-admin/post.php?post=33040&action=edit
 
 */
