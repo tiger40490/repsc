c28bd511acc04432a8cb9cdb12752008d2f84ce3
unknown <35847749+tiger40490@users.noreply.github.com>
2019-08-31 19:17:27 -0400

    new file

---
+++ cpp/lang_33template/tmpl_tmpl_param.cpp
@@ -0,0 +1,50 @@
+/*
+showcase: using 'list', 'vector' without <>
+*/
+#include <list>
+#include <vector>
+#include <deque>
+#include <iostream>
+using namespace std;
+template<typename T, class Cont> class StackSimple{
+  Cont underlying;
+public:
+  size_t size(){return underlying.size(); }
+  void push(const T & val){
+    underlying.push_back(val);
+  }
+  void test(){
+    this->push(0);
+    cout<<this->size()<<endl;
+  }
+};
+//compiler erros out only when you use this object !
+StackSimple<string, list<int>> compilerFail;
+StackSimple<double, list<int>> badInst; // bad instantiation but compiles fine
+
+///////////// Above is a traditional implementation, below implementation uses a tmpl-tmpl-param
+template <typename T, template<typename> class Cont> class StackTTP{
+  Cont<T> underlying;
+public:
+  size_t size(){return underlying.size(); }
+  void push(const T & val){
+    underlying.push_back(val);
+  }
+  void test(){
+    this->push(0);
+    this->push(333);
+    cout<<this->size()<<endl;
+  }
+};
+//Compiler catches this subtle error even if you don't use this oject :)
+//StackTTP<double, list<int>> badInstCaughtAtDefinition;
+
+//StackTTP<double, list> goodInst;
+StackTTP<double, vector> goodInst;
+
+int main(){
+  //compilerFail.test();
+  badInst.test();
+  goodInst.test();
+}/*illustration of TTP, based on [[c++common knowledge]]
+*/
