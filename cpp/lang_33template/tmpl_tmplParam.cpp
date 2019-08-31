5417a1eb09a51597af6ce9d87d7ff0f9be2ff6e6
unknown <35847749+tiger40490@users.noreply.github.com>
2019-08-31 19:10:49 -0400

    minor

--- cpp/lang_33template/tmpl_tmplParam.cpp
+++ cpp/lang_33template/tmpl_tmplParam.cpp
@@ -1,5 +1,5 @@
 /*
-showcase: using list, vector without <>
+showcase: using 'list', 'vector' without <>
 */
 #include <list>
 #include <vector>
@@ -18,11 +18,11 @@ public:
     cout<<this->size()<<endl;
   }
 };
-//compiler fails only when you use this object !
+//compiler erros out only when you use this object !
 Stack1<string, list<int>> compilerFail;
-// bad instantiation but compiles fine
-Stack1<double, list<int>> badInst;
-/////////////
+Stack1<double, list<int>> badInst; // bad instantiation but compiles fine
+
+///////////// Above is a traditional implementation, below implementation uses a tmpl-tmpl-param
 template <typename T, template<typename> class Cont> class StackTTP{
   Cont<T> underlying;
 public:
@@ -32,14 +32,15 @@ public:
   }
   void test(){
     this->push(0);
+    this->push(333);
     cout<<this->size()<<endl;
   }
 };
-//Compiler catches this error even if you don't use this oject :)
+//Compiler catches this subtle error even if you don't use this oject :)
 //StackTTP<double, list<int>> badInstCaughtAtDefinition;
+
 //StackTTP<double, list> goodInst;
-//StackTTP<double, vector> goodInst;
-StackTTP<double, deque> goodInst;
+StackTTP<double, vector> goodInst;
 
 int main(){
   //compilerFail.test();
