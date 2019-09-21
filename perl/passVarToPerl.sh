8317a0c11d6cdb9f6bb5fabdbb8b5812f06983aa
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2019-09-21 21:46:17 +0800

    Create passVarToPerl.sh

---
+++ perl/passVarToPerl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+shVar=/apps/qa
+export |grep shVar #nothing
+
+# I think the single-quote around $shVar is essential
+echo '/abc/apps/qa/xyz' | perl -ne "print '$shVar __ ' . \$1  if m|$shVar/(\w*)\b| "
+
+printf "\n^^^^ above is double-quoted perl command; below is single-quoted vvvv \n"
+
+echo '/abc/apps/qa/xyz' | perl -ne 'print $shVar. " __ " . $1 if m|$shVar/(\w*)\b| '
+# I think $shVar is Not passed to the perl process due to single-quote around perl command
+
