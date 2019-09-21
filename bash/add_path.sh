d591963fb8f6e8d1136211e6b3175ad70518de4f
Victor Tan Bin <35847749+tiger40490@users.noreply.github.com>
2019-09-21 13:43:08 +0800

    Create add_path.sh

---
+++ bash/add_path.sh
@@ -0,0 +1,17 @@
+add_path(){
+  dir=`echo $1|perl -pe 's|/$||'`
+  [ -r $dir ] || return
+  echo $PATH|grep -P "$dir(:|$)" > /dev/null && return 0
+
+  #todo: check world write perm, esp for root
+  isPre=false #this is a demo of a reusable feature, not really needed here
+  [[ "$2" = "-p" ]] && isPre=true && echo Prepending $dir to PATH
+  if $isPre; then
+    PATH=$dir:$PATH
+  else
+    PATH=$PATH:$dir
+  fi
+  cnt=$(echo $PATH |perl -pe 's/:/\n/g' | wc -l)
+  #echo adding $dir to PATH and returning $ret
+  return $cnt
+}
