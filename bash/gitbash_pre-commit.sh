a0ac27ef843005304e38dcb0a97ee0bfb87cda8a
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-11-04 07:59:15 +0800

    hp  console msg minor fix

--- bash/gitbash_pre-commit.sh
+++ bash/gitbash_pre-commit.sh
@@ -5,8 +5,9 @@
 # Based on https://stackoverflow.com/questions/19387073/how-to-detect-commit-amend-by-pre-commit-hook
 # If head is strictly ahead (eg: child) of $upstream, this hook is quiet.
 # if head is strictly behind (eg: parent of) $upstream, this hook would complain, correctly.
-# if head is same as $upstream, then --amend vs regular append are indistinguishable so this hook has to prompt user to confirm.
-# Risk in "amend" scenario is too high, much higher than the inconvenience of "append" scenario.
+# if head is same as $upstream, then --amend vs regular append are _indistinguishable_ so this hook has to "err on the side of caution" and prompt user to confirm.
+# Risk in "Amend" scenario is too high, much higher than the inconvenience of "Append" scenario.
+# The inconvenience is a necessary evil -- all my git hooks are.
 
 ############################
 # Are we on a branch?  If not, don't object
@@ -18,7 +19,7 @@ head=$(git rev-parse -q --verify HEAD) || exit 0
 # upstream is the hash of remote tip
 #https://stackoverflow.com/questions/19474577/what-does-the-argument-u-mean-in-git explains @{u}
 upstream=$(git rev-parse -q --verify @{u}) || exit 0
-printf "FYI\n $upstream = upstream\n $head = head\n"
+printf "FYI\n  $upstream = upstream\n  $head = head\n"
 
 if git merge-base --is-ancestor HEAD $upstream; then
   printf "Amending || appending on $localBranch branch-tip while original commit is on remote upstream?\n( Tip 1: Once inside commit-msg editor, you will have one more chance to cancel this commit via :cq )\n( Tip 2: Successful commit always print details to console. If you don't see them then nothing committed. )\n Continue? [ y / any_other_key to quit] "
