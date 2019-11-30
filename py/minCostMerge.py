9c5b2b397942a7c382f5628f86a3fe9d840a9f97
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-11-30 16:41:20 +0800

    hp  trivial

--- py/minCostMerge.py
+++ py/minCostMerge.py
@@ -1,12 +1,14 @@
 '''
+showcase binsect.insort
 
 '''
 import bisect
 
 def solution(li):
   cumcost=0
-  #removing from end of vector. Note a linked list would be more efficient
-  li.sort(reverse=True) 
+  li.sort(
+    reverse=True)   #Will remove from end of vector. Note a linked list would be more efficient
+    
   while len(li)>=2:
     sum = ( li[-1]+li[-2] )
     del li[-1]
@@ -19,6 +21,6 @@ def solution(li):
 def main():
   assert 1700 == solution([100, 250, 1000])
 main()
-'''Req: Given N sorted arrays to be merged into one, find the mininum cost merging strategy.
-Each pair-wise merge has a cost = sum of both lengths.
+'''Req: Given N sorted arrays to be merged into one, find the mininum-cost merging strategy.
+Each pair-wise merge has a cost = sum of both lengths. We need a total of N-1 merges, with a cummulative cost.
 '''
