34aa18198634b020a67281266d7c5eb46a1b5ffc
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-12-15 22:54:52 +0800

    hp  trivialRefac

--- py/algo_str/maxSubstrUpTo_K_badChars.py
+++ py/algo_str/maxSubstrUpTo_K_badChars.py
@@ -1,7 +1,10 @@
 '''
 showcase: convert from integer ordinal to ASCII character
 showcase: python set
-showcase: dump string with indices below
+showcase: dump string with indices underneath
+
+My sliding window idea was simple but it took me a hour+ to implement it.
+
 idea: 2-pointer moving in?
 '''
 def solve(ss, k, charValue): # sliding expanding window in O(N)
@@ -23,7 +26,7 @@ def solve(ss, k, charValue): # sliding expanding window in O(N)
   #le, ri and cnt all initialized. Now start sliding our expanding window
   for ri in xrange(k-1, len(ss)-1):
     print ' '.join(list(ss[le:ri+1]))
-    print ' '.join(str(i) for i in xrange(le, ri+1)), '<-- after validating, new le=',le, '; new ri=',ri, '; cnt =', cnt, ('<-BAD' if cnt > k else '')
+    print ' '.join(str(i%10) for i in xrange(le, ri+1)), '<-- after validating, new le=',le, '; new ri=',ri, '; cnt =', cnt, ('<-BAD' if cnt > k else '')
 
     if cnt > k: # slide the window, hope to reduce cnt, but cnt could keep growing as we slide
       if ss[le  ] in badChars: cnt -= 1
@@ -35,12 +38,13 @@ def solve(ss, k, charValue): # sliding expanding window in O(N)
     assert cnt <= k #expand by moving ri
     print 'fwd-checking', ss[ri+1]
     if ss[ri+1] in badChars: 
-      cnt += 1
+      cnt += 1 #increment
       #print 'new cnt =', cnt, 'may need to slide'
     if cnt<= k:
+      # we could reach here with or without the 'increment' operation !
       wsize=ri+2-le
       print 'new wsize =', wsize
-      assert wsize > ret
+      assert wsize > ret, 'our window never shinks'
       ret = wsize
     
   print '========== returning', ret, cnt
