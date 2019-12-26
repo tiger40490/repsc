540623ce21192aeef90316c25e774f488d8d34f2
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2019-12-26 09:18:20 +0800

    hp  new99023

--- py/algo_arr/maxProfit_MaxSubarraySum.py
+++ py/algo_arr/maxProfit_MaxSubarraySum.py
@@ -23,6 +23,30 @@ def byDeltaByLevel(mode, deltas): # use byLevel algo internally
   return byLevel(levels)
 def byLevel(levels):
   return watermark(levels)
+  #return scan2way(levels)
+def scan2way(levels): # untested
+  sz = len(levels)
+  fromLe = [0] * sz
+  fromRi = fromLe[:] #clone
+  diff = fromLe[:]
+  print levels
+  
+  fromLe[0] = levels[0]
+  for i,v in enumerate(levels):
+    if i == 0: continue
+    fromLe[i] = min(fromLe[i-1], v)
+  print fromLe
+
+  fromRi[-1] = levels[-1]
+  for i, v in reversed(list(enumerate(levels))):
+    if i == sz-1: continue
+    fromRi[i] = max(fromRi[i+1], v)
+  print fromRi  
+  
+  for i in xrange(sz):
+    diff[i] = fromRi[i] - fromLe[i]
+  print diff
+  return max(diff)
 def watermark(levels):
   lowWaterMark=levels[0]
   maxProfit=float('-inf') # final max could be negative
@@ -42,6 +66,8 @@ def watermark(levels):
 ################
 
 def test():
+  #scan2way([5,3,2,8,4,6,1,3,6,2])
+  #return
   a=((-2, -3, 4, -1, -2, 1, 5, -3))
   assert (7,-5)==watermark1pass(a)
   assert(7,2,6)==disposableCurSubarr(a)
