21028c5032a7c5b7301afa3490655c5850369f8a
root <35847749+tiger40490@users.noreply.github.com>
2020-01-10 20:26:16 +0800

    299 minorRefac

--- cpp/algo_binTree/BST_show_level.cpp
+++ cpp/algo_binTree/BST_show_level.cpp
@@ -1,37 +1,42 @@
-struct Node {
-    int data;
-    Node *left, *right, *next;
-    Node(int x, Node * le = NULL, Node * ri = NULL) : data(x), left(le), right(ri), next(NULL) {}
-};
-    Node _15(15);
-    Node _14(14);
-    Node _13(13);
-    Node _12(12);
-    Node _11(11);
-    Node _10(10);
-    Node _9(9);
-    Node _8(8);
-    Node _7(7, &_14, &_15);
-    Node _6(6, NULL, &_13);
-    Node _5(5, &_10, NULL);
-    Node _4(4, NULL, &_9);
-    Node _3(3, &_6,  &_7);
-    Node _2(2, &_4,  &_5);
-    Node root(1, &_2, &_3);
-    Node marker(-1); //to be added to queue to mark new level
+#include "binTreeUtil.h"
+#include <iostream>
+#include <queue>
+using namespace std;
 
-Node * dequeue(queue<Node*> & q){
+struct TNode {
+    int data;
+    TNode *left, *right, *next;
+    TNode(int x, TNode * le = NULL, TNode * ri = NULL) : data(x), left(le), right(ri), next(NULL) {}
+};
+    TNode _15(15);
+    TNode _14(14);
+    TNode _13(13);
+    TNode _12(12);
+    TNode _11(11);
+    TNode _10(10);
+    TNode _9(9);
+    TNode _8(8);
+    TNode _7(7, &_14, &_15);
+    TNode _6(6, NULL, &_13);
+    TNode _5(5, &_10, NULL);
+    TNode _4(4, NULL, &_9);
+    TNode _3(3, &_6,  &_7);
+    TNode _2(2, &_4,  &_5);
+    TNode root(1, &_2, &_3);
+    TNode marker(-1); //to be added to queue to mark new level
+
+TNode * dequeue(queue<TNode*> & q){
     assert(!q.empty());
-    Node * n = q.front();
+    TNode * n = q.front();
     q.pop();
     return n;
 }
 void BFT(){
-  queue<Node *> q;
+  queue<TNode *> q;
   size_t height=0;
 
   for( q.push(&marker), q.push(&root); !q.empty();){
-    Node * n = dequeue(q);
+    TNode * n = dequeue(q);
     if (n == &marker){
         if (q.empty()){
                 cout<<"  ^^^^^^^^^^"<<endl;
