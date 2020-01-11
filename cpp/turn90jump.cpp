4f207c7e2120fb13da6cd69cbbe706d727ab0ec3
root <35847749+tiger40490@users.noreply.github.com>
2020-01-11 16:56:40 +0800

    299 minorRefac

--- cpp/turn90jump.cpp
+++ cpp/turn90jump.cpp
@@ -1,4 +1,5 @@
 // showcase integer-ratio simplication using GreatestCommonDenominator algo.
+// showcase to_string from integer
 #include <cstdlib>
 #include <cassert>
 #include <string>
@@ -13,21 +14,20 @@ unsigned int gcd(int u, int v){
   u=abs(u); v=abs(v);
   return (v == 0) ? u : gcd(v, u % v);
 }
+// returns the first point (C) encountered after turning right 90 degrees
 string solution(int ax, int ay, int bx, int by){
   int dy=by-ay, dx=bx-ax;
-  if (dy*dx ==0){
-    if (dy==0){
-      dy = -sgn(dx);     
-      dx=0;
-    }else {
+  if      (dy==0){
+      dy = -sgn(dx);
+      dx=0; // cx == bx
+  }else if(dx==0){
       dx = sgn(dy);
-      dy=0; 
-    }
+      dy=0; // cy == by
   }else{  
     size_t tmp=gcd(dy, dx);
     dy /= tmp; 
     dx /= tmp;
-    cout<<dy<<"/"<<dx<<" after simplify\n";
+    cout<<dy<<"/"<<dx<<" after simplifying the ratio representing the slope\n";
     swap(dy,dx);
     dy = -dy;
     cout<<dy<<"/"<<dx<<" after swap and negate\n";
@@ -39,5 +39,5 @@ int main(){
   assert("1,-3" == solution(2,2,2,-3)); //orig 
   assert("2,-1" == solution(-1,3,3,1)); //orig 
   assert("-2,1" == solution(3,1,-1,-1)); //-1-1, -1+2
-}/*Req: given a 2D grid with all points having signed integer coordinates, starting from point A(ax,ay) to point B(bx,by), and turning 90 degrees, what's the first point encountered?
+}/*Req: given a 2D grid (i.e. all intersection having signed integer coordinates), starting from intersection A(ax,ay) moving to intersection B(bx,by), and turning 90 degrees, what's the first intersection encountered?
 */
