854c904e9d7c90792cbdd972af6953394c278df7
root <35847749+tiger40490@users.noreply.github.com>
2020-01-11 14:31:11 +0800

    299 trivial

--- cpp/zigzag_jump.cpp
+++ cpp/zigzag_jump.cpp
@@ -1,16 +1,19 @@
-#include <cstdlib>
 #include <cassert>
-#include <string>
+#include <vector>
 #include <iostream>
 using namespace std;
+using Height=size_t;
 
-int solution(vector<size_t> arr){
-  
+/*first pass, try a rising line-up i.e. short,tall,short,tall
+2nd pass, try a falling line-up, i.e. tall,short,tall,short
+
+In each pass, shorten a tree to minimum (i.e. 1 meter) and move on
+*/
+size_t solution(vector<Height> const & arr){
+  return 0;
 }
 int main(){
-}/*Req: given a line of trees, we want them to look like short,tall,short,tall (or tall,short,tall,,)
-We can cut any tree to shorter. What's the minimum number of trees to shorten?
+}/*Req: given a line-up of trees, the gardener wants them to look like short,tall,short,tall (or tall,short,tall,,). What's the minimum number of trees to shorten?
 
-minium 2 trees
-max height of each tree is 1000
+Each tree height is [2,1000], but if all the trees are initially 2m, then we can shorten some to 1m.
 */
