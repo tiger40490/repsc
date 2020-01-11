57109ab23eca47857a3416be232a609885045f9a
root <35847749+tiger40490@users.noreply.github.com>
2020-01-11 13:38:51 +0800

    299 2 new files

---
+++ cpp/redwhite_jump.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cassert>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int solution(string const & S){
+  
+}
+int main(){
+  assert("1,-3" == solution(2,2,2,-3)); //orig 
+  assert("2,-1" == solution(-1,3,3,1)); //orig 
+  assert("-2,1" == solution(3,1,-1,-1)); //-1-1, -1+2
+}/*Req: 
+https://leetcode.com/discuss/interview-question/414660/microsoft-oa-2019-min-swaps-to-group-red-balls
+*/
---
+++ cpp/zigzag_jump.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cassert>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int solution(vector<size_t> arr){
+  
+}
+int main(){
+}/*Req: given a line of trees, we want them to look like short,tall,short,tall (or tall,short,tall,,)
+We can cut any tree to shorter. What's the minimum number of trees to shorten?
+
+minium 2 trees
+max height of each tree is 1000
+*/
