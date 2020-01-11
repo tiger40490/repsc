0ad68e5f36e5faaeb98a9e9cfb86f946478ea9db
root <35847749+tiger40490@users.noreply.github.com>
2020-01-11 18:58:44 +0800

    299 fix CompilationError

--- cpp/lang_33template/containerOutputOperator.cpp
+++ cpp/lang_33template/containerOutputOperator.cpp
@@ -45,10 +45,15 @@ std::ostream& operator<<(std::ostream& os, Container<V, Alloc> const& c){
 }
 int main(){
   list<double> li={3.1, 5,2,9};
-  Set<string> myset={"s1", "s2", "s2", "s1"};
-  vector<string> vec={"dabao", "meimei", "princess"};
-  Map<int, int> tm={{1,11}, {2,22}};
+  Set<short> myset={1, 2, 42, 5};
+#if (__cplusplus >= 201703L)
+  vector<float> vec={3.5, 1.9, 0.8};
+#else  
+  vector<string> vec={"dabao", "meimei", "princess"}; //works under g++ -std=c++14 but not c++17
+#endif
+  Map<int, bool> tm={{11,true}, {22,false}};
   ss<<vec<<tm;
   ss<<" <-- vector and map; list and set -->\n";
   ss<<li<<myset;
+  ss<<__cplusplus <<" = __cplusplus \n";
 }
