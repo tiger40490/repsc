dcd1922794eb7293af488c8da5a2e8d480ff543f
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2020-01-12 10:12:33 +0800

    hp  major changes

--- cpp/gardener_Jump.cpp
+++ cpp/gardener_Jump.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -25,24 +26,48 @@ In each pass, shorten a tree to minimum (i.e. 1 meter) and move on
 size_t linearTime2pass(vector<Height> const & orig){
   vector<Height> arr=orig;
   cout<<arr;
-  size_t ret=0;
+  size_t ret1=0, ret2=0;
   assert(arr.size() > 1);
+  assert(*min_element(arr.begin(), arr.end()) >=2);
+  //falling line-up
   for (Idx i=1; i<arr.size(); ++i ){
-    if (i%2==0) {
-      cout<<"should rise to #"<<i<<"\n";
-      if (arr[i-1] >= arr[i]){
-        ++ret;
+    if (i%2) {
+      //cout<<"should see a low@ #"<<i<<"\n";
+      if (arr[i-1] <= arr[i] || arr[i] >= arr[i+1]){
+        ++ret1;
         arr[i]=1;
-        cout<<i<<"'th tree shortened to minimum height\n";
+        //cout<<i<<"'th tree shortened to minimum height\n";        
+      }else{
+        //cout<<"no change needed:)\n";
       }
     }
   }
-  //falling line-up
-  cout<<arr<<" ^  beautified  ^\n";
-  return 0;
+  cout<<arr<<" ^  beautified #F  ^\n";
+  arr=orig;
+  //rising line-up
+  if (arr[0] >= arr[1]){
+        ++ret2;
+        arr[0]=1;
+  }
+  for (Idx i=2; i<arr.size(); ++i ){
+    if (i%2==0) {
+      cout<<"should see a low@ #"<<i<<"\n";
+      if (arr[i-1] <= arr[i] || arr[i] >= arr[i+1]){
+        ++ret2;
+        arr[i]=1;
+        cout<<i<<"'th tree shortened to minimum height\n";        
+      }else{
+        cout<<"no change needed:)\n";
+      }
+    }
+  }
+  cout<<arr<<" ^  beautified #R  ^\n";  
+  size_t ret = min(ret1, ret2);
+  return ret;
 }
 int main(){
-  linearTime2pass({7,5,2,3,6,4});
+  assert(2==linearTime2pass({7,5,2,3,6,4}));
+  assert(1==linearTime2pass({2,5,2,3,6,4}));
 }/*Req: given a line-up of trees, the gardener wants them to look like short,tall,short,tall (or tall,short,tall,,). What's the minimum count of trees to shorten?
 
 Each tree height is [2,1000], but if all the trees are initially 2m, then we can shorten some to 1m.
