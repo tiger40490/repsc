bafe5e1b258b9eb4cc62fe345f0a85de0bd292e7
unknown <35847749+tiger40490@users.noreply.github.com>
2020-01-28 23:59:53 +0800

    116 trivial

--- cpp/algo_numerical/printAllSquaresInIntRange.cpp
+++ cpp/algo_numerical/printAllSquaresInIntRange.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int const limit = 100;
 
-int main(){
+int main(){ //O(sqrt(N)) solution
   cout<<"Here are all the squares found within the range [1, "<<limit<<"]: \n";
   for (int r=0, g=0; ; ++r){
     if ( (g += 2*r+1)  > limit) return 0;
     cout<<g<<" ";
   }
 }
-/*Req (Yihai's GS IV): given a continuous range of integers starting from 1, output each item that has a integer square root. 
+/*Req (Yihai's GS IV): given a continuous range of integers starting from 1 (passed in as a list or array), output each item that has an integer square root. 
 
 Eg: For input 1 to 100, should print 1,4,9,16,25,,,100
 */
