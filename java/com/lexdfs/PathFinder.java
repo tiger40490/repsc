6714df56d14b4d95fb31debd0cb2d5b907e1f284
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2020-02-18 22:39:12 +0800

    hp  minorRefac

--- java/com/lexdfs/PathFinder.java
+++ java/com/lexdfs/PathFinder.java
@@ -1,6 +1,6 @@
-/* todo: more tests
+/* 
  * todo: efficiency improvement
- * todo: simplify data structures
+ * todo: simplify data structures and everything else
  * showcase: linkedHashSet as dynamic sorted collection with O(1) remove but without insert. Not available in other languages.
  * showcase: as much as possible, all collections store integers rather than node references. Simplifies instrumentation greatly.
  * showcase: Arrays.asList(1,22,33)
@@ -17,12 +17,12 @@ public class PathFinder {
 	static void test0() {
 		Graph inst = new Graph();
 		inst.build1graph(Arrays.asList(11,22,33), Arrays.asList(22,33,11));
-		inst.findPath1pair(33,11);		
+		inst.findPathFor1pair(33,11);		
 	}
 	static void test1() {
 		Graph inst = new Graph();
 		inst.build1graph(Arrays.asList(0,1,2,3), Arrays.asList(1,2,3,0));
-		inst.findPath1pair(0,2);		
+		inst.findPathFor1pair(0,2);		
 	}
 	static void test2() {
 		Graph inst = new Graph();
@@ -30,26 +30,27 @@ public class PathFinder {
 		//assert(1==inst.findPath1pair(0,1));		
 		//assert(4==inst.findPath1pair(3,4));		
 		//assert(4==inst.findPath1pair(2,4));		
-		assert(4==inst.findPath1pair(1,2));		
+		assert(4==inst.findPathFor1pair(1,2));		
 	}
 }
 class Graph{
 	final private HashSet<Integer> grey = new HashSet<>();
 	void build1graph(List<Integer> end1, List<Integer> end2) {
+		if (! Node.globalLookup.isEmpty()) return;
 		assert(end1.size() == end2.size());
 		for (int i=0; i<end1.size(); ++i) {
 			final int idA = end1.get(i);
 			final int idB = end2.get(i);
-			GraphNode nodeA, nodeB;
-			if (GraphNode.globalLookup.containsKey(idA)) {
-				nodeA = GraphNode.globalLookup.get(idA);
+			Node nodeA, nodeB;
+			if (Node.globalLookup.containsKey(idA)) {
+				nodeA = Node.globalLookup.get(idA);
 			}else {
-				nodeA = new GraphNode(idA);
+				nodeA = new Node(idA);
 			}
-			if (GraphNode.globalLookup.containsKey(idB)) {
-				nodeB = GraphNode.globalLookup.get(idB);
+			if (Node.globalLookup.containsKey(idB)) {
+				nodeB = Node.globalLookup.get(idB);
 			}else {
-				nodeB = new GraphNode(idB);
+				nodeB = new Node(idB);
 			}
 			nodeA.addNeighbor(idB);
 			nodeB.addNeighbor(idA);
@@ -58,17 +59,17 @@ class Graph{
 	/*
 	 * Run one lexdfs cycle for one pair of start/target
 	 */
-	int findPath1pair(int start, int tgt) {
-		for (Entry<Integer, GraphNode> entry : GraphNode.globalLookup.entrySet()) {
-			final GraphNode n = entry.getValue();
-			n.edgeList = new LinkedHashSet<> (n.sortedList);
+	int findPathFor1pair(int start, int tgt) {
+		for (Entry<Integer, Node> entry : Node.globalLookup.entrySet()) {
+			final Node n = entry.getValue();
+			n.edgeList = new LinkedHashSet<> (n.sortedView);
 		}
-		for (Entry<Integer, GraphNode> entry : GraphNode.globalLookup.entrySet()) {
+		for (Entry<Integer, Node> entry : Node.globalLookup.entrySet()) {
 			System.out.println(entry.getValue());
 		}
 		System.out.println("^^^ end of graph dump ^^^\n Now starting dfs from " + start + " -> " + tgt);
-		assert (GraphNode.globalLookup.containsKey(start));
-		assert (GraphNode.globalLookup.containsKey(tgt));
+		assert (Node.globalLookup.containsKey(start));
+		assert (Node.globalLookup.containsKey(tgt));
 		int rank = lexdfs(start, tgt);
 		System.out.println("findPath1pair() returning " + rank);
 		return rank;
@@ -80,7 +81,7 @@ class Graph{
 		grey.add(cur);
 		System.out.println("entering lexdfs() with " + cur );
 		
-		GraphNode node = GraphNode.globalLookup.get(cur);
+		Node node = Node.globalLookup.get(cur);
 		assert (node != null);
 		for (final int child: node.edgeList) {
 			if (grey.contains(child)) {
@@ -96,21 +97,21 @@ class Graph{
 		}
 		for (final int neighbor: node.edgeList) {
 			System.out.println(cur + " (black node) removed as neighbor of" + neighbor);
-			GraphNode.globalLookup.get(neighbor).edgeList.remove(cur);
+			Node.globalLookup.get(neighbor).edgeList.remove(cur);
 		}
 		grey.remove(cur);
 		return Integer.MIN_VALUE;
 	}
 }
-class GraphNode{
-	public static final HashMap<Integer, GraphNode> globalLookup=new HashMap<>();
+class Node{
+	public static final HashMap<Integer, Node> globalLookup=new HashMap<>();
 	public final int id;
 	private final TreeSet<Integer> internal = new TreeSet<>();
-	public final SortedSet<Integer> sortedList = Collections.unmodifiableSortedSet(internal);
+	public final SortedSet<Integer> sortedView = Collections.unmodifiableSortedSet(internal);
 	public  LinkedHashSet<Integer> edgeList;
-	public GraphNode(int i) {
+	public Node(int i) {
 		this.id=i;
-		GraphNode.globalLookup.put(i, this);
+		Node.globalLookup.put(i, this);
 	}
 	public void addNeighbor(int i) {
 		this.internal.add(i);
