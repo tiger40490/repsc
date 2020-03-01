86baa4bfcec3e53711fcaf3737333c7cd9046e06
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2020-03-01 22:00:48 +0800

    hp  minorRefac

--- py/88miscIVQ/lottery_Ashish.py
+++ py/88miscIVQ/lottery_Ashish.py
@@ -1,30 +1,32 @@
 from collections import defaultdict
 from pprint import pprint
-def calcCoupon(id):
+def calcCoupon(tic):
   ret = 0
-  while id > 0:
-    ret += id%10
-    id /= 10
+  while tic > 0:
+    ret += tic%10
+    tic /= 10
   #print ret
   return ret
   
 def solve(lo, hi):
-  best = 1; ticketsForSameCoupon=defaultdict(list)
-  for id in xrange(lo, hi+1):
-    coupon = calcCoupon(id)
-    ticketsForSameCoupon[coupon].append(id)
-    best = max(best, len(ticketsForSameCoupon[coupon]))
-  pprint(ticketsForSameCoupon)
-  bestCoupons = 0; 
-  for coupon, li in ticketsForSameCoupon.iteritems():
-    if best == len(li):
-      bestCoupons += 1
-  return bestCoupons, best
+  maxClubSize = 0; 
+  sameCouponTickets=defaultdict(list) #each dict Value is a club of tickets, having the same coupon code
+  for tic in xrange(lo, hi+1):
+    coupon = calcCoupon(tic)
+    sameCouponTickets[coupon].append(tic) # ticket joins the correct club
+    maxClubSize = max(maxClubSize, len(sameCouponTickets[coupon]))
+  pprint(sameCouponTickets)
+  
+  howManyMaxClubs = 0; #count of clubs have the same max membership
+  for coupon, li in sameCouponTickets.iteritems():
+    if maxClubSize == len(li):
+      howManyMaxClubs += 1
+  return howManyMaxClubs, maxClubSize
 def main():  
   assert (1,2) == solve(1,10)
   assert (5,1) == solve(1,5)
 main()
-'''Req: in a lottery each ticket has a positive int id. It has a (non-unique) coupon code equal to the sum of its digits.
+'''Req: in a lottery each ticket has a positive int tic. It has a (non-unique) coupon code equal to the sum of its digits.
 
 For a range of ticket IDs, find the coupon code with the largest population of tickets
 '''
