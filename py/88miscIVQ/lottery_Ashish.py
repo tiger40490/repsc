f592ab5271ea48c943dee0b56726496360260dd1
root <35847749+tiger40490@users.noreply.github.com>
2020-03-02 22:13:24 +0800

    299 instrumentation

--- py/88miscIVQ/lottery_Ashish.py
+++ py/88miscIVQ/lottery_Ashish.py
@@ -13,11 +13,13 @@ def calcCoupon(tic):
     tic //= 10
   #print orig, '->', 
   if orig%10 ==0: 
-    print '\n', '   '*(orig/10-10),
+    cnt=ret-1 #(orig/10)%10
+    print '  (%d..%d)\n' % (orig-10,orig-1),
+    print '   '*cnt,
   print "%2d"%ret,
   return ret
-  
-def solve(lo, hi):
+
+def solveInLinearTime(lo, hi):
   maxClubSize = 0; 
   sameCouponTickets=defaultdict(list) #each dict Value is a club of tickets, having the same coupon code
   for tic in xrange(lo, hi+1): # O(N) loop for N tickets
@@ -25,19 +27,20 @@ def solve(lo, hi):
     sameCouponTickets[coupon].append(tic) # ticket joins the correct club
     maxClubSize = max(maxClubSize, len(sameCouponTickets[coupon]))
   print
-  pprint(sameCouponTickets)
+  #pprint(sameCouponTickets)
   
   howManyMaxClubs = 0; #count of clubs have the same max membership
   for coupon, li in sameCouponTickets.iteritems(): #O(C) loop for C unique coupon codes
     if maxClubSize == len(li):
       howManyMaxClubs += 1
   return howManyMaxClubs, maxClubSize
+def solve(lo,hi):
+  return solveInLinearTime(lo, hi)
 def main():  
-  #assert (1,2) == 
-  solve(99,151)
-  return
+  assert (1,2) == solve(1,10)
   assert (5,1) == solve(1,5)
   assert (1,2) == solve(3,12)
+  solve(79,1141)
 main()
 '''Req: in a lottery each ticket has a positive int tic. It has a (non-unique) coupon code equal to the sum of its digits.
 
