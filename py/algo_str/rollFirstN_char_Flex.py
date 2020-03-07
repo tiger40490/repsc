1b5e7ef84a0ade31eb2bbe46cb3d59a77056be0a
root <35847749+tiger40490@users.noreply.github.com>
2020-03-07 22:13:32 +0800

    299 tested

---
+++ py/algo_str/rollFirstN_char_Flex.py
@@ -0,0 +1,32 @@
+from bisect import *
+def rollBy(ch, increment):
+  old = ord(ch)-ord('a')
+  new = (old + increment) % 26 + ord('a')
+  return chr(new)
+def solve(orig, roll):
+  roll.sort(reverse=True)
+  for i in range(len(roll)-1,-1,-1):
+    if roll[i] == 0: roll.pop()
+  roll.reverse()
+  print roll
+  sz = len(orig); rollCnt=len(roll)
+  ret = list()
+  for pos in xrange(sz):
+    # compute how many rolls affect this position. use bisect to find pos within roll
+    howManyMissMe = bisect_right(roll, pos)
+    ch = orig[pos]
+    print howManyMissMe,'=howManyMissMe; ch=',ch
+    
+    newCh = rollBy(ch, rollCnt-howManyMissMe)
+    print ch, newCh
+    ret.append(newCh)
+  print ret
+  return ''.join(ret)
+def main():
+  assert 'dda' == solve('abz', [1,2,3])
+  assert 'bca' == solve('abz', [3])
+  assert 'aaaaa' == solve('vwxyz', [1,2,3,4,5])
+#  solve('abc', [0,0,1,1,2,2,2,3,3])
+main()
+'''Req: 
+'''
