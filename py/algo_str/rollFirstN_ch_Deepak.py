25b8216ebc8fb99879c43ad6ca161e6c68922048
tiger40490 <35847749+tiger40490@users.noreply.github.com>
2020-03-09 00:01:19 +0800

    hp  commentsi+renameFile

--- py/algo_str/rollFirstN_ch_Deepak.py
+++ py/algo_str/rollFirstN_ch_Deepak.py
@@ -3,9 +3,10 @@ showcase idiom: reverse-iterate a list using the index values last, last-1, ...0
 
 showcase: character relacement by position in a string is not allowed in python, so I populate a list of chars one by one. I think this is efficient and clean. Note str.replace() creates a copy anywah. No in-place replacement.
 
+All hackerrank tests passed, but not sure if this solution is efficient or elegant in implementation
 '''
 from bisect import *
-def rollBy(ch, increment):
+def rollBy(ch, increment): # roll a character (say 'a') by the increment (say 13) would produce 'n'
   old = ord(ch)-ord('a')
   new = (old + increment) % 26 + ord('a')
   return chr(new)
@@ -34,6 +35,5 @@ def main():
   assert 'aaaaa' == solve('vwxyz', [1,2,3,4,5])
 #  solve('abc', [0,0,1,1,2,2,2,3,3])
 main()
-'''Req: See Deepak email 7 Mar 2019
-All hackerrank tests passed
+'''Req: See Deepak email 7 Mar 2019. Fairly contrived and unnatural IMHO.
 '''
