dddaeeadaef131255319b8b2867e22dc739740b0
unknown <35847749+tiger40490@users.noreply.github.com>
2020-09-06 11:32:26 +0800

    a95  asserts

--- py/88miscIVQ/5minInterval_MS.py
+++ py/88miscIVQ/5minInterval_MS.py
@@ -1,8 +1,6 @@
 '''
-todo: add more test cases
-todo: count skipped  and assert
 todo: call input as 'minis' and outputs as windows
-showcase assertions on lineCnt == cumSize and size <= 5 to give much-needed quality assurance
+showcase assertions on goodLineCnt == cumSize and size <= 5 to give much-needed quality assurance
 showcase calling superclass ctor
 '''
 INVALID_FLOAT = -1
@@ -12,25 +10,23 @@ isVerbose = True
 
 class Interval:
   def __init__(self, windowStart=''):
-    self.windowStart=windowStart
-    self.li=list() # should be fixed size 
+    self.windowStart=windowStart # earliest minute permitted in this interval
+    self.li=list()
     self.open=self.close=self.high=self.low=self.vol=INVALID_FLOAT
   def __repr__(self): # supports pprint
     return self.__str__()
-  def __str__(self): # format as required
+  def __str__(self):
     return date + ',' + self.windowStart + ',' + self.open + ',' + str(self.high) + ',' + str(self.low) + ',' + self.close + ',' + str(self.vol)
     
 class MiniInterval(Interval):
   def __init__(self, fields):
     Interval.__init__(self)
-    #print "input Interval size =", self.size
     #self.tstamp=fields[0]
     self.open =fields[1]
     self.close=fields[4]
     self.high =float(fields[2])
     self.low  =float(fields[3])
     self.vol  =float(fields[5])
-    #print self
     
 class AggInterval(Interval):
   def __init__(self, window_Start=''):
@@ -38,7 +34,7 @@ class AggInterval(Interval):
   def add(self, inInt):
     assert len(self.li) < interval_length 
     self.li.append(inInt)
-  def wrapUp(self):
+  def wrapUp(self, adj=1):
     global cumSize
     if len(self.li)==0: return
     self.open = self.li[0].open
@@ -46,21 +42,22 @@ class AggInterval(Interval):
     self.high = max(rec.high for rec in self.li)
     self.low = min(rec.low for rec in self.li)
     self.vol = sum(rec.vol for rec in self.li)
+    
     if isVerbose: print "wrap----up: size=", len(self.li), '[', self,']\n'
     assert len(self.li) <= interval_length
     cumSize += len(self.li)
-    assert lineCnt == cumSize, 'lineCnt = %d, cumSize = %d' % (lineCnt, cumSize)
+    assert lineCnt-adj-skipCntA-skipCntB == goodLineCnt == cumSize , 'lineCnt = %d, skipCntA = %d, skipCntB = %d, goodLineCnt = %d, cumSize = %d' % (lineCnt, skipCntA, skipCntA, goodLineCnt, cumSize)
     if isVerbose: print >>outfile, self
     
 # global variables:
 windowStart=""
 windowEnd=""
 outInt=None
-lineCnt=cumSize=0
+lineCnt = goodLineCnt = skipCntA=skipCntB = cumSize=0
 date=''
 outfile = None
 
-def updateWindow(isIncremented=False): # to rename
+def updateWindow(isIncremented=False):
   global outInt, windowStart, windowEnd
   HH,mm = windowStart.split(":")
   hour   = int(HH)
@@ -84,10 +81,10 @@ def resetDate(dt):
   updateWindow(True)
 
 def load1file(filename="AM2"):
-  global date, lineCnt, isVerbose, windowStart, outInt, lineCnt, cumSize, outfile
+  global date, goodLineCnt, isVerbose, windowStart, outInt, lineCnt, goodLineCnt, skipCntA, skipCntB, cumSize, outfile
   windowStart=""
   outInt=None
-  lineCnt=cumSize=0
+  lineCnt = goodLineCnt = skipCntA=skipCntB = cumSize=0
   date=''
   outfile = open('5min_%s.txt' % filename, 'w');
 
@@ -95,6 +92,7 @@ def load1file(filename="AM2"):
   f = open('input1min/' + filename + '.txt', "r"); lines = f.readlines(); f.close()
   if len(lines) > 9999: isVerbose = False
   for line in lines:
+    lineCnt += 1
     fields = line.strip().split(",")
     if isVerbose: print fields[:2]
 
@@ -105,20 +103,26 @@ def load1file(filename="AM2"):
     time = fields[0]
     if isMktHourOnly and 16 <= int(time.split(":")[0] ):
       if isVerbose: print 'input is after market close.... ignored';
+      skipCntA += 1
       continue
     while windowEnd < time:
       outInt.wrapUp() # must wrap up current object before updateWindow() wipes it out
       updateWindow()
 
-    assert time <= windowEnd
-    if windowStart <= time :
+    if time < windowStart :
+      if isVerbose: print 'input is after market close.... ignored';
+      skipCntB += 1
+      continue    
+    assert windowStart <= time <= windowEnd
+    if True:
       rec = MiniInterval(fields)
       outInt.add(rec)
       if isVerbose: print windowStart + ' : adding to window: ', time, rec
-      lineCnt += 1
-  print 'final wrapUp()...'
-  outInt.wrapUp()
-  print 'lineCnt = %d, cmSize = %d' % (lineCnt, cumSize)
+      goodLineCnt += 1
+    
+  print 'final wrapUp() ... ZERO lineCnt adjustment needed'
+  outInt.wrapUp(0)
+  print 'lineCnt = %d, skipCntA = %d, skipCntB = %d, goodLineCnt = %d, cumSize = %d' % (lineCnt, skipCntA, skipCntA, goodLineCnt, cumSize)
   
 def main():
   load1file('AA')
