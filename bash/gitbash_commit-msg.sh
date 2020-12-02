db66009afc3bff0331d0167f4041cc06da7bfbc5
unknown <35847749+tiger40490@users.noreply.github.com>
2020-12-03 01:35:52 +0800

    299 major

--- bash/gitbash_commit-msg.sh
+++ bash/gitbash_commit-msg.sh
@@ -5,16 +5,22 @@
 # showcase printf
 # showcase string match
 # showcase compound if-block
+# showcase bash string regex.. No quote please.
 
 hn=`hostname`
 [[ $hn == 'LAPTOP-3U2UKQPB' ]] && prefix='hp '
-[[ $hn == 'DESKTOP-3V5I3UV' ]] && prefix='a299' #Acer$299 
-[[ $hn == 'MAXIMUSIII' ]]      && prefix='116'
+[[ $hn == 'acer299' ]] && prefix='299' #Acer$299 
 [[ $hn == 'DSIOTE10077' ]]     && prefix='mlp'
 # prefix should be space-saving, fairly unique
 orig=`cat $1`
+
+#below uses non-regex
 [[ $orig == "$prefix"* ]] && {
  printf "vvv-- commit-msg --> q[$prefix] is already a prefix in the user-supplied commit msg\n" >&2
  exit 0
 } 
-printf "$prefix $orig" > $1
+[[ $orig =~ ^new$|^minor$|^major$|^inter$ ]] && {
+  suffix="_$RANDOM"
+  # printf "suffix = $suffix\n" >&2
+}
+printf "$prefix $orig$suffix" > $1
