9147e180be4a741054b0c82a531ffe69cd3975a5
unknown <35847749+tiger40490@users.noreply.github.com>
2020-12-24 22:53:15 +0800

    299 major_1530

--- py/algo_str/prodOf2bigInt_AshS.py
+++ py/algo_str/prodOf2bigInt_AshS.py
@@ -1,23 +1,24 @@
 '''
 todo: more tests
-todo: rename to include company name?
+fix assert on 9*9
 '''
 def multiply2unlimitedInts(strA, strB): # returns str (not list) for easy testing 
   assert len(strA) <= len(strB)
   liA=[int(digit) for digit in reversed(strA)]
   liB=[int(digit) for digit in reversed(strB)]
-  revProduct=[0] * (1+len(strA + strB)) # up to 2 digits in each element
+  revProduct=[0] * (1+len(strA + strB)) # each list element is up to 81
   print liB
-  for idxA in xrange(len(liA)):
+  for idxA in xrange(len(liA)): # 0 to last index
     digitA=liA[idxA]
     #print digitA
     for idxB in xrange(len(liB)):
       digitB=liB[idxB]
       revProduct[idxA+idxB] += (digitA * digitB)
   print revProduct, ': Each element to become single-digit ...'
+  
   for i in xrange(len(revProduct)):
     smallProd=revProduct[i]
-    assert smallProd <=9*9
+    #assert smallProd <=9*9
     tmp = list(str(smallProd))
     #print tmp
     if smallProd > 9:
@@ -35,10 +36,12 @@ def multiply2unlimitedInts(strA, strB): # returns str (not list) for easy testin
   assert int(ret) == int(strA) * int(strB)
   return ret
 def test():
+  multiply2unlimitedInts('89', '798')
+  #return
   multiply2unlimitedInts('24', '18')
   multiply2unlimitedInts('4', '18446744073709551613')
 test()
 
-'''Req: given two valie but superlong strings represent unsigned ints, mutiply them using only 32-bit int multiplication
+'''Req: given two valid but superlong strings represent unsigned ints, mutiply them using only 32-bit int multiplication
 
 '''
