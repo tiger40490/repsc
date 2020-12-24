229d9ad4b4ddabf4230062624b255d526016780d
unknown <35847749+tiger40490@users.noreply.github.com>
2020-12-25 00:54:53 +0800

    299 major_25769

--- py/algo_str/prodOf2bigInt_AshS.py
+++ py/algo_str/prodOf2bigInt_AshS.py
@@ -1,33 +1,27 @@
 '''
 todo: more tests
-todo: fix assert on 9*9
 '''
 def multiply2unlimitedInts(strA, strB): # returns str (not list) for easy testing 
   assert len(strA) <= len(strB), 'left string would drive the outer loop'
   liA=[int(digit) for digit in reversed(strA)]
   liB=[int(digit) for digit in reversed(strB)]
-  revProduct=[0] * (1+len(strA + strB)) # each list element is up to ...?
+  revProduct=[0] * (1+len(strA + strB)) # some list element can exceed 1000
   print liB
   for idxA in xrange(len(liA)): # 0 to last index
     digitA=liA[idxA]
     #print digitA
     for idxB in xrange(len(liB)):
       digitB=liB[idxB]
-      revProduct[idxA+idxB]   += digitA * digitB
+      revProduct[idxA+idxB] += digitA * digitB
   print revProduct, ': Each element to become single-digit ...'
   
   for i in xrange(len(revProduct)-1):
-    tmp=revProduct[i]
-    revProduct[i]    = tmp%10
-    revProduct[i+1] += tmp/10
-  print revProduct
-  ret=''; isWithinLeadingZeros=True
+    tmpCopy=revProduct[i]
+    revProduct[i]    = tmpCopy%10
+    revProduct[i+1] += tmpCopy/10
+  print revProduct, ': Each element should be single-digit'
   
-  # now reverse and remove leading zeros
-  for digit in reversed(revProduct):
-    if isWithinLeadingZeros and digit==0: continue
-    isWithinLeadingZeros=False
-    ret += str(digit)
+  ret = ''.join(str(digit) for digit in reversed(revProduct))
   print ret
   assert int(ret) == int(strA) * int(strB)
   return ret
