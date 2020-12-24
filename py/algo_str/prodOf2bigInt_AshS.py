f47fcc237c6e728232dfc98fb6eadcc61ea0e9b6
unknown <35847749+tiger40490@users.noreply.github.com>
2020-12-25 00:14:59 +0800

    299 bugFix

--- py/algo_str/prodOf2bigInt_AshS.py
+++ py/algo_str/prodOf2bigInt_AshS.py
@@ -1,29 +1,25 @@
 '''
 todo: more tests
-fix assert on 9*9
+todo: fix assert on 9*9
 '''
 def multiply2unlimitedInts(strA, strB): # returns str (not list) for easy testing 
-  assert len(strA) <= len(strB)
+  assert len(strA) <= len(strB), 'left string would drive the outer loop'
   liA=[int(digit) for digit in reversed(strA)]
   liB=[int(digit) for digit in reversed(strB)]
-  revProduct=[0] * (1+len(strA + strB)) # each list element is up to 81
+  revProduct=[0] * (1+len(strA + strB)) # each list element is up to ...?
   print liB
   for idxA in xrange(len(liA)): # 0 to last index
     digitA=liA[idxA]
     #print digitA
     for idxB in xrange(len(liB)):
       digitB=liB[idxB]
-      revProduct[idxA+idxB] += (digitA * digitB)
+      revProduct[idxA+idxB]   += digitA * digitB
   print revProduct, ': Each element to become single-digit ...'
   
-  for i in xrange(len(revProduct)):
-    smallProd=revProduct[i]
-    #assert smallProd <=9*9
-    tmp = list(str(smallProd))
-    #print tmp
-    if smallProd > 9:
-      revProduct[i] = tmp[1] #lower digit
-      revProduct[i+1] +=int(tmp[0]) #higher digit
+  for i in xrange(len(revProduct)-1):
+    tmp=revProduct[i]
+    revProduct[i]    = tmp%10
+    revProduct[i+1] += tmp/10
   print revProduct
   ret=''; isWithinLeadingZeros=True
   
@@ -32,14 +28,15 @@ def multiply2unlimitedInts(strA, strB): # returns str (not list) for easy testin
     if isWithinLeadingZeros and digit==0: continue
     isWithinLeadingZeros=False
     ret += str(digit)
-  #print ret
+  print ret
   assert int(ret) == int(strA) * int(strB)
   return ret
 def test():
+  multiply2unlimitedInts('9999999999999', '9999999999999999')
   multiply2unlimitedInts('89', '798')
   #return
   multiply2unlimitedInts('24', '18')
-  multiply2unlimitedInts('4', '18446744073709551613')
+  multiply2unlimitedInts('84', '98446744073709551613')
 test()
 
 '''Req: given two valid but superlong strings represent unsigned ints, mutiply them using only 32-bit int multiplication
