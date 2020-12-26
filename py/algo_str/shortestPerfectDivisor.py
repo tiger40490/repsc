0013833787d0e09516ded54fd7c733421a465559
unknown <35847749+tiger40490@users.noreply.github.com>
2020-12-26 14:28:21 +0800

    a95 minor_26173

--- py/algo_str/shortestPerfectDivisor.py
+++ py/algo_str/shortestPerfectDivisor.py
@@ -19,28 +19,27 @@ class Profile:
       self.len = len(last) + len(suff)
       self.ft = profiles[last].ft.copy()
     for c in suff:
-      self.ft[c] += 1     
+      self.ft[c] += 1
+    if self.len > 22: print self.ft
   def __str__(self):
     ret = ""
     ret += str(self.ft)
     return ret
 profiles = dict() # str -> Profile
 lastDiv=''
+lenH=0
 def slowCheck(haystack, lenD):
-  lenH = len(haystack)
   reps = lenH / lenD
+  divisor=haystack[:lenD]
   for i in xrange(reps):
-    if haystack[:lenD] != haystack[i*lenD: i*lenD+lenD]: return False
+    if divisor != haystack[i*lenD: i*lenD+lenD]: return False
   print ':) slowCheck() passed :)'
   return True  
-    
 def quickCheck(haystack, lenD):
-  ''' O[ len(D) + 26 ], assuming haystack profile already constructed. No need for O(H) scan of entire haystack :)
+  ''' O[ lenD + 26 ], assuming haystack profile already constructed at start-up.
   '''
-  global lastDiv
   div = haystack[:lenD]
   print 'div = '+div
-  lenH = len(haystack)
   if (lenH % lenD ) > 0: return 'length check failed'
   reps = lenH / lenD
   
@@ -48,22 +47,22 @@ def quickCheck(haystack, lenD):
     profiles[haystack] = Profile(haystack)
   frqTableH = profiles[haystack].ft
   
-  if div not in profiles:
+  if div not in profiles: #O[lenD] loop
     suffix = haystack[ len(lastDiv) : lenD ]
     assert len(lastDiv) + len(suffix) == lenD
     profiles[div] = Profile(suffix, lastDiv)
     #profiles[div] = Profile(haystack[:lenD])
   frqTableD = profiles[div].ft
-  lastDiv = div
+  global lastDiv;  lastDiv = div
   
   if frqTableH.keys() != frqTableD.keys(): return "unique_letters set don't match"
-  for k,v in frqTableD.items():
-    v2 = frqTableH[k]
-    if v2 != v * reps: return k + ' : has a mismatched count in div vs haystack'
+  for ch,v in frqTableD.items(): #O[26] loop assuming only 26 unique chars present
+    v2 = frqTableH[ch]
+    if v2 != v * reps: return ch + ' : has a mismatched count in div vs haystack'
   return 0 # good
 
 def solFT(haystack): # solution based on frq table
-  global lastDiv;  lastDiv=''
+  global lastDiv, lenH;  lastDiv=''; lenH = len(haystack)
   print '==== new haystack ==== ' + haystack
   for lenD in xrange(1, len(haystack)/2+1):
     res = quickCheck(haystack, lenD)
@@ -71,6 +70,15 @@ def solFT(haystack): # solution based on frq table
       return lenD
     #print res
   return -1 # unsuccessful
+def main():
+  assert 10 == solFT('abbabbabb-abbabbabb-abbabbabb-')
+  assert 3  == solFT('abbabbabb')
+  assert -1 == solFT('dkdkdkkdkkkdkkdkdkkdkddkdddkdkd')
+main()
+'''Req: given a long string named haystack, find the shortest divisor string dd such that some repetition of dd equals haystack. In other words, haystack can split into a series of the divisor string dd. Return length of dd, or -1 if unsuccessful.
+
+https://bintanvictor.wordpress.com/wp-admin/post.php?post=39095&action=edit&classic-editor has some comments.
+'''
 def SolA (s, t): #AshS, tests unknown
   if not isPerfectDivisor(s,t): return -1
   occrence_set = set()
@@ -86,12 +94,3 @@ def SolA (s, t): #AshS, tests unknown
       occrence_set.add(c)
   if not isPerfectDivisor(t, smallest_substr): return -1
   return len(smallest_substr)
-def main():
-  assert 10 == solFT('abbabbabb-abbabbabb-abbabbabb-')
-  assert 3  == solFT('abbabbabb')
-  assert -1 == solFT('dkdkdkkdkkkdkkdkdkkdkddkdddkdkd')
-main()
-'''Req: given a long string named haystack, find the shortest divisor string dd such that some repetition of dd equals haystack. In other words, haystack can split into a series of the divisor string dd. Return length of dd, or -1 if unsuccessful.
-
-https://bintanvictor.wordpress.com/wp-admin/post.php?post=39095&action=edit&classic-editor has some comments.
-'''
