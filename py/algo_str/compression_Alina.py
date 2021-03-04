07c01242160db2b1c343d13ebbe80c706b7d613d
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-04 16:15:47 +0800

    299 comment

--- py/algo_str/compression_Alina.py
+++ py/algo_str/compression_Alina.py
@@ -1,10 +1,7 @@
 '''
->>> ord('a')
-97
->>> chr(97)
-'a'
->>> chr(ord('a') + 3)
-'d'
+re.sub() to replace every matched substr with a transformed version.
+This technique uses capture group.
+
 '''
 import re
 def betterCompression(s):
@@ -18,11 +15,11 @@ def betterCompression(s):
     frq = int(li[2*i + 1])
     table[ord(ch)-ord('a')] += frq
     #print ch, frq, table[ord(ch)-ord('a')]
-	
+
   output=''
   for i in xrange(26):
     if table[i]:
       output += chr(i+97) + str(table[i])
   print output
-	
+
 betterCompression('a3c9b222c1')
