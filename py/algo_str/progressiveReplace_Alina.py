a37f76b35fa9b9ddb14d817bc7acc2372429648f
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-04 16:30:40 +0800

    299 minor_13282

--- py/algo_str/progressiveReplace_Alina.py
+++ py/algo_str/progressiveReplace_Alina.py
@@ -4,6 +4,7 @@ This technique uses capture group.
 
 '''
 import re
+offset = ord('a')
 def betterCompression(s):
   table = [0] * 26
   readable = re.sub(r'([a-z])', r' \1 ', s)
@@ -13,13 +14,13 @@ def betterCompression(s):
   for i in xrange(sz/2):
     ch  = li[2*i]
     frq = int(li[2*i + 1])
-    table[ord(ch)-ord('a')] += frq
+    table[ord(ch)-offset] += frq
     #print ch, frq, table[ord(ch)-ord('a')]
 
   output=''
   for i in xrange(26):
     if table[i]:
-      output += chr(i+97) + str(table[i])
+      output += chr(i+offset) + str(table[i])
   print output
 
 betterCompression('a3c9b222c1')
