4ee03585927e3d5ce16efe73b5932750f74d26e3
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-04 12:37:43 +0800

    299 new

---
+++ py/algo_str/compression_Alina.py
@@ -0,0 +1,28 @@
+'''
+>>> ord('a')
+97
+>>> chr(97)
+'a'
+>>> chr(ord('a') + 3)
+'d'
+'''
+import re
+def betterCompression(s):
+  table = [0] * 26
+  readable = re.sub(r'([a-z])', r' \1 ', s)
+  #print readable
+  li = readable.split()
+  sz = len(li); assert sz % 2 == 0
+  for i in xrange(sz/2):
+    ch  = li[2*i]
+    frq = int(li[2*i + 1])
+    table[ord(ch)-ord('a')] += frq
+    #print ch, frq, table[ord(ch)-ord('a')]
+	
+  output=''
+  for i in xrange(26):
+    if table[i]:
+      output += chr(i+97) + str(table[i])
+  print output
+	
+betterCompression('a3c9b222c1')
