97eaa78edfe9b961b05770941597ed5105ed2068
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-06 01:40:19 +0800

    a95 majorRefactor

--- cpp/lang_11rvr/arrayHolder.cpp
+++ cpp/lang_11rvr/arrayHolder.cpp
@@ -3,10 +3,9 @@
 #include<iostream>
 using namespace std;
 struct S {
-	int length;
+	int length; //without saving the original length, it's impossible to recover the initial size of array-new
 	int* ptr; // int array on heap, owned by this instance not shared.
-    S(int px = 1) : 
-	  length{ px },  ptr{new int[length]} { cout << "S()\n";  }
+    S(int px = 1) : length{ px },  ptr{new int[length]} { cout << "S()\n";  }
 	
 	~S() {
 		delete [] ptr;
@@ -15,6 +14,7 @@ struct S {
 	S& operator=( S const & other) {
 	  if (&other != this) {
 		length = other.length;
+		
 		delete [] ptr;
 		ptr = new int[length]; 
 		// copy content from other.ptr
@@ -22,10 +22,13 @@ struct S {
 	  }
 	  return *this;
 	}
+	//explicit 
 	S(S const & other) : length{ other.length }, ptr{new int[length]} { 
 	  // copy content from other.ptr
 	  cout << "cp-ctor\n"; 
 	}
+	
+	// now the move facilities:
 	S& operator=(S&& other) {
 	  if (&other != this) {		
 		delete[] ptr;
@@ -38,22 +41,35 @@ struct S {
 	}
 	S(S&& other) : length{ other.length }, ptr{ other.ptr }  { other.ptr = nullptr;	cout << "S(S&& other)\n";	} 
 };
-S IncrementIdAndReturnByRvRef(S&& src) { src.length++; 	return src; }
+S truncateByRVR(S&& src) { 
+  --src.length; // src instance is still usable until now
+  return move(src); // src is now used in a move-construction, iFF you use std::move()
+}
 
-S IncrementIdAndReturnByRef(const S& src) { 	return src; }
-
+S truncateByConstRef(S const& src) { 
+ S ret(src); 
+ --ret.length;
+ return ret;
+}
 S GetS() { return S(); }
 
 int main(){
-	S s;
-	S obj(s); //Copy constructor
-	S obj0(move(s)); // I do not need s anymore
-	S obj1 = GetS();  //move elison RVO
-	S obj2 (GetS()); //Move elison RVO
+	S obj9;
+	S obj(obj9); //Copy constructor
+	S obj0(move(obj9)); // I do not need obj9 anymore
+	S obj1 = GetS();  //RVO
+	cout<<33333<<endl;
+	S obj2 (GetS()); //RVO
+	cout<<4444<<endl;
 	S obj3 (move(GetS()));  //move constructor. Obj2 construction is more performant than this. 
+	cout<<5555<<endl;
 	obj2 = GetS(); //Call move assigmemnt operator to assign temporary/dying/expiring value to the obj2. 
-	auto a = IncrementIdAndReturnByRvRef(GetS());
-	auto b = IncrementIdAndReturnByRef(GetS()); // Temporary cannot be modified without const_cast
+	cout<<6666<<endl;
+	
+	auto a = truncateByRVR(GetS());
+	cout<<7777<<endl;
+	
+	auto b = truncateByConstRef(GetS()); 
 	cout << a.length << " " << b.length << endl;
 }
 /* Modified from demo program from my friend Ashish.
