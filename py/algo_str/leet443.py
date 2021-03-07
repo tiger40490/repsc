ca208ccd36c0160c38aca4e839a1df1ca10ab53c
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-07 13:45:11 +0800

    a95 new_31916

---
+++ py/algo_str/leet443.py
@@ -0,0 +1,35 @@
+'''
+findall() is NOT useful when leetcode requirement is stringent like O(1) space. With those requirmeents, you need to write low-level code that's similar in C, java or python
+
+'''
+import itertools
+# ------------solution 1 based on Alina ------------
+def groupby(chars): # breaks O(1) space 
+    ret = 0    
+    result_list = []
+    for k, consecutiveItems in itertools.groupby(chars):
+        cnt = len(list(consecutiveItems))
+        result_list.append(k)
+        if cnt==1: ret +=1
+        else: 
+            ret += 1 #the char itself
+            theStr = str(cnt)
+            ret += len(theStr)
+            result_list.extend(list(theStr))
+    #print(result_list)
+    chars[:ret] = result_list
+    print chars
+    return ret
+def main():
+  assert 5==groupby(list('abbccc'))
+  assert 6==groupby(list('aabbccc'))
+  assert 1==groupby(list('a'))
+  assert 4==groupby(list('abbbbbbbbbbbb'))
+  assert 6==groupby(list('aaabbaa'))
+main()  
+'''Req: https://leetcode.com/problems/string-compression/
+There are two outputs:
+1) the function need to return an integer
+2) the function need to modify (in place) the input list, replacing the left portion with the compression result_list
+
+'''
