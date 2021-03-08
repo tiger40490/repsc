dc176dd06a693c36c703a6deda7c807f81e0e99c
unknown <35847749+tiger40490@users.noreply.github.com>
2021-03-08 14:22:41 +0800

    299 new_21500

---
+++ py/algo_str/logSummary_Alina.py
@@ -0,0 +1,29 @@
+'''
+showcase re.sub() to remove quoted substring (containing spaces)
+
+showcase file read/write using context manager. No file close needed.
+'''
+import re
+from collections import defaultdict
+def do1file(filename):
+  hm = defaultdict(int)
+
+  with open(filename) as infile:
+    for line in infile:
+      #print line
+      line = re.sub(r'\[.*\]', '', line)
+      line = re.sub(r'\".*\"', '', line)
+      li=line.split()
+      #print li
+      host = li[0]
+      bytes = li[-1]
+      hm[host] += int(bytes)
+
+# write to output file
+  with open('records_'+filename, 'w') as outfile :
+    for k,v in hm.items():
+      print k,v
+      outfile.write(k+' '+str(v)+'\n')
+  
+# testing:
+do1file("input.txt")
