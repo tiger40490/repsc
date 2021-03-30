6f333f9f811d47c969043f86adc7ab939bd5deb3
victan <35847749+tiger40490@users.noreply.github.com>
2021-03-30 15:03:26 +0800

    mlp minor_30463

--- csharp/demo1.cs
+++ csharp/demo1.cs
@@ -1,14 +1,13 @@
 using System;
-public class Program
-{
-    public static void Main(string [] args)
-    {
+public class Program{
+    public static void Main(string [] args){
         Console.WriteLine("Hello world");
     }
 }
 /* based on https://abundantcode.com/how-to-run-the-c-program-from-commandline-using-csc-exe/
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe demo.cs 
-demo.exe
+PATH=$PATH:/C/Windows/Microsoft.NET/Framework/v4.0.30319/
+
+csc.exe //nologo //out:a.exe demo1.cs && ./a.exe
 
 */
