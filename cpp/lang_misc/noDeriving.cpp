30502476e6c60b1a52a047c3f947aeb7ae096889
unknown <35847749+tiger40490@users.noreply.github.com>
2021-04-30 19:46:59 +0800

    299 minor_30589

--- cpp/lang_misc/noDeriving.cpp
+++ cpp/lang_misc/noDeriving.cpp
@@ -1,28 +1,26 @@
-// "virtual" inheritance forces Derived to call StopInheritance ctor directly. 
+// showcase: calling ctor on stack to create an unnamed temp, then use it in a function call.
 #include <iostream>
 using namespace std;
-
-class StopInheritance{
+class PoisonPill{
   friend class FinalClassPreCpp11;
-  StopInheritance(){} //private ctor
+  PoisonPill(){} //private ctor
 };
 
-struct FinalClassPreCpp11: virtual /*needed*/ StopInheritance{
-  void Display(){
-    cout<<" My FinalClassPreCpp11 Class"<<endl;
+struct FinalClassPreCpp11: virtual/*needed*/ PoisonPill{
+// "virtual" inheritance forces Derived to call PoisonPill ctor DIRECTLY, without FinalClassPreCpp11 as an agent
+  void show(){
+    cout<<"FinalClassPreCpp11"<<endl;
   }
 };
 
 struct Derived: public FinalClassPreCpp11{
-  void Display(){
-    cout<<" My derived Class"<<endl;
-  }
+  void show(){ cout<<"Derived"<<endl; }
 };
 int main(){  
-  //Derived obj; //breaks compiler
-  FinalClassPreCpp11 obj;
-  obj.Display();
+  //Derived().show(); //breaks compiler if using "virtual" inheritance
+  FinalClassPreCpp11().show();
 }
-/*Req: design a class to be uninheritable but can be instantiated as usual. See https://btv-gz.dreamhosters.com/wp-admin/post.php?post=40465&action=edit
+/*Req: design an uninheritable class that we will instantiate as usual. 
 
+See https://btv-gz.dreamhosters.com/wp-admin/post.php?post=40465&action=edit
 */
