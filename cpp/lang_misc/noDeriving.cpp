a8111bda243c84b989e2ab58ab2f638d9bb092e3
unknown <35847749+tiger40490@users.noreply.github.com>
2021-04-30 19:12:56 +0800

    299 new_16473

---
+++ cpp/lang_misc/noDeriving.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class StopInheritance{
+  friend class FinalClassPreCpp11;
+  StopInheritance(){} //private ctor
+};
+
+struct FinalClassPreCpp11: virtual StopInheritance{
+  void Display(){
+    cout<<" My FinalClassPreCpp11 Class"<<endl;
+  }
+};
+
+struct Derived: public FinalClassPreCpp11{
+  void Display(){
+    cout<<" My derived Class"<<endl;
+  }
+};
+int main(){  
+  //Derived obj; //breaks compiler
+  FinalClassPreCpp11 obj;
+  obj.Display();
+}
+/*Req: design a class to be uninheritable. See https://btv-gz.dreamhosters.com/wp-admin/post.php?post=40465&action=edit
+
+*/
