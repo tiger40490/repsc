977482b21472b4fb50dc8bca2245afe92273c630
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-22 03:18:43 +0800

    a95 minor_17607

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -1,5 +1,7 @@
 '''
-Shall we formulate our vocab around the payload objects? Each object has an old id and a new id.
+todo: improve var naming
+todo: simplify
+
 '''
 a=['A','B','C', 'D']
 newTenantIdAt=list()
@@ -27,14 +29,13 @@ def solve(input):
     for i in xrange(sz):
       if newTenantIdAt[i] == -1: continue
       targetPos = i
-      evictedId = newTenantIdAt[targetPos]
       shelter = homeless(targetPos, a[targetPos]); print 'shelter has: ', shelter
       break
     else:
       print 'All positions filled'
       break
     while True: #one round
-        sourcePos = newTenantIdAt[targetPos]
+        sourcePos = newTenantIdAt[targetPos] # evictedId ??
         if sourcePos == shelter.id: 
           a[targetPos] = shelter.val
           newTenantIdAt[targetPos] = -1
@@ -46,8 +47,8 @@ def solve(input):
         targetPos = sourcePos
 def main():
   #solve([1,2,3,0]) # one round enough
-#  solve([1,0,3,2]) # two rounds
-  solve([1,0,2,3]) # one small round enough.
+  solve([1,0,3,2]) # two rounds
+  #solve([1,0,2,3]) # one small round enough.
 main()
 '''Req: you are given an array of positions, each a subscript into an original array.
 Eg: [3,2,1,0] means after the reshuffle, it looks like [ a[3], a[2], a[1], a[0] ]. However, you need to do this in-situ, in O(1) space complexity, but you can modify the input array.
