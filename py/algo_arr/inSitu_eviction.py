b097ee83d6c4bb3bf697c81447e3e86df1990423
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-22 13:53:53 +0800

    a95 minorRef

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -1,8 +1,9 @@
 '''
-todo: improve var naming
 todo: simplify
 
 showcase: global variable list don't need 'global' keyword. Instead, you can reassign entire list with slice assignment
+
+Variable naming reflects the metaphor, which is crucial to the comprehension of this algo.
 '''
 FILLED = -1 # indicates a target slot is not to be filled again
 newTenentId4dump=list()
@@ -26,7 +27,7 @@ def solve(input):
   newTenantIdAt = {pos: newTenentId for pos, newTenentId in enumerate(input)}
   # dict is slightly less confusing than array
   assert len(newTenantIdAt) == sz
-  assert reduce((lambda x,y: x+y*y), newTenantIdAt.values())==reduce((lambda x,y: x+y*y), xrange(sz))
+  assert range(sz) == sorted(newTenantIdAt.values())
   newTenentId4dump[:]=input
   # initialization done
   
@@ -47,19 +48,21 @@ def solve(input):
         newTenantIdAt[targetPos] = FILLED
         dump()
         break
-      sourcePos = newTenantIdAt[targetPos]; assert newTenantIdAt[targetPos] == sourcePos
-      # "targetPos -> sourcePos" inside the dict
-      
-      a[targetPos] = a[sourcePos] #copy the payload from sourcePos to targetPos
+      tenantOldPos = newTenantIdAt[targetPos]       
+      a[targetPos] = a[tenantOldPos] #copy the payload from tenantOldPos to targetPos
       dump()
       newTenantIdAt[targetPos] = FILLED # to indicate targetPos is filled
-      # Now the room at sourcePos is vacant, so sourcePos becomes the new targetPos
-      targetPos = sourcePos 
+      # Now tenantOldPos is vacant, so tenantOldPos becomes the new targetPos
+      targetPos = tenantOldPos 
       
 def main():
+  assert 'DCBA'==solve([3,2,1,0]) # two rounds
+  assert 'ACBD'==solve([0,2,1,3]) # one round enough
   assert 'BCDA'==solve([1,2,3,0]) # one round enough
   assert 'BADC'==solve([1,0,3,2]) # two rounds
   assert 'BACD'==solve([1,0,2,3]) # one small round enough.
+  assert 'ABCD'==solve([0,1,2,3]) # no change
+  
 main()
 '''Req: you are given an array of positions, each a subscript into an original array.
 Eg: [3,2,1,0] means after the reshuffle, it looks like [ a[3], a[2], a[1], a[0] ]. However, you need to do this in-situ, in O(1) space complexity, but you can modify the input array.
