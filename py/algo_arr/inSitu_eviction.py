93f8bf4266b4d961919038cd3509f8f1059372f5
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-22 15:17:41 +0800

    a95 comment

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -65,5 +65,6 @@ def main():
   
 main()
 '''Req: you are given an array of positions, each a subscript into an original array.
-Eg: [3,2,1,0] means after the reshuffle, it looks like [ a[3], a[2], a[1], a[0] ]. However, you need to do this in-situ, in O(1) space complexity, but you can modify the input array.
+Eg: [3,2,1,0] means after the reshuffle, the array should look like [ a[3], a[2], a[1], a[0] ]. However, you need to do this in-situ, in O(1) space complexity, but you can modify the input "instruction" array in adition to the original array.
+Time complexity is less important.
 '''
