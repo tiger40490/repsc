49ef6c43b1091b2cf8c47e4f8d034cec26113d32
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-22 18:43:28 +0800

    a95 majorRef

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -1,12 +1,12 @@
 '''
 todo: simplify
+todo: if target and source pos identical, mark it as filled
 
 showcase: global variable list don't need 'global' keyword. Instead, you can reassign entire list with slice assignment
 
 Variable naming reflects the metaphor, which is crucial to the comprehension of this algo.
 '''
 FILLED = -1 # indicates a target slot is not to be filled again
-newTenentId4dump=list()
 a=list()
 
 class homeless(object):
@@ -16,10 +16,10 @@ class homeless(object):
   def __str__(self):
     return str(self.id) + '=old_pos..payload='+ str(self.val)
 
-def dump():
+def dump(d):
   print '  '.join(a), '<-- a[]'
   print '  '.join(str(i%10) for i in xrange(sz))
-  print ' '.join( '%-2d' %p for p in newTenentId4dump), '<---- newTenantIdAt'
+  print ' '.join( '%-2d' %p for key, p in sorted(d.iteritems())), '<---- newTenantIdAt'
 
 def solve(input):
   a[:]=list('ABCD')
@@ -28,13 +28,14 @@ def solve(input):
   # dict is slightly less confusing than array
   assert len(newTenantIdAt) == sz
   assert range(sz) == sorted(newTenantIdAt.values())
-  newTenentId4dump[:]=input
   # initialization done
   
+  scanStart = 0
   while True:
-    for i in xrange(sz):
+    print 'sanning for first unfilled targetPos from ..', scanStart
+    for i in xrange(scanStart, sz):
       if newTenantIdAt[i] == FILLED: continue
-      targetPos = evictedId = i # first unfilled targetPos
+      targetPos = evictedId = scanStart = i # first unfilled targetPos
       theHomeless = homeless(evictedId, a[evictedId]) 
       print 'theHomeless is ', theHomeless #only 1 homeless
       break
@@ -46,20 +47,20 @@ def solve(input):
       if newTenantIdAt[targetPos] == theHomeless.id: 
         a[targetPos] = theHomeless.val
         newTenantIdAt[targetPos] = FILLED
-        dump()
+        dump(newTenantIdAt)
         break
       tenantOldPos = newTenantIdAt[targetPos]       
       a[targetPos] = a[tenantOldPos] #copy the payload from tenantOldPos to targetPos
-      dump()
+      dump(newTenantIdAt)
       newTenantIdAt[targetPos] = FILLED # to indicate targetPos is filled
       # Now tenantOldPos is vacant, so tenantOldPos becomes the new targetPos
       targetPos = tenantOldPos 
       
 def main():
-  assert 'DCBA'==solve([3,2,1,0]) # two rounds
   assert 'ACBD'==solve([0,2,1,3]) # one round enough
-  assert 'BCDA'==solve([1,2,3,0]) # one round enough
+  assert 'DCBA'==solve([3,2,1,0]) # two rounds
   assert 'BADC'==solve([1,0,3,2]) # two rounds
+  assert 'BCDA'==solve([1,2,3,0]) # one round enough
   assert 'BACD'==solve([1,0,2,3]) # one small round enough.
   assert 'ABCD'==solve([0,1,2,3]) # no change
   
