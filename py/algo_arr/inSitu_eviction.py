6207a9143103fdb62a07f9291a80c4bc8a7fd610
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-22 12:19:38 +0800

    a95 minor_2659

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -1,11 +1,12 @@
 '''
+add asserts to tests
 todo: improve var naming
 todo: simplify
 
 '''
-a=['A','B','C', 'D']
+FILLED = -1 # indicates a target slot is not to be filled again
 newTenantIdAt=list()
-sz=len(a)
+a=['A','B','C', 'D']
 
 class homeless(object):
   def __init__(self, id, payload):
@@ -20,34 +21,37 @@ def dump():
   print ' '.join( '%-2d' %p for p in newTenantIdAt), '<---- newTenantIdAt'
 
 def solve(input):
-  global newTenantIdAt; newTenantIdAt=input
+  a[:]=['A','B','C', 'D']
+  newTenantIdAt[:]=input # to help dump()
+  global sz; sz=len(a)
   assert len(newTenantIdAt) == sz
   assert reduce((lambda x,y: x+y*y), newTenantIdAt)==reduce((lambda x,y: x+y*y), xrange(sz))
   
   while True:
     for i in xrange(sz):
-      if newTenantIdAt[i] == -1: continue
+      if newTenantIdAt[i] == FILLED: continue
       targetPos = i
-      shelter = homeless(targetPos, a[targetPos]); print 'shelter has: ', shelter
+      shelter = homeless(targetPos, a[targetPos]) #only 1 homeless guy in shelter
+      print 'shelter has: ', shelter
       break
     else:
       print 'All positions filled'
-      break
+      return ''.join(a)
     while True: #one round
         sourcePos = newTenantIdAt[targetPos] # evictedId ??
         if sourcePos == shelter.id: 
           a[targetPos] = shelter.val
-          newTenantIdAt[targetPos] = -1
+          newTenantIdAt[targetPos] = FILLED
           dump()
           break
         a[targetPos] = a[sourcePos] #copy the payload from sourcePos to targetPos
         dump()
-        newTenantIdAt[targetPos] = -1 # to indicate targetPos filled
+        newTenantIdAt[targetPos] = FILLED # to indicate targetPos filled
         targetPos = sourcePos
 def main():
-  #solve([1,2,3,0]) # one round enough
-  solve([1,0,3,2]) # two rounds
-  #solve([1,0,2,3]) # one small round enough.
+  assert 'BCDA'==solve([1,2,3,0]) # one round enough
+  assert 'BADC'==solve([1,0,3,2]) # two rounds
+  assert 'BACD'==solve([1,0,2,3]) # one small round enough.
 main()
 '''Req: you are given an array of positions, each a subscript into an original array.
 Eg: [3,2,1,0] means after the reshuffle, it looks like [ a[3], a[2], a[1], a[0] ]. However, you need to do this in-situ, in O(1) space complexity, but you can modify the input array.
