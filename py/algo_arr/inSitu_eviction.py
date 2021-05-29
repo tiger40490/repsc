ed83a4fc6823a7536e1f6b16d12659363ba1b052
unknown <35847749+tiger40490@users.noreply.github.com>
2021-05-29 21:48:56 +0800

    a95 addTest

--- py/algo_arr/inSitu_eviction.py
+++ py/algo_arr/inSitu_eviction.py
@@ -6,6 +6,7 @@ showcase: global variable list don't need 'global' keyword. Instead, you can rea
 
 Variable naming reflects the metaphor, which is crucial to the comprehension of this algo.
 '''
+import random, string
 FILLED = -1 # indicates a target slot is not to be filled again
 a=list()
 
@@ -21,10 +22,9 @@ def dump(d):
   print '  '.join(str(i%10) for i in xrange(sz))
   print ' '.join( '%-2d' %p for p in sorted(d)), '<---- newTenantIdAt'
 
-def solve(input):
-  a[:]=list('ABCD')
+def solve(newTenantIdAt, original='ABCD'):
+  a[:]=list(original)
   global sz; sz=len(a)
-  newTenantIdAt = input
   ## {pos: newTenentId for pos, newTenentId in enumerate(input)} # dict is (slower but) slightly more intuitive than array
   assert len(newTenantIdAt) == sz
   assert range(sz) == sorted(newTenantIdAt)
@@ -48,22 +48,31 @@ def solve(input):
         break
       tenantOldHome = newTenantIdAt[curTargetHome]       
       a[curTargetHome] = a[tenantOldHome] #copy the payload from tenantOldHome to curTargetHome
-      dump(newTenantIdAt)
+      #dump(newTenantIdAt)
       newTenantIdAt[curTargetHome] = FILLED # to indicate curTargetHome is filled
       # Now tenantOldHome is vacant, so tenantOldHome becomes the new curTargetHome
       curTargetHome = tenantOldHome 
   print ' '*30 + '^ ^ ^ ^   All positions filled   ^ ^ ^ ^'
   return ''.join(a)    
-  
+
+def idx2chr(i):
+  return chr(ord('a')+i)  if i <26 else  chr(ord('A')+ i-26)
+
+def constructTestCase(testSz):
+  assert testSz <=52
+  input=range(testSz); random.shuffle(input)
+  expected = ''.join(map(idx2chr,   input))
+  print input, '= input; expected =', expected
+  assert expected == solve(input, original=string.ascii_letters[:testSz])
       
 def main():
   assert 'ACBD'==solve([0,2,1,3]) # one round enough
-  #return
   assert 'DCBA'==solve([3,2,1,0]) # two rounds
   assert 'BADC'==solve([1,0,3,2]) # two rounds
   assert 'BCDA'==solve([1,2,3,0]) # one round enough
   assert 'BACD'==solve([1,0,2,3]) # one small round enough.
   assert 'ABCD'==solve([0,1,2,3]) # no change
+  constructTestCase(52)
   
 main()
 '''Req: you are given an array of positions, each a subscript into an original array.
