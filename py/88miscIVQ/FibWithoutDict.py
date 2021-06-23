a21529c0bb35decbc7a09df14c241cd2dbe5a6b9
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-23 18:57:19 +0800

    299 tested briefly

---
+++ py/88miscIVQ/FibWithoutDict.py
@@ -0,0 +1,20 @@
+fibNums=[0,1,1]
+def fib(num):
+  if num < len(fibNums): return fibNums[num]
+  print 'computing #', num
+  fibNums.append( fib(num-2)+fib(num-1) )
+  print 'done #', num
+  return fibNums[-1]
+
+expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34,55,89,144,233,377,610,987,1597,2584,4181,6765]  
+def test(num):
+  assert expected[num] == fib(num)
+def main():
+  test(20)
+  print '-------------'
+  test(19)
+main()
+'''This solution implements top-down DP with memoization using an array (pre-sized vector), rather than dict. Sounds like more daunting, but actually same complexity.
+
+It turned out that we only need append and lookup, so hashtable is overkill
+'''
