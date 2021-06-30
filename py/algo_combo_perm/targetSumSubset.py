6b879bc245e5fcf798d762ab4d5aa833e3935ec3
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 23:41:56 +0800

    a95 minor_24633

--- py/algo_combo_perm/targetSumSubset.py
+++ py/algo_combo_perm/targetSumSubset.py
@@ -3,7 +3,7 @@
 #from pprint import pprint
 from json import dumps
 from collections import defaultdict
-def genShortestFirst(original):
+def genSubGroupSums(original):
   '''Supose we have scanned 3 elements in the original, and we have a collection of N abbreviations. 
   For the next element 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
   Join the lists into a list of 2N elements.
@@ -15,15 +15,14 @@ def genShortestFirst(original):
       for subsum, abbr in subsums.items():
         tmp[subsum+elem] = abbr + (elem,)
       subsums.update(tmp)
-      print str(len(subsums))+'-element subset Sums:', subsums.keys()
+      print str(len(subsums))+'-element subgroup Sums:', subsums.keys()
   assert len(subsums) <= 2**len(original)
   for k,v in subsums.items(): 
-    print k, 'is a subset sum. One of the subset(s) is', v
+    print k, 'is a subgroup sum. One of the subgroup(s) is', v
   return subsums
 
 def main():
-  genShortestFirst([0,1,1,2])
+  genSubGroupSums([0,1,1,2])
 if __name__ == '__main__': main()
 '''Req: https://btv-gz.dreamhosters.com/wp-admin/post.php?post=41382&action=edit
-A subset is a part of the original objects, perhaps including 2 duplicate objects.
 '''
