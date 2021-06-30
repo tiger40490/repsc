17aeafc06c227cfbceb5afb604d3a7e2244ee8af
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 21:37:12 +0800

    a95 minor_8678

--- py/algo_combo_perm/cbDrill_abbr.py
+++ py/algo_combo_perm/cbDrill_abbr.py
@@ -1,30 +1,29 @@
 '''
-todo: print the list but also a "list sum". Use a lookup table.
 '''
-subSum=dict()
+from collections import defaultdict
+subSum=defaultdict(int)
 def gen(fn, pool, n):
   if n == 0: yield (); return
   for pos, ch in enumerate(pool):
-    for newList in gen(fn, fn(pool,pos), n-1):
-      new = (ch,) + newList
-      # compute list sum based on list sum of newList
-      print newList, '->', new
-      subSum[new] = subSum.get(newList, 0) + int(ch,16)
+    for smallTuple in gen(fn, fn(pool,pos), n-1):
+      new = (ch,) + smallTuple
+      #print smallTuple, '->', new
+      if new not in subSum:
+        subSum[new] = subSum[smallTuple] + int(ch,16)
       yield new
 	  
 pool='0123a' # 2^5 abbreviations if we include "" and the original string.
 def f(pool, pos):
   return pool[pos+1:]
 
-def main():
+def allAbbr():
  for length in xrange(1,len(pool)):
   print '  vvvvvvvvvvv  length =', length
   for abbr in gen(f, pool, length):
     print abbr, '-- sum =', subSum[abbr]
     
-# all abbr
-main()
+allAbbr()
 '''
 Based on cookbook.py, this script was written as a practice.
-Now I think this way of generating 2^N -2 abbreviations has worse time complexity than O(2^N).
+Now I think this way of generating 2^N -2 abbreviations is worse than O(2^N).
 '''
