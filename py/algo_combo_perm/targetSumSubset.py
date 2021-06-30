e93c98d120f85ed1cdf01e38c90510278e445082
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 23:34:19 +0800

    a95 major_24503

--- py/algo_combo_perm/targetSumSubset.py
+++ py/algo_combo_perm/targetSumSubset.py
@@ -1,29 +1,28 @@
 '''
 '''
 #from pprint import pprint
+from json import dumps
 from collections import defaultdict
-subSum=defaultdict(int)
+#subSum=dict()
 def genShortestFirst(original):
   '''Supose we have scanned 3 elements in the original, and we have a collection of N abbreviations. 
   For the next element 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
   Join the lists into a list of 2N elements.
   '''  
   print ' v ---  shortestFirst starting --  v'
-  growing=[()] # start with a single empty string
+  growing={0:()} # start with a single empty string
   for elem in original: # I would now avoid this in favor of the more common for i in range(len(original))
-      sz=len(growing)
-      for abbr in growing[:sz] :
-        newAbbr = abbr + (elem,)
-        growing.append(newAbbr)
-        if newAbbr not in subSum:
-          subSum[newAbbr] = subSum[abbr] + elem
-      print str(len(growing))+'-element', growing
-  assert len(growing) == 2**len(original)
-  print(subSum)
+      tmp = dict()
+      for subsum, abbr in growing.items():
+        tmp[subsum+elem] = abbr + (elem,)
+      growing.update(tmp)
+      print str(len(growing))+'-element subsetSums:', growing.keys()
+  assert len(growing) <= 2**len(original)
+  for k,v in growing.items(): print k,v
   return growing
 
 def main():
-  genShortestFirst([0,1,2,3])
+  genShortestFirst([0,1,1,2])
 if __name__ == '__main__': main()
 '''Req: https://btv-gz.dreamhosters.com/wp-admin/post.php?post=41382&action=edit
 A subset is a part of the original objects, perhaps including 2 duplicate objects.
