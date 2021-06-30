6616542179fcdd6425f8f4b533e0eb85b330c6fa
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 16:18:08 +0800

    a95 minor_22785

--- py/algo_combo_perm/cbDrill_abbr.py
+++ py/algo_combo_perm/cbDrill_abbr.py
@@ -4,10 +4,14 @@ def gen(fn, pool, n):
     for newList in gen(fn, fn(pool,pos), n-1):
       yield [ch] + newList
 	  
-pool='abcde'
+pool='abcde' # 2^5 abbreviations if we include "" and the original string.
 def f(pool, pos):
   return pool[pos+1:]
-for length in xrange(1,len(pool)):
+
+def main():
+ for length in xrange(1,len(pool)):
   for abbr in gen(f, pool, length):
     print abbr
+    
 # all abbr
+main()
