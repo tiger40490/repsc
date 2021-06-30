2738c8bbc701e822b67626803e0d6c3a01c14cbb
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 21:32:15 +0800

    a95 major_25130

--- py/algo_combo_perm/cbDrill_abbr.py
+++ py/algo_combo_perm/cbDrill_abbr.py
@@ -1,20 +1,30 @@
+'''
+todo: print the list but also a "list sum". Use a lookup table.
+'''
+subSum=dict()
 def gen(fn, pool, n):
-  if n == 0: yield []; return
+  if n == 0: yield (); return
   for pos, ch in enumerate(pool):
     for newList in gen(fn, fn(pool,pos), n-1):
-      yield [ch] + newList
+      new = (ch,) + newList
+      # compute list sum based on list sum of newList
+      print newList, '->', new
+      subSum[new] = subSum.get(newList, 0) + int(ch,16)
+      yield new
 	  
-pool='abcde' # 2^5 abbreviations if we include "" and the original string.
+pool='0123a' # 2^5 abbreviations if we include "" and the original string.
 def f(pool, pos):
   return pool[pos+1:]
 
 def main():
  for length in xrange(1,len(pool)):
+  print '  vvvvvvvvvvv  length =', length
   for abbr in gen(f, pool, length):
-    print abbr
+    print abbr, '-- sum =', subSum[abbr]
     
 # all abbr
 main()
 '''
 Based on cookbook.py, this script was written as a practice.
+Now I think this way of generating 2^N -2 abbreviations has worse time complexity than O(2^N).
 '''
