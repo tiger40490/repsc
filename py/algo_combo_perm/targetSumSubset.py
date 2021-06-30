8b643215e0100cf9163983985ac6c1a9c6fd90a0
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 22:41:37 +0800

    a95 minor_10337

--- py/algo_combo_perm/targetSumSubset.py
+++ py/algo_combo_perm/targetSumSubset.py
@@ -1,17 +1,20 @@
 '''
+todo: genShortestFirst() need to return the set of sums. Let's build a hash table
 
 '''
+from collections import defaultdict
+subSum=defaultdict(int)
 def genShortestFirst(original):
-  '''Supose we have scanned 3 chars in the original, and we have a collection of N abbreviations. 
-  For the next char 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
-  Join the lists into a list of 2N.
+  '''Supose we have scanned 3 elements in the original, and we have a collection of N abbreviations. 
+  For the next element 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
+  Join the lists into a list of 2N elements.
   '''  
   print ' v ---  shortestFirst starting --  v'
-  growing=[[]] # start with a single empty string
+  growing=[()] # start with a single empty string
   for ch in original: # I would now avoid this in favor of the more common for i in range(len(original))
       sz=len(growing)
       for abbr in growing[:sz] :
-        growing.append(abbr+[ch])
+        growing.append(abbr+(ch,))
       print str(len(growing))+'-element', growing
   assert len(growing) == 2**len(original)
   return growing
