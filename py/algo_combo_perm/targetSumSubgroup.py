8ea576edf27fa8119d5afecfcb7cf1eea8319ee0
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 23:44:20 +0800

    a95 minor_921

--- py/algo_combo_perm/targetSumSubgroup.py
+++ py/algo_combo_perm/targetSumSubgroup.py
@@ -1,14 +1,14 @@
 '''
 '''
 #from pprint import pprint
-from json import dumps
-from collections import defaultdict
+#from json import dumps
+#from collections import defaultdict
 def genSubGroupSums(original):
   '''Supose we have scanned 3 elements in the original, and we have a collection of N abbreviations. 
   For the next element 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
   Join the lists into a list of 2N elements.
   '''  
-  print ' v ---  shortestFirst starting --  v'
+  print ' v ---  genSubGroupSums starting --  v'
   subsums={0:()} # start with a single empty string
   for elem in original: 
       tmp = dict()
@@ -19,7 +19,7 @@ def genSubGroupSums(original):
   assert len(subsums) <= 2**len(original)
   for k,v in subsums.items(): 
     print k, 'is a subgroup sum. One of the subgroup(s) is', v
-  return subsums
+  return subsums.keys()
 
 def main():
   genSubGroupSums([0,1,1,2])
