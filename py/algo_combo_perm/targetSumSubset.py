4c78274c3130e61362be9a4b18f0ebf55414cea5
unknown <35847749+tiger40490@users.noreply.github.com>
2021-06-30 23:38:32 +0800

    a95 minor_15577

--- py/algo_combo_perm/targetSumSubset.py
+++ py/algo_combo_perm/targetSumSubset.py
@@ -3,23 +3,23 @@
 #from pprint import pprint
 from json import dumps
 from collections import defaultdict
-#subSum=dict()
 def genShortestFirst(original):
   '''Supose we have scanned 3 elements in the original, and we have a collection of N abbreviations. 
   For the next element 'j', we append 'j' to each existing abbr to generate N new abbreviations. 
   Join the lists into a list of 2N elements.
   '''  
   print ' v ---  shortestFirst starting --  v'
-  growing={0:()} # start with a single empty string
-  for elem in original: # I would now avoid this in favor of the more common for i in range(len(original))
+  subsums={0:()} # start with a single empty string
+  for elem in original: 
       tmp = dict()
-      for subsum, abbr in growing.items():
+      for subsum, abbr in subsums.items():
         tmp[subsum+elem] = abbr + (elem,)
-      growing.update(tmp)
-      print str(len(growing))+'-element subsetSums:', growing.keys()
-  assert len(growing) <= 2**len(original)
-  for k,v in growing.items(): print k,v
-  return growing
+      subsums.update(tmp)
+      print str(len(subsums))+'-element subset Sums:', subsums.keys()
+  assert len(subsums) <= 2**len(original)
+  for k,v in subsums.items(): 
+    print k, 'is a subset sum. One of the subset(s) is', v
+  return subsums
 
 def main():
   genShortestFirst([0,1,1,2])
