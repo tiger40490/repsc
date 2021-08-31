e812f006b9beecb3391ef3c57ec1fb6665c52957
unknown <35847749+tiger40490@users.noreply.github.com>
2021-09-01 00:31:27 +0800

    a95 minor_17301

--- py/88lang/iter_gen_erator.py
+++ py/88lang/iter_gen_erator.py
@@ -1,16 +1,23 @@
+'''
+'''
 def myYieldFunction1():
   yield 'a'
   yield 'b'
   yield 'c'
 
-def main():
+def getGeneratorObj():
   retObj = myYieldFunction1()
-  print type(retObj) # a generator
+  print type(retObj) # a generator object
   print dir(retObj) # shows __iter__() and next()
-  print next(retObj), retObj.next() 
+  return retObj
+  
+def main():
+  generator = getGeneratorObj()
+  print next(generator), generator.next(), '------------'
   
   # now investating a builtin iterable object:
   rng = xrange(5)
+  print dir(rng) # shows __iter__ not next()
   itr = iter(rng) 
   print dir(itr) #shows __iter__() and next()
 main()
