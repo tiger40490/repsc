a006083b271debed9fc5194d00ace564968cd746
unknown <35847749+tiger40490@users.noreply.github.com>
2021-09-03 08:10:21 +0800

    a95 minor_3525

--- py/algo_combo_perm/abbrPortfolio.py
+++ py/algo_combo_perm/abbrPortfolio.py
@@ -1,37 +1,37 @@
 '''
 showcase yield
-showcase namedtuple
+showcase namedtuple as a simple VO
 showcase deepcopy
 '''
 from pprint import pprint
 from copy import deepcopy
 from collections import namedtuple
 
-Ptf = namedtuple('Ptf', ['sum', 'IDs']) #IDs = a vector of indices into original table of stock prices
+Ptf = namedtuple('Ptf', ['totalCost', 'IDs']) #IDs = a vector of indices into original table of stock prices
 
 def genAllPtf(table, budget):
-  subsets = [Ptf([0],[]) ] #the empty portfolio
+  subsets = [Ptf([0],[]) ] #the empty portfolio. The [0] is a mutable number inside an immutable tuple
   for i, val in enumerate(table):
     newSubsets = deepcopy(subsets) #shallow copy only duplicates references to each vector
     for p in newSubsets:
       p.IDs.append(i)
-      p.sum[0] += val
-      if p.sum[0] > budget: 
+      p.totalCost[0] += val
+      if p.totalCost[0] > budget: 
         #print p.IDs, ' --> :('
         continue
       yield p
     subsets.extend(newSubsets)
     #pprint(subsets)
   
-def testPortfolio(table, expCnt, budget=99):
+def testPortfolio(table, expCntOfSubsets, budget=99):
   cnt = 0 
   for p in genAllPtf(table, budget): 
     print 'a subset -----> ', p
     cnt += 1
-  assert cnt == expCnt
+  assert cnt == expCntOfSubsets
 
 def main():
-  testPortfolio([77,11,22,33], expCnt=10)
+  testPortfolio([77,11,22,33], expCntOfSubsets=10)
 if __name__ == '__main__': main()
 ''' Req: generate all subsets of a fixed table of stock prices, subject to a budget
 Each table entry's subscript is the stock's ID.
