5559809670820a4c487ee33848037015d927a203
unknown <35847749+tiger40490@users.noreply.github.com>
2021-09-07 22:01:46 +0800

    a95 new file

---
+++ py/boyMath.py
@@ -0,0 +1,10 @@
+def compute(n):
+  return 0
+
+def arithSeq():
+  print "Enter a number for n: ",
+  n = raw_input()
+  print 'You entered n =', n
+  actual = compute(n)
+  print 'The software has an output: ', actual
+arithSeq()
