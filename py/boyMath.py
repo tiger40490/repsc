f92b50d4965f9990e39ed3e9f851d3b49c7c4842
unknown <35847749+tiger40490@users.noreply.github.com>
2021-09-10 08:22:04 +0800

    a95 major_14272

--- py/boyMath.py
+++ py/boyMath.py
@@ -1,10 +1,10 @@
-def compute(n):
-  return 0
+def Tn(n):
+  return 67-n*4 
 
 def arithSeq():
   print "Enter a number for n: ",
   n = raw_input()
   print 'You entered n =', n
-  actual = compute(n)
+  actual = Tn(int(n))
   print 'The software has an output: ', actual
 arithSeq()
