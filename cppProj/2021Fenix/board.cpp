7242e7de316a4e91a943c60475f79fae523673be
unknown <35847749+tiger40490@users.noreply.github.com>
2021-10-23 19:43:04 +0800

    a95 runnable237

--- cppProj/2021Fenix/board.cpp
+++ cppProj/2021Fenix/board.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
-list<Mirror> mirrors; // list is good for erase.
-// need a way to populate it from the file
+list<Mirror> mirrors; 
+/* list is good for erase.
+A global variable, needed by the directHit/indirectHit etc. Those function need to use this variable to remove expired mirrors.
 
+// need a way to populate it from the file
+*/
 //^^^^^^^^ part of the board class
 
 void testMove1(){
   Photon photon = {{1,2}, {1,0}}; // one photon
   ss<<photon<<endl;
   mirrors.push_back({{2,1},-1});
-  mirrors.push_back({{2,2},-1});
+  //mirrors.push_back({{2,2},-1});
   //ss<<photon.distanceTo(mirrors.back()); return;
   mirrors.push_back({{2,3},-1});
   ss<<mirrors;
--- cppProj/2021Fenix/utils.h
+++ cppProj/2021Fenix/utils.h
@@ -24,32 +24,46 @@ struct Mirror{
     os<<"["<<c.cell<<"]ttl="<<c.ttl; return os;
   }
 };
-bool isSqrt2(float f){
-  return 1.414 < f && f < 1.415;
+bool isSqrt2(float f){  return 1.414 < f && f < 1.415; }
+float distance(Cell const & cellP, Cell const & cellQ){ //check2
+    float aa = cellP.first - cellQ.first;
+    float bb = cellP.second - cellQ.second;
+    float ret = sqrt(aa*aa + bb*bb); 
+    return ret;
 }
-
-struct Photon{ // the photon's direction and current position
+struct Photon{ 
   Cell cur;
   Step next;
   friend std::ostream & operator<<(std::ostream & os, Photon const & c){
     os<<"["<<c.cur<<"],<"<<c.next<<">"; return os;
   }
-  float distanceTo(Mirror const & m) const{
-    float aa = m.cell.first - cur.first;
-    float bb = m.cell.second - cur.second;
-    float ret = sqrt(aa*aa + bb*bb);
+  float distanceTo(Mirror const & m) const{ //check2
+    return distance(m.cell, this->cur);
+  }
+  Cell getTargetCell() const{ //check1 .. rename to target()
+    Cell ret = this->cur;
+    ret.first += this->next.first;
+    ret.second+= this->next.second;
+    ss<<ret<<" returned from getTargetCell()\n";
     return ret;
   }
-  char advance(){
-    //this->cur to be updated
+  char advance(){ // LG2: how to indicate leaving
+    this->cur = getTargetCell();
     return 0;
   }
   char directHit(Mirror & m){ 
     ss<<"directHit \n";
+    //Cell const & originalTarget = this->getTargetCell();
+    assert( this->getTargetCell() == m.cell );
+    // need to adjust the photon to indicate Absorbed
     return 0;
   }
-  char indirectHit(std::list<Mirror> & li){ 
-    ss<<"INdirectHit \n";
+  char indirectHit(std::vector<Mirror> & vec){ 
+    ss<<"indirectHit \n";
+    Cell const & originalTarget = this->getTargetCell();
+    for (auto const & aMirror: vec){
+      assert(1==distance(aMirror.cell, originalTarget));
+    }
     return 0;
   }
 
@@ -59,7 +73,7 @@ struct Photon{ // the photon's direction and current position
     // handle the edge scenarios
     
     // if one distance is 1, then break; otherwise collect those mirrors at distance 1.42 and pass them as a collection
-    std::list<Mirror> diagonalMirrors;
+    std::vector<Mirror> diagonalMirrors;
     for (auto& aMirror: survivingMirrors){
       float dist = distanceTo(aMirror);
       if (dist == 1){
