c1963f8b59aca0c2e9a7cf52bbc85582a58068fc
unknown <35847749+tiger40490@users.noreply.github.com>
2021-10-23 17:46:27 +0800

    299 avoid using namespace std

--- cpp/lang_33template/containerOutputOperator.cpp
+++ cpp/lang_33template/containerOutputOperator.cpp
@@ -13,47 +13,45 @@
 // Below is minimum code block for copy-paste.
 #include <iostream>
 #include <iomanip>
-#define Map unordered_map
-#define Set set
-#define ss if(1>0)cout //to mass-disable cout before uploading to hacker rank
-using namespace std;
+#define Map std::unordered_map
+#define Set std::set
+#define ss if(1>0)std::cout //to mass-disable cout before uploading to hacker rank
+//using namespace std;
 
-template<typename K, typename V> ostream & operator<<(ostream & os, pair<K,V> const & p){
-   //stringstream tmp2;
+template<typename K, typename V> std::ostream & operator<<(std::ostream & os, std::pair<K,V> const & p){
    os<<p.first<<":"<<p.second;
-   //os<<tmp2.str();   
    return os;
 }
-template<typename K, typename V, int min_width=8> ostream & operator<<(ostream & os,  Map<K,V> const & c){
-   for(auto it = c.begin(); it != c.end(); ++it){ os<<setw(min_width)<<*it<<" "; }
-   os<<endl;
+template<typename K, typename V, int min_width=8> std::ostream & operator<<(std::ostream & os,  Map<K,V> const & c){
+   for(auto it = c.begin(); it != c.end(); ++it){ os<<std::setw(min_width)<<*it<<" "; }
+   os<<std::endl;
    return os;
 }
-template<typename T,             int min_width=2> ostream & operator<<(ostream & os, Set<T> const & c){
-   for(auto const & it: c){ os<<setw(min_width)<<it<<" "; }
-   os<<endl;
+template<typename T,             int min_width=2> std::ostream & operator<<(std::ostream & os, Set<T> const & c){
+   for(auto const & it: c){ os<<std::setw(min_width)<<it<<" "; }
+   os<<std::endl;
    return os;
 }
 template<  template<typename,class> class Container, 
 typename V, class Alloc, int min_width=8>
 std::ostream& operator<<(std::ostream& os, Container<V, Alloc> const& c){
    // takes any (container or otherwise) template having 2 templ params, but not maps (4) or sets (3)
-   for(auto const & it : c){ os<<setw(min_width)<<it<<" "; }
-   os<<endl;
-   for(int i=0; i<c.size(); ++i){ os<<setw(min_width)<<i<<" "; }
-   os<<endl;
+   for(auto const & it : c){ os<<std::setw(min_width)<<it<<" "; }
+   os<<std::endl;
+   for(int i=0; i<c.size(); ++i){ os<<std::setw(min_width)<<i<<" "; }
+   os<<std::endl;
    return os;
 }
 
 //template<> std::ostream& operator<< <std::__cxx11::basic_string, char, char_traits<char> >(std::ostream& os, string const&) = delete; //Not effective :( because even deleted template-specialization can cause ambiguous overload under c++17
 
 int main(){
-  list<double> li={3.1, 5,2,9};
+  std::list<double> li={3.1, 5,2,9};
   Set<short> myset={1, 2, 42, 5};
 #if (__cplusplus >= 201703L)
-  vector<float> vec={3.5, 1.9, 0.8};
+  std::vector<float> vec={3.5, 1.9, 0.8};
 #else  
-  vector<string> vec={"dabao", "meimei", "princess"}; //works under g++ -std=c++14 but not c++17
+  std::vector<std::string> vec={"dabao", "meimei", "princess"}; //works under g++ -std=c++14 but not c++17
 #endif
   Map<int, bool> tm={{11,true}, {22,false}};
   ss<<vec<<tm;
