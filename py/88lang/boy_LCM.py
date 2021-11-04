d3289b42d10c756b24579268b7ff7f69ee1e8cc3
unknown <35847749+tiger40490@users.noreply.github.com>
2021-11-04 12:09:22 +0800

    299 comment

--- py/88lang/boy_LCM.py
+++ py/88lang/boy_LCM.py
@@ -6,9 +6,10 @@ def lcm2(a, b):
   return a*b // gcd(a,b)
   
 def LCM(*args):
-  # somehow, args feels like a list
+  # somehow, q[args] feels like a list
+  
   if len(args) == 1: return
-  # better put the recursion terminal condition here. It serves as an assert len(args) > 1
+  # better put recursion exit condition here rather then "rightB4callingMyself". It serves as an assert len(args) > 1
   
   print args, '<-- are the remaining inputs. Tony will pick first two inputs\n ', args[0],args[1],' and compute their LCM: ',
   tmp = lcm2(args[0], args[1])
