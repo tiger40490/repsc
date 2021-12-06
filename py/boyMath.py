311aa2e99cf23d2e8a893898898506631226bd68
unknown <35847749+tiger40490@users.noreply.github.com>
2021-12-06 17:35:51 +0800

    a95 new_1462

--- py/boyMath.py
+++ py/boyMath.py
@@ -8,19 +8,28 @@ def arithSeq():
   print 'The software has an output: ', actual
 
 def any2digitNumber(xy):
-  x = y = xx = yy = magic = 0
   # validate input
+  if xy > 99 or xy <= 20 :
+    print xy, 'is out of range'
+    return
   # split into 2 digits x and y
+  x = xy/10
+  y = xy - x*10
   print [x,y], 'are the two digits of your input'
   # now subtract the sum from the original number
+  magic = xy - (x + y)
   print magic, 'is the (magic) number after subtracting from your input'
   # split magic into 2 digits xx and yy
-  print xx+yy, 'is the sum of the magic two digits'
+  xx = magic/10 # follow line 14
+  yy = magic - xx*10
+  print [xx,yy], 'are the two digits of magic'
+  print xx+yy, 'is the sum of the magic two digitss'
   assert xx+yy == 9
   
 def getUserInput():
   print "Enter a two-digit number greater than 20 : ",
   n = raw_input()
+  #n=20
   print 'You entered n =', n
   try:  
     any2digitNumber(int(n))
