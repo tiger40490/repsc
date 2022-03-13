1f91018a4025b5d02deccab1e41c942d620a182e
unknown <35847749+tiger40490@users.noreply.github.com>
2022-03-13 14:55:16 +0800

    a95 new + minor

--- py/88lang/bisectUsages.py
+++ py/88lang/bisectUsages.py
@@ -5,8 +5,14 @@ float_list = [0, 1.0, 2, 2, 2, 5.0]
 
 left = bisect_left(float_list, needle)
 assert left == 2
-print 'left (should be lower) =', left
+print 'bisect_le (should be lower) =', left
 
 right = bisect_right(float_list, needle)
 assert right == 5
-print 'right (should be higher) =', right
+print 'bisect_ri (should be higher) =', right
+
+# if no hit, then both return the same
+needle=2.99999
+left = bisect_left(float_list, needle)
+right = bisect_right(float_list, needle)
+assert left == right == 5
---
+++ py/88miscIVQ/interpolate_extrapolate_Sachin.py
@@ -0,0 +1,69 @@
+'''
+'''
+from bisect import *
+def slope(ins, price, le, ri):
+  assert le < ri
+  ret = float(price[ri] - price[le]) / float(ins[ri] - ins[le])
+  print le, ri, 'slope=', ret
+  return ret
+
+def format2(flt):
+  ret = "%.2f" %round(flt, 2)
+  return ret
+  
+def interpolate(n, origI, origP):
+  # first filter out bad prices
+  instances=[]; price=[]
+  for i in xrange(len(origP)):
+    if origP[i] <= 0: continue
+    price.    append(origP[i]);
+    instances.append(origI[i])
+
+  sz = len(price)
+  lastid = sz-1 #lastIndex
+
+  if sz == 1: return format2(price[0])
+  assert sz >= 2
+  
+  if n < instances[0]:
+    print n, 'is too low'
+    slp = slope(instances, price, 0, 1)
+    ret = -slp*(instances[0]-n) + price[0]
+    return format2(ret)
+  if instances[lastid] < n:
+    print n, 'is too high'
+    slp = slope(instances, price, lastid-1, lastid)
+    ret = slp*(n-instances[lastid]) + price[lastid]
+    return format2(ret)
+    
+  left = bisect_left(instances, n)
+  print 'bisect returned', left
+  if n == instances[left]: #exact match
+    return format2(price[left])
+    
+  print n, 'is between', instances[left-1], instances[left]
+  slp =  slope(instances, price, left-1, left)
+  ret = slp*(n-instances[left]) + price[left]
+  return format2(ret)
+  
+def test1(expected, n, ins, price):
+  ret = interpolate(n, ins, price)
+  print "returning", ret
+  assert expected == ret
+
+test1("54.25", 2, [10,25,50,100,500], [0, 0, 0, 0, 54.25])
+
+# singular, with bad data
+test1("5.00", 20, [50,5555], [5, 0])
+
+# too high
+test1("6.13", 1999, [10,25,50,100,500], [27.32, 23.13, 21.25, 18, 15.5])
+
+# too low
+test1("5.20", 20, [25,50,100], [5,4,3])
+
+# in-between
+test1("3.50", 75, [25,50,100], [5,4,3])
+
+#exact match:
+test1("2.46", 10, [10,25,50,100,500], [2.46, 2.58,2.0, 2.25, 3.0])
