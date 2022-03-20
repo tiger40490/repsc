6152f6845b020582c41021261698e4e7fbe76c48
unknown <35847749+tiger40490@users.noreply.github.com>
2022-03-20 21:43:31 +0800

    a95 untested

--- py/Q3caret.py
+++ py/Q3caret.py
@@ -3,41 +3,64 @@ if Z is beyond lineAbove, then that line is printed.
 if Z cust into part of lineAbove, then ... look at the eg
 if Z cuts into lineCtr on the left, then there will be no lineAbove
 if Z is 0, then special handling?
-
-Will focus on the right side first. 
 '''
 
-def riSide(S,Y,Z):
-  pass
+def lineB(S,Y,Z, vec):
+# what if markerB is -999999?
+  if markerB < 0: return # nothing to print for lineBelow
+  #print 'in lineB()', markerB, Y+Z
+  ret = S[markerC+1 : 1+min(markerB, Y+Z)]
+  print ret
+  vec.append(ret)
 
-def lineC(S,Y,Z):
-  # print the central line
-  # if markerC is bad, then just print an error msg.. too complicated
+def lineC(S,Y,Z, vec):
   if markerC > 0:
-    distA = abs(Y-markerA) 
-    distC = abs(Y-markerC) 
     distB = abs(Y-markerB) #not needed in this lineC()
-    if Z > distA and Z > distC: # should it be >=?
-      print S[markerA+1: markerC+1] #, 'case 11'
-      print (' ' * (distA-1)) + '^\n'
-    if Z > distA and Z < distC:
-      print S[markerA+1:Z+Y] #, 'case 22'
-      print (' ' * (distA-1)) + '^\n'
+    distA = abs(Y-markerA) 
+    distC = abs(Y-markerC)
+    #print 'distA,distC ..', distA,distC
+    if Z >= distA and Z >= distC: # 
+      vec.append( S[markerA+1: markerC+1] ) #, 'case 11'
+      vec.append(  (' ' * (distA-1)) + '^\n' )
+    if Z >= distA and Z < distC:
+      vec.append(  S[markerA+1 : Z+Y+1] )#, 'case 22'
+      vec.append(  (' ' * (distA-1)) + '^\n' )
     if Z < distA and Z < distC:
-      print S[Y-Z: Y+Z+1]+'\n'
-      print (' ' *Z) + '^\n'
-    if Z < distA and Z > distC:
-      print S[Y-Z, markerC+1]
-      print (' ' *Z) + '^\n'
+      vec.append(  S[Y-Z: Y+Z+1]+'\n' )#, 'case 33'
+      vec.append(  (' ' *Z) + '^\n' )
+    if Z < distA and Z >= distC:
+      vec.append(  S[Y-Z: markerC+1] )#, 'case 44'
+      vec.append(  (' ' *min(Y,Z)) + '^\n' )
+def lineA(S,Y,Z, vec):
+  # print lineAbove 
+  # what if markerA is -999?
+  if markerA < 0: return #nothing to print for lineAbove
+  ret = S[max(markerP, Y-Z) : markerA+1]
+  print ret
+  vec.append(ret)
+
+def test():
+  ret = solution('// comment\nint main() {\n    return 0\n}\n', 36, 126)
+  assert ret == ['\nint main() {\n', '    return 0\n', '            ^\n', '}\n']
+  ret = solution('abcde\nfghij\nklmno\n', 8, 5)
+  assert ret == ['de\n', 'fghij\n', '  ^\n', 'kl']
+  #solution('abcde\nfghij1234\nklmno\n', 8, 3)
+  ret = solution('123',1,0)
+  assert ret == ['2\n', '^\n']
+  #solution('0abc',2, 33)
 
 def solution(S,Y,Z):
+  vec = list()
   find3markers(S,Y)
-  # print lineAbove .. todo
-  lineC(S,Y,Z)
+  lineA(S,Y,Z, vec)
+  lineC(S,Y,Z, vec)
+  lineB(S,Y,Z, vec)
+  print vec
+  return vec
 
 def find3markers(S,Y):
-  global markerC, markerA, markerB
-  markerA = markerC = markerB = -999
+  global markerP, markerC, markerA, markerB
+  markerP = markerA = markerC = markerB = -999999
   # locate the 3 nlMarkers and find the distance to Y
   if S[-1] != '\n': S=S+'\n'
   newlines = list()
@@ -46,19 +69,16 @@ def find3markers(S,Y):
       sz = len(newlines)
       if pos >= Y: # 
         markerC = pos # markerC is the position of the newline on the central line
-        markerA = newlines[-1] if sz >= 1 else -999
+        if sz >= 2: markerP = newlines[-2]
+        if sz >= 1: markerA = newlines[-1] 
         tmp = S.find('\n', pos+1)
-        markerB = -999 if (tmp == -1) else tmp
-        print 'returning with markerA, markerC, markerB .. ', markerA, markerC, markerB
+        if (tmp != -1): markerB = tmp
+        print 'returning with markerP, markerA, markerC, markerB .. ', markerP, markerA, markerC, markerB
         return 3
       newlines.append(pos)
   print 'no newline at all!'
   return 0
-  
-def test():
-  #solution('// comment\nint main() {\n    return 0\n}\n', 36, 126)
-  #solution('abcde\nfghij\nklmno\n', 8, 5)
-  solution('123',1,0)
-  #solution('123',1,1)
 
 test()
+''' req: https://btv-gz.dreamhosters.com/3005/careterrorwithcontext-sachin-hrt/
+'''
