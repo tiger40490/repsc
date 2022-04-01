1b4661ad2dd6bd22f73ebc20ed1a26bb12fe8abf
unknown <35847749+tiger40490@users.noreply.github.com>
2022-04-02 00:48:53 +0800

    minorRef

--- py/QCP1.py
+++ py/QCP1.py
@@ -1,13 +1,6 @@
 '''
-group strings by length and sort the clubs by that length.
-start from shortest words and determine its (immutable) chainlen
-
-pass two consecutive clubs to a cross2group(). 
-if lengh diff is exactly 1, then it picks pairs and pass each pair into a func pair(). 
-
-This func tests the 2 words, and uses (1+strs's chainlen) as strb's chainlen if the latter is unset. 
-
-If strb already has a chainlen, then tryUpdate.
+todo: check2str needs clean-up:
+Perhaps use two pointers
 '''
 import os
 from collections import defaultdict
@@ -24,57 +17,58 @@ def makeClubs(words):
   clubs = dict(clubs)
   clubKeys.extend (sorted (clubs.keys()))
   print clubs
-def check2str(strs, strb):
-  print strs, strb
-  sz = len(strs)
+def check2str(strd, strc):
+  print strd, strc
+  sz = len(strd)
   shift=0
   for i in xrange(sz):
     #print 'i=',i,'shift=',shift
-    if strs[i]!=strb[i+shift]: 
+    if strd[i]!=strc[i+shift]: 
       if shift == 1:   #already shifted index in B
         return False      
       assert shift == 0
       shift = 1
       #print 'shift set to 1'
-      #if strs[i]==strb[i+shift]: continue
+      #if strd[i]==strc[i+shift]: continue
       #print 333333
       #return False
     
-    if strs[i]==strb[i+shift]: 
+    if strd[i]==strc[i+shift]: 
       if i+1 == sz: 
         #print 'reached end'
         return True
-def cross2club(keyS, keyB):
+def cross2club(keyd, keyc):
   global isOver
-  assert keyS +1 == keyB
-  lis, lib = clubs[keyS], clubs[keyB]
-  #print lis, lib
-  for strs in lis:
-    for strb in lib:
-      #print strs, strb
-      if not check2str(strs, strb): continue
-      inc = registry[strs]+1
-      if strb not in registry or registry[strb] < inc:
-         registry[strb] = inc
+  assert keyd +1 == keyc
+  listd, listc = clubs[keyd], clubs[keyc]
+  #print listd, listc
+  for strd in listd:
+    for strc in listc:
+      #print strd, strc
+      if not check2str(strd, strc): continue
+      inc = registry[strd]+1
+      if strc not in registry or registry[strc] < inc:
+         registry[strc] = inc
          if inc > winner[0]:
            del winner[:]
-           winner.extend([inc, strb])
+           winner.extend([inc, strc])
            print 'new winner:' , winner
            if inc == clubKeys[-1]: 
              isOver=True
              return
          continue
-      assert registry[strb] >= inc
+      assert registry[strc] >= inc
 def longestChain(words):
   words=set(words)
   makeClubs(words)
   for w in clubs[ clubKeys[0] ]: registry[w] = 1
   for i in xrange(1, len(clubKeys)):
-    keyS, keyB = clubKeys[i-1], clubKeys[i]
-    if keyS +1 == keyB: 
+    keyd, keyc = clubKeys[i-1], clubKeys[i]
+    if keyd +1 == keyc: 
       if isOver: break
-      cross2club(keyS, keyB)
+      cross2club(keyd, keyc)
   print registry
+  print 'returning winner =', winner[0]
   return winner[0]
 
 assert check2str('a', 'aa')
@@ -83,3 +77,5 @@ assert check2str('a', 'ba')
 assert not check2str('a', 'bc')
 
 longestChain(['a', 'b', 'ba', 'bca', 'bda', 'bdca'])
+'''Req: https://btv-gz.dreamhosters.com/42720/word-chain-civ-by-ash-s/
+'''
--- py/QCP2.py
+++ py/QCP2.py
@@ -1,23 +1,41 @@
-# Need to pre-process
-import math, sys
+'''
+One big pitfall is the one-based vs zero-based. I need clear and short var names. Every time we look up in array, we must use zero-based, but input/output is always dateId, i.e. one-based.
+
+winner var is a list. Can be a scalar in this problem, but in general, a list is versatile as a global var as it can be updated/overwritten without "global winner"
+
+Q: should initialize winner with the default return value of -1? 
+A: often a good simplification, but not in this case. This is an important observation, an important question in speed coding
+A: using -1 would give too many responsibilities to the default value. Very hard to manage. Instead, separate variables should take up those responsibilities depending on the algorithm design.
+'''
+import sys
 def predictAnswer(stockData, queries):
   ret = list()
-  for q in queries:
-    ret.append( do1(stockData, q))
+  for date in queries:
+    nearestDate = query1(stockData, date)
+    ret.append( nearestDate )
   return ret
-def do1(stockData, q):
-  pos = q-1
-  winner = [sys.maxint] #default return value???
+
+def query1(stockData, date):
+  pos = date-1 # "i" is already in use. I could use pos. "idx" is too similar to "i"
+  
+  winner = [sys.maxint]
   for i,v in enumerate(stockData):
     #print i,v
-    if v >= stockData[pos]: continue # discard all days above
+    if v >= stockData[pos]: continue
     dist = pos - i
-    #print i,v, 'dist=', dist
-    if abs(dist) < abs(pos-winner[0]): # if two candidates, then the earlier day wins
-      #print "new winner", i, v
+    print i,v, 'dist=', dist
+    if abs(dist) < abs(winner[0]-pos): # if two candidates tie, then the earlier day wins and we don't enter here. 
+    # This is by design, since we scan from left
+      print "new winner", i, v
       winner = [i]
   if winner[0] == sys.maxint: return -1
   return winner[0]+1
 
-#print predictAnswer([5,6,8,4,9,10,8,3,6,4], [3,1,8])
-print predictAnswer([5,6,8,4,9,10,8,3,6,4], [6])
+assert predictAnswer([5,6,8,4,9,10,8,3,6,4], [3,1,8]) == [2,4,-1]
+'''Req: You have a time-series of daily prices. The array-index is dateId-1.
+For each target dateId, find the nearest dateId when price is strictly lower than the target. 
+
+If none, return -1
+
+If tie, then return the earliest.
+'''
