75daa7e7e0346129e8f0663ea1128d5971a475bd
tiger <35847749+tiger40490@users.noreply.github.com>
2022-04-03 19:34:19 +0800

    L36 minor_25420

--- bash/gitbash_commit-msg.sh
+++ bash/gitbash_commit-msg.sh
@@ -11,7 +11,7 @@ hn=`hostname`
 case "$hn" in
   acer156     ) prefix='a95' ;;
   DSIOTE10077 ) prefix='mlp' ;;
-  LAPTOP-3U2UKQPB ) prefix='hp ' ;;
+  #LAPTOP-3U2UKQPB ) prefix='h71' ;;
   *           ) prefix="$hn" ;; #default
 esac
 # prefix should be space-saving, fairly unique
