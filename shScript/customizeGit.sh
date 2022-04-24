83cbfe43540a2997069f76a7557b3d9f64eca152
tiger <35847749+tiger40490@users.noreply.github.com>
2022-04-24 18:26:42 +0800

    L36 new_20631

--- shScript/customizeGit.sh
+++ shScript/customizeGit.sh
@@ -4,6 +4,8 @@ git config --global alias.alias 'config --get-regexp alias'
 git config --global alias.comit   'commit'
 git config --global alias.commmit 'commit'
 git config --global alias.diffn 'diff --name-status'
-git config --global alias.shown 'show --name-status'
 
+# less useful in repo1 as it often downloads a disconnected branch 
+git config --global alias.pullb '!git fetch origin ${1}:${1}' 
+git config --global alias.shown 'show --name-status'
 git alias
