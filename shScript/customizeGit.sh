17201cd496eee40d34a9bcf4625dec01f5995474
tiger <35847749+tiger40490@users.noreply.github.com>
2023-04-03 17:11:34 +0800

    L36 new_26242

--- shScript/customizeGit.sh
+++ shScript/customizeGit.sh
@@ -7,6 +7,7 @@ git config --global alias.comit   'commit'
 git config --global alias.commmit 'commit'
 git config --global alias.diffn 'diff --name-status'
 git config --global alias.shown 'show --name-status'
+git config --global alias.force 'push --force-with-lease'
 
 # less useful in repo1 as it often downloads a disconnected branch 
 git config --global alias.pullb '!git fetch origin ${1}:${1}' 
