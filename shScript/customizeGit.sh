24a30cb97eb70f408d695b8081136bef09c5a5f9
tiger <35847749+tiger40490@users.noreply.github.com>
2023-07-25 13:55:38 +0800

    L36 newContent

--- shScript/customizeGit.sh
+++ shScript/customizeGit.sh
@@ -1,13 +1,13 @@
-# todo: remove the shell alias/functions that are no longer required
-
 # script to be run normally i.e. in a subsehll, not sourced
+
 git config --global alias.alias 'config --get-regexp alias'
-git config --global alias.amend   'commit --am --reset-author' #would update timestamp even if no new content. To preserve timestamp, use :cq to exit vi.
-git config --global alias.comit   'commit'
+git config --global alias.amend 'commit --am --reset-author' #would update timestamp even if no new content. To preserve timestamp, use :cq to exit vi.
+git config --global alias.comit  'commit'
 git config --global alias.commmit 'commit'
 git config --global alias.diffn 'diff --name-status'
-git config --global alias.shown 'show --name-status'
 git config --global alias.force 'push --force-with-lease'
+git config --global alias.shown 'show --name-status'
+git config --global alias.swapLast2 "!GIT_SEQUENCE_EDITOR=\"sed -i -n 'h;1n;2p;g;p' \" git rebase -i HEAD~2"
 
 # less useful in repo1 as it often downloads a disconnected branch 
 git config --global alias.pullb '!git fetch origin ${1}:${1}' 
