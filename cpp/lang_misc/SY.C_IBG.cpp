602b539e77824263280c4c548500d4bd6c17cc11
victan <35847749+tiger40490@users.noreply.github.com>
2023-07-29 18:28:14 +0800

    L399 renamVar

--- cpp/lang_misc/SY.C_IBG.cpp
+++ cpp/lang_misc/SY.C_IBG.cpp
@@ -2,15 +2,7 @@
 
 #include <iostream>
 using namespace std;
-int main2(){
-	int a=5;
-	int b=0;
-	int &r=a;
-	r=b++;
-	cout<<a<<r<<b;
-	return 0;
-}
-/*
+
 struct A{
 	~A(){cout<<"~A"; }
 };
@@ -21,7 +13,8 @@ int main3(){
 	A *a=new B();
 	delete a;
 	return 0;
-}*/
+}
+/////////////
 struct Base{
 	virtual void print(int x=1){
 		cout<<"Base:" <<x;
@@ -32,23 +25,23 @@ struct Derived: Base{
 		cout<<"Derived : "<<x;
 	}
 };
-
 int main4(){
 	Base *B = new Derived;
 	B->print();
 	//cout<<c<<" "<<a;
 	return 0;
 }
-struct A{ 
-  virtual ~A(){ f();}
-  virtual void f(){cout<<"A::f\n";}
+///////////
+struct Base4{ 
+  virtual ~Base4(){ f();}
+  virtual void f(){cout<<"Base4::f\n";}
 };
-struct B: public A{ 
-	~B(){}
-  virtual void f(){cout<<"B::f\n";}
+struct Der4: public Base4{ 
+	~Der4(){}
+  virtual void f(){cout<<"Der4::f\n";}
 };
 int main(){
-	A* a=new B;
+	Base4* a=new Der4;
 	delete a;
 	return 0;
 }
