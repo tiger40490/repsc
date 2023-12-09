d97b96c83cdd04b4df1e10727fcca08ed1555784
unknown <35847749+tiger40490@users.noreply.github.com>
2023-12-09 13:41:58 +0800

    HP71 new_1137

--- shScript/customizeGit.sh
+++ shScript/customizeGit.sh
@@ -8,6 +8,7 @@ git config --global alias.diffn 'diff --name-status'
 git config --global alias.force 'push --force-with-lease'
 git config --global alias.shown 'show --name-status'
 git config --global alias.swapLast2 "!GIT_SEQUENCE_EDITOR=\"sed -i -n 'h;1n;2p;g;p' \" git rebase -i HEAD~2"
+git config --global alias.reword '!git commit --fixup reword:$1 && GIT_EDITOR=true && git rebase --autosquash --interactive --rebase-merges $1~1 && echo "Successful reword on"'
 
 # less useful in repo1 as it often downloads a disconnected branch 
 git config --global alias.pullb '!git fetch origin ${1}:${1}' 
